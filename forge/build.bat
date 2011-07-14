987ad5acd25dd08002ea82bbda0cdf072029d425
Space Toad <spacetoadcraft@gmail.com>
2011-07-14 11:19:22 +0000

    fixed previous checkin

--- forge/build.bat
+++ forge/build.bat
@@ -4,4 +4,7 @@ rmdir /S /Q src
 mkdir src
 xcopy  /Y /E src_work\* src\
 
+xcopy /Y /E forge\forge_common\net\* src\minecraft\net
+xcopy /Y /E forge\forge_common\net\* src\minecraft_server\net
+
 cmd /C recompile.bat
