3cf1792becf5cf73f6fd88e7940113e369908af2
Space Toad <spacetoadcraft@gmail.com>
2011-07-15 23:13:09 +0000

    added headers

--- forge_common/net/minecraft/src/forge/IBiomePopulator.java
+++ forge_common/net/minecraft/src/forge/IBiomePopulator.java
@@ -1,3 +1,8 @@
+/**
+ * This software is provided under the terms of the Minecraft Forge Public 
+ * License v1.0.
+ */
+
 package net.minecraft.src.forge;
 
 import net.minecraft.src.BiomeGenBase;
@@ -5,6 +10,9 @@ import net.minecraft.src.World;
 
 public interface IBiomePopulator {
 
+    /** 
+     * This is called for 
+     */
     public void populate(World world, BiomeGenBase biomegenbase, int x, int z);
 
 }
--- forge_common/net/minecraft/src/forge/ITextureProvider.java
+++ forge_common/net/minecraft/src/forge/ITextureProvider.java
@@ -1,18 +1,20 @@
+/**
+ * This software is provided under the terms of the Minecraft Forge Public 
+ * License v1.0.
+ */
+
 package net.minecraft.src.forge;
 
 /**
- * This interface has to be implemented by a Block, and provide custom texture
- * capabilities.
+ * This interface has to be implemented either by an instance of Block or Item.
+ * It allow to use texture files different from terrain.png or items.png.
  */
 public interface ITextureProvider {
 
     /**
      * This interface has to return the path to a file that is the same size as
-     * terrain.png, but not named terrain.png. If the block implements
-     * getRenderType() by returning any of the following:
-     * BuildCraftCore.customTextureModel BuildCraftCore.pipeModel it will use
-     * that terrain file to render texture instead of the default terrain.png
-     * one.
+     * terrain.png, but not named terrain.png. It will be used instead of the
+     * regular terrain file to render blocks and items.
      */
     public String getTextureFile();
 
