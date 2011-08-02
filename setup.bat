bedcc2fc64bb9d3b0d00513a18316c4805fc8be7
Eloraam <jen@elaan.com>
2011-08-02 05:18:52 +0000

    Fixed a little path bug.

--- setup.bat
+++ setup.bat
@@ -8,7 +8,7 @@ rmdir /S /Q src_work
 ) ELSE (
 cmd /C decompile.bat
 cd src
-..\runtime\bin\python\python_mcp forge\lfcr.py ..\forge\modLoaderMP.patch ..\forge\modLoaderMP-win.patch
+..\runtime\bin\python\python_mcp ..\forge\lfcr.py ..\forge\modLoaderMP.patch ..\forge\modLoaderMP-win.patch
 ..\runtime\bin\applydiff.exe -uf -p2 < ..\forge\modLoaderMP.patch
 cd ..
 cmd /C recompile.bat
@@ -20,6 +20,6 @@ mkdir src_work
 xcopy  /Y /E src\* src_base
 xcopy  /Y /E src\* src_work
 cd src_work
-..\runtime\bin\python\python_mcp forge\lfcr.py ..\forge\minecraft.patch ..\forge\minecraft-win.patch
+..\runtime\bin\python\python_mcp ..\forge\lfcr.py ..\forge\minecraft.patch ..\forge\minecraft-win.patch
 ..\runtime\bin\applydiff.exe -uf -p2 < ..\forge\minecraft-win.patch
 pause
