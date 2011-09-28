07dfc35e0e764f6f623cede43bf982fb467db36b
Eloraam <jen@elaan.com>
2011-09-28 23:35:31 +0000

    Fixed TileEntities, bumped version number.

--- forge/forge_common/net/minecraft/src/forge/ForgeHooks.java
+++ forge/forge_common/net/minecraft/src/forge/ForgeHooks.java
@@ -138,8 +138,8 @@ public class ForgeHooks {
 	}
 
 	public static final int majorVersion=1;
-	public static final int minorVersion=0;
-	public static final int revisionVersion=6;
+	public static final int minorVersion=1;
+	public static final int revisionVersion=0;
 	static {
 		System.out.printf("MinecraftForge V%d.%d.%d Initialized\n",majorVersion,minorVersion,revisionVersion);
 	}
