e233005138b85c3ed263b8e21d72bad156af7acf
Space Toad <spacetoadcraft@gmail.com>
2011-10-02 19:14:20 +0000

    ISidedInventory is now explicitely an inventory

--- forge_common/net/minecraft/src/forge/ISidedInventory.java
+++ forge_common/net/minecraft/src/forge/ISidedInventory.java
@@ -5,12 +5,15 @@
 
 package net.minecraft.src.forge;
 
+import net.minecraft.src.IInventory;
+
 /** Inventory ranges mapped by side.  This class is implemented by TileEntities
  * that provide different inventory slot ranges to different sides.
  */
-public interface ISidedInventory {
+public interface ISidedInventory extends IInventory {
 
-	/** Get the start of the side inventory.
+	/** 
+	 * Get the start of the side inventory.
 	 * @param side The global side to get the start of range.
 	 *      0: -Y (bottom side)
 	 *      1: +Y (top side)
@@ -21,7 +24,8 @@ public interface ISidedInventory {
 	 */
 	int getStartInventorySide(int side);
 
-	/** Get the size of the side inventory.
+	/**
+	 * Get the size of the side inventory.
 	 * @param side The global side.
 	 */
 	int getSizeInventorySide(int side);
