30b1f4c2ed53c98fe9d791a0276ec17fc6ff649f
Space Toad <spacetoadcraft@gmail.com>
2011-11-30 19:02:10 +0000

    bumped version number

--- forge_common/net/minecraft/src/forge/ForgeHooks.java
+++ forge_common/net/minecraft/src/forge/ForgeHooks.java
@@ -230,8 +230,8 @@ public class ForgeHooks {
 	}
 
 	public static final int majorVersion=1;
-	public static final int minorVersion=1;
-	public static final int revisionVersion=3;
+	public static final int minorVersion=2;
+	public static final int revisionVersion=0;
 	static {
 		System.out.printf("MinecraftForge V%d.%d.%d Initialized\n",majorVersion,minorVersion,revisionVersion);
 	}
