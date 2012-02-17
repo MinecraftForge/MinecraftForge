67a49ccdd2fc691af5b872b015ac38453f82e30b
LexManos <LexManos@gmail.com>
2012-02-16 20:04:40 -0800

    Split mod_MinecraftForge so that Clients arnt required to have MLMP installed if they don't use a MP mod.

---
+++ forge/forge_client/src/net/minecraft/src/mod_MinecraftForge.java
@@ -0,0 +1,24 @@
+package net.minecraft.src;
+
+import net.minecraft.src.forge.ForgeHooks;
+import net.minecraft.src.forge.MinecraftForge;
+
+/**
+ * This class is just here to make the Forge version show up nicely in the ModLoader logs/Crash Screen
+ */
+public class mod_MinecraftForge extends BaseMod
+{
+    @Override
+    public String getVersion() 
+    {        
+        return String.format("%d.%d.%d.%d", 
+                ForgeHooks.majorVersion,    ForgeHooks.minorVersion, 
+                ForgeHooks.revisionVersion, ForgeHooks.buildVersion);
+    }
+
+    @Override
+    public void load() 
+    {
+        MinecraftForge.getDungeonLootTries(); //Random thing to make things Initialize
+    }
+}
