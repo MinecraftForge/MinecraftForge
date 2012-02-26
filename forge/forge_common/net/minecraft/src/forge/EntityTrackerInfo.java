431288f40439899b751015ee9b4af1d2a5300487
Deveducer <thedeveducer@gmail.com>
2012-02-25 18:48:05 -0800

    Fixed spelling mistake ('Frequancy')

--- forge/forge_common/net/minecraft/src/forge/EntityTrackerInfo.java
+++ forge/forge_common/net/minecraft/src/forge/EntityTrackerInfo.java
@@ -7,15 +7,15 @@ public class EntityTrackerInfo
     public final NetworkMod Mod;
     public final int ID;
     public final int Range;
-    public final int UpdateFrequancy;
+    public final int UpdateFrequency;
     public final boolean SendVelocityInfo;
 
-    public EntityTrackerInfo(NetworkMod mod, int ID, int range, int updateFrequancy, boolean sendVelocityInfo)
+    public EntityTrackerInfo(NetworkMod mod, int ID, int range, int updateFrequency, boolean sendVelocityInfo)
     {
         Mod = mod;
         this.ID = ID;
         Range = range;
-        UpdateFrequancy = updateFrequancy;
+        UpdateFrequancy = updateFrequency;
         SendVelocityInfo = sendVelocityInfo;
     }
 }
--- forge/forge_common/net/minecraft/src/forge/MinecraftForge.java
+++ forge/forge_common/net/minecraft/src/forge/MinecraftForge.java
@@ -850,17 +850,17 @@ public class MinecraftForge
      * @param mod The BaseMod that provides this Entity.
      * @param ID The ID for the Entity. Needs to be unique combination of Mod and ID.
      * @param range How close a player has to be to be informed this Entity exists.
-     * @param updateFrequancy How many ticks between checking and sending information updates for this Entity.
+     * @param updateFrequency How many ticks between checking and sending information updates for this Entity.
      * @param sendVelocityInfo If velocity information should be included in the update information.
      * @return True, if successfully registered. False if the class is already registered.
      */
-    public static boolean registerEntity(Class entityClass, NetworkMod mod, int ID, int range, int updateFrequancy, boolean sendVelocityInfo)
+    public static boolean registerEntity(Class entityClass, NetworkMod mod, int ID, int range, int updateFrequency, boolean sendVelocityInfo)
     {
         if (ForgeHooks.entityTrackerMap.containsKey(entityClass))
         {
             return false;
         }
-        ForgeHooks.entityTrackerMap.put(entityClass, new EntityTrackerInfo(mod, ID, range, updateFrequancy, sendVelocityInfo));
+        ForgeHooks.entityTrackerMap.put(entityClass, new EntityTrackerInfo(mod, ID, range, updateFrequency, sendVelocityInfo));
         return true;
     }
 
