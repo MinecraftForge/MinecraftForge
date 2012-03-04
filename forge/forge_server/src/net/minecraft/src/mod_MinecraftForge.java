2b40928113c59f73fd0155918565823ad8271063
LexManos <LexManos@gmail.com>
2012-03-03 18:17:23 -0800

    Made mod_MinecraftForge extend NetworkMod, and changed NetworkMod to extend BaseModMP server side.

--- forge/forge_server/src/net/minecraft/src/mod_MinecraftForge.java
+++ forge/forge_server/src/net/minecraft/src/mod_MinecraftForge.java
@@ -2,11 +2,12 @@ package net.minecraft.src;
 
 import net.minecraft.src.forge.ForgeHooks;
 import net.minecraft.src.forge.MinecraftForge;
+import net.minecraft.src.forge.NetworkMod;
 
 /**
  * This class is just here to make the Forge version show up nicely in the ModLoader logs/Crash Screen
  */
-public class mod_MinecraftForge extends BaseModMp
+public class mod_MinecraftForge extends NetworkMod
 {
     @Override
     public String getVersion()
@@ -21,4 +22,16 @@ public class mod_MinecraftForge extends BaseModMp
     {
         MinecraftForge.getDungeonLootTries(); //Random thing to make things Initialize
     }
+
+	@Override
+	public boolean clientSideRequired() 
+	{
+		return false;
+	}
+
+	@Override
+	public boolean serverSideRequired() 
+	{
+		return false;
+	}
 }
