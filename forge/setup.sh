0dce2e6ef06d8916f88f8d2c8ad0344c656d53fb
Christian Weeks <cpw@weeksfamily.ca>
2012-03-05 17:05:09 -0500

    Rebuild the setup.sh script to setup the environment identically to the setup.bat command
    Extraneous stuff put into extrasetup.sh

--- forge/setup.sh
+++ forge/setup.sh
@@ -3,70 +3,51 @@
 echo "MinecraftForge Linux Setup Program"
 echo 
 
-pushd .. > /dev/null
+SETUP_SCRIPT=$(readlink -e $0)
+FORGEPATH=${SETUP_SCRIPT%/*}
+MCPPATH=${FORGEPATH%/*}
 
-if [ -d src ] ; then
-	echo "!!! WARNING !!!"
-	echo
-	echo "This setup will erase your src directory.  Make sure to back up"
-	echo "any changes before continuing."
-	echo
-
-	read -p "Continue (y/n)? " var
-
-	if [ "$var" != "y" ]; then exit ; fi
+if [ ! -x ${MCPPATH}/cleanup.sh ]; then
+  echo "The path ${MCPPATH} does not appear to be a correctly setup MCP directory."
+  exit 1
 fi
 
-if [ ! -f ./runtime/bin/fernflower.jar ]
-then
-    pushd forge
-        python download_fernflower.py
-    popd
-fi
+echo ${FORGEPATH} ${MCPPATH}
 
-if [ ! -f ./runtime/bin/fernflower.jar ]
-then
+if [ "$1" = "-skipdecompile" ]; then
+  pushd ${MCPPATH} >/dev/null
+  ${MCPPATH}/updatenames.sh -f
+	popd >/dev/null
+else
+  if [ ! -f ${MCPPATH}/runtime/bin/fernflower.jar ]; then
+      python ${FORGEPATH}/download_fernflower.py
+  fi
+  if [ ! -f ${MCPPATH}/runtime/bin/fernflower.jar ]; then
     echo "Failed to download fernflower, install it manually and re-run setup."
     exit 1
-fi
+  fi
+  rm -rf ${MCPPATH}/src
+  
+  pushd ${MCPPATH} > /dev/null
+	${MCPPATH}/decompile.sh
+	popd >/dev/null
+fi  
 
-rm -rf src src_work src_forge
+rm -rf ${MCPPATH}/src_base ${MCPPATH}/src_work
 
-if [ ! -d src_base ] ; then
-	./decompile.sh
-	pushd src > /dev/null
+find ${MCPPATH}/src -name '*.java' -execdir sed -i 's/\r//g' {} +
+cp ${FORGEPATH}/MLProp.java ${MCPPATH}/src/minecraft/net/minecraft/src/MLProp.java
+cp ${FORGEPATH}/MLProp.java ${MCPPATH}/src/minecraft_server/net/minecraft/src/MLProp.java
 
-	find . -name *.java -exec sed -i 's/\r//g' \{\} \;
-    cp ../forge/MLProp.java minecraft/net/minecraft/src/MLProp.java
-    cp ../forge/MLProp.java minecraft_server/net/minecraft/src/MLProp.java
-	patch -p2 -i ../forge/modLoaderMP.patch
+pushd ${MCPPATH} > /dev/null
+${MCPPATH}/updatemd5.sh -f
+popd > /dev/null
 
-	popd > /dev/null
+cp -a ${MCPPATH}/src ${MCPPATH}/src_base
+cp -a ${MCPPATH}/src ${MCPPATH}/src_work
 
-	mv src src_base
-fi
-
-cp -a src_base src_work
-pushd src_work > /dev/null
-	for i in `find ../forge/patches/ -type f -name \*.patch`
-	do
-            patch -p2 -i $i
+pushd ${MCPPATH}/src_work > /dev/null
+	for i in $(find ${FORGEPATH}/patches/ -type f -name '*.patch'); do
+    patch -p2 -i ${i}
 	done
 popd > /dev/null
-
-cp -a src_work src_forge
-
-cp -a forge/forge_client/src/net/* src_forge/minecraft/net/
-cp -a forge/forge_common/net/* src_forge/minecraft/net/
-cp -a forge/forge_common/net/* src_forge/minecraft_server/net/
-cp -a forge/forge_server/src/net/* src_forge/minecraft_server/net/
-
-pushd forge > /dev/null
-./inject_version.sh $OLDPWD/src_forge/minecraft/net/minecraft/src/forge/ForgeHooks.java
-./inject_version.sh $OLDPWD/src_forge/minecraft_server/net/minecraft/src/forge/ForgeHooks.java
-popd > /dev/null
-
-cp -a src_forge src
-
-./updatemd5.sh
-
