70901fe2f99349a005b4d9aafeb2b16b2bee7db0
mjevans <mjevans1983@gmail.com>
2012-03-09 12:46:10 -0800

    inject_version.sh can now accept up to shell input limit arguments and processes each with a single sed operation.  Feeding xargs this sed instead would be better, but at least this can be invoked by xargs and/or as desired.

--- forge/inject_version.sh
+++ forge/inject_version.sh
@@ -1,14 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-target=$1
 read major minor rev build <version.txt
 
-echo Injecting Revision $major.$minor.$rev.$build $target
-
-sed s/majorVersion=0/majorVersion=$major/ <$target | \
-sed s/minorVersion=0/minorVersion=$minor/ | \
-sed s/revisionVersion=0/revisionVersion=$rev/ | \
-sed s/buildVersion=0/buildVersion=$build/ | \
-tr -d '\r' > $target.tmp
-
-mv $target.tmp $target
+while [ -n "$1" ] ; do
+  echo Injecting Revision $major.$minor.$rev.$build $1
+  sed -i "s/$(printf '\r\n')\$/$(printf '\n')/;s/majorVersion=0/majorVersion=$major/;s/minorVersion=0/minorVersion=$minor/;s/revisionVersion=0/revisionVersion=$rev/;s/buildVersion=0/buildVersion=$build/" "$1"
+  shift
+done
