d7cbbfb3d2a4ab5295238540a1cd7ab1b108345c
newthead <newthead@bigstring.com>
2012-03-16 21:28:08 -0700

    Added render code for inventory items and entity items.

--- forge/forge_client/src/net/minecraft/src/forge/IEntityItemRenderer.java
+++ forge/forge_client/src/net/minecraft/src/forge/IEntityItemRenderer.java
@@ -1,8 +1,9 @@
 package net.minecraft.src.forge;
 
+import net.minecraft.src.EntityItem;
 import net.minecraft.src.RenderBlocks;
 
 public interface IEntityItemRenderer
 {
-    public void renderEntityItem(RenderBlocks render, int itemID, int metadata);
+    public void renderEntityItem(RenderBlocks render, EntityItem item, int itemID, int metadata);
 }
