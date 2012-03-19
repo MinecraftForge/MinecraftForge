6a1cdc8e0c69b38cc860861e3ed37e4eb048a6cd
CovertJaguar <covertjaguar@gmail.com>
2012-03-19 08:09:00 -0700

    Fix for network code.

--- forge/forge_common/net/minecraft/src/forge/MessageManager.java
+++ forge/forge_common/net/minecraft/src/forge/MessageManager.java
@@ -416,7 +416,7 @@ public class MessageManager
             }
         }
 
-        if (connections.contains(manager))
+        if (connections.containsKey(manager))
         {
             ConnectionInstance con = getConnection(manager);
             IPacketHandler[] handlers = con.getChannelHandlers(channel);
