114c534db4967f1fab76b7107687d9ae099ff25a
LexManos <LexManos@gmail.com>
2012-03-27 07:26:39 -0700

    Changed update_patches script to use python for the bulk, Prevents making 1800 temporary useless files.

--- forge/update_patches.bat
+++ forge/update_patches.bat
@@ -3,46 +3,13 @@
 
 set PATH=.\bin;%PATH%
 
-echo Creating directories
-for /f %%i in ('find ../src_work -type d') do (
-  set file=%%i
-  set file=!file:~11!
-  rem echo patches!file!
-  "./bin/mkdir.exe" --parents "patches!file!"
-)
-echo Creating patches
-for /f %%i in ('find ../src_work -type f') do (
-  set file=%%i
-  set file=!file:~11!
-  rem echo !file!
-  if "!file:~-1!" NEQ "#" (
-    if "!file:~-1!" NEQ "~" (
-      diff -u ../src_base!file! ../src_work!file! -r --strip-trailing-cr --new-file | sed -e "1,2s/[0-9-]* [0-9:\.]* [+-][0-9]*\b/0000-00-00 00:00:00.000000000 -0000/" | tr -d \r  > patches!file!.patch
-    )
-  )
-)
-
-echo Removing empty patches
-find patches -size 0 -type f -regex .+\.patch -delete
-for /f %%i in ('find patches -depth -empty -type d') do (
-  set file=%%i
-  if "!file:.svn=!" EQU "!file!" (
-    rmdir "%%i"
-  )
-)
+python update_patches.py
 
 echo Grabbing copy of conf folder
 for /f %%i in ('find conf -type f') do (
   set file=%%i
-  if "!file:.svn=!" EQU "!file!" (
+  if %%i NEQ ".gitignore" (
     del !file:/=\!
-  )
-)
-
-for /f %%i in ('find conf -depth -empty -type d') do (
-  set file=%%i
-  if "!file:.svn=!" EQU "!file!" (
-    rmdir "%%i"
   )
 )
 
--- forge/update_patches.sh
+++ forge/update_patches.sh
@@ -1,36 +1,12 @@
-echo Creating directories
-
 fdir=$PWD
-cd ..
 
-for i in `find src_work -type d`
-do 
-	mkdir --parents "$fdir/patches${i##src_work}"
-done
-
-echo Creating patches
-
-cd src_work
-for i in `find ../src_work -type f`
-do
-	ibase=${i##../src_work}
-	diff -u ../src_base$ibase $i -r --strip-trailing-cr --new-file | sed -e "1,2s/[0-9-]* [0-9:\.]* [+-][0-9]*\b/0000-00-00 00:00:00.000000000 -0000/" | tr -d "\r" > $fdir/patches$ibase.patch
-done
-cd ..
-
-echo Removing empty patches
-find $fdir/patches -size 0 -type f -name \*.patch -delete
-for i in `find $fdir/patches -depth -empty -type d | grep -a -v "svn"` 
-do 
-	rmdir "$i"
-done
-
+python update_patches.py
 echo Grabbing copy of conf folder
-for i in `find $fdir/conf -type f | grep -a -v "svn"` 
+for i in `find $fdir/conf -type f | grep -a -v ".gitignore"` 
 do 
 	rm $i 
 done
-for i in `find $fdir/conf -depth -empty -type d | grep -a -v "svn"` 
+for i in `find $fdir/conf -depth -empty -type d | grep -a -v ".git"` 
 do 
 	rmdir "$i"
 done
