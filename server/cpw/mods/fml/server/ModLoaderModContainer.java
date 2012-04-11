78f2e8f38ac3c3ccdcfd4de9e09e298d1352fcbd
Christian <cpw@weeksfamily.ca>
2012-04-10 22:22:13 -0400

    Merge in fixes from chickenbones for tick related problem and straighten out variable naming in ModLoaderModContainer

--- server/cpw/mods/fml/server/ModLoaderModContainer.java
+++ server/cpw/mods/fml/server/ModLoaderModContainer.java
@@ -429,13 +429,14 @@ public class ModLoaderModContainer implements ModContainer
 
             if ("required-before".equals(depparts[0]) || "before".equals(depparts[0]))
             {
-                preDependencies.add(depparts[1]);
+            	postDependencies.add(depparts[1]);
             }
 
             if ("required-after".equals(depparts[0]) || "after".equals(depparts[0]))
             {
-                postDependencies.add(depparts[1]);
+                preDependencies.add(depparts[1]);
             }
+
         }
     }
 
@@ -458,7 +459,7 @@ public class ModLoaderModContainer implements ModContainer
             computeDependencies();
         }
 
-        return preDependencies;
+        return postDependencies;
     }
 
     @Override
@@ -468,9 +469,9 @@ public class ModLoaderModContainer implements ModContainer
         {
             computeDependencies();
         }
-
-        return postDependencies;
+        return preDependencies;
     }
+
 
     public String toString()
     {
