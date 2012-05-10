f4862dc40d8d83518ae6fe0ffa36947171c54e5a
Christian Weeks <cpw@weeksfamily.ca>
2012-05-10 02:01:23 -0400

    Fixes

--- fml/generatechangedfilelist.py
+++ fml/generatechangedfilelist.py
@@ -36,6 +36,27 @@ def main():
           clazz=clazz[len("net/minecraft/src/"):]
         fh.write("minecraft_server/%s.class\n" %(clazz))
 
+    prelist = os.path.join(mcp_root,"temp","client.md5")
+    postlist = os.path.join(mcp_root,"temp","client_reobf.md5")
+    cmd = 'diff --unchanged-group-format='' --old-group-format='' --new-group-format=\'%%>\' --changed-group-format=\'%%>\' %s %s' % (prelist, postlist)
+    process = subprocess.Popen(cmdsplit(cmd), stdout=subprocess.PIPE, bufsize=-1)
+    difflist,_= process.communicate()
+    srg_data = parse_srg(os.path.join(mcp_root,"temp","client_rg.srg"))
+    classes = {}
+    for row in srg_data['CL']:
+      classes[row['deobf_name']] = row['obf_name']
+
+    with open(list_file, 'w') as fh:
+      for diff in difflist.splitlines():
+        diffrow=diff.strip().split()
+        clazz=diffrow[0]
+        if clazz in classes:
+          clazz=classes[clazz]
+        if clazz.startswith("net/minecraft/src/"):
+          clazz=clazz[len("net/minecraft/src/"):]
+        fh.write("minecraft/%s.class\n" %(clazz))
+
+
     
 if __name__ == '__main__':
     main()
