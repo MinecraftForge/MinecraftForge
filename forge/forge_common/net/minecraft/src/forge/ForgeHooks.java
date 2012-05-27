eee81351d4d1c32af80fd88a78415fdc910785ce
LexManos <LexManos@gmail.com>
2012-05-26 20:28:24 -0700

    Bump version number for official release.

--- forge/forge_common/net/minecraft/src/forge/ForgeHooks.java
+++ forge/forge_common/net/minecraft/src/forge/ForgeHooks.java
@@ -637,7 +637,7 @@ public class ForgeHooks
     //This number is incremented every Minecraft version, and never reset
     public static final int majorVersion    = 3;
     //This number is incremented every official release, and reset every Minecraft version
-    public static final int minorVersion    = 1;
+    public static final int minorVersion    = 2;
     //This number is incremented every time a interface changes, and reset every Minecraft version
     public static final int revisionVersion = 3;
     //This number is incremented every time Jenkins builds Forge, and never reset. Should always be 0 in the repo code.
