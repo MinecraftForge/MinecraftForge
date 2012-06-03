eeeaf140a7946368a5031dce2ec24b7a1c482d83
Christian Weeks <cpw@weeksfamily.ca>
2012-06-02 22:40:45 -0400

    Helps to have a retention policy for runtime annotations

--- fml/client/net/minecraft/src/SidedProxy.java
+++ fml/client/net/minecraft/src/SidedProxy.java
@@ -14,10 +14,17 @@
 
 package net.minecraft.src;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @author cpw
  *
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface SidedProxy
 {
     /**
