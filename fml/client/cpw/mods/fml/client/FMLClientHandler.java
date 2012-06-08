4a050fbdba4ae2d1424ae15464f14927843a3724
Christian <cpw@weeksfamily.ca>
2012-06-08 15:22:40 -0400

    Fix parameter ordering in ticks

--- fml/client/cpw/mods/fml/client/FMLClientHandler.java
+++ fml/client/cpw/mods/fml/client/FMLClientHandler.java
@@ -239,7 +239,7 @@ public class FMLClientHandler implements IFMLSidedHandler
     {
         if (client.field_6324_e != null) {
             // For the client world ticks and game ticks are the same
-            FMLCommonHandler.instance().tickStart(EnumSet.of(TickType.WORLD,TickType.GAME,TickType.WORLDGUI), 0.0f, client.field_6324_e, client.field_6313_p);
+            FMLCommonHandler.instance().tickStart(EnumSet.of(TickType.WORLD,TickType.GAME,TickType.WORLDGUI), 0.0f, client.field_6313_p, client.field_6324_e);
         }
     }
 
@@ -250,7 +250,7 @@ public class FMLClientHandler implements IFMLSidedHandler
     {
         if (client.field_6324_e != null) {
             // For the client world ticks and game ticks are the same
-            FMLCommonHandler.instance().tickEnd(EnumSet.of(TickType.WORLD,TickType.GAME,TickType.WORLDGUI), 0.0f, client.field_6324_e, client.field_6313_p);
+            FMLCommonHandler.instance().tickEnd(EnumSet.of(TickType.WORLD,TickType.GAME,TickType.WORLDGUI), 0.0f, client.field_6313_p, client.field_6324_e);
         }
         for (IKeyHandler entry : keyHandlers)
         {
