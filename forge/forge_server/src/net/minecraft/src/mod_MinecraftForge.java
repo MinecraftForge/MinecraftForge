84613d5a926894c629abc5a64561479466fb7f2d
LexManos <LexManos@gmail.com>
2012-06-24 14:48:09 -0700

    Fucking spelling nazis

--- forge/forge_server/src/net/minecraft/src/mod_MinecraftForge.java
+++ forge/forge_server/src/net/minecraft/src/mod_MinecraftForge.java
@@ -17,7 +17,7 @@ public class mod_MinecraftForge extends NetworkMod
     @MLProp(info = "Set to false to reproduce a vinella bug that prevents mobs from spawning on inverted half-slabs and inverted stairs.")
     public static boolean SPAWNER_ALLOW_ON_INVERTED = true;
     
-    @MLProp(info = "The kick message used when a client tries to connect but does nto have Minecraft Forge installed.")
+    @MLProp(info = "The kick message used when a client tries to connect but does not have Minecraft Forge installed.")
     public static String NO_FORGE_KICK_MESSAGE = "This server requires you to have Minecraft Forge installed. http://MinecraftForge.net/";
 
     @MLProp(info = "Set to true to randomly shuffle the potential chunks for spawning, this is useful in FTB challange maps where you don't want one side hogging the spawns")
