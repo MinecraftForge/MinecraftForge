b51ce21b70112ba905f7565b4ce0fdea30119487
Christian <cpw@weeksfamily.ca>
2012-06-29 20:18:04 -0400

    Initial checkin. Some patches are probably missing, but client compiles.

--- fml/getversion.py
+++ fml/getversion.py
@@ -40,7 +40,7 @@ def main():
       f.write("%s=%s\n" %("fmlbuild.minor.number",minor))
       f.write("%s=%s\n" %("fmlbuild.revision.number",rev))
       f.write("%s=%s\n" %("fmlbuild.githash",githash))
-      f.write("%s=%s\n" %("fmlbuild.mcversion","1.2.5"))
+      f.write("%s=%s\n" %("fmlbuild.mcversion","12w24a"))
     
 if __name__ == '__main__':
     main()
