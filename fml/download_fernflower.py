20902645994c9a0b9db88164ba632adc91479265
Christian <cpw@weeksfamily.ca>
2012-07-02 15:54:25 -0400

    Some more fixes to the ant stuff: apply patches now happens at the right time, I grab fernflower as part of setup and added an updatepatches call too

--- fml/download_fernflower.py
+++ fml/download_fernflower.py
@@ -1,10 +1,17 @@
 import urllib
 import zipfile
+import sys
+import os
 
 if __name__ == '__main__':
     try:
         urllib.urlretrieve("http://goo.gl/PnJHp", './fernflower.zip')
+        if len(sys.argv)>1:
+          path = os.path.abspath(os.path.join(sys.argv[1],'runtime/bin'))
+        else:
+          path = os.path.abspath('../runtime/bin')
         zf = zipfile.ZipFile('fernflower.zip')
-        zf.extract('fernflower.jar', '../runtime/bin')
+        zf.extract('fernflower.jar', path)
+        print "Fernflower downloaded into MCP at %s" % (path)
     except:
         print "Downloading Fernflower failed download manually from http://goo.gl/PnJHp"
