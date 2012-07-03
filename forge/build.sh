a82498a083a2d9110a52d2e567302995271c4332
LexManos <LexManos@gmail.com>
2012-07-02 20:05:42 -0700

    Updated some remaining scripts to call there python versions.

--- forge/build.sh
+++ forge/build.sh
@@ -1,23 +1,2 @@
 #!/bin/bash
-
-cd ..
-
-rm -rf src bin src_forge
-
-cp -a src_work src_forge
-
-cp -a forge/forge_client/src/net/* src_forge/minecraft/net/
-cp -a forge/forge_server/src/net/* src_forge/minecraft_server/net/
-cp -a forge/forge_common/net/* src_forge/minecraft/net/
-cp -a forge/forge_common/net/* src_forge/minecraft_server/net/
-
-pushd forge > /dev/null
-./inject_version.sh $OLDPWD/src_forge/minecraft/net/minecraft/src/forge/ForgeHooks.java
-./inject_version.sh $OLDPWD/src_forge/minecraft_server/net/minecraft/src/forge/ForgeHooks.java
-popd > /dev/null
-
-cp -a src_forge src
-
-./recompile.sh
-
-
+python build.py "$@"
