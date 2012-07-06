765b0cd7926dc34094e2a0fbec364e32e14b7d47
MALfunction84 <MALfunction84@gmail.com>
2012-07-06 00:59:42 -0400

    Adding client-side block activation handler.

--- forge/forge_common/net/minecraft/src/forge/ForgeHooks.java
+++ forge/forge_common/net/minecraft/src/forge/ForgeHooks.java
@@ -100,6 +100,39 @@ public class ForgeHooks
     }
     static LinkedList<ISleepHandler> sleepHandlers = new LinkedList<ISleepHandler>();
 
+    /**
+     * Custom block activation handler.  This can be used to override normal block
+     * activation behavior.
+     * 
+     * @param block the Block type being activated
+     * @param world the World that contains the activated Block
+     * @param blockX the X coordinate of the Block being activated
+     * @param blockY the Y coordinate of the Block being activated
+     * @param blockZ the Z coordinate of the Block being activated
+     * @param sideHit which side was hit. If its -1 then it went the full length of
+     *        the ray trace. Bottom = 0, Top = 1, East = 2, West = 3, North = 4,
+     *        South = 5.
+     * @param player the Player who activated the Block
+     * @return true if a handler activated the Block, false otherwise.  If false,
+     *         behavior should fall through and invoke the behavior defined in the
+     *         {@link Block#blockActivated(net.minecraft.src.World, int, int, int, net.minecraft.src.EntityPlayer) Block} itself
+     * 
+     * @see IBlockActivationHandler Create a custom handler
+     * @see MinecraftForge#registerBlockActivationHandler(net.minecraft.src.Block, net.minecraft.src.forge.IBlockActivationHandler) Register a custom handler
+     * @see Block#blockActivated(net.minecraft.src.World, int, int, int, net.minecraft.src.EntityPlayer) Default behavior
+     */
+    public static boolean activateBlock(Block block, World world, int blockX, int blockY, int blockZ, int sideHit, EntityPlayer player) {
+        LinkedList<IBlockActivationHandler> handlers = blockActivationHandlers.get(block);
+        if (handlers != null) {
+            for (IBlockActivationHandler handler : handlers) {
+                if (handler != null && handler.activateBlock(block, world, blockX, blockY, blockZ, sideHit, player)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    static HashMap<Block, LinkedList<IBlockActivationHandler>> blockActivationHandlers = new HashMap<Block, LinkedList<IBlockActivationHandler>>();
 
     public static void onMinecartUpdate(EntityMinecart minecart, int x, int y, int z)
     {
