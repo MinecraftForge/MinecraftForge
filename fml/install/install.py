3d2651132294de13394ff8cb0bab878e17b326a2
LexManos <LexManos@gmail.com>
2012-08-08 23:44:32 -0700

    Include conf folder and merge config in source zip

--- fml/install/install.py
+++ fml/install/install.py
@@ -2,9 +2,9 @@ import os, os.path, sys
 
 from fml import setup_fml, finish_setup_fml, apply_fml_patches, setup_mcp
 
-def fml_main(fml_dir, mcp_dir):
+def fml_main(fml_dir, mcp_dir, dont_gen_conf=True):
     print '================ Forge ModLoader Setup Start ==================='
-    setup_mcp(fml_dir, mcp_dir)
+    setup_mcp(fml_dir, mcp_dir, dont_gen_conf)
     setup_fml(fml_dir, mcp_dir)
     apply_fml_patches(fml_dir, mcp_dir, os.path.join(mcp_dir, 'src'))
     finish_setup_fml(fml_dir, mcp_dir)
