f374723610e481204f6eb5c54b192f5b8d28e01a
LexManos <LexManos@gmail.com>
2012-08-10 18:15:10 -0700

    Update fml, impelemnted Diemsnion API stuff.

--- common/net/minecraftforge/common/DimensionManager.java
+++ common/net/minecraftforge/common/DimensionManager.java
@@ -1,20 +1,23 @@
 package net.minecraftforge.common;
 
 import java.util.Hashtable;
+import java.util.Map.Entry;
 import java.util.logging.Level;
 
 import cpw.mods.fml.common.FMLCommonHandler;
 
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.src.*;
 
 public class DimensionManager
 {
-    private static Hashtable<Integer, WorldProvider> providers = new Hashtable<Integer, WorldProvider>();
+    private static Hashtable<Integer, Class<? extends WorldProvider>> providers = new Hashtable<Integer, Class<? extends WorldProvider>>();
     private static Hashtable<Integer, Boolean> spawnSettings = new Hashtable<Integer, Boolean>();
-    private static Hashtable<Integer, World> worlds = new Hashtable<Integer, World>();
+    private static Hashtable<Integer, WorldServer> worlds = new Hashtable<Integer, WorldServer>();
     private static boolean hasInit = false;
+    private static Hashtable<Integer, Integer> dimensions = new Hashtable<Integer, Integer>();
 
-    public static boolean registerDimension(int id, WorldProvider provider, boolean keepLoaded)
+    public static boolean registerProviderType(int id, Class<? extends WorldProvider> provider, boolean keepLoaded)
     {
         if (providers.containsValue(id))
         {
@@ -31,38 +34,79 @@ public class DimensionManager
         {
             return;
         }
-        registerDimension( 0, new WorldProviderSurface(), true);
-        registerDimension(-1, new WorldProviderHell(),    true);
-        registerDimension( 1, new WorldProviderEnd(),     false);
+        registerProviderType( 0, WorldProviderSurface.class, true);
+        registerProviderType(-1, WorldProviderHell.class,    true);
+        registerProviderType( 1, WorldProviderEnd.class,     false);
+        registerDimension( 0,  0);
+        registerDimension(-1, -1);
+        registerDimension( 1,  1);
     }
 
-    public static WorldProvider getProvider(int id)
+    public static void registerDimension(int id, int providerType)
     {
-        return providers.get(id);
+        if (!providers.containsKey(providerType))
+        {
+            throw new IllegalArgumentException(String.format("Failed to register dimensiuon for id %d, provider type %d does not exist", id, providerType));
+        }
+        if (dimensions.containsKey(id))
+        {
+            throw new IllegalArgumentException(String.format("Failed to register dimensiuon for id %d, One is already registered", id));
+        }
+        dimensions.put(id, providerType);
+    }
+
+    public static int getProviderType(int dim)
+    {
+        if (!dimensions.containsKey(dim))
+        {
+            throw new IllegalArgumentException(String.format("Could not get provider type for dimension %d, does not exist", dim));
+        }
+        return dimensions.get(dim);
     }
 
     public static Integer[] getIDs()
     {
-        return providers.keySet().toArray(new Integer[0]);
+        return dimensions.keySet().toArray(new Integer[0]);
     }
 
-    public static void setWorld(int id, World world)
+    public static void setWorld(int id, WorldServer world)
     {
         worlds.put(id, world);
+
+        WorldServer[] tmp = new WorldServer[worlds.size() > 3 ? worlds.size() : 3];
+        for (int x = -1; x <= 1; x++)
+        {
+            tmp[(x == 0 ? 0 : x == -1 ? 1 : 2)] = worlds.get(x);
+        }
+
+        int x = 3;
+        for (Entry<Integer, WorldServer> entry : worlds.entrySet())
+        {
+            int dim = entry.getKey();
+            if (dim >= -1 && dim <= 1)
+            {
+                continue;
+            }
+            tmp[x++] = entry.getValue();
+        }
+
+        MinecraftServer.getServer().dimensionServerList = tmp;
+        MinecraftServer.getServer().worldTickTimes.put(id, new long[100]);
     }
 
-    public static World getWorld(int id)
+    public static WorldServer getWorld(int id)
     {
         return worlds.get(id);
     }
 
-    public static World[] getWorlds()
+    public static WorldServer[] getWorlds()
     {
-        return worlds.values().toArray(new World[0]);
+        return worlds.values().toArray(new WorldServer[0]);
     }
 
-    public static boolean shouldLoadSpawn(int id)
+    public static boolean shouldLoadSpawn(int dim)
     {
+        int id = getProviderType(dim);
         return spawnSettings.contains(id) && spawnSettings.get(id);
     }
 
@@ -71,15 +115,25 @@ public class DimensionManager
         init();
     }
 
-	public static WorldProvider createProviderFor(int i) {
-		try {
-			if (providers.containsKey(i))
-				return getProvider(i).getClass().newInstance();
-			else
-				return null;
-		} catch (Exception e) {
-			FMLCommonHandler.instance().getFMLLogger().log(Level.SEVERE,String.format("An error occured trying to create an instance of WorldProvider %d (%s)",i,getProvider(i).getClass().getSimpleName()),e);
-			throw new RuntimeException(e);
-		}
-	}
+    public static WorldProvider createProviderFor(int dim)
+    {
+        try
+        {
+            if (dimensions.containsKey(dim))
+            {
+                return providers.get(getProviderType(dim)).newInstance();
+            }
+            else
+            {
+                return null;
+            }
+        } 
+        catch (Exception e)
+        {
+            FMLCommonHandler.instance().getFMLLogger().log(Level.SEVERE,String.format("An error occured trying to create an instance of WorldProvider %d (%s)",
+                    dim,
+                    providers.get(getProviderType(dim)).getSimpleName()),e);
+            throw new RuntimeException(e);
+        }
+    }
 }
