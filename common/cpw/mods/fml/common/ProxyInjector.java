d96a81e4f2c3f9fd7d40a8c6ea07e66043d772ac
Christian <cpw@weeksfamily.ca>
2012-08-11 02:24:29 -0400

    Fix up proxy injector to inject properly

--- common/cpw/mods/fml/common/ProxyInjector.java
+++ common/cpw/mods/fml/common/ProxyInjector.java
@@ -33,7 +33,7 @@ public class ProxyInjector
     public static void inject(ModContainer mod, ASMDataTable data, Side side)
     {
         FMLLog.fine("Attempting to inject @SidedProxy classes into %s", mod.getModId());
-        Set<ASMData> targets = data.getAnnotationsFor(mod).get(Type.getDescriptor(SidedProxy.class));
+        Set<ASMData> targets = data.getAnnotationsFor(mod).get(SidedProxy.class.getName());
         ClassLoader mcl = Loader.instance().getModClassLoader();
 
         for (ASMData targ : targets)
@@ -52,7 +52,7 @@ public class ProxyInjector
                 String targetType = side.isClient() ? target.getAnnotation(SidedProxy.class).clientSide() : target.getAnnotation(SidedProxy.class).serverSide();
                 Object proxy=Class.forName(targetType, true, mcl).newInstance();
 
-                if ((target.getModifiers() & Modifier.STATIC) != 0 )
+                if ((target.getModifiers() & Modifier.STATIC) == 0 )
                 {
                     FMLLog.severe("Attempted to load a proxy type %s into %s.%s, but the field is not static", targetType, targ.getClassName(), targ.getObjectName());
                     throw new LoaderException();
