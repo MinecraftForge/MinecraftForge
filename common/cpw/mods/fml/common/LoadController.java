785bdde6bde46cbb6e04d83b0b33f44d812df239
Christian <cpw@weeksfamily.ca>
2012-08-16 08:34:12 -0400

    Fix critical bug in FML load controller that complains about injected mod containers

--- common/cpw/mods/fml/common/LoadController.java
+++ common/cpw/mods/fml/common/LoadController.java
@@ -147,7 +147,7 @@ public class LoadController
             {
                 builder.put(mc, mc.getMod());
             }
-            if (mc.getMod()==null)
+            if (mc.getMod()==null && !mc.isImmutable())
             {
                 FMLLog.severe("There is a severe problem with %s - it appears not to have constructed correctly", mc.getModId());
                 if (state != LoaderState.CONSTRUCTING)
