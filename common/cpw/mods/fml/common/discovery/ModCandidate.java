a697d043fc362d76a4c5d2df71d829ab69903dd2
Christian <cpw@weeksfamily.ca>
2012-09-04 11:39:51 -0400

    Rescan mod packages where there was a possible mod identified to try and make sure we load it. Fixes Matmos r16

--- common/cpw/mods/fml/common/discovery/ModCandidate.java
+++ common/cpw/mods/fml/common/discovery/ModCandidate.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import com.google.common.collect.Lists;
 
+import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.ModContainer;
+import cpw.mods.fml.common.discovery.asm.ASMModParser;
 
 
 public class ModCandidate
@@ -16,6 +18,7 @@ public class ModCandidate
     private boolean classpath;
     private List<String> baseModTypes = Lists.newArrayList();
     private boolean isMinecraft;
+    private List<ASMModParser> baseModCandidateTypes = Lists.newArrayListWithCapacity(1);
 
     public ModCandidate(File classPathRoot, File modContainer, ContainerType sourceType)
     {
@@ -46,7 +49,16 @@ public class ModCandidate
     }
     public List<ModContainer> explore(ASMDataTable table)
     {
-        return sourceType.findMods(this, table);
+        List<ModContainer> mods = sourceType.findMods(this, table);
+        if (!baseModCandidateTypes.isEmpty())
+        {
+            FMLLog.info("Attempting to reparse the mod container %s", getModContainer().getName());
+            return sourceType.findMods(this, table);
+        }
+        else
+        {
+            return mods;
+        }
     }
 
     public boolean isClasspath()
@@ -64,5 +76,9 @@ public class ModCandidate
     public boolean isMinecraftJar()
     {
         return isMinecraft;
+    }
+    public void rememberModCandidateType(ASMModParser modParser)
+    {
+        baseModCandidateTypes.add(modParser);
     }
 }
