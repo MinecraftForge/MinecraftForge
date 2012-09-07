a451af55b818c002c6b4563c2ae863184b4ac9ad
XCompWiz <xcompwiz@gmail.com>
2012-09-07 16:54:34 +0300

    Moves Spawn Conversion and adds Docs
    -Adds java docs to the IAdvancedDroppable interface
    -Moves check for and usage of IAdvanceDroppable implementation to
    World.spawnEntityInWorld

--- common/net/minecraftforge/common/IAdvancedDroppable.java
+++ common/net/minecraftforge/common/IAdvancedDroppable.java
@@ -4,7 +4,22 @@ import net.minecraft.src.Entity;
 import net.minecraft.src.ItemStack;
 import net.minecraft.src.World;
 
+/**
+ * This interface can be implemented by an instance of Item.
+ * It allows for an entity to replace the normal floating EntityItem
+ * when an item is dropped or otherwise spawned into the world
+ * (including ejected from a container).
+ */
 public interface IAdvancedDroppable {
 
-	public Entity createEntity(World world, Entity location, ItemStack itemstack);
+    /**
+     * This function should return a new entity to replace the dropped item.
+     * Returning null here will not kill the EntityItem and will leave it to function normally.
+     * Called when the item it placed in a world.
+     * @param world The world object
+     * @param location The EntityItem object, useful for getting the position of the entity
+     * @param itemstack The
+     * @return A new Entity object to spawn or null
+     */
+    public Entity createEntity(World world, Entity location, ItemStack itemstack);
 }
