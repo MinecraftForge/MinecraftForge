65ddfd49af016f21d245279039e9921c4be266d8
LexManos <LexManos@gmail.com>
2012-09-11 17:34:37 -0700

    Make EntityJoinWorldEvent Cancelable properly

--- common/net/minecraftforge/event/entity/EntityJoinWorldEvent.java
+++ common/net/minecraftforge/event/entity/EntityJoinWorldEvent.java
@@ -2,7 +2,9 @@ package net.minecraftforge.event.entity;
 
 import net.minecraft.src.Entity;
 import net.minecraft.src.World;
+import net.minecraftforge.event.Cancelable;
 
+@Cancelable
 public class EntityJoinWorldEvent extends EntityEvent
 {
 
