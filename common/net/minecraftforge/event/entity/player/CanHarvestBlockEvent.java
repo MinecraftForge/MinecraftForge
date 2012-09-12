749881dd1df7f468bce0db32979e33b479155269
Timothy Tomkins <t.tomkins@gmail.com>
2012-09-12 19:46:54 -0400

    Added Javadoc's,
    Also rolledback todays changes to EntityPlayer.java.patch

--- common/net/minecraftforge/event/entity/player/CanHarvestBlockEvent.java
+++ common/net/minecraftforge/event/entity/player/CanHarvestBlockEvent.java
@@ -9,6 +9,16 @@ public class CanHarvestBlockEvent extends PlayerEvent
     public boolean result;
     public final Block block;
     
+    /**
+     * Fired while a player is mining a block,
+     * Used to determine if anything drops when the block is mined.
+     * 
+     * Returns the value of result
+     * 
+     * @param player -EntityPlayer who is mining the block
+     * @param block  -Block being mined
+     * @param result -Boolean value from EntityPlayer.inventory.canHarvestBlock
+     */
     public CanHarvestBlockEvent(EntityPlayer player, Block block, boolean result)
     {
         super(player);
--- common/net/minecraftforge/event/entity/player/PlayerStrVsBlockEvent.java
+++ common/net/minecraftforge/event/entity/player/PlayerStrVsBlockEvent.java
@@ -12,6 +12,17 @@ public class PlayerStrVsBlockEvent extends PlayerEvent
     public final int meta;
     private boolean handeled = false;
     
+    /**
+     * Fired while a player is mining a block,
+     * Used to determine mining speed.
+     * 
+     * Canceling the event stops the normal check from being done and returns the value of result.
+     * result defaults to 0F (unable to mine)
+     * 
+     * @param player -EntityPlayer who is mining the block
+     * @param block  -Block being mined
+     * @param meta   -Metadata of the block being mined
+     */
     public PlayerStrVsBlockEvent(EntityPlayer player, Block block, int meta)
     {
         super(player);
