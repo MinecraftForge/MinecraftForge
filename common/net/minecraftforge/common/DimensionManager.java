62700fa7a89576004303c388d3c8279ccf00f740
LexManos <LexManos@gmail.com>
2012-09-15 19:36:08 -0700

    96537dc - Fixup Item in hand rendering of textures > 16x e88ddc5 - Make the exception print for headless servers if the Downloader cant initalize. f24ff07 - Remove a bunch of warning about javadoc, not a real fix, but no more warning. 3420ea7 - Update MCP Mapings 87104f0 - Makes GetModSrc check the common folder when attempting to extract source files 640446b - Tie parent/child containers together again. Should close #96 2cf4917 - Village mating produces added villagers, and the egg should place added villagers too. Closes #97. 6394ee8 - Clean up stale references to the Player stored in the NetworkRegistry, closes #100 7299e05 - Merge pull request #99 from CovertJaguar/patch-1 c4bd040 - More readable init logs

--- common/net/minecraftforge/common/DimensionManager.java
+++ common/net/minecraftforge/common/DimensionManager.java
@@ -94,7 +94,7 @@ public class DimensionManager
             tmp.add(entry.getValue());
         }
 
-        MinecraftServer.getServer().theWorldServer = tmp.toArray(new WorldServer[0]);
+        MinecraftServer.getServer().worldServers = tmp.toArray(new WorldServer[0]);
         MinecraftServer.getServer().worldTickTimes.put(id, new long[100]);
     }
 
