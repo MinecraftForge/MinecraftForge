5fb999fa229686a166e5859bc9afe6294545142e
Christian <cpw@weeksfamily.ca>
2012-09-26 08:52:40 -0400

    Add in an "EntityEvent.EnteringChunk" event. Useful for your entity chunkloading stuff.

--- common/net/minecraftforge/event/entity/EntityEvent.java
+++ common/net/minecraftforge/event/entity/EntityEvent.java
@@ -6,18 +6,35 @@ import net.minecraftforge.event.Event;
 public class EntityEvent extends Event
 {
     public final Entity entity;
-    
+
     public EntityEvent(Entity entity)
     {
         this.entity = entity;
     }
-    
+
     public static class CanUpdate extends EntityEvent
     {
         public boolean canUpdate = false;
         public CanUpdate(Entity entity)
         {
             super(entity);
+        }
+    }
+
+    public static class EnteringChunk extends EntityEvent
+    {
+        public int newChunkX;
+        public int newChunkZ;
+        public int oldChunkX;
+        public int oldChunkZ;
+
+        public EnteringChunk(Entity entity, int newChunkX, int newChunkZ, int oldChunkX, int oldChunkZ)
+        {
+            super(entity);
+            this.newChunkX = newChunkX;
+            this.newChunkZ = newChunkZ;
+            this.oldChunkX = oldChunkX;
+            this.oldChunkZ = oldChunkZ;
         }
     }
 }
