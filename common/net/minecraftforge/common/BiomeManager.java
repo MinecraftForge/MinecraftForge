c2f2edc7c15bf026fdc1d99201a5b78fbe690f71
Battlefield <5er9755@gmail.com>
2012-09-30 14:07:07 +0200

    Fixed most of the things that were pointed out

--- common/net/minecraftforge/common/BiomeManager.java
+++ common/net/minecraftforge/common/BiomeManager.java
@@ -1,10 +1,16 @@
 package net.minecraftforge.common;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import com.google.common.collect.Sets;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 
 import net.minecraft.src.BiomeGenBase;
+import net.minecraft.src.MapGenStronghold;
 import net.minecraft.src.MapGenVillage;
 
 /**
@@ -13,7 +19,6 @@ import net.minecraft.src.MapGenVillage;
  * @author battlefield
  *
  */
-
 public class BiomeManager
 {
     private static ArrayList<BiomeGenBase> spawnBiomes = new ArrayList<BiomeGenBase>();
@@ -21,55 +26,27 @@ public class BiomeManager
     private static ArrayList<BiomeGenBase> villageBiomes = new ArrayList<BiomeGenBase>();
 
     /**
-     * Equivalent to GameRegistry.addBiome
-     * @param biome
-     */
-    public static void addBiome(BiomeGenBase biome)
-    {
-        GameRegistry.addBiome(biome);
-    }
-
-    /**
-     * Equivalent to GameRegistry.removeBiome
-     * @param biome
-     */
-    public static void removeBiome(BiomeGenBase biome)
-    {
-        GameRegistry.removeBiome(biome);
-    }
-
-    /**
      * Adds the possibility to generate villages in your custom biomes
      * @param biome		Your biome
      * @param canSpawn	True if you want to add it to the list, false if you want to remove it
      */
-
     public static void setVillageCanSpawnInBiome(BiomeGenBase biome, boolean canSpawn)
     {
+    	ArrayList<BiomeGenBase> biomesForVillage = new ArrayList<BiomeGenBase>(MapGenVillage.villageSpawnBiomes);
         if (canSpawn)
         {
-            if (!villageBiomes.contains(biome))
-            {
-                villageBiomes.add(biome);
-            }
-
-            ArrayList list = new ArrayList();
-            list.addAll(MapGenVillage.villageSpawnBiomes);
-            list.add(biome);
-            MapGenVillage.villageSpawnBiomes = list.subList(0, list.size());
+        	if(!villageBiomes.contains(biome))
+        	{
+	            villageBiomes.add(biome);
+	            biomesForVillage.add(biome);
+        	}
         }
         else
         {
-            if (villageBiomes.contains(biome))
-            {
-                villageBiomes.remove(biome);
-            }
-
-            ArrayList list = new ArrayList();
-            list.addAll(MapGenVillage.villageSpawnBiomes);
-            list.remove(biome);
-            MapGenVillage.villageSpawnBiomes = list.subList(0, list.size());
+            villageBiomes.remove(biome);
+            biomesForVillage.remove(biome);
         }
+        MapGenVillage.villageSpawnBiomes = biomesForVillage;
     }
 
     /**
@@ -77,22 +54,18 @@ public class BiomeManager
      * @param biome		Your biome
      * @param canSpawn	True if you want to add it to the list, false if you want to remove it
      */
-
     public static void setStrongholdCanSpawnInBiome(BiomeGenBase biome, boolean canSpawn)
     {
         if (canSpawn)
         {
-            if (!strongholdBiomes.contains(biome))
-            {
-                strongholdBiomes.add(biome);
-            }
+        	if(!strongholdBiomes.contains(biome))
+        	{
+            	strongholdBiomes.add(biome);
+        	}
         }
         else
         {
-            if (strongholdBiomes.contains(biome))
-            {
-                strongholdBiomes.remove(biome);
-            }
+            strongholdBiomes.remove(biome);
         }
     }
 
@@ -101,22 +74,18 @@ public class BiomeManager
      * @param biome		Your biome
      * @param canSpawn	True if you want to add it to the list, false if you want to remove it
      */
-
     public static void setPlayerCanSpawnInBiome(BiomeGenBase biome, boolean canSpawn)
     {
         if (canSpawn)
         {
-            if (!spawnBiomes.contains(biome))
-            {
-                spawnBiomes.add(biome);
-            }
+        	if(!spawnBiomes.contains(biome))
+        	{
+            	spawnBiomes.add(biome);
+        	}
         }
         else
         {
-            if (spawnBiomes.contains(biome))
-            {
-                spawnBiomes.remove(biome);
-            }
+            spawnBiomes.remove(biome);
         }
     }
 
