4af51b0cadd647fc651d72303a34a8f1faaf11c8
Christian <cpw@weeksfamily.ca>
2012-10-16 13:24:29 -0400

    Fix the test mod

--- fml/eclipse/coremodtest/src/cpw/mods/testcoremod/FMLLoadPlugin.java
+++ fml/eclipse/coremodtest/src/cpw/mods/testcoremod/FMLLoadPlugin.java
@@ -1,5 +1,7 @@
 package cpw.mods.testcoremod;
 
+import java.util.Map;
+
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 
 public class FMLLoadPlugin implements IFMLLoadingPlugin
@@ -14,6 +16,27 @@ public class FMLLoadPlugin implements IFMLLoadingPlugin
     public String[] getASMTransformerClass()
     {
         return null;
+    }
+
+    @Override
+    public String getModContainerClass()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getSetupClass()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data)
+    {
+        // TODO Auto-generated method stub
+
     }
 
 }
