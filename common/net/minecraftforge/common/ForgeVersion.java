f20c0f20ad5db71b25ef29e5fa617fb08a67ae61
LexManos <LexManos@gmail.com>
2012-10-19 18:57:24 -0700

    Bump version to 4.3, final for 1.3.2

--- common/net/minecraftforge/common/ForgeVersion.java
+++ common/net/minecraftforge/common/ForgeVersion.java
@@ -10,7 +10,7 @@ public class ForgeVersion
     //This number is incremented every Minecraft version, and never reset
     public static final int majorVersion    = 4;
     //This number is incremented every official release, and reset every Minecraft version
-    public static final int minorVersion    = 2;
+    public static final int minorVersion    = 3;
     //This number is incremented every time a interface changes or new major feature is added, and reset every Minecraft version
     public static final int revisionVersion = 5;
     //This number is incremented every time Jenkins builds Forge, and never reset. Should always be 0 in the repo code.
