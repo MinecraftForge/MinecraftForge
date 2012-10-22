4b9e43d3c92edf37aaa5e9eed95fa53b8e37d7d9
LexManos <LexManos@gmail.com>
2012-10-22 00:29:28 -0700

    Fix up the setHandeled/setHandled for events, it is now a generalized setResult, the meaning of which is defined by each event.

--- common/net/minecraftforge/event/entity/player/EntityItemPickupEvent.java
+++ common/net/minecraftforge/event/entity/player/EntityItemPickupEvent.java
@@ -4,26 +4,29 @@ import net.minecraft.src.Entity;
 import net.minecraft.src.EntityItem;
 import net.minecraft.src.EntityPlayer;
 import net.minecraftforge.event.Cancelable;
+import net.minecraftforge.event.Event;
 
 @Cancelable
+@Event.HasResult
 public class EntityItemPickupEvent extends PlayerEvent
 {
+    /**
+     * This event is called when a player collides with a EntityItem on the ground.
+     * The event can be canceled, and no further processing will be done.
+     * 
+     *  You can set the result of this event to ALLOW which will trigger the 
+     *  processing of achievements, FML's event, play the sound, and kill the 
+     *  entity if all the items are picked up.
+     *  
+     *  setResult(ALLOW) is the same as the old setHandled()
+     */
+
     public final EntityItem item;
     private boolean handled = false;
-    
+
     public EntityItemPickupEvent(EntityPlayer player, EntityItem item)
     {
         super(player);
         this.item = item;
-    }
-    
-    public boolean isHandled()
-    {
-        return handled;
-    }
-
-    public void setHandled()
-    {
-        handled = true;
     }
 }
