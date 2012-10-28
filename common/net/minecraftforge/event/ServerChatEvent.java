6ded8b4664a7e813250533bf325e79479d20d323
ShadwDrgn <csendek@gmail.com>
2012-10-28 06:58:23 -0400

    Add event to cancel or change chat messages sent from server

--- common/net/minecraftforge/event/ServerChatEvent.java
+++ common/net/minecraftforge/event/ServerChatEvent.java
@@ -4,15 +4,15 @@ import net.minecraft.src.EntityPlayerMP;
 
 @Cancelable
 public class ServerChatEvent extends Event {
-        public final String message, username;
-        public final EntityPlayerMP player;
-        public String line;
-        public ServerChatEvent(EntityPlayerMP player, String message, String line)
-        {
-            super();
-            this.message = message;
-            this.player = player;
-            this.username = player.username;
-            this.line = "<" + username + "> " + message;
-        }
+    public final String message, username;
+    public final EntityPlayerMP player;
+    public String line;
+    public ServerChatEvent(EntityPlayerMP player, String message, String line)
+    {
+        super();
+        this.message = message;
+        this.player = player;
+        this.username = player.username;
+        this.line = "<" + username + "> " + message;
+    }
 }
