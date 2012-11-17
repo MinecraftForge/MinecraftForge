a2e5395036b52195a625de4040e89c2a8e1bbe76
Emiel Tasseel <Emiel Tasseel>
2012-11-17 17:47:26 +0100

    Added a cancellable PollInputEvent

---
+++ client/net/minecraftforge/client/event/PollInputEvent.java
@@ -0,0 +1,10 @@
+package net.minecraftforge.client.event;
+
+import net.minecraftforge.event.Cancelable;
+import net.minecraftforge.event.Event;
+
+@Cancelable
+public class PollInputEvent extends Event
+{
+    
+}
