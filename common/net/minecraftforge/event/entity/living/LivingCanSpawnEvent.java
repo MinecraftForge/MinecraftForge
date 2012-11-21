d3bd0831a48edda6e0248711734f8cbec4dcc26e
Scott Killen <scott@scottkillen.com>
2012-11-21 03:32:38 -0500

    Added events to all mobs

---
+++ common/net/minecraftforge/event/entity/living/LivingCanSpawnEvent.java
@@ -0,0 +1,24 @@
+package net.minecraftforge.event.entity.living;
+
+import net.minecraft.src.EntityLiving;
+import net.minecraft.src.World;
+import net.minecraftforge.event.Event.HasResult;
+
+/**
+ * Fires before mob spawn events.
+ * 
+ * Result is significant:
+ *    DEFAULT: use vanilla spawn rules
+ *    ALLOW:   allow the spawn
+ *    DENY:    deny the spawn
+ *
+ */
+@HasResult
+public class LivingCanSpawnEvent extends LivingSpawnEvent
+{
+
+    public LivingCanSpawnEvent(EntityLiving entity, World world, float x, float y, float z)
+    {
+        super(entity, world, x, y, z);
+    }
+}
--- common/net/minecraftforge/event/entity/living/LivingSpecialSpawnEvent.java
+++ common/net/minecraftforge/event/entity/living/LivingSpecialSpawnEvent.java
@@ -3,32 +3,13 @@ package net.minecraftforge.event.entity.living;
 import net.minecraft.src.EntityLiving;
 import net.minecraft.src.World;
 import net.minecraftforge.event.Cancelable;
-import net.minecraftforge.event.Event.HasResult;
 
-/**
- * Fires before special spawn events. Cancel the event to cancel special spawn.
- * 
- * On slime spawns, result is significant:
- *    DEFAULT: use vanilla spawn rules
- *    ALLOW:   allow the spawn
- *    DENY:    deny the spawn
- *
- */
 @Cancelable
-@HasResult
-public class LivingSpecialSpawnEvent extends LivingEvent
+public class LivingSpecialSpawnEvent extends LivingSpawnEvent
 {
-    public final World world;
-    public final float x;
-    public final float y;
-    public final float z;
-    
+
     public LivingSpecialSpawnEvent(EntityLiving entity, World world, float x, float y, float z)
     {
-        super(entity);
-        this.world = world;
-        this.x = x;
-        this.y = y;
-        this.z = z;
+        super(entity, world, x, y, z);
     }
 }
