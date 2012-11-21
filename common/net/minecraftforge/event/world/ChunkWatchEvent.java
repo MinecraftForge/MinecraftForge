83655c6146616b1428bc8aad1a5a78dd4ee802ba
Chicken Bones <mehvids@gmail.com>
2012-11-21 12:30:13 +1000

    Added events for chunk watching and unwatching. Made PlayerInstance getter public

---
+++ common/net/minecraftforge/event/world/ChunkWatchEvent.java
@@ -0,0 +1,28 @@
+package net.minecraftforge.event.world;
+
+import net.minecraft.src.Chunk;
+import net.minecraft.src.ChunkCoordIntPair;
+import net.minecraft.src.EntityPlayerMP;
+import net.minecraftforge.event.Event;
+
+public class ChunkWatchEvent extends Event
+{
+	public final ChunkCoordIntPair chunk;
+	public final EntityPlayerMP player;
+	
+	public ChunkWatchEvent(ChunkCoordIntPair chunk, EntityPlayerMP player)
+	{
+		this.chunk = chunk;
+		this.player = player;
+	}
+	
+	public static class Watch extends ChunkWatchEvent
+	{
+		public Watch(ChunkCoordIntPair chunk, EntityPlayerMP player) { super(chunk, player); }		
+	}
+	
+	public static class UnWatch extends ChunkWatchEvent
+	{
+		public UnWatch(ChunkCoordIntPair chunkLocation, EntityPlayerMP player) { super(chunkLocation, player); }		
+	}
+}
