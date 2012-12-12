a513060a81ac4b245b4f19b5ac3e589eb15e3515
Christian <cpw@weeksfamily.ca>
2012-12-11 22:37:07 -0500

    Change to download bouncy castle as well. We also finally properly use the downloaded libs even in vanilla code!

--- common/cpw/mods/fml/relauncher/CoreFMLLibraries.java
+++ common/cpw/mods/fml/relauncher/CoreFMLLibraries.java
@@ -2,8 +2,8 @@ package cpw.mods.fml.relauncher;
 
 public class CoreFMLLibraries implements ILibrarySet
 {
-    private static String[] libraries = { "argo-2.25.jar","guava-12.0.1.jar","asm-all-4.0.jar" };
-    private static String[] checksums = { "bb672829fde76cb163004752b86b0484bd0a7f4b", "b8e78b9af7bf45900e14c6f958486b6ca682195f", "98308890597acb64047f7e896638e0d98753ae82" };
+    private static String[] libraries = { "argo-2.25.jar","guava-12.0.1.jar","asm-all-4.0.jar", "bcprov-jdk15on-147.jar" };
+    private static String[] checksums = { "bb672829fde76cb163004752b86b0484bd0a7f4b", "b8e78b9af7bf45900e14c6f958486b6ca682195f", "98308890597acb64047f7e896638e0d98753ae82", "b6f5d9926b0afbde9f4dbe3db88c5247be7794bb" };
 
     @Override
     public String[] getLibraries()
