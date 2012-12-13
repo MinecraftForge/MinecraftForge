31695d5ec1160337d93c473d76d8fbb333c94771
LexManos <LexManos@gmail.com>
2012-12-13 00:22:30 -0800

    Fix var name messup

--- install/fml.py
+++ install/fml.py
@@ -702,7 +702,7 @@ def setup_mcp(fml_dir, mcp_dir, dont_gen_conf=True):
     if not os.path.isfile(os.path.join(fml_dir, 'fmlbuild.properties-sample')):
         mcp_eclipse = os.path.join(mcp_dir, 'eclipse')
         
-        if os.path.isdir(os.path.join(mcp_eclipse, 'Client')) and os.path.isdir(os.path.join(eclipse_dir, 'Server')):
+        if os.path.isdir(os.path.join(mcp_eclipse, 'Client')) and os.path.isdir(os.path.join(mcp_eclipse, 'Server')):
             shutil.rmtree(mcp_eclipse)
             
         if not os.path.isdir(mcp_eclipse) and os.path.isdir(os.path.join(fml_dir, 'eclipse')):
