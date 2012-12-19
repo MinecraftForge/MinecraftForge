2055543042e9b699fdcf76a4733a09bec02cdcb6
LexManos <LexManos@gmail.com>
2012-12-19 02:58:09 -0800

    Add quotes around javac command.

--- install/fml.py
+++ install/fml.py
@@ -268,14 +268,14 @@ def setup_fml(fml_dir, mcp_dir):
                 sys.exit(1)
             
             self.logger.info('> Running MCPMerger')
-            forkcmd = ('%s -classpath "{classpath}" cpw.mods.fml.common.asm.transformers.MCPMerger "{mergecfg}" "{client}" "{server}"' % self.cmdjava).format(
+            forkcmd = ('"%s" -classpath "{classpath}" cpw.mods.fml.common.asm.transformers.MCPMerger "{mergecfg}" "{client}" "{server}"' % self.cmdjava).format(
                 classpath=class_path, mergecfg=os.path.join(fml_dir, 'mcp_merge.cfg'), client=jars[CLIENT], server=jars[SERVER])
                 
             if not runcmd(self, forkcmd):
                 sys.exit(1)
         
         self.logger.info('> Running AccessTransformer')
-        forkcmd = ('%s -classpath "{classpath}" cpw.mods.fml.common.asm.transformers.AccessTransformer "{jar}" "{fmlconfig}"' % self.cmdjava).format(
+        forkcmd = ('"%s" -classpath "{classpath}" cpw.mods.fml.common.asm.transformers.AccessTransformer "{jar}" "{fmlconfig}"' % self.cmdjava).format(
             classpath=class_path, jar=jars[side], fmlconfig=os.path.join(fml_dir, 'common', 'fml_at.cfg'))
             
         forge_cfg = os.path.join(fml_dir, '..', 'common', 'forge_at.cfg')
