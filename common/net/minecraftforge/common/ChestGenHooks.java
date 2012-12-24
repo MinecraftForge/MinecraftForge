e34e3a44fe5322d953312e1f7a7dc6e130901bcd
LexManos <LexManos@gmail.com>
2012-12-24 01:39:08 -0800

    Fixed parameter ordering u.u

--- common/net/minecraftforge/common/ChestGenHooks.java
+++ common/net/minecraftforge/common/ChestGenHooks.java
@@ -77,7 +77,7 @@ public class ChestGenHooks
     
     static void addDungeonLoot(ChestGenHooks dungeon, ItemStack item, int weight, int min, int max)
     {
-        dungeon.addItem(new WeightedRandomChestContent(item, weight, max, max));
+        dungeon.addItem(new WeightedRandomChestContent(item, max, max, weight));
     }
 
     private static void addInfo(String category, WeightedRandomChestContent[] items, int min, int max)
