723c2d98eb40afeeeb68785bcfc2f075128c1eca
LexManos <LexManos@gmail.com>
2013-01-11 13:16:23 -0800

    FML will now rename local variables to have JAD-style names instead of FernFlower var## names. New script to do this, to allow for modders to run it over there own code: python rename_vars.py -mcp [folders to rename]...

--- install/fml.py
+++ install/fml.py
@@ -190,6 +190,7 @@ def reset_logger():
 
 count = 0
 def cleanup_source(path):
+    from rename_vars import rename_class
     path = os.path.normpath(path)
     regex_cases_before = re.compile(r'((case|default).+\r?\n)\r?\n', re.MULTILINE) #Fixes newline after case before case body
     regex_cases_after = re.compile(r'\r?\n(\r?\n[ \t]+(case|default))', re.MULTILINE) #Fixes newline after case body before new case
@@ -205,10 +206,13 @@ def cleanup_source(path):
             global count
             count += 1
             return match.group(1)
-            
+
         buf = regex_cases_before.sub(fix_cases, buf)
         buf = regex_cases_after.sub(fix_cases, buf)
-        if count > 0:
+        old = buf.replace('\r', '')
+        buf = rename_class(old, MCP=True)
+
+        if count > 0 or buf != old:
             with open(tmp_file, 'w') as fh:
                 fh.write(buf)
             shutil.move(tmp_file, src_file)
