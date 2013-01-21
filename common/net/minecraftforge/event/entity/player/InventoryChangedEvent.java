cfd5a5ba08d699c4f90f1adfa32f50bd5d8e130e
FusionLord <FusionLord@live.com>
2013-01-20 18:30:00 -0700

    Update common/net/minecraftforge/event/entity/player/InventoryChangedEvent.java
    Changed the java doc

--- common/net/minecraftforge/event/entity/player/InventoryChangedEvent.java
+++ common/net/minecraftforge/event/entity/player/InventoryChangedEvent.java
@@ -5,18 +5,18 @@ import net.minecraft.item.ItemStack;
 
 public class InventoryChangedEvent extends PlayerEvent {
 	
-	 /**
-     * This event is called when a slot in the player's inventory is changed.
-	 *
-	 * This event has the following properties:
-	 *    int slot; This is the slot that was changed.
-	 *    ItemStack itemstack; This is the ItemStack in the slot.
-	 *    boolean isArmor; This is wether or not the slot was one of the armorslots.
-	 *       36 = boots
-	 *       37 = pants
-	 *       38 = chest
-	 *       39 = helmet
-     */
+    /**
+     *This event is called when a slot in the player's inventory is changed.
+     *
+     * This event has the following properties:
+     * @param slot The slot being changed. 
+     * @param ItemStack The ItemStack from the slot.
+     * @param isArmor Whether or not the slot is an armor slot.
+     *       36 = boots
+     *       37 = pants
+     *       38 = chest
+     *       39 = helmet
+     **/
 	
 	public final int slot;
 	public final ItemStack itemstack;
@@ -27,7 +27,6 @@ public class InventoryChangedEvent extends PlayerEvent {
 		this.slot = slot;
 		this.itemstack = itemstack;
 		this.isArmor = (slot > 35 && slot < 40);
-
 	}
 
 }
