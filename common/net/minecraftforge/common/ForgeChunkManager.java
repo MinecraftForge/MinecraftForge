ae7e328228af47a8f1c792d45f65700674e94aae
Christian <cpw@weeksfamily.ca>
2013-01-25 21:21:20 -0500

    Fix a potential problem with "Entity already added" when using the dormant chunk cache capability. The entities in the dormant chunk cache will get new IDs prior to the cached chunk returning.

--- common/net/minecraftforge/common/ForgeChunkManager.java
+++ common/net/minecraftforge/common/ForgeChunkManager.java
@@ -880,7 +880,18 @@ public class ForgeChunkManager
     public static Chunk fetchDormantChunk(long coords, World world)
     {
         Cache<Long, Chunk> cache = dormantChunkCache.get(world);
-        return cache == null ? null : cache.getIfPresent(coords);
+        Chunk chunk = cache.getIfPresent(coords);
+        if (chunk != null)
+        {
+            for (List<Entity> eList : chunk.entityLists)
+            {
+                for (Entity e: eList)
+                {
+                    e.resetEntityId();
+                }
+            }
+        }
+        return chunk;
     }
 
     static void captureConfig(File configDir)
