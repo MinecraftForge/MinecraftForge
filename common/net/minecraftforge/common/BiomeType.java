06f4644b45cac90e3bb7a87f7434e4fee4d42485
Emasher <evan@spearman.mb.ca>
2013-03-03 12:31:29 -0600

    Create BiomeType.java

---
+++ common/net/minecraftforge/common/BiomeType.java
@@ -0,0 +1,6 @@
+package net.minecraftforge.common;
+
+public enum BiomeType
+{
+  FOREST, PLAINS, MOUNTAIN, HILLS, SWAMP, WATER, DESERT, FROZEN, JUNGLE, WASTELAND, BEACH, NETHER, END, MUSHROOM, NULL;
+}
