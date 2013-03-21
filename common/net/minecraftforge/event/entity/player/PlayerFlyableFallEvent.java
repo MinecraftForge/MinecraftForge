fef7539c484e988fd8f1495c0782cc67aea930bf
Mithion <jesse@jcluthedevelopment.com>
2013-03-21 11:04:15 -0400

    Renamed PlayerCreativeFallEvent to PlayerFlyableFallEvent
    more appropriate based on the code, as it doesn't just apply to
    creative.

--- common/net/minecraftforge/event/entity/player/PlayerFlyableFallEvent.java
+++ common/net/minecraftforge/event/entity/player/PlayerFlyableFallEvent.java
@@ -2,10 +2,10 @@ package net.minecraftforge.event.entity.player;
 
 import net.minecraft.entity.player.EntityPlayer;
 
-public class PlayerCreativeFallEvent extends PlayerEvent{
+public class PlayerFlyableFallEvent extends PlayerEvent{
 
     public float distanceFallen;
-    public PlayerCreativeFallEvent(EntityPlayer player, float f)
+    public PlayerFlyableFallEvent(EntityPlayer player, float f)
     {
         super(player);
         this.distanceFallen = f;
