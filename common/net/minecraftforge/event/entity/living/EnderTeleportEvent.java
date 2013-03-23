e13dff305eae348f5d7ceda90b7062eb0f2ae83c
Mithion <jesse@jcluthedevelopment.com>
2013-03-23 14:12:42 -0400

    Ender Teleport Changes
    Renamed Ender Teleport Event and added it in to ender pearls.

--- common/net/minecraftforge/event/entity/living/EnderTeleportEvent.java
+++ common/net/minecraftforge/event/entity/living/EnderTeleportEvent.java
@@ -5,18 +5,18 @@ import net.minecraftforge.event.Cancelable;
 import net.minecraftforge.event.Event;
 
 /**
- * Event for when an Enderman teleports.  Can be used to either modify the target position, or cancel the teleport outright.
+ * Event for when an Enderman teleports or an ender pearl is used.  Can be used to either modify the target position, or cancel the teleport outright.
  * @author Mithion
  *
  */
 @Cancelable
-public class EndermanTeleportEvent extends LivingEvent{
+public class EnderTeleportEvent extends LivingEvent{
     
     public double targetX;
     public double targetY;
     public double targetZ;
     
-    public EndermanTeleportEvent(EntityLiving entity, double targetX, double targetY, double targetZ)
+    public EnderTeleportEvent(EntityLiving entity, double targetX, double targetY, double targetZ)
     {
         super(entity);
         this.targetX = targetX;
