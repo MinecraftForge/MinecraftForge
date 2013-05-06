25d71726adca6685b7a828c1cfda95011380780a
ExE Boss <ExE-Boss@seznam.cz>
2013-05-06 20:31:05 +0200

    Expanded ItemStack with one method and additions to the OreStack class
    Just added copyWithSize method to the ItemStack class (I know that I
    will use it and I'm pretty sure that other mod devs will too), added add
    methods to the OreStack class, added isItemEqual (was required for
    recipes) and the copy methods.

--- common/net/minecraftforge/oredict/OreStack.java
+++ common/net/minecraftforge/oredict/OreStack.java
@@ -1,6 +1,8 @@
 package net.minecraftforge.oredict;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Collection;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
@@ -20,8 +22,13 @@ public final class OreStack
 {
 	/** The oredictionary name */
 	public String ore;
-	public ArrayList<ItemStack> ores = new ArrayList<ItemStack>();
+	public List<ItemStack> ores = new ArrayList<ItemStack>();
 	private NBTTagCompound stackTagCompound;
+	
+	private OreStack()
+	{
+		
+	}
 	
 	public OreStack(String ore, int stackSize)
 	{
@@ -113,6 +120,99 @@ public final class OreStack
 		for(ItemStack stack : ores)
 			stack.setTagCompound(this.stackTagCompound);
 		
+		return this;
+	}
+	
+	public boolean isItemEqual(ItemStack stack)
+	{
+		for(ItemStack stack : ores)
+			if(stack.isItemEqual(stack))
+				return true;
+		return false;
+	}
+	
+	public boolean isItemEqualRecipe(ItemStack stack)
+	{
+		for(ItemStack stack : ores)
+			if(stack.isItemEqual(stack) && stack.stackSize >= getStackSize())
+				return true;
+		return false;
+	}
+	
+	public OreStack copy()
+	{
+		OreStack stack = new OreStack();
+		stack.ore = this.ore;
+		if(this.stackTagCompound != null)
+		{
+			stack.stackTagCompound = this.stackTagCompound.copy();
+			stack.saveStackCompoud();
+		}
+		for(ItemStack ore : ores)
+			stack.ores.add(ore);
+	}
+	
+	public OreStack copyWithSize(int stackSize)
+	{
+		OreStack stack = this.copy();
+		stack.setStackSize(stackSize);
+		return stack;
+	}
+	
+	public OreStack addAll(Collection<?> oresToAdd)
+	{
+		List<ItemStack> oresToActuallyAdd = new ArrayList<ItemStack>();
+		
+		for(Object obj : oresToAdd)
+		{
+			if(obj instanceof Item)
+				oresToActuallyAdd.add(new ItemStack((Item)obj, this.getStackSize(), WILDCARD_VALUE));
+			else if(obj instanceof Block)
+				oresToActuallyAdd.add(new ItemStack((Block)obj, this.getStackSize(), WILDCARD_VALUE));
+			else if(obj instanceof ItemStack)
+			{
+				ItemStack stack = ((ItemStack)obj).copy();
+				stack.stackSize = this.getStackSize();
+				oresToActuallyAdd.add(stack);
+			}
+		}
+		
+		this.ores.addAll(oresToActuallyAdd);
+		
+		this.saveStackCompoud();
+	}
+	
+	public OreStack add(ItemStack stack)
+	{
+		ItemStack stack2 = stack.copy();
+		stack2.stackSize = this.getStackSize();
+		oresToActuallyAdd.add(stack2);
+	}
+	
+	public OreStack add(Item item)
+	{
+		ores.add(new ItemStack(item, this.getStackSize(), WILDCARD_VALUE));
+	}
+	
+	public OreStack add(Block block)
+	{
+		ores.add(new ItemStack(block, this.getStackSize(), WILDCARD_VALUE));
+	}
+	
+	public OreStack add(String str)
+	{
+		List<ItemStack> oresToActuallyAdd = new ArrayList<ItemStack>();
+		
+		List<ItemStack> oresToAdd = getOres(str);
+		
+		if(oresToAdd.isEmpty())
+			return this;
+		
+		for(ItemStack stack : oresToAdd)
+			oresToActuallyAdd.add(stack.copyWithSize(this.getStackSize()));
+		
+		ores.addAll(oresToActuallyAdd);
+		this.saveStackCompoud();
 		return this;
 	}
 }
