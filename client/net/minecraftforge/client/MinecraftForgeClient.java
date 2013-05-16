5ca854efa85e49758d63acd4d7a159324701d6a7
Christian <cpw@weeksfamily.ca>
2013-05-15 20:29:47 -0400

    Add in a very simple stencil bit registry to try and arbitrate between mods wanting to use stencil bits in rendering

--- client/net/minecraftforge/client/MinecraftForgeClient.java
+++ client/net/minecraftforge/client/MinecraftForgeClient.java
@@ -5,6 +5,8 @@
 
 package net.minecraftforge.client;
 
+import java.util.BitSet;
+
 import org.lwjgl.opengl.Display;
 
 import net.minecraft.block.Block;
@@ -60,5 +62,40 @@ public class MinecraftForgeClient
     public static int getStencilBits()
     {
         return ForgeHooksClient.stencilBits;
+    }
+
+
+    private static BitSet stencilBits = new BitSet(getStencilBits());
+    static
+    {
+        stencilBits.set(0,getStencilBits());
+    }
+
+    /**
+     * Reserve a stencil bit for use in rendering
+     *
+     * @return A bit or -1 if no further stencil bits are available
+     */
+    public static int reserveStencilBit()
+    {
+        int bit = stencilBits.nextSetBit(0);
+        if (bit >= 0)
+        {
+            stencilBits.clear(bit);
+        }
+        return bit;
+    }
+
+    /**
+     * Release the stencil bit for other use
+     *
+     * @param bit The bit from {@link #reserveStencilBit()}
+     */
+    public static void releaseStencilBit(int bit)
+    {
+        if (bit >= 0 && bit < getStencilBits())
+        {
+            stencilBits.set(bit);
+        }
     }
 }
