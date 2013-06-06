1261c7a3e0166a42efd0963632168f0ebe269826
Christian <cpw@weeksfamily.ca>
2013-06-06 10:02:48 -0400

    Add in an optional modid identifier for @SidedProxy. It's main use is when both scala and java @Mods reside in the same package, and you want the @SidedProxy behaviour for a specific @Mod language type. In general it should not be needed otherwise.

--- fml/common/cpw/mods/fml/common/SidedProxy.java
+++ fml/common/cpw/mods/fml/common/SidedProxy.java
@@ -5,7 +5,7 @@
  * are made available under the terms of the GNU Lesser Public License v2.1
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- * 
+ *
  * Contributors:
  *     cpw - implementation
  */
@@ -35,8 +35,17 @@ public @interface SidedProxy
      */
     String serverSide() default "";
 
+    @Deprecated
     /**
+     * Not implemented
      * The name of a special bukkit plugin class to load and populate
      */
     String bukkitSide() default "";
+
+    /**
+     * The (optional) name of a mod to load this proxy for. This will help ensure correct behaviour when loading a combined
+     * scala/java mod package. It is almost never going to be required, unless you ship both Scala and Java {@link Mod} content
+     * in a single jar.
+     */
+    String modId() default "";
 }
