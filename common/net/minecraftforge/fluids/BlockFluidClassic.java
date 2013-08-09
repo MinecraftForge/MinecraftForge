59bc707da9a274a93984ba29cb1df7666cc91b2f
tommy1019 <tommy.stanley96@gmail.com>
2013-08-08 21:55:13 -0500

    Fixed Classic Fluid Duplication Bug

--- common/net/minecraftforge/fluids/BlockFluidClassic.java
+++ common/net/minecraftforge/fluids/BlockFluidClassic.java
@@ -1,4 +1,3 @@
-
 package net.minecraftforge.fluids;
 
 import java.util.Random;
@@ -303,7 +302,13 @@ public class BlockFluidClassic extends BlockFluidBase
             return false;
         }
         
-        if (this.density > getDensity(world, x, y, z))
+        int density = getDensity(world, x, y, z);
+        if (density == Integer.MAX_VALUE) 
+        {
+             return true;
+        }
+        
+        if (this.density > density)
         {
         	return true;
         }
