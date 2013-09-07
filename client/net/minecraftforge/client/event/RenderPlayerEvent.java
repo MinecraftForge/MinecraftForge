519da32391c5ba80006a94ad54b2ccf401647986
Christian <cpw@weeksfamily.ca>
2013-09-07 16:25:30 -0400

    The partial tick should be available for all render player events. Weird it wasn't.

--- client/net/minecraftforge/client/event/RenderPlayerEvent.java
+++ client/net/minecraftforge/client/event/RenderPlayerEvent.java
@@ -9,30 +9,33 @@ import net.minecraftforge.event.entity.player.PlayerEvent;
 public abstract class RenderPlayerEvent extends PlayerEvent
 {
     public final RenderPlayer renderer;
+    public final float partialRenderTick;
 
-    public RenderPlayerEvent(EntityPlayer player, RenderPlayer renderer)
+    public RenderPlayerEvent(EntityPlayer player, RenderPlayer renderer, float partialRenderTick)
     {
         super(player);
         this.renderer = renderer;
+        this.partialRenderTick = partialRenderTick;
     }
 
     @Cancelable
     public static class Pre extends RenderPlayerEvent
     {
-        public Pre(EntityPlayer player, RenderPlayer renderer){ super(player, renderer); }
+        public Pre(EntityPlayer player, RenderPlayer renderer, float tick){ super(player, renderer, tick); }
     }
 
     public static class Post extends RenderPlayerEvent
     {
-        public Post(EntityPlayer player, RenderPlayer renderer){ super(player, renderer); }
+        public Post(EntityPlayer player, RenderPlayer renderer, float tick){ super(player, renderer, tick); }
     }
     
     public abstract static class Specials extends RenderPlayerEvent
     {
+        @Deprecated
         public final float partialTicks;
         public Specials(EntityPlayer player, RenderPlayer renderer, float partialTicks)
         {
-            super(player, renderer);
+            super(player, renderer, partialTicks);
             this.partialTicks = partialTicks;
         }
 
@@ -59,11 +62,12 @@ public abstract class RenderPlayerEvent extends PlayerEvent
          */
         public int result = -1;
         public final int slot;
+        @Deprecated
         public final float partialTick;
         public final ItemStack stack;
         public SetArmorModel(EntityPlayer player, RenderPlayer renderer, int slot, float partialTick, ItemStack stack)
         {
-            super(player, renderer);
+            super(player, renderer, partialTick);
             this.slot = slot;
             this.partialTick = partialTick;
             this.stack = stack;
