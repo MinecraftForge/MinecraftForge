f9087cb291a095fa85d5d55b866002fa9b4cb6ee
Christian <cpw@weeksfamily.ca>
2013-09-14 18:43:18 -0400

    Revert "Added a RenderType that allows Map-style rendering w/o inheriting from"
    This reverts commit eb4688bf5ea132cd8ddc802a7dad6d423ad50543.
    
    Conflicts:
    	patches/minecraft/net/minecraft/client/renderer/ItemRenderer.java.patch

--- client/net/minecraftforge/client/IItemRenderer.java
+++ client/net/minecraftforge/client/IItemRenderer.java
@@ -69,17 +69,7 @@ public interface IItemRenderer
          * RenderEngine engine - The RenderEngine instance
          * MapData mapData - The map data
          */
-        FIRST_PERSON_MAP,
-        
-        /**
-         * The render type used when in first person and an item similar to a map should be rendered.
-         * All appropriate rotations have been applied.
-         * 
-         * Data Parameters:
-         * EntityPlayer player - The player holding the map
-         * RenderEngine engine - The RenderEngine instance
-         */
-        FIRST_PERSON_HOLDING,
+        FIRST_PERSON_MAP
     }
     
     public enum ItemRendererHelper
@@ -112,17 +102,7 @@ public interface IItemRenderer
          * Determines if the item should be rendered in GUI inventory slots as a 3D
          * block or as a 2D texture.
          */
-        INVENTORY_BLOCK,
-        
-        /**
-         * Determines if hands should be rendered when being held.
-         */
-        HOLD_HANDS,
-        
-        /**
-         * Determines if the map background should be rendered when being held.
-         */
-        HOLD_BACKGROUND,
+        INVENTORY_BLOCK
     }
     
     /** 
