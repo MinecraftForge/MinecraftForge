ec8d9ed7b5aeb23c3d97dd0750ea9b25c89348a2
Mikee Franklin <mikeefranklin@gmail.com>
2013-09-25 13:11:04 +0100

    changed slightly to reduce patch size

---
+++ common/net/minecraftforge/event/world/ExplosionEvent.java
@@ -0,0 +1,29 @@
+package net.minecraftforge.event.world;
+
+import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
+import net.minecraft.world.World;
+import net.minecraftforge.event.Cancelable;
+import net.minecraftforge.event.Event;
+
+@Cancelable
+public class ExplosionEvent extends Event {
+
+    public final Entity entity;
+    public final double x;
+    public final double y;
+    public final double z;
+    public final float strength;
+    public final boolean isFlaming;
+    public final boolean destroysBlocks;
+    
+    public ExplosionEvent(Entity entity, double x, double y, double z, float strength, boolean isFlaming, boolean destroysBlocks) {
+        this.entity = entity;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.strength = strength;
+        this.isFlaming = isFlaming;
+        this.destroysBlocks = destroysBlocks;
+    }
+}
