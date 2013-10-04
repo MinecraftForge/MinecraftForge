d7396311f10ab72d1d62753af06ff630381cc60f
neptunepink <purpleposeidon@gmail.com>
2013-10-04 16:56:11 -0700

    cleanup BlockItemizeHelper

--- common/net/minecraftforge/common/BlockItemizeHelper.java
+++ common/net/minecraftforge/common/BlockItemizeHelper.java
@@ -37,127 +37,44 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 
-public class BlockItemizeHelper {
+public class BlockItemizeHelper
+{
     public static enum BlockStyle {
-        UNDECIDED, NOTHING, USE_GET_DAMAGE_VALUE, USE_ID_DROPPED, USE_GET_BLOCK_DROPPED, CLONE_MD, CLONE_ID,
+        // No value has been cached. This is the default value.
+        UNDECIDED,
+        // Do not return an ItemStack.
+        NOTHING,
+        // Call Block.getDamageValue to get the ItemStack
+        USE_GET_DAMAGE_VALUE,
+        // Call Block.idDropped to get the ItemStack
+        USE_ID_DROPPED,
+        // Return the first thing from Block.getBlockDropped.
+        USE_GET_BLOCK_DROPPED,
+        // Return the block with its metadata
+        CLONE_MD,
 
-        STEM, SLAB, CAKE, CROP, DOOR, REDSTONE_ORE, PISTON_EXTENSION, BED
+        // These blocks do weird things and need to be special-cased
+        STEM,
+        SLAB,
+        CAKE,
+        CROP,
+        DOOR,
+        REDSTONE_ORE,
+        PISTON_EXTENSION,
+        BED
     }
 
-    static BlockStyle classifyBlock(Block block)
+    static BlockStyle getBlockStyle(Block block)
     {
-        if (block.placingItemStyle != UNDECIDED)
+        if (block.placingItemStyle == UNDECIDED)
         {
-            return block.placingItemStyle;
+            block.placingItemStyle = classifyBlock(block);
         }
-        if (block instanceof BlockHalfSlab)
-        {
-            return SLAB;
-        }
-        if (block instanceof BlockCocoa)
-        {
-            return USE_GET_BLOCK_DROPPED;
-        }
-        if (block instanceof BlockSign)
-        {
-            return USE_ID_DROPPED;
-        }
-        if (block instanceof BlockFlowerPot)
-        {
-            return USE_ID_DROPPED;
-        }
-        if (block instanceof BlockRedstoneWire)
-        {
-            return USE_ID_DROPPED;
-        }
-        if (block instanceof BlockBrewingStand)
-        {
-            return USE_ID_DROPPED;
-        }
-        if (block instanceof BlockReed)
-        {
-            return USE_ID_DROPPED;
-        }
-        if (block instanceof BlockTripWire)
-        {
-            return USE_ID_DROPPED;
-        }
-        if (block instanceof BlockCauldron)
-        {
-            return USE_ID_DROPPED;
-        }
-        if (block instanceof BlockRedstoneRepeater)
-        {
-            return USE_ID_DROPPED;
-        }
-        if (block instanceof BlockComparator)
-        {
-            return USE_ID_DROPPED;
-        }
-        if (block instanceof BlockNetherStalk)
-        {
-            return USE_GET_BLOCK_DROPPED;
-        }
-        if (block instanceof BlockSkull)
-        {
-            return USE_GET_BLOCK_DROPPED;
-        }
-        if (block instanceof BlockRedstoneTorch)
-        {
-            return USE_ID_DROPPED;
-        }
-        if (block instanceof BlockFarmland)
-        {
-            return USE_ID_DROPPED;
-        }
-        if (block instanceof BlockCrops)
-        {
-            return CROP;
-        }
-        if (block instanceof BlockPistonMoving)
-        {
-            return NOTHING;
-        }
-        if (block instanceof BlockPortal)
-        {
-            return NOTHING;
-        }
-        if (block instanceof BlockEndPortal)
-        {
-            return NOTHING;
-        }
-        if (block instanceof BlockSilverfish)
-        {
-            return NOTHING;
-        }
-        if (block instanceof BlockMobSpawner)
-        {
-            return NOTHING;
-        }
-        if (block instanceof BlockFurnace)
-        {
-            return USE_ID_DROPPED;
-        }
-        if (block instanceof BlockOre)
-        {
-            return CLONE_MD;
-        }
-        if (block instanceof BlockBed)
-        {
-            return BED;
-        }
-        if (block instanceof BlockDoor)
-        {
-            return DOOR;
-        }
-        if (block instanceof BlockMushroomCap)
-        {
-            return USE_ID_DROPPED;
-        }
-        if (block instanceof BlockRedstoneLight)
-        {
-            return USE_ID_DROPPED;
-        }
+        return block.placingItemStyle;
+    }
+
+    private static BlockStyle classifyBlock(Block block)
+    {
         // These blocks don't have a good way of extracting the item,
         // so no instanceof.
         if (block == Block.cake)
@@ -176,6 +93,43 @@ public class BlockItemizeHelper {
         {
             return STEM;
         }
+        if (block instanceof BlockSign || block instanceof BlockFlowerPot || block instanceof BlockRedstoneWire || block instanceof BlockBrewingStand
+                || block instanceof BlockReed || block instanceof BlockTripWire || block instanceof BlockCauldron || block instanceof BlockRedstoneRepeater
+                || block instanceof BlockComparator || block instanceof BlockRedstoneTorch || block instanceof BlockFarmland || block instanceof BlockFurnace
+                || block instanceof BlockMushroomCap || block instanceof BlockRedstoneLight)
+        {
+            return USE_ID_DROPPED;
+        }
+        if (block instanceof BlockCocoa || block instanceof BlockNetherStalk || block instanceof BlockSkull)
+        {
+            return USE_GET_BLOCK_DROPPED;
+        }
+        if (block instanceof BlockPistonMoving || block instanceof BlockPortal || block instanceof BlockEndPortal || block instanceof BlockSilverfish
+                || block instanceof BlockMobSpawner)
+        {
+            return NOTHING;
+        }
+        if (block instanceof BlockOre)
+        {
+            return CLONE_MD;
+        }
+        // Special blocks
+        if (block instanceof BlockHalfSlab)
+        {
+            return SLAB;
+        }
+        if (block instanceof BlockCrops)
+        {
+            return CROP;
+        }
+        if (block instanceof BlockBed)
+        {
+            return BED;
+        }
+        if (block instanceof BlockDoor)
+        {
+            return DOOR;
+        }
         return USE_GET_DAMAGE_VALUE;
     }
 
@@ -193,64 +147,62 @@ public class BlockItemizeHelper {
         int md;
         switch (classifyBlock(block))
         {
-        case UNDECIDED:
-        case NOTHING:
-        case PISTON_EXTENSION:
-            return null;
-        case USE_GET_DAMAGE_VALUE:
-            return new ItemStack(block, 1, block.getDamageValue(world, x, y, z));
-        case USE_ID_DROPPED:
-        case BED:
-            md = world.getBlockMetadata(x, y, z);
-            return makeItemStack(block.idDropped(md, world.rand, 0), 1, 0);
-        case USE_GET_BLOCK_DROPPED:
-            md = world.getBlockMetadata(x, y, z);
-            ArrayList<ItemStack> drops = block.getBlockDropped(world, x, y, z, md, 0);
-            if (drops.isEmpty())
-            {
+            default:
+            case UNDECIDED:
+            case NOTHING:
+            case PISTON_EXTENSION:
                 return null;
-            }
-            return drops.get(0);
-        case CLONE_MD:
-            md = world.getBlockMetadata(x, y, z);
-            return new ItemStack(block, 1, md);
-        case CLONE_ID:
-            return new ItemStack(block);
-        case STEM:
-            if (block == Block.pumpkinStem)
-            {
-                return new ItemStack(Item.pumpkinSeeds);
-            }
-            else if (block == Block.melonStem)
-            {
-                return new ItemStack(Item.melonSeeds);
-            }
-            else
-            {
-                return null;
-            }
-        case SLAB:
-            md = world.getBlockMetadata(x, y, z);
-            int slabId = block.idDropped(md, world.rand, 0);
-            int dropped = block.quantityDropped(world.rand);
-            return makeItemStack(slabId, dropped, block.damageDropped(md));
-        case CAKE:
-            md = world.getBlockMetadata(x, y, z);
-            return md == 0 ? new ItemStack(Item.cake) : null;
-        case CROP:
-            return new ItemStack(block.idDropped(0, world.rand, 0), 1, block.getDamageValue(world, x, y, z));
-        case DOOR:
-            md = world.getBlockMetadata(x, y, z);
-            int doorId = block.idDropped(md, world.rand, 0);
-            if (doorId == 0)
-            {
-                return null;
-            }
-            return new ItemStack(doorId, 1, 0);
-        case REDSTONE_ORE:
-            return new ItemStack(Block.oreRedstone);
+            case USE_GET_DAMAGE_VALUE:
+                return new ItemStack(block, 1, block.getDamageValue(world, x, y, z));
+            case USE_ID_DROPPED:
+            case BED:
+                md = world.getBlockMetadata(x, y, z);
+                return makeItemStack(block.idDropped(md, world.rand, 0), 1, 0);
+            case USE_GET_BLOCK_DROPPED:
+                md = world.getBlockMetadata(x, y, z);
+                ArrayList<ItemStack> drops = block.getBlockDropped(world, x, y, z, md, 0);
+                if (drops.isEmpty())
+                {
+                    return null;
+                }
+                return drops.get(0);
+            case CLONE_MD:
+                md = world.getBlockMetadata(x, y, z);
+                return new ItemStack(block, 1, md);
+            case STEM:
+                if (block == Block.pumpkinStem)
+                {
+                    return new ItemStack(Item.pumpkinSeeds);
+                }
+                else if (block == Block.melonStem)
+                {
+                    return new ItemStack(Item.melonSeeds);
+                }
+                else
+                {
+                    return null;
+                }
+            case SLAB:
+                md = world.getBlockMetadata(x, y, z);
+                int slabId = block.idDropped(md, world.rand, 0);
+                int dropped = block.quantityDropped(world.rand);
+                return makeItemStack(slabId, dropped, block.damageDropped(md));
+            case CAKE:
+                md = world.getBlockMetadata(x, y, z);
+                return md == 0 ? new ItemStack(Item.cake) : null;
+            case CROP:
+                return new ItemStack(block.idDropped(0, world.rand, 0), 1, block.getDamageValue(world, x, y, z));
+            case DOOR:
+                md = world.getBlockMetadata(x, y, z);
+                int doorId = block.idDropped(md, world.rand, 0);
+                if (doorId == 0)
+                {
+                    return null;
+                }
+                return new ItemStack(doorId, 1, 0);
+            case REDSTONE_ORE:
+                return new ItemStack(Block.oreRedstone);
         }
-        return null;
     }
 
     public static ItemStack getDescribingItem(Block block, MovingObjectPosition target, World world, int x, int y, int z)
@@ -258,23 +210,23 @@ public class BlockItemizeHelper {
         int md;
         switch (classifyBlock(block))
         {
-        case CAKE:
-            return new ItemStack(Item.cake);
-        case CROP:
-            return new ItemStack(block.idDropped(0, world.rand, 0), 1, block.getDamageValue(world, x, y, z));
-        case DOOR:
-        case BED:
-            int id = block.idDropped(0, world.rand, 0);
-            if (id == 0)
-            {
-                return null;
-            }
-            return new ItemStack(id, 1, 0);
-        case PISTON_EXTENSION:
-            md = world.getBlockMetadata(x, y, z);
-            return new ItemStack((md & 8) != 0 ? Block.pistonStickyBase : Block.pistonBase);
-        default:
-            return getPlacingItem(block, target, world, x, y, z);
+            case CAKE:
+                return new ItemStack(Item.cake);
+            case CROP:
+                return new ItemStack(block.idDropped(0, world.rand, 0), 1, block.getDamageValue(world, x, y, z));
+            case DOOR:
+            case BED:
+                int id = block.idDropped(0, world.rand, 0);
+                if (id == 0)
+                {
+                    return null;
+                }
+                return new ItemStack(id, 1, 0);
+            case PISTON_EXTENSION:
+                md = world.getBlockMetadata(x, y, z);
+                return new ItemStack((md & 8) != 0 ? Block.pistonStickyBase : Block.pistonBase);
+            default:
+                return getPlacingItem(block, target, world, x, y, z);
         }
     }
 }
