8113d2efc33360c2eed0899b477d821ce1a12098
OniBait <onibait@gmail.com>
2013-10-09 19:06:40 -0500

    Changes for logging. Can now control log levels per mod in bukkit.yml under mcpc -> loglevels

--- spigot/org/bukkit/craftbukkit/Main.java
+++ spigot/org/bukkit/craftbukkit/Main.java
@@ -18,6 +18,7 @@ public class Main {
     public static YamlConfiguration configuration;
     public static File configFile;
     public static void main(String[] args) {
+        loadOptions(args);
         net.minecraft.server.MinecraftServer.main(args);
     }
 
