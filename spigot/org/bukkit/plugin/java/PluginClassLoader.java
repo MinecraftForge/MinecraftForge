4f31413744e400141b7daffd6ca5ee524b703dc6
OniBait <onibait@gmail.com>
2013-10-15 16:50:48 -0500

    Fix Bukkit naming inconsistency

--- spigot/org/bukkit/plugin/java/PluginClassLoader.java
+++ spigot/org/bukkit/plugin/java/PluginClassLoader.java
@@ -98,7 +98,7 @@ public class PluginClassLoader extends URLClassLoader {
         boolean useCustomClassLoader = config.getBoolean("mcpc.plugin-settings.default.custom-class-loader", true);
         debug = config.getBoolean("mcpc.plugin-settings.default.debug", false);
         boolean useGuava10 = config.getBoolean("mcpc.plugin-settings.default.use-guava10", true);
-        boolean remapNMS164 = config.getBoolean("mcpc.plugin-settings.default.remap-nms-v1_6_R4", true);
+        boolean remapNMS164 = config.getBoolean("mcpc.plugin-settings.default.remap-nms-v1_6_R3", true);
         boolean remapNMS162 = config.getBoolean("mcpc.plugin-settings.default.remap-nms-v1_6_R2", true);
         boolean remapNMS161 = config.getBoolean("mcpc.plugin-settings.default.remap-nms-v1_6_R1", true);
         boolean remapNMS152 = config.getBoolean("mcpc.plugin-settings.default.remap-nms-v1_5_R3", true);
@@ -107,7 +107,7 @@ public class PluginClassLoader extends URLClassLoader {
         boolean remapNMS147 = config.getBoolean("mcpc.plugin-settings.default.remap-nms-v1_4_R1", true);
         boolean remapNMS146 = config.getBoolean("mcpc.plugin-settings.default.remap-nms-v1_4_6", true);
         String remapNMSPre = config.getString("mcpc.plugin-settings.default.remap-nms-pre", "false");
-        boolean remapOBC164 = config.getBoolean("mcpc.plugin-settings.default.remap-obc-v1_6_R4", false);
+        boolean remapOBC164 = config.getBoolean("mcpc.plugin-settings.default.remap-obc-v1_6_R3", false);
         boolean remapOBC162 = config.getBoolean("mcpc.plugin-settings.default.remap-obc-v1_6_R2", false);
         boolean remapOBC161 = config.getBoolean("mcpc.plugin-settings.default.remap-obc-v1_6_R1", false);
         boolean remapOBC152 = config.getBoolean("mcpc.plugin-settings.default.remap-obc-v1_5_R3", true);
@@ -126,7 +126,7 @@ public class PluginClassLoader extends URLClassLoader {
         useCustomClassLoader = config.getBoolean("mcpc.plugin-settings."+pluginName+".custom-class-loader", useCustomClassLoader);
         debug = config.getBoolean("mcpc.plugin-settings."+pluginName+".debug", debug);
         useGuava10 = config.getBoolean("mcpc.plugin-settings."+pluginName+".use-guava10", useGuava10);
-        remapNMS164 = config.getBoolean("mcpc.plugin-settings."+pluginName+".remap-nms-v1_6_R4", remapNMS164);
+        remapNMS164 = config.getBoolean("mcpc.plugin-settings."+pluginName+".remap-nms-v1_6_R3", remapNMS164);
         remapNMS162 = config.getBoolean("mcpc.plugin-settings."+pluginName+".remap-nms-v1_6_R2", remapNMS162);
         remapNMS161 = config.getBoolean("mcpc.plugin-settings."+pluginName+".remap-nms-v1_6_R1", remapNMS161);
         remapNMS152 = config.getBoolean("mcpc.plugin-settings."+pluginName+".remap-nms-v1_5_R3", remapNMS152);
@@ -135,7 +135,7 @@ public class PluginClassLoader extends URLClassLoader {
         remapNMS147 = config.getBoolean("mcpc.plugin-settings."+pluginName+".remap-nms-v1_4_R1", remapNMS147);
         remapNMS146 = config.getBoolean("mcpc.plugin-settings."+pluginName+".remap-nms-v1_4_6", remapNMS146);
         remapNMSPre = config.getString("mcpc.plugin-settings."+pluginName+".remap-nms-pre", remapNMSPre);
-        remapOBC164 = config.getBoolean("mcpc.plugin-settings."+pluginName+".remap-obc-v1_6_R4", remapOBC164);
+        remapOBC164 = config.getBoolean("mcpc.plugin-settings."+pluginName+".remap-obc-v1_6_R3", remapOBC164);
         remapOBC162 = config.getBoolean("mcpc.plugin-settings."+pluginName+".remap-obc-v1_6_R2", remapOBC162);
         remapOBC161 = config.getBoolean("mcpc.plugin-settings."+pluginName+".remap-obc-v1_6_R1", remapOBC161);
         remapOBC152 = config.getBoolean("mcpc.plugin-settings."+pluginName+".remap-obc-v1_5_R3", remapOBC152);
@@ -331,7 +331,7 @@ public class PluginClassLoader extends URLClassLoader {
             }
 
             if ((flags & F_REMAP_NMS164) != 0) {
-                loadNmsMappings(jarMapping, "v1_6_R4");
+                loadNmsMappings(jarMapping, "v1_6_R3");
             }
 
             if ((flags & F_REMAP_NMS162) != 0) {
@@ -364,7 +364,7 @@ public class PluginClassLoader extends URLClassLoader {
             }
 
             if ((flags & F_REMAP_OBC164) != 0) {
-                jarMapping.packages.put(org_bukkit_craftbukkit+"/v1_6_R4", org_bukkit_craftbukkit+"/"+getNativeVersion());
+                jarMapping.packages.put(org_bukkit_craftbukkit+"/v1_6_R3", org_bukkit_craftbukkit+"/"+getNativeVersion());
             }
 
             if ((flags & F_REMAP_OBC162) != 0) {
@@ -411,7 +411,7 @@ public class PluginClassLoader extends URLClassLoader {
                 String filename;
                 switch (flags & F_REMAP_NMSPRE_MASK)
                 {
-                    case 0x01640000: filename = "mappings/v1_6_R4/cb2numpkg.srg"; break;
+                    case 0x01640000: filename = "mappings/v1_6_R3/cb2numpkg.srg"; break;
                     case 0x01620000: filename = "mappings/v1_6_R2/cb2numpkg.srg"; break;
                     case 0x01610000: filename = "mappings/v1_6_R1/cb2numpkg.srg"; break;
                     case 0x01510000: filename = "mappings/v1_5_R2/cb2numpkg.srg"; break;
