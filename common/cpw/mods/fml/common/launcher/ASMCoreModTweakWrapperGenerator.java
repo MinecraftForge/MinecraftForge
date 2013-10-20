243a21a353e6b7717f64008776928c7132110ddf
Christian <cpw@weeksfamily.ca>
2013-10-20 11:53:07 +0200

    Wrapping coremods as tweakers. Part 1.

---
+++ common/cpw/mods/fml/common/launcher/ASMCoreModTweakWrapperGenerator.java
@@ -0,0 +1,12 @@
+package cpw.mods.fml.common.launcher;
+
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+
+public class ASMCoreModTweakWrapperGenerator {
+    private IFMLLoadingPlugin coreModToWrap;
+    
+    public ASMCoreModTweakWrapperGenerator(IFMLLoadingPlugin toWrap) {
+        
+    }
+
+}
