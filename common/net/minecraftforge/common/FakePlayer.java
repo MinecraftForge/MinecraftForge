45cc0bd87c302a4f3bbeb54c56a412839460dcff
Lex Manos <LexManos@gmail.com>
2013-11-16 15:26:52 -0800

    Fix NPE in FakePlayers when they are created in a purely client side environment. To remove in 1.7 as FakePlayers should be used for server side interaction with the world on a player's behalf, not for client rendering.

--- common/net/minecraftforge/common/FakePlayer.java
+++ common/net/minecraftforge/common/FakePlayer.java
@@ -1,12 +1,15 @@
 package net.minecraftforge.common;
 
 import cpw.mods.fml.common.FMLCommonHandler;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemInWorldManager;
+import net.minecraft.network.packet.Packet204ClientInfo;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.stats.StatBase;
 import net.minecraft.util.ChatMessageComponent;
 import net.minecraft.util.ChunkCoordinates;
+import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 
 //Preliminary, simple Fake Player class 
@@ -30,4 +33,16 @@ public class FakePlayer extends EntityPlayerMP
     public void addStat(StatBase par1StatBase, int par2){}
     @Override
     public void openGui(Object mod, int modGuiId, World world, int x, int y, int z){}
+    @Override 
+    public boolean isEntityInvulnerable(){ return true; }
+    @Override
+    public boolean canAttackPlayer(EntityPlayer player){ return false; }
+    @Override
+    public void onDeath(DamageSource source){ return; }
+    @Override
+    public void onUpdate(){ return; }
+    @Override
+    public void travelToDimension(int dim){ return; }
+    @Override
+    public void updateClientInfo(Packet204ClientInfo pkt){ return; }
 }
