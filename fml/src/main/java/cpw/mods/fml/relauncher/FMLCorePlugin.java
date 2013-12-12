7956c2f659a71a7d6ff82fbbb88956850af8d203
Christian <cpw@weeksfamily.ca>
2013-12-12 01:38:34 -0500

    Fix AT, Fix exc. Almost works now!

--- fml/src/main/java/cpw/mods/fml/relauncher/FMLCorePlugin.java
+++ fml/src/main/java/cpw/mods/fml/relauncher/FMLCorePlugin.java
@@ -20,7 +20,6 @@ public class FMLCorePlugin implements IFMLLoadingPlugin
     public String[] getASMTransformerClass()
     {
         return new String[] {
-                             "cpw.mods.fml.common.asm.transformers.AccessTransformer",
                              "cpw.mods.fml.common.asm.transformers.MarkerTransformer",
                              "cpw.mods.fml.common.asm.transformers.SideTransformer",
                             };
