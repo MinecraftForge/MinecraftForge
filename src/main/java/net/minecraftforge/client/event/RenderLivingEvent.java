5f3ece629c83615184ecd1adffea462555257d95
iChun <ohai.iChun@gmail.com>
2013-12-25 17:03:19 +0800

    Add render offset to event.

--- src/main/java/net/minecraftforge/client/event/RenderLivingEvent.java
+++ src/main/java/net/minecraftforge/client/event/RenderLivingEvent.java
@@ -9,35 +9,41 @@ public abstract class RenderLivingEvent extends Event
 {
     public final EntityLivingBase entity;
     public final RendererLivingEntity renderer;
+    public final double x;
+    public final double y;
+    public final double z;
 
-    public RenderLivingEvent(EntityLivingBase entity, RendererLivingEntity renderer)
+    public RenderLivingEvent(EntityLivingBase entity, RendererLivingEntity renderer, double x, double y, double z)
     {
         this.entity = entity;
         this.renderer = renderer;
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
     
     @Cancelable
     public static class Pre extends RenderLivingEvent
     {
-        public Pre(EntityLivingBase entity, RendererLivingEntity renderer){ super(entity, renderer); }
+        public Pre(EntityLivingBase entity, RendererLivingEntity renderer, double x, double y, double z){ super(entity, renderer, x, y, z); }
     }
     public static class Post extends RenderLivingEvent
     {
-        public Post(EntityLivingBase entity, RendererLivingEntity renderer){ super(entity, renderer); }
+        public Post(EntityLivingBase entity, RendererLivingEntity renderer, double x, double y, double z){ super(entity, renderer, x, y, z); }
     }
 
     public abstract static class Specials extends RenderLivingEvent
     {
-        public Specials(EntityLivingBase entity, RendererLivingEntity renderer){ super(entity, renderer); }
+        public Specials(EntityLivingBase entity, RendererLivingEntity renderer, double x, double y, double z){ super(entity, renderer, x, y, z); }
 
         @Cancelable
         public static class Pre extends Specials
         {
-            public Pre(EntityLivingBase entity, RendererLivingEntity renderer){ super(entity, renderer); }
+            public Pre(EntityLivingBase entity, RendererLivingEntity renderer, double x, double y, double z){ super(entity, renderer, x, y, z); }
         }
         public static class Post extends Specials
         {
-            public Post(EntityLivingBase entity, RendererLivingEntity renderer){ super(entity, renderer); }
+            public Post(EntityLivingBase entity, RendererLivingEntity renderer, double x, double y, double z){ super(entity, renderer, x, y, z); }
         }
     }
 }
