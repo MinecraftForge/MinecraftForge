ee9c6566d2ec11540681f1dd9669a907b8034183
Travis Watkins <amaranth@ubuntu.com>
2014-01-13 14:11:39 -0500

    Load chunks asynchronously for players.
    When a player triggers a chunk load via walking around or teleporting
    there is no need to stop everything and get this chunk on the main thread.
    The client is used to having to wait some time for this chunk and the
    server doesn't immediately do anything with it except send it to the
    player. At the same time chunk loading is the last major source of file IO
    that still runs on the main thread.
    
    These two facts make it possible to offload chunks loaded for this reason
    to another thread. However, not all parts of chunk loading can happen off
    the main thread. For this we use the new AsynchronousExecutor system to
    split chunk loading in to three pieces. The first is loading data from
    disk, decompressing it, and parsing it in to an NBT structure.  The second
    piece is creating entities and tile entities in the chunk and adding them
    to the world, this is still done on the main thread. The third piece is
    informing everyone who requested a chunk load that the load is finished.
    For this we register callbacks and then run them on the main thread once
    the previous two stages are finished.
    
    There are still cases where a chunk is needed immediately and these will
    still trigger chunk loading entirely on the main thread. The most obvious
    case is plugins using the API to request a chunk load. We also must load
    the chunk immediately when something in the world tries to access it. In
    these cases we ignore any possibly pending or in progress chunk loading
    that is happening asynchronously as we will have the chunk loaded by the
    time they are finished.
    
    The hope is that overall this system will result in less CPU time and
    pauses due to blocking file IO on the main thread thus giving more
    consistent performance. Testing so far has shown that this also speeds up
    chunk loading client side although some of this is likely to be because
    we are sending less chunks at once for the client to process.
    
    Thanks for ammaraskar for help with the implementation of this feature.
    
    This commit is based off the following :
    
    Bukkit/CraftBukkit@b8fc6ab2c12e9b4c8d7b5370e44f23cc838014b2
    Bukkit/CraftBukkit@85f5776df2a9c827565e799f150ae8a197086a98
    Bukkit/CraftBukkit@0714971ca2a31bc729bdd78ded8c69ffb2284813
    Bukkit/CraftBukkit@7f49722f457dcc31f8cac8e011871ff1b7fd3306
    Bukkit/CraftBukkit@53ad0cf1abe9c060ef411a86e9a16352f3e5197e

---
+++ src/main/java/net/minecraftforge/common/chunkio/ChunkIOProvider.java
@@ -0,0 +1,61 @@
+package net.minecraftforge.common.chunkio;
+
+
+import net.minecraft.world.ChunkCoordIntPair;
+import net.minecraftforge.common.util.AsynchronousExecutor;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+class ChunkIOProvider implements AsynchronousExecutor.CallBackProvider<QueuedChunk, net.minecraft.world.chunk.Chunk, Runnable, RuntimeException> {
+    private final AtomicInteger threadNumber = new AtomicInteger(1);
+
+    // async stuff
+    public net.minecraft.world.chunk.Chunk callStage1(QueuedChunk queuedChunk) throws RuntimeException {
+        net.minecraft.world.chunk.storage.AnvilChunkLoader loader = queuedChunk.loader;
+        Object[] data = null;
+        try {
+            data = loader.loadChunk__Async(queuedChunk.world, queuedChunk.x, queuedChunk.z);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (data != null) {
+            queuedChunk.compound = (net.minecraft.nbt.NBTTagCompound) data[1];
+            return (net.minecraft.world.chunk.Chunk) data[0];
+        }
+
+        return null;
+    }
+
+    // sync stuff
+    public void callStage2(QueuedChunk queuedChunk, net.minecraft.world.chunk.Chunk chunk) throws RuntimeException {
+        if(chunk == null) {
+            // If the chunk loading failed just do it synchronously (may generate)
+            queuedChunk.provider.loadChunk(queuedChunk.x, queuedChunk.z);
+            return;
+        }
+
+        queuedChunk.loader.loadEntities(queuedChunk.world, queuedChunk.compound.getCompoundTag("Level"), chunk);
+        chunk.lastSaveTime = queuedChunk.provider.worldObj.getTotalWorldTime();
+        queuedChunk.provider.loadedChunkHashMap.add(ChunkCoordIntPair.chunkXZ2Int(queuedChunk.x, queuedChunk.z), chunk);
+        queuedChunk.provider.loadedChunks.add(chunk);
+        chunk.onChunkLoad();
+
+        if (queuedChunk.provider.currentChunkProvider != null) {
+            queuedChunk.provider.currentChunkProvider.recreateStructures(queuedChunk.x, queuedChunk.z);
+        }
+
+        chunk.populateChunk(queuedChunk.provider, queuedChunk.provider, queuedChunk.x, queuedChunk.z);
+    }
+
+    public void callStage3(QueuedChunk queuedChunk, net.minecraft.world.chunk.Chunk chunk, Runnable runnable) throws RuntimeException {
+        runnable.run();
+    }
+
+    public Thread newThread(Runnable runnable) {
+        Thread thread = new Thread(runnable, "Chunk I/O Executor Thread-" + threadNumber.getAndIncrement());
+        thread.setDaemon(true);
+        return thread;
+    }
+}
