9cab2ab36e7981c847e3e9ae8c3fbbb36531ba6d
Christian <cpw@weeksfamily.ca>
2014-01-16 18:07:25 -0500

    Add in some tests and examples for the "simple" network stuff

---
+++ src/test/java/cpw/mods/fml/test/simplenet/SimpleNetTestMessage1.java
@@ -0,0 +1,17 @@
+package cpw.mods.fml.test.simplenet;
+
+import io.netty.buffer.ByteBuf;
+import cpw.mods.fml.common.network.simpleimpl.IMessage;
+
+public class SimpleNetTestMessage1 implements IMessage {
+    @Override
+    public void fromBytes(ByteBuf buf)
+    {
+    }
+
+    @Override
+    public void toBytes(ByteBuf buf)
+    {
+    }
+
+}
