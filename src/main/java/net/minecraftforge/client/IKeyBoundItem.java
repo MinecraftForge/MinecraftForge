39539c3817ef32fb71fbf3c872c27e03ebd6829e
Joseph Parr <joseph2jsh@gmail.com>
2014-03-31 11:31:54 -0700

    Removed Copyrights

--- src/main/java/net/minecraftforge/client/IKeyBoundItem.java
+++ src/main/java/net/minecraftforge/client/IKeyBoundItem.java
@@ -3,10 +3,6 @@ package net.minecraftforge.client;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 
-/**
- * @author BalthezarOi
- * @CC-License Created under the CC BY-NC-ND license by BalthezarOi
- */
 public interface IKeyBoundItem {
     public void keyPressed(ItemStack stack, EntityPlayer player);
 
