3d8dbba8570fa8049343fe692bc301c62cd76b14
DylanGore <admin@dylangore.info>
2014-06-07 13:06:17 +0100

    Add Stone Brick blocks to OreDict
    Added: 
    stonebrick
    stonebrick_moss
    stonebrick_cracked
    stonebrick_chiseled

--- src/main/java/net/minecraftforge/oredict/OreDictionary.java
+++ src/main/java/net/minecraftforge/oredict/OreDictionary.java
@@ -96,6 +96,10 @@ public class OreDictionary
             registerOre("cropPotato",  Items.potato);
             registerOre("cropCarrot",  Items.carrot);
             registerOre("stone",       Blocks.stone);
+            registerOre("stonebrick",  new ItemStack(Blocks.stonebrick, 1, 0));
+            registerOre("stonebrick_moss", new ItemStack(Blocks.stonebrick, 1, 1);
+            registerOre("stonebrick_cracked", new ItemStack(Blocks.stonebrick, 1, 2);
+            registerOre("stonebrick_chiseled", new ItemStack(Blocks.stonebrick, 1, 3);
             registerOre("cobblestone", Blocks.cobblestone);
             registerOre("sandstone",   new ItemStack(Blocks.sandstone, 1, WILDCARD_VALUE));
             registerOre("dye",         new ItemStack(Items.dye, 1, WILDCARD_VALUE));
