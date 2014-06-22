2b1438dbf12c3572fa7250bf73c7fcddf347ee1d
Christian <cpw@weeksfamily.ca>
2014-06-21 22:44:25 -0400

    Fix server side to write properly

--- src/main/java/cpw/mods/fml/common/launcher/FMLServerTweaker.java
+++ src/main/java/cpw/mods/fml/common/launcher/FMLServerTweaker.java
@@ -13,7 +13,9 @@ public class FMLServerTweaker extends FMLTweaker {
     @Override
     public void injectIntoClassLoader(LaunchClassLoader classLoader)
     {
-        classLoader.addClassLoaderExclusion("com.mojang.util.");
+        // The mojang packages are excluded so the log4j2 queue is correctly visible from
+        // the obfuscated and deobfuscated parts of the code. Without, the UI won't show anything
+        classLoader.addClassLoaderExclusion("com.mojang.");
         classLoader.addTransformerExclusion("cpw.mods.fml.repackage.");
         classLoader.addTransformerExclusion("cpw.mods.fml.relauncher.");
         classLoader.addTransformerExclusion("cpw.mods.fml.common.asm.transformers.");
