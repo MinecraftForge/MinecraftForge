06ab104c9ab798af6d2726e02a238211ff8124e1
cpw <cpw@weeksfamily.ca>
2014-06-24 15:37:39 -0400

    Force the descriptors to the right type for the field they're referencing. Fixes the sand issue

--- src/main/java/cpw/mods/fml/common/asm/transformers/deobf/FMLRemappingAdapter.java
+++ src/main/java/cpw/mods/fml/common/asm/transformers/deobf/FMLRemappingAdapter.java
@@ -13,7 +13,11 @@
 package cpw.mods.fml.common.asm.transformers.deobf;
 
 import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.commons.Remapper;
 import org.objectweb.asm.commons.RemappingClassAdapter;
+import org.objectweb.asm.commons.RemappingMethodAdapter;
 
 public class FMLRemappingAdapter extends RemappingClassAdapter {
     public FMLRemappingAdapter(ClassVisitor cv)
@@ -30,5 +34,42 @@ public class FMLRemappingAdapter extends RemappingClassAdapter {
         }
         FMLDeobfuscatingRemapper.INSTANCE.mergeSuperMaps(name, superName, interfaces);
         super.visit(version, access, name, signature, superName, interfaces);
+    }
+
+    @Override
+    protected MethodVisitor createRemappingMethodAdapter(int access, String newDesc, MethodVisitor mv)
+    {
+        return new StaticFixingMethodVisitor(access, newDesc, mv, remapper);
+    }
+
+    private static class StaticFixingMethodVisitor extends RemappingMethodAdapter
+    {
+
+        public StaticFixingMethodVisitor(int access, String desc, MethodVisitor mv, Remapper remapper)
+        {
+            super(access, desc, mv, remapper);
+        }
+
+        @Override
+        public void visitFieldInsn(int opcode, String originalType, String originalName, String desc)
+        {
+            // This method solves the problem of a static field reference changing type. In all probability it is a
+            // compatible change, however we need to fix up the desc to point at the new type
+            String type = remapper.mapType(originalType);
+            String fieldName = remapper.mapFieldName(originalType, originalName, desc);
+            String newDesc = remapper.mapDesc(desc);
+            if (opcode == Opcodes.GETSTATIC && type.startsWith("net/minecraft/") && newDesc.startsWith("Lnet/minecraft/"))
+            {
+                String replDesc = FMLDeobfuscatingRemapper.INSTANCE.getStaticFieldType(originalType, originalName, type, fieldName);
+                if (replDesc != null)
+                {
+                    newDesc = remapper.mapDesc(replDesc);
+                }
+            }
+            // super.super
+            if (mv != null) {
+                mv.visitFieldInsn(opcode, type, fieldName, newDesc);
+            }
+        }
     }
 }
