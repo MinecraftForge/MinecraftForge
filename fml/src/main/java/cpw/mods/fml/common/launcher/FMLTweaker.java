b9db0026143366248ae98a9ffd52a48dc9b462c6
cpw <cpw@weeksfamily.ca>
2014-06-25 17:49:00 -0400

    Fix accessing guava from transformers

--- fml/src/main/java/cpw/mods/fml/common/launcher/FMLTweaker.java
+++ fml/src/main/java/cpw/mods/fml/common/launcher/FMLTweaker.java
@@ -117,6 +117,7 @@ public class FMLTweaker implements ITweaker {
     public void injectIntoClassLoader(LaunchClassLoader classLoader)
     {
         classLoader.addClassLoaderExclusion("org.apache.");
+        classLoader.addClassLoaderExclusion("com.google.common.");
         classLoader.addTransformerExclusion("cpw.mods.fml.repackage.");
         classLoader.addTransformerExclusion("cpw.mods.fml.relauncher.");
         classLoader.addTransformerExclusion("cpw.mods.fml.common.asm.transformers.");
