478253606414da5d8ed9b782261819c6372e4871
Gerard Bruwn <siribby@outlook.com>
2014-06-30 11:54:08 -0700

    Add RenderItemInFrameEvent

---
+++ src/main/java/net/minecraftforge/client/event/RenderItemInFrameEvent.java
@@ -0,0 +1,27 @@
+package net.minecraftforge.client.event;
+
+import net.minecraft.client.renderer.tileentity.RenderItemFrame;
+import net.minecraft.entity.item.EntityItemFrame;
+import net.minecraft.item.ItemStack;
+import cpw.mods.fml.common.eventhandler.Cancelable;
+import cpw.mods.fml.common.eventhandler.Event;
+
+/**
+ * This event is called when an item is rendered in an item frame.
+ * 
+ * You can set canceled to do no further vanilla processing.
+ */
+@Cancelable
+public class RenderItemInFrameEvent extends Event
+{
+    public final ItemStack item;
+    public final EntityItemFrame entityItemFrame;
+    public final RenderItemFrame renderer;
+    
+    public RenderItemInFrameEvent(EntityItemFrame itemFrame, RenderItemFrame renderItemFrame)
+    {
+        item = itemFrame.getDisplayedItem();
+        entityItemFrame = itemFrame;
+        renderer = renderItemFrame;
+    }
+}
