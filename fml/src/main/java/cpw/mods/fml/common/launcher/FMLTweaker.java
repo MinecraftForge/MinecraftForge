d969137b193f87a7bf3400146c79808029287707
Lex Manos <LexManos@gmail.com>
2014-07-07 09:29:34 -0700

    Force preferIPv4Stack to true early in the load chain to combat netty loopback issues.

--- fml/src/main/java/cpw/mods/fml/common/launcher/FMLTweaker.java
+++ fml/src/main/java/cpw/mods/fml/common/launcher/FMLTweaker.java
@@ -29,6 +29,7 @@ public class FMLTweaker implements ITweaker {
 
     public FMLTweaker()
     {
+        System.setProperty("java.net.preferIPv4Stack", "true"); //Lets do this as early as possible. Vanilla does it in Main.main
         try
         {
             System.setSecurityManager(new FMLSecurityManager());
