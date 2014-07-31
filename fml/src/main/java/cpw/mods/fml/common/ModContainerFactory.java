0607bffb92e230b958a36858465458f0d4f6d79f
Christian <cpw@weeksfamily.ca>
2014-07-31 10:02:47 -0230

    Kill net.minecraft.src warning. It hasn't served a purpose in a long time now. Closes #313

--- fml/src/main/java/cpw/mods/fml/common/ModContainerFactory.java
+++ fml/src/main/java/cpw/mods/fml/common/ModContainerFactory.java
@@ -47,14 +47,6 @@ public class ModContainerFactory
             container.rememberBaseModType(className);
         }
 
-        // We warn if it's not a basemod instance -- compatibility requires it to be in net.minecraft.src *sigh*
-        if (className.startsWith("net.minecraft.src.") && container.isClasspath() && !container.isMinecraftJar())
-        {
-            FMLLog.severe("FML has detected a mod that is using a package name based on 'net.minecraft.src' : %s. This is generally a severe programming error. "
-                    + " There should be no mod code in the minecraft namespace. MOVE YOUR MOD! If you're in eclipse, select your source code and 'refactor' it into "
-                    + "a new package. Go on. DO IT NOW!",className);
-        }
-
         for (ModAnnotation ann : modParser.getAnnotations())
         {
             if (ann.getASMType().equals(Type.getType(Mod.class)))
