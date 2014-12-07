3951c2da473dcd220916863885c8e3c2adfd15c5
Lex Manos <LexManos@gmail.com>
2014-12-07 02:35:07 -0800

    Add LivingHealEvent called from EntityLivingBase.heal() Closes #1282

--- src/main/java/net/minecraftforge/event/entity/living/LivingHealEvent.java
+++ src/main/java/net/minecraftforge/event/entity/living/LivingHealEvent.java
@@ -1,6 +1,10 @@
 package net.minecraftforge.event.entity.living;
 
+<<<<<<< HEAD
 import net.minecraftforge.fml.common.eventhandler.Cancelable;
+=======
+import cpw.mods.fml.common.eventhandler.Cancelable;
+>>>>>>> Add LivingHealEvent called from EntityLivingBase.heal() Closes #1282
 import net.minecraft.entity.EntityLivingBase;
 
 /**
