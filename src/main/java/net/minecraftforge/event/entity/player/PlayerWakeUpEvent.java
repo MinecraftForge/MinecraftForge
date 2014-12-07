f9955efb0832bc89be227a6f5770fe1829e89727
Lex Manos <LexManos@gmail.com>
2014-12-07 05:48:26 -0800

    Expand PlayerWakupEvent to expose the three parameters passed into EntityPlayer.wakeUp.

--- src/main/java/net/minecraftforge/event/entity/player/PlayerWakeUpEvent.java
+++ src/main/java/net/minecraftforge/event/entity/player/PlayerWakeUpEvent.java
@@ -9,8 +9,29 @@ import net.minecraft.entity.player.EntityPlayer;
  */
 public class PlayerWakeUpEvent extends PlayerEvent
 {
-    public PlayerWakeUpEvent(EntityPlayer player)
+    /**
+     * Used for the 'wake up animation'.
+     * This is false if the player is considered 'sleepy' and the overlay should slowly fade away.
+     */
+    public final boolean wakeImmediatly;
+
+    /**
+     * Indicates if the server should be notified of sleeping changes.
+     * This will only be false if the server is considered 'up to date' already, because, for example, it initiated the call.
+     */
+    public final boolean updateWorld;
+
+    /**
+     * Indicates if the player's sleep was considered successful.
+     * In vanilla, this is used to determine if the spawn chunk is to be set to the bed's position.
+     */
+    public final boolean setSpawn;
+
+    public PlayerWakeUpEvent(EntityPlayer player, boolean wakeImmediatly, boolean updateWorld, boolean setSpawn)
     {
         super(player);
+        this.wakeImmediatly = wakeImmediatly;
+        this.updateWorld = updateWorld;
+        this.setSpawn = setSpawn;
     }
 }
