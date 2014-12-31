a2462f9b601e59446d6a72396ce64ca3c935af3f
iLexiconn <ilexiconn@gmail.com>
2014-12-31 14:11:02 +0100

    Added partialTicks to RenderLivingEvent

--- src/main/java/net/minecraftforge/client/event/RenderLivingEvent.java
+++ src/main/java/net/minecraftforge/client/event/RenderLivingEvent.java
@@ -12,38 +12,40 @@ public abstract class RenderLivingEvent extends Event
     public final double x;
     public final double y;
     public final double z;
+    public final float partialTicks;
 
-    public RenderLivingEvent(EntityLivingBase entity, RendererLivingEntity renderer, double x, double y, double z)
+    public RenderLivingEvent(EntityLivingBase entity, RendererLivingEntity renderer, double x, double y, double z, float partialTicks)
     {
         this.entity = entity;
         this.renderer = renderer;
         this.x = x;
         this.y = y;
         this.z = z;
+        this.partialTicks = partialTicks;
     }
     
     @Cancelable
     public static class Pre extends RenderLivingEvent
     {
-        public Pre(EntityLivingBase entity, RendererLivingEntity renderer, double x, double y, double z){ super(entity, renderer, x, y, z); }
+        public Pre(EntityLivingBase entity, RendererLivingEntity renderer, double x, double y, double z, float partialTicks){ super(entity, renderer, x, y, z, partialTicks); }
     }
     public static class Post extends RenderLivingEvent
     {
-        public Post(EntityLivingBase entity, RendererLivingEntity renderer, double x, double y, double z){ super(entity, renderer, x, y, z); }
+        public Post(EntityLivingBase entity, RendererLivingEntity renderer, double x, double y, double z, float partialTicks){ super(entity, renderer, x, y, z, partialTicks); }
     }
 
     public abstract static class Specials extends RenderLivingEvent
     {
-        public Specials(EntityLivingBase entity, RendererLivingEntity renderer, double x, double y, double z){ super(entity, renderer, x, y, z); }
+        public Specials(EntityLivingBase entity, RendererLivingEntity renderer, double x, double y, double z, float partialTicks){ super(entity, renderer, x, y, z, partialTicks); }
 
         @Cancelable
         public static class Pre extends Specials
         {
-            public Pre(EntityLivingBase entity, RendererLivingEntity renderer, double x, double y, double z){ super(entity, renderer, x, y, z); }
+            public Pre(EntityLivingBase entity, RendererLivingEntity renderer, double x, double y, double z, float partialTicks){ super(entity, renderer, x, y, z, partialTicks); }
         }
         public static class Post extends Specials
         {
-            public Post(EntityLivingBase entity, RendererLivingEntity renderer, double x, double y, double z){ super(entity, renderer, x, y, z); }
+            public Post(EntityLivingBase entity, RendererLivingEntity renderer, double x, double y, double z, float partialTicks){ super(entity, renderer, x, y, z, partialTicks); }
         }
     }
 }
