b2f1b036de8881511a5d49575e2964d0647b5dcb
iChun <ohai.iChun@gmail.com>
2015-04-04 22:30:23 +0100

    Readded but deprecated the old RenderPlayerEvent that were deleted. Sorry, Lex.

--- src/main/java/net/minecraftforge/client/event/RenderPlayerEvent.java
+++ src/main/java/net/minecraftforge/client/event/RenderPlayerEvent.java
@@ -28,10 +28,55 @@ public abstract class RenderPlayerEvent extends PlayerEvent
     public static class Pre extends RenderPlayerEvent
     {
         public Pre(EntityPlayer player, RenderPlayer renderer, float tick, double x, double y, double z){ super(player, renderer, tick, x, y, z); }
+        @Deprecated
+        public Pre(EntityPlayer player, RenderPlayer renderer, float tick){ this(player, renderer, tick, 0D, 0D, 0D); }
     }
 
     public static class Post extends RenderPlayerEvent
     {
         public Post(EntityPlayer player, RenderPlayer renderer, float tick, double x, double y, double z){ super(player, renderer, tick, x, y, z); }
+        @Deprecated
+        public Post(EntityPlayer player, RenderPlayer renderer, float tick){ this(player, renderer, tick, 0D, 0D, 0D); }
+    }
+    
+    @Deprecated
+    public abstract static class Specials extends RenderPlayerEvent
+    {
+        public Specials(EntityPlayer player, RenderPlayer renderer, float partialTicks)
+        {
+            super(player, renderer, partialTicks, 0D, 0D, 0D);
+        }
+
+        @Cancelable
+        public static class Pre extends Specials
+        {
+            public boolean renderHelmet = true;
+            public boolean renderCape = true;
+            public boolean renderItem = true;
+            public Pre(EntityPlayer player, RenderPlayer renderer, float partialTicks){ super(player, renderer, partialTicks); }
+        }
+
+        public static class Post extends Specials
+        {
+            public Post(EntityPlayer player, RenderPlayer renderer, float partialTicks){ super(player, renderer, partialTicks); }
+        }
+    }
+
+    @Deprecated
+    public static class SetArmorModel extends RenderPlayerEvent
+    {
+        /**
+         * Setting this to any value besides -1 will result in the function being 
+         * Immediately exited with the return value specified.
+         */
+        public int result = -1;
+        public final int slot;
+        public final ItemStack stack;
+        public SetArmorModel(EntityPlayer player, RenderPlayer renderer, int slot, float partialTick, ItemStack stack)
+        {
+            super(player, renderer, partialTick, 0D, 0D, 0D);
+            this.slot = slot;
+            this.stack = stack;
+        }
     }
 }
