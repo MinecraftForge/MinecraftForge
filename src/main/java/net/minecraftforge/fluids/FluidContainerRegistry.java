186f92479d2d43ee6e92113d94dd8ab768140494
King Lemming <kinglemming@gmail.com>
2015-04-10 16:38:45 -0400

    Fixes FluidContainerRegistry properly - no more corner case where client/server mods disagree.
    Also clarifies the 2x Fluid registration error message somewhat.
    
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/net/minecraftforge/fluids/FluidContainerRegistry.java
+++ src/main/java/net/minecraftforge/fluids/FluidContainerRegistry.java
@@ -51,7 +51,7 @@ public abstract class FluidContainerRegistry
             code = 31*code + container.getItem().hashCode();
             code = 31*code + container.getItemDamage();
             if (stack != null)
-                code = 31*code + stack.getFluidID();
+                code = 31*code + stack.fluid.hashCode(); 
             return code;
         }
         @Override
