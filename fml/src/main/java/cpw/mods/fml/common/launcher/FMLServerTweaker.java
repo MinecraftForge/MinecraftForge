65ab79cf694cf755294fe6934faa8f864da045a3
cpw <cpw@weeksfamily.ca>
2015-05-04 08:49:46 -0400

    Revert "Merge pull request #650 from luacs1998/1.7.10" This undoes the seriously broken change from Sponge to support Mixins, that breaks a wide variety of coremods. Given the widespread incompatibility it introduces, it won't be re-added at 1.7.10.
    This reverts commit 8ccfa24764a3f4854f5334c0da1224286175e13b, reversing
    changes made to 7c10b93a2ded2799d41b73b67a2766c31e992d8a.

--- fml/src/main/java/cpw/mods/fml/common/launcher/FMLServerTweaker.java
+++ fml/src/main/java/cpw/mods/fml/common/launcher/FMLServerTweaker.java
@@ -16,7 +16,6 @@ public class FMLServerTweaker extends FMLTweaker {
         // The mojang packages are excluded so the log4j2 queue is correctly visible from
         // the obfuscated and deobfuscated parts of the code. Without, the UI won't show anything
         classLoader.addClassLoaderExclusion("com.mojang.");
-        classLoader.addClassLoaderExclusion("org.objectweb.asm.");
         classLoader.addTransformerExclusion("cpw.mods.fml.repackage.");
         classLoader.addTransformerExclusion("cpw.mods.fml.relauncher.");
         classLoader.addTransformerExclusion("cpw.mods.fml.common.asm.transformers.");
