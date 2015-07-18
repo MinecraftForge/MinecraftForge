c308a47124394f025bc2ba553646ecf8b6c31ba2
cpw <cpw@weeksfamily.ca>
2015-07-18 12:47:31 -0400

    Cherry pick some changes from 1.8 for inner class discovery, also fix the negativecache. Closes #1872

--- fml/src/main/java/cpw/mods/fml/common/discovery/DirectoryDiscoverer.java
+++ fml/src/main/java/cpw/mods/fml/common/discovery/DirectoryDiscoverer.java
@@ -38,7 +38,7 @@ public class DirectoryDiscoverer implements ITypeDiscoverer
         @Override
         public boolean accept(File file)
         {
-            return (file.isFile() && classFile.matcher(file.getName()).find()) || file.isDirectory();
+            return (file.isFile() && classFile.matcher(file.getName()).matches()) || file.isDirectory();
         }
     }
 
--- fml/src/main/java/cpw/mods/fml/common/discovery/ITypeDiscoverer.java
+++ fml/src/main/java/cpw/mods/fml/common/discovery/ITypeDiscoverer.java
@@ -5,7 +5,7 @@
  * are made available under the terms of the GNU Lesser Public License v2.1
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- * 
+ *
  * Contributors:
  *     cpw - implementation
  */
@@ -19,7 +19,8 @@ import cpw.mods.fml.common.ModContainer;
 
 public interface ITypeDiscoverer
 {
-    public static Pattern classFile = Pattern.compile("([^\\s$]+).class$");
+    // main class part, followed by an optional $ and an "inner class" part. $ cannot be last, otherwise scala breaks
+    public static Pattern classFile = Pattern.compile("[^\\s\\$]+(\\$[^\\s]+)?\\.class$");
 
     public List<ModContainer> discover(ModCandidate candidate, ASMDataTable table);
 }
