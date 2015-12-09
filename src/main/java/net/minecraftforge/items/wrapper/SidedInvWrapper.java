d94402f5e2037aee0406f1addff314fdf91541b3
rwtema <rwtema@gmail.com>
2015-12-09 13:02:14 +0000

    Change formatting to be more consistent.

--- src/main/java/net/minecraftforge/items/wrapper/SidedInvWrapper.java
+++ src/main/java/net/minecraftforge/items/wrapper/SidedInvWrapper.java
@@ -7,133 +7,168 @@ import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemHandlerHelper;
 
 public class SidedInvWrapper implements IItemHandler {
-	public SidedInvWrapper(ISidedInventory inv) {
-		this.inv = inv;
-	}
+    ISidedInventory inv;
 
-	ISidedInventory inv;
+    public SidedInvWrapper(ISidedInventory inv)
+    {
+        this.inv = inv;
+    }
 
-	@Override
-	public int getSlots(EnumFacing side) {
-		return getSlots(inv, side);
-	}
+    public static int getSlots(ISidedInventory inv, EnumFacing side)
+    {
+        return inv.getSlotsForFace(side).length;
+    }
 
-	@Override
-	public ItemStack getStackInSlot(int slot, EnumFacing side) {
-		return getStackInSlot(inv, slot, side);
-	}
+    public static int getSlot(ISidedInventory inv, int slot, EnumFacing side)
+    {
+        int[] slots = inv.getSlotsForFace(side);
+        if (slot < slots.length)
+            return slots[slot];
+        return -1;
+    }
 
-	@Override
-	public ItemStack insertItem(int slot, ItemStack stack, EnumFacing side, boolean simulate) {
-		return insertItem(inv, slot, stack, side, simulate);
-	}
+    public static ItemStack getStackInSlot(ISidedInventory inv, int slot, EnumFacing side)
+    {
+        int i = getSlot(inv, slot, side);
+        return i == -1 ? null : inv.getStackInSlot(i);
+    }
 
-	@Override
-	public ItemStack extractItem(int slot, int amount, EnumFacing side, boolean simulate) {
-		return extractItem(inv, slot, amount, side, simulate);
-	}
+    public static ItemStack insertItem(ISidedInventory inv, int s, ItemStack insertStack, EnumFacing side, boolean simulate)
+    {
 
+        if (insertStack == null)
+            return null;
 
-	public static int getSlots(ISidedInventory inv, EnumFacing side) {
-		return inv.getSlotsForFace(side).length;
-	}
+        int slot = getSlot(inv, s, side);
 
-	public static int getSlot(ISidedInventory inv, int slot, EnumFacing side) {
-		int[] slots = inv.getSlotsForFace(side);
-		if (slot < slots.length)
-			return slots[slot];
-		return -1;
-	}
+        if (slot == -1)
+            return insertStack;
 
-	public static ItemStack getStackInSlot(ISidedInventory inv, int slot, EnumFacing side) {
-		int i = getSlot(inv, slot, side);
-		return i == -1 ? null : inv.getStackInSlot(i);
-	}
+        if (!inv.isItemValidForSlot(slot, insertStack) || !inv.canInsertItem(slot, insertStack, side))
+            return insertStack;
 
+        ItemStack stackInSlot = inv.getStackInSlot(slot);
 
-	public static ItemStack insertItem(ISidedInventory inv, int s, ItemStack insertStack, EnumFacing side, boolean simulate) {
+        int m;
+        if (stackInSlot != null)
+        {
+            if (!ItemHandlerHelper.canItemStacksStack(insertStack, stackInSlot))
+                return insertStack;
 
-		if (insertStack == null) return null;
+            m = Math.min(insertStack.getMaxStackSize(), inv.getInventoryStackLimit()) - stackInSlot.stackSize;
 
-		int slot = getSlot(inv, s, side);
+            if (insertStack.stackSize <= m)
+            {
+                if (!simulate)
+                {
+                    ItemStack copy = insertStack.copy();
+                    copy.stackSize += stackInSlot.stackSize;
+                    inv.setInventorySlotContents(slot, copy);
+                }
 
-		if (slot == -1) return insertStack;
+                return null;
+            }
+            else
+            {
+                if (!simulate)
+                {
+                    ItemStack copy = insertStack.splitStack(m);
+                    copy.stackSize += stackInSlot.stackSize;
+                    inv.setInventorySlotContents(slot, copy);
+                    return insertStack;
+                }
+                else
+                {
+                    insertStack.stackSize -= m;
+                    return insertStack;
+                }
+            }
+        }
+        else
+        {
+            m = Math.min(insertStack.getMaxStackSize(), inv.getInventoryStackLimit());
+            if (m < insertStack.stackSize)
+            {
+                if (!simulate)
+                {
+                    inv.setInventorySlotContents(slot, insertStack.splitStack(m));
+                    return insertStack;
+                }
+                else
+                {
+                    insertStack.stackSize -= m;
+                    return insertStack;
+                }
+            }
+            else
+            {
+                if (!simulate)
+                    inv.setInventorySlotContents(slot, insertStack);
+                return null;
+            }
+        }
 
-		if (!inv.isItemValidForSlot(slot, insertStack) || !inv.canInsertItem(slot, insertStack, side))
-			return insertStack;
+    }
 
-		ItemStack stackInSlot = inv.getStackInSlot(slot);
+    public static ItemStack extractItem(ISidedInventory inv, int s, int amount, EnumFacing side, boolean simulate)
+    {
+        if (amount == 0)
+            return null;
 
-		int m;
-		if (stackInSlot != null) {
-			if (!ItemHandlerHelper.canItemsStack(insertStack, stackInSlot))
-				return insertStack;
+        int slot = getSlot(inv, s, side);
 
-			m = Math.min(insertStack.getMaxStackSize(), inv.getInventoryStackLimit()) - stackInSlot.stackSize;
+        if (slot == -1)
+            return null;
 
-			if (insertStack.stackSize <= m) {
-				if (!simulate) {
-					ItemStack copy = insertStack.copy();
-					copy.stackSize += stackInSlot.stackSize;
-					inv.setInventorySlotContents(slot, copy);
-				}
+        ItemStack stackInSlot = inv.getStackInSlot(slot);
 
-				return null;
-			} else {
-				if (!simulate) {
-					ItemStack copy = insertStack.splitStack(m);
-					copy.stackSize += stackInSlot.stackSize;
-					inv.setInventorySlotContents(slot, copy);
-					return insertStack;
-				} else {
-					insertStack.stackSize -= m;
-					return insertStack;
-				}
-			}
-		} else {
-			m = Math.min(insertStack.getMaxStackSize(), inv.getInventoryStackLimit());
-			if (m < insertStack.stackSize) {
-				if (!simulate) {
-					inv.setInventorySlotContents(slot, insertStack.splitStack(m));
-					return insertStack;
-				} else {
-					insertStack.stackSize -= m;
-					return insertStack;
-				}
-			} else {
-				if (!simulate) inv.setInventorySlotContents(slot, insertStack);
-				return null;
-			}
-		}
+        if (stackInSlot == null)
+            return null;
 
-	}
+        if (!inv.canExtractItem(slot, stackInSlot, side))
+            return null;
 
-	public static ItemStack extractItem(ISidedInventory inv, int s, int amount, EnumFacing side, boolean simulate) {
-		if (amount == 0) return null;
+        if (simulate)
+        {
+            if (stackInSlot.stackSize < amount)
+            {
+                return stackInSlot.copy();
+            }
+            else
+            {
+                ItemStack copy = stackInSlot.copy();
+                copy.stackSize = amount;
+                return copy;
+            }
+        }
+        else
+        {
+            int m = Math.min(stackInSlot.stackSize, amount);
+            return inv.decrStackSize(slot, m);
+        }
+    }
 
-		int slot = getSlot(inv, s, side);
+    @Override
+    public int getSlots(EnumFacing side)
+    {
+        return getSlots(inv, side);
+    }
 
-		if (slot == -1) return null;
+    @Override
+    public ItemStack getStackInSlot(int slot, EnumFacing side)
+    {
+        return getStackInSlot(inv, slot, side);
+    }
 
-		ItemStack stackInSlot = inv.getStackInSlot(slot);
+    @Override
+    public ItemStack insertItem(int slot, ItemStack stack, EnumFacing side, boolean simulate)
+    {
+        return insertItem(inv, slot, stack, side, simulate);
+    }
 
-		if (stackInSlot == null)
-			return null;
-
-		if (!inv.canExtractItem(slot, stackInSlot, side))
-			return null;
-
-		if (simulate) {
-			if (stackInSlot.stackSize < amount) {
-				return stackInSlot.copy();
-			} else {
-				ItemStack copy = stackInSlot.copy();
-				copy.stackSize = amount;
-				return copy;
-			}
-		} else {
-			int m = Math.min(stackInSlot.stackSize, amount);
-			return inv.decrStackSize(slot, m);
-		}
-	}
+    @Override
+    public ItemStack extractItem(int slot, int amount, EnumFacing side, boolean simulate)
+    {
+        return extractItem(inv, slot, amount, side, simulate);
+    }
 }
