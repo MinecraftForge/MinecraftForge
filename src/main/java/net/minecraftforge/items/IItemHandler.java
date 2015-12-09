edbd67d757edd551f1cca43c701fd8140d6abc91
rwtema <rwtema@gmail.com>
2015-12-09 13:38:05 +0000

    Fix some typos and clarified some things.

--- src/main/java/net/minecraftforge/items/IItemHandler.java
+++ src/main/java/net/minecraftforge/items/IItemHandler.java
@@ -6,7 +6,7 @@ import net.minecraft.util.EnumFacing;
 public interface IItemHandler {
     /**
      * Returns the number of slots available on a given side.
-     * Slots on different side are not necessarily the same slots.
+     * Slots on different sides are not necessarily the same slots.
      *
      * @param side Orientation being queried.
      * @return The number of slots available on the side.
@@ -15,6 +15,7 @@ public interface IItemHandler {
 
     /**
      * Returns the ItemStack in a given slot on the given side. This should not be used to manipulate contents.
+     * The resultant value's stacksize may be greater than the stacks max limit.
      *
      * @param slot Slot to query
      * @param side Orientation being queried.
@@ -23,24 +24,25 @@ public interface IItemHandler {
     ItemStack getStackInSlot(int slot, EnumFacing side);
 
     /**
-     * Inserts an ItemStack into the given slot on the given side.
+     * Inserts an ItemStack into the given slot on the given side and return the remainder.
+     * Note: This behaviour is subtly different from IFluidHandlers.fill()
      *
      * @param side     Orientation the ItemStack is inserted from.
      * @param slot     Slot to insert into.
      * @param stack    ItemStack to insert
      * @param simulate If true, the insertion is only simulated
-     * @return The remaining ItemStack that could not be inserted (if the whole stack is accepted then return null)
+     * @return The remaining ItemStack that was not inserted (if the entire stack is accepted, then return null)
      **/
     ItemStack insertItem(int slot, ItemStack stack, EnumFacing side, boolean simulate);
 
     /**
-     * Inserts an ItemStack into the given slot on the given side.
+     * Extracts an ItemStack from the given slot on the given side. This must respect max stack limits.
      *
      * @param side     Orientation the ItemStack is extracted from.
      * @param slot     Slot to extract into.
      * @param amount   ItemStack to extract
      * @param simulate If true, the extraction is only simulated
-     * @return ItemStack extracted from the slot
+     * @return ItemStack extracted from the slot (should be null, if nothing can be extracted)
      **/
     ItemStack extractItem(int slot, int amount, EnumFacing side, boolean simulate);
 }
