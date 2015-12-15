6674ae7004f70fc8e9ee84ad8751677991780170
Zaggy1024 <Zaggy1024@gmail.com>
2015-12-15 11:14:44 -0600

    Added ISeed to replace IPlantable for items, allowing them to be ItemStack aware. Made IPlantable/ISeed return a collection of soil types that the plant can survive on, and have a method to check if a plant can be placed at a position in the world. Blocks will now also return a collection of soil types that they can survive on, and the plant can use a static method in Forge that defines the default behavior to check that one of the soil types matches with a plant's. Fixed Forge making it so that farmland doesn't revert to dirt when a non-crop plant is on top of it. Renamed IPlantable -> IPlant and EnumPlantType -> SoilType.

---
+++ src/main/java/net/minecraftforge/common/IPlant.java
@@ -0,0 +1,23 @@
+package net.minecraftforge.common;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
+public interface IPlant
+{
+    /**
+     * Behavior is the same as
+     * {@link net.minecraft.block.BlockBush#canBlockStay(World, BlockPos, IBlockState) BlockBush.canBlockStay}.
+     * @return Whether the plant can stay at this location in the world.<br>
+     */
+    public boolean canPlantStay(World world, BlockPos pos, IBlockState state);
+    
+    /**
+     * @return Whether the plant grows (through spreading or aging).<br>
+     * Under the assumption that plants that grow on the Crops soil type are affected by hydration,
+     * returning true prevents farmland from turning into dirt.
+     */
+    public boolean isGrowable(IBlockAccess world, BlockPos pos, IBlockState state);
+}
--- src/test/java/net/minecraftforge/debug/EnumTest.java
+++ src/test/java/net/minecraftforge/debug/EnumTest.java
@@ -1,14 +1,14 @@
 package net.minecraftforge.debug;
 
 import net.minecraftforge.common.BiomeManager.BiomeType;
-import net.minecraftforge.common.EnumPlantType;
+import net.minecraftforge.common.SoilType;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
-@Mod(modid = "enumplanttypetest")
-public class EnumPlantTypeTest 
+@Mod(modid = "enumtest")
+public class EnumTest 
 {
 	private static Logger LOGGER = LogManager.getLogger();
 	
@@ -29,21 +29,19 @@ public class EnumPlantTypeTest
 		    if (biomeType == null || !biomeType.name().equals("FAKE")) 
 			    LOGGER.warn("EnumHelper in BiomeType is working incorrectly!");
 		}
-	    EnumPlantType plantType = null;
-	    if (plantType == null || !plantType.name().equals("FAKE"))
-	    	;
+	    SoilType plantType = null;
 	    try 
 		{ 
-	    	 plantType = EnumPlantType.getPlantType("FAKE");
-		} 
+	    	 plantType = SoilType.getType("FAKE");
+		}
 		catch (NullPointerException npe)
 		{
-			LOGGER.warn("EnumHelper in EnumPlantType is working incorrectly!", npe);
+			LOGGER.warn("SoilType.getSoilType is working incorrectly!", npe);
 		}
 		finally
 		{
-		    if (plantType == null || !plantType.name().equals("FAKE")) 
-		    	LOGGER.warn("EnumHelper in EnumPlantType is working incorrectly!");
+		    if (plantType == null || !plantType.getName().equals("FAKE")) 
+		    	LOGGER.warn("SoilType.getSoilType is working incorrectly!");
 		}
 	}
 }
