d40716b077c56203c204cac7b3271fc39e1e6524
Shadowfacts <me@shadowfacts.net>
2016-01-25 14:42:44 -0500

    Helper method, tabs -> spaces, JavaDoc

--- src/main/java/net/minecraftforge/event/ServerSendChatEvent.java
+++ src/main/java/net/minecraftforge/event/ServerSendChatEvent.java
@@ -4,25 +4,29 @@ import net.minecraft.util.IChatComponent;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.Cancelable;
 
+/**
+ * Fired when a chat message is sent from the server (e.g. using the /say command).
+ * Fired from {@link net.minecraft.server.management.ServerConfigurationManager#sendChatMsg}.
+ */
 @Cancelable
 public class ServerSendChatEvent extends Event
 {
 	
-	private IChatComponent message;
-	
-	public ServerSendChatEvent(IChatComponent message)
-	{
-		this.message = message;
-	}
-	
-	public IChatComponent getMessage()
-	{
-		return message;
-	}
-	
-	public void setMessage(IChatComponent message)
-	{
-		this.message = message;
-	}
+    private IChatComponent message;
+
+    public ServerSendChatEvent(IChatComponent message)
+    {
+        this.message = message;
+    }
+
+    public IChatComponent getMessage()
+    {
+        return message;
+    }
+
+    public void setMessage(IChatComponent message)
+    {
+        this.message = message;
+    }
 
 }
