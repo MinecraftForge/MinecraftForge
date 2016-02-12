743dbea48f3f4dcdb571f074b25518e83345c16c
minzmann <minzmann@byom.de>
2016-02-12 18:27:41 +0100

    Javadoc

--- src/main/java/net/minecraftforge/event/world/FarmlandTrampleEvent.java
+++ src/main/java/net/minecraftforge/event/world/FarmlandTrampleEvent.java
@@ -5,11 +5,18 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.eventhandler.Cancelable;
+import net.minecraftforge.fml.common.eventhandler.Event.HasResult;
 
 /**
  * Called just before farmland would change to dirt.
- *
  * If the event is cancelled, the farmland (and plant) will remain intact.
+ * <br>
+ * This event is {@link Cancelable}.<br>
+ * This event does not use {@link HasResult}.<br>
+ * This event is fired on the {@link MinecraftForge#EVENT_BUS}.<br>
+ * 
+ * @param pos The position of the farmland block
+ * @param entity The entity that trampled the farmland
  */
 @Cancelable
 public class FarmlandTrampleEvent extends BlockEvent
