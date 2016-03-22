8610d2aa19423201da43b46f8dd93af3d25bd65c
Shadowfacts <me@shadowfacts.net>
2016-03-22 10:11:26 -0400

    Rewrite ScreenshotEvent

--- src/main/java/net/minecraftforge/client/ForgeHooksClient.java
+++ src/main/java/net/minecraftforge/client/ForgeHooksClient.java
@@ -640,16 +640,11 @@ public class ForgeHooksClient
         return !from.getItem().shouldCauseReequipAnimation(from, to, changed);
     }
 
-    public static ScreenshotEvent.Pre onScreenshotPre(BufferedImage image, File screenshotFile)
+    public static ScreenshotEvent onScreenshot(BufferedImage image, File screenshotFile)
     {
-        ScreenshotEvent.Pre event = new ScreenshotEvent.Pre(image, screenshotFile);
+        ScreenshotEvent event = new ScreenshotEvent(image, screenshotFile);
         MinecraftForge.EVENT_BUS.post(event);
         return event;
-    }
-
-    public static void onScreenshotPost(File screenshotFile)
-    {
-        MinecraftForge.EVENT_BUS.post(new ScreenshotEvent.Post(screenshotFile));
     }
 
 }
--- src/main/java/net/minecraftforge/client/event/ScreenshotEvent.java
+++ src/main/java/net/minecraftforge/client/event/ScreenshotEvent.java
@@ -11,74 +11,50 @@ import java.io.File;
 /**
  * This event is fired before and after a screenshot is taken
  * This event is fired on the {@link net.minecraftforge.common.MinecraftForge#EVENT_BUS}
+ * This event is {@link Cancelable}
  *
  * {@link #screenshotFile} contains the file the screenshot will be/was saved to
+ * {@link #image} contains the {@link BufferedImage} that will be saved
+ * {@link #cancelReason} contains the {@link ITextComponent} to be used if the event is canceled
  */
+@Cancelable
 public class ScreenshotEvent extends Event
 {
 
-    protected File screenshotFile;
+    private BufferedImage image;
+    private File screenshotFile;
 
-    public File getScreenshotFile() {
+    private ITextComponent cancelReason = new TextComponentString("Screenshot canceled for unknown reason");
+
+    public ScreenshotEvent(BufferedImage image, File screenshotFile)
+    {
+        this.image = image;
+        this.screenshotFile = screenshotFile;
+    }
+
+    public BufferedImage getImage()
+    {
+        return image;
+    }
+
+    public File getScreenshotFile()
+    {
         return screenshotFile;
     }
 
-    /**
-     * This event is fired before a screenshot is taken
-     * This event is {@link Cancelable}
-     *
-     * {@link #screenshotFile} contains the file the screenshot will be/was saved to
-     * {@link #image} contains the {@link BufferedImage} containing the screenshot
-     * {@link #cancelReason} contains the {@link ITextComponent} to be used if the event is canceled
-     */
-    @Cancelable
-    public static class Pre extends ScreenshotEvent
+    public void setScreenshotFile(File screenshotFile)
     {
-        private final BufferedImage image;
-        private ITextComponent cancelReason = new TextComponentString("Screenshot canceled for unknown reason");
-
-        public Pre(BufferedImage image, File screenshotFile)
-        {
-            this.image = image;
-            this.screenshotFile = screenshotFile;
-        }
-
-        public void setScreenshotFile(File screenshotFile)
-        {
-            this.screenshotFile = screenshotFile;
-        }
-
-        public ITextComponent getCancelReason()
-        {
-            return cancelReason;
-        }
-
-        public void setCancelReason(ITextComponent cancelReason)
-        {
-            this.cancelReason = cancelReason;
-        }
-
-        public BufferedImage getImage()
-        {
-            return image;
-        }
-
+        this.screenshotFile = screenshotFile;
     }
 
-    /**
-     * This event is fired after a screenshot is taken
-     * This event is <b>not</b> {@link Cancelable}
-     *
-     * {@link #screenshotFile} contains the file the screenshot will be/was saved to
-     */
-    public static class Post extends ScreenshotEvent
+    public ITextComponent getCancelReason()
     {
+        return cancelReason;
+    }
 
-        public Post(File screenshotFile)
-        {
-            this.screenshotFile = screenshotFile;
-        }
-
+    public void setCancelReason(ITextComponent cancelReason)
+    {
+        this.cancelReason = cancelReason;
     }
 
 }
