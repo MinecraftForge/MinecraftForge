fa21f2b44cf9fee71b1d96ecbd655ab476f3dda1
iTitus <iTitus@users.noreply.github.com>
2016-04-13 20:53:25 +0200

    Fix forge:default-block transformation. (#2760)
    The first-person left-hand rotation was a little bit of.

--- src/main/java/net/minecraftforge/client/model/ForgeBlockStateV1.java
+++ src/main/java/net/minecraftforge/client/model/ForgeBlockStateV1.java
@@ -510,7 +510,7 @@ public class ForgeBlockStateV1 extends Marker
                             builder.put(TransformType.THIRD_PERSON_RIGHT_HAND, thirdperson);
                             builder.put(TransformType.THIRD_PERSON_LEFT_HAND,  leftify(thirdperson));
                             builder.put(TransformType.FIRST_PERSON_RIGHT_HAND, get(0, 0, 0, 0, 45, 0, 0.4f));
-                            builder.put(TransformType.FIRST_PERSON_LEFT_HAND,  get(0, 0, 0, 0, 255, 0, 0.4f));
+                            builder.put(TransformType.FIRST_PERSON_LEFT_HAND,  get(0, 0, 0, 0, 225, 0, 0.4f));
                             ret.state = Optional.<IModelState>of(new SimpleModelState(builder.build()));
                         }
                         // item/generated
