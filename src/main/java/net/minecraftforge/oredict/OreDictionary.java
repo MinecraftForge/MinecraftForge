eb9f2d393229bd3b2da0f04f6093a5ac04aeeab6
LexManos <LexManos@gmail.com>
2016-05-09 01:35:01 +0200

    Fixed map extension recipie. (cherry picked from commit a7d5820)

--- src/main/java/net/minecraftforge/oredict/OreDictionary.java
+++ src/main/java/net/minecraftforge/oredict/OreDictionary.java
@@ -327,7 +327,7 @@ public class OreDictionary
         // Search vanilla recipes for recipes to replace
         for(Object obj : recipes)
         {
-            if(obj instanceof ShapedRecipes)
+            if(obj.getClass() == ShapedRecipes.class)
             {
                 ShapedRecipes recipe = (ShapedRecipes)obj;
                 ItemStack output = recipe.getRecipeOutput();
@@ -342,7 +342,7 @@ public class OreDictionary
                     recipesToAdd.add(new ShapedOreRecipe(recipe, replacements));
                 }
             }
-            else if(obj instanceof ShapelessRecipes)
+            else if(obj.getClass() == ShapelessRecipes.class)
             {
                 ShapelessRecipes recipe = (ShapelessRecipes)obj;
                 ItemStack output = recipe.getRecipeOutput();
