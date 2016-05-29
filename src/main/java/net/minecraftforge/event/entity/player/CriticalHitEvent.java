2eeb37fc40145951691e8d6d4774618d3b919403
mcenderdragon <goto.minecraft@googlemail.com>
2016-05-29 11:46:28 +0200

    solved merge conflict

---
+++ src/main/java/net/minecraftforge/event/entity/player/CriticalHitEvent.java
@@ -0,0 +1,46 @@
+package net.minecraftforge.event.entity.player;
+
+import net.minecraftforge.fml.common.eventhandler.Cancelable;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+
+/**
+ * AttackEntityEvent is fired when a player is about to make a critical hit.<br>
+ * This event is fired whenever a player attacks an Entity in
+ * EntityPlayer#attackTargetEntityWithCurrentItem(Entity).<br>
+ * <br>
+ * {@link #livingTarget} contains the Entity that was damaged by the player. <br>
+ * <br>
+ * This event is {@link Cancelable}.<br>
+ * If this event is canceled, the player does not make a critical hit (but he still will attack!).<br>
+ * <br>
+ * This event does not have a result. {@link HasResult}<br>
+ * <br>
+ * This event is fired on the {@link MinecraftForge#EVENT_BUS}.
+ **/
+@Cancelable
+public class CriticalHitEvent extends AttackEntityEvent
+{
+    public final EntityLivingBase livingTarget;
+    private float damageModifier;
+    public CriticalHitEvent(EntityPlayer player, EntityLivingBase target)
+    {
+        super(player, target);
+        this.livingTarget = target;
+        this.damageModifier = 1.5F;
+    }
+    
+     /**
+     * This is how much damage you will deal. By default you deal 150% of you normal damage.
+     * If you set it to 0, then the particles are still generated but you don't do damage.
+    */
+    public void setDamageModifier(float mod)
+    {
+        this.damageModifier = mod;
+    }
+    
+    public float getDamageModifier()
+    {
+        return this.damageModifier;
+    }
+}
