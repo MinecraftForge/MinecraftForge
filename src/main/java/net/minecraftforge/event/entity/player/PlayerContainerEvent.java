8e3bc83c3b3d048af3f7ccd66dfd77ab7df138f9
luacs1998 <luacs1998@users.noreply.github.com>
2016-06-26 00:34:42 +0800

    Simple implementation of a Open/Close container event (#3017)

---
+++ src/main/java/net/minecraftforge/event/entity/player/PlayerContainerEvent.java
@@ -0,0 +1,53 @@
+/*
+ * Minecraft Forge
+ * Copyright (c) 2016.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package net.minecraftforge.event.entity.player;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
+
+public class PlayerContainerEvent extends PlayerEvent
+{
+    private final Container container;
+    public PlayerContainerEvent(EntityPlayer player, Container container)
+    {
+        super(player);
+        this.container = container;
+    }
+
+    public static class Open extends PlayerContainerEvent
+    {
+        public Open(EntityPlayer player, Container container)
+        {
+            super(player, container);
+        }
+    }
+    public static class Close extends PlayerContainerEvent
+    {
+        public Close(EntityPlayer player, Container container)
+        {
+            super(player, container);
+        }
+    }
+
+    public Container getContainer()
+    {
+        return container;
+    }
+}
