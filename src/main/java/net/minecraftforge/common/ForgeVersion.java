6ad65f4e6439611b0f4f5a7c0f0e65f65ad24f16
LexManos <LexManos@gmail.com>
2016-06-28 22:27:47 -0700

    Fixed MC version number... I could of SWORN we removed this in favor of a automated system...

--- src/main/java/net/minecraftforge/common/ForgeVersion.java
+++ src/main/java/net/minecraftforge/common/ForgeVersion.java
@@ -58,7 +58,7 @@ public class ForgeVersion
     //This number is incremented every time Jenkins builds Forge, and never reset. Should always be 0 in the repo code.
     public static final int buildVersion    = 0;
     // This is the minecraft version we're building for - used in various places in Forge/FML code
-    public static final String mcVersion = "1.10";
+    public static final String mcVersion = "1.10.2";
     // This is the MCP data version we're using
     public static final String mcpVersion = "9.32";
     @SuppressWarnings("unused")
