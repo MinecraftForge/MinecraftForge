6a7f871b8e91319dd3e382569b4d2c56dc57f817
Madsthunder <madsthunder109@gmail.com>
2016-07-31 18:00:43 -0400

    Update to return an existing object after deserializing.

--- src/main/java/net/minecraftforge/common/util/INBTSerializable.java
+++ src/main/java/net/minecraftforge/common/util/INBTSerializable.java
@@ -25,8 +25,8 @@ import net.minecraft.nbt.NBTBase;
  * An interface designed to unify various things in the Minecraft
  * code base that can be serialized to and from a NBT tag.
  */
-public interface INBTSerializable<T extends NBTBase>
+public interface INBTSerializable<N, T extends NBTBase>
 {
     T serializeNBT();
-    void deserializeNBT(T nbt);
+    N deserializeNBT(T nbt);
 }
