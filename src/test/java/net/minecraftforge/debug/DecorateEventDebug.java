f4e8804c8ac6e5c181c9ffea75a76ee7c01d44b1
LexManos <LexManos@gmail.com>
2016-08-02 15:55:18 -0700

    Restone binary compatibility with registry substitution rework. Modders please switch to using the provided RegistryBuilder to ensure more forward compatbility. Closes #3144

--- src/test/java/net/minecraftforge/debug/DecorateEventDebug.java
+++ src/test/java/net/minecraftforge/debug/DecorateEventDebug.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.eventhandler.Event.Result;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = DecorateEventDebug.MODID, name = DecorateEventDebug.NAME, version = DecorateEventDebug.VERSION)
-public class DecorateEventDebug 
+public class DecorateEventDebug
 {
 
     public static final String MODID = "decorateeventdebug";
@@ -17,14 +17,14 @@ public class DecorateEventDebug
     public static final String VERSION = "1.0.0";
 
     @EventHandler
-    public void init(FMLInitializationEvent event) 
+    public void init(FMLInitializationEvent event)
     {
-	    MinecraftForge.TERRAIN_GEN_BUS.register(this);
+        MinecraftForge.TERRAIN_GEN_BUS.register(this);
     }
 
     @SubscribeEvent
-    public void decorateEvent(DecorateBiomeEvent.Decorate event) 
+    public void decorateEvent(DecorateBiomeEvent.Decorate event)
     {
-	    event.setResult(Result.DENY);
+        event.setResult(Result.DENY);
     }
 }
