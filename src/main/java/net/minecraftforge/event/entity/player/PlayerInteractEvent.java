6e1e696caf677e76cc1b3250e167e0c9d0469dd0
LatvianModder <latvianmodder@gmail.com>
2016-08-03 16:31:16 +0300

    Removed unused import

--- src/main/java/net/minecraftforge/event/entity/player/PlayerInteractEvent.java
+++ src/main/java/net/minecraftforge/event/entity/player/PlayerInteractEvent.java
@@ -27,7 +27,6 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.IBlockAccess;
