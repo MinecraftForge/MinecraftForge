89eb8f067d783c1528eeba197377e825dd3b9617
James Mitchell <tehgeek@gmail.com>
2016-09-15 01:03:52 -0700

    Remove oredict for Bone Block recipe, stop white dye to bonemeal exploit (#3255)

--- src/main/java/net/minecraftforge/oredict/OreDictionary.java
+++ src/main/java/net/minecraftforge/oredict/OreDictionary.java
@@ -336,6 +336,7 @@ public class OreDictionary
             new ItemStack(Blocks.DARK_OAK_STAIRS),
             new ItemStack(Blocks.WOODEN_SLAB),
             new ItemStack(Blocks.GLASS_PANE),
+            new ItemStack(Blocks.field_189880_di), // Bone Block, to prevent conversion of dyes into bone meal.
             null //So the above can have a comma and we don't have to keep editing extra lines.
         };
 
