ce961d9078288c3a8ca41d4588351d2e20a52487
Steven Blom <atomicblom@binaryvibrance.net>
2016-09-18 22:05:24 +0930

    Changes to support custom registries

--- src/test/java/net/minecraftforge/debug/ObjectHolderTest.java
+++ src/test/java/net/minecraftforge/debug/ObjectHolderTest.java
@@ -4,21 +4,20 @@ import net.minecraft.block.Block;
 import net.minecraft.potion.Potion;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.common.registry.IForgeRegistry;
+import net.minecraftforge.fml.common.registry.IForgeRegistryEntry;
+import net.minecraftforge.fml.common.registry.RegistryBuilder;
 
-@Mod(modid= ObjectHolderTest.MODID)
+@Mod(modid= ObjectHolderTest.MODID, name = "ObjectHolderTests", version = "1.0")
 public class ObjectHolderTest
 {
-    public static final String MODID = "ObjectHolderTest";
-
-    @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-        Potion forge = new PotionForge(new ResourceLocation(MODID, "forgePotion"), false, 0xff00ff); // test automatic id distribution
-        GameRegistry.register(forge);
-    }
+    public static final String MODID = "objectholdertest";
+    public static IForgeRegistry registry;
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
@@ -30,14 +29,69 @@ public class ObjectHolderTest
         assert VanillaObjectHolder.air == null;
         //verified unexpected name should not have defaulted to AIR.
         assert VanillaObjectHolder.nonExistentBlock == null;
+        //verifies interfaces are supported
+        assert CustomRegistryObjectHolder.customEntryByInterface != null;
     }
 
-    protected class PotionForge extends Potion {
+    protected static class PotionForge extends Potion {
         protected PotionForge(ResourceLocation location, boolean badEffect, int potionColor) {
             super(badEffect, potionColor);
             setPotionName("potion." + location.getResourcePath());
             setRegistryName(location);
         }
+    }
+}
+
+class CustomRegistryEntry implements IForgeRegistryEntry<CustomRegistryEntry>
+{
+    private ResourceLocation name;
+
+    protected CustomRegistryEntry(ResourceLocation location) {
+        setRegistryName(location);
+    }
+
+    @Override
+    public CustomRegistryEntry setRegistryName(ResourceLocation name)
+    {
+        this.name = name;
+        return this;
+    }
+
+    @Override
+    public ResourceLocation getRegistryName()
+    {
+        return name;
+    }
+
+    @Override
+    public Class<? super CustomRegistryEntry> getRegistryType()
+    {
+        return CustomRegistryEntry.class;
+    }
+}
+
+@Mod.EventBusSubscriber
+class Registration {
+    @SubscribeEvent
+    public static void newRegistry(RegistryEvent.NewRegistry event) {
+        new RegistryBuilder()
+                .setType(CustomRegistryEntry.class)
+                .setName(new ResourceLocation("PotionTest"))
+                .create();
+    }
+
+    @SubscribeEvent
+    public static void registerPotions(RegistryEvent.Register<Potion> event) {
+        event.getRegistry().register(
+                new ObjectHolderTest.PotionForge(new ResourceLocation(ObjectHolderTest.MODID, "forgePotion"), false, 0xff00ff) // test automatic id distribution
+        );
+    }
+
+    @SubscribeEvent
+    public static void registerInterfaceRegistryForge(RegistryEvent.Register<CustomRegistryEntry> event) {
+        event.getRegistry().register(
+                new CustomRegistryEntry(new ResourceLocation(ObjectHolderTest.MODID, "customEntryByInterface"))
+        );
     }
 }
 
@@ -56,4 +110,11 @@ class ForgeObjectHolder
 {
     //Tests using subclasses for injections
     public static final ObjectHolderTest.PotionForge forgePotion = null;
+}
+
+@GameRegistry.ObjectHolder(ObjectHolderTest.MODID)
+class CustomRegistryObjectHolder
+{
+    //Tests whether interfaces can be used
+    public static final CustomRegistryEntry customEntryByInterface = null;
 }
