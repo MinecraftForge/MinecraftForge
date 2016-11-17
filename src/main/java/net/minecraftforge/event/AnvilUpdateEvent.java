5fbec1a2bcb2906ec4e2de38eb5b7c150f047fa3
mezz <tehgeek@gmail.com>
2016-11-17 00:33:40 -0800

    Fix nullability issues around ForgeHooks

--- src/main/java/net/minecraftforge/event/AnvilUpdateEvent.java
+++ src/main/java/net/minecraftforge/event/AnvilUpdateEvent.java
@@ -23,6 +23,8 @@ import net.minecraftforge.fml.common.eventhandler.Cancelable;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraft.item.ItemStack;
 
+import javax.annotation.Nonnull;
+
 /**
  * 
  * AnvilUpdateEvent is fired when a player places items in both the left and right slots of a anvil.
@@ -33,27 +35,34 @@ import net.minecraft.item.ItemStack;
 @Cancelable
 public class AnvilUpdateEvent extends Event
 {
+    @Nonnull
     private final ItemStack left;  // The left side of the input
+    @Nonnull
     private final ItemStack right; // The right side of the input
     private final String name;     // The name to set the item, if the user specified one.
+    @Nonnull
     private ItemStack output;      // Set this to set the output stack
     private int cost;              // The base cost, set this to change it if output != null
     private int materialCost; // The number of items from the right slot to be consumed during the repair. Leave as 0 to consume the entire stack.
 
-    public AnvilUpdateEvent(ItemStack left, ItemStack right, String name, int cost)
+    public AnvilUpdateEvent(@Nonnull ItemStack left, @Nonnull ItemStack right, String name, int cost)
     {
         this.left = left;
         this.right = right;
+        this.output = ItemStack.field_190927_a;
         this.name = name;
         this.setCost(cost);
         this.setMaterialCost(0);
     }
 
+    @Nonnull
     public ItemStack getLeft() { return left; }
+    @Nonnull
     public ItemStack getRight() { return right; }
     public String getName() { return name; }
+    @Nonnull
     public ItemStack getOutput() { return output; }
-    public void setOutput(ItemStack output) { this.output = output; }
+    public void setOutput(@Nonnull ItemStack output) { this.output = output; }
     public int getCost() { return cost; }
     public void setCost(int cost) { this.cost = cost; }
     public int getMaterialCost() { return materialCost; }
