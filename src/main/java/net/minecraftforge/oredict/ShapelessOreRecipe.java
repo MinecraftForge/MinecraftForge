bf090cb4c2f76dc4bd6f58f7cbcb6b3487407dae
Larsg310 <larsgerrits99@gmail.com>
2016-11-17 19:43:22 +0100

    Change OreDictionary over from "null" to the new ItemStack system. (#3400)

--- src/main/java/net/minecraftforge/oredict/ShapelessOreRecipe.java
+++ src/main/java/net/minecraftforge/oredict/ShapelessOreRecipe.java
@@ -34,15 +34,17 @@ import net.minecraft.util.NonNullList;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeHooks;
 
+import javax.annotation.Nonnull;
+
 public class ShapelessOreRecipe implements IRecipe
 {
-    protected ItemStack output = null;
-    protected ArrayList<Object> input = new ArrayList<Object>();
+    protected ItemStack output = ItemStack.field_190927_a;
+    protected NonNullList<Object> input = NonNullList.func_191196_a();
 
     public ShapelessOreRecipe(Block result, Object... recipe){ this(new ItemStack(result), recipe); }
     public ShapelessOreRecipe(Item  result, Object... recipe){ this(new ItemStack(result), recipe); }
 
-    public ShapelessOreRecipe(ItemStack result, Object... recipe)
+    public ShapelessOreRecipe(@Nonnull ItemStack result, Object... recipe)
     {
         output = result.copy();
         for (Object in : recipe)
@@ -108,13 +110,14 @@ public class ShapelessOreRecipe implements IRecipe
     @Override
     public boolean matches(InventoryCrafting var1, World world)
     {
-        ArrayList<Object> required = new ArrayList<Object>(input);
+        NonNullList<Object> required = NonNullList.func_191196_a();
+        required.addAll(input);
 
         for (int x = 0; x < var1.getSizeInventory(); x++)
         {
             ItemStack slot = var1.getStackInSlot(x);
 
-            if (slot != null)
+            if (!slot.func_190926_b())
             {
                 boolean inRecipe = false;
                 Iterator<Object> req = required.iterator();
@@ -161,7 +164,7 @@ public class ShapelessOreRecipe implements IRecipe
      * manipulate the values in this array as it will effect the recipe itself.
      * @return The recipes input vales.
      */
-    public ArrayList<Object> getInput()
+    public NonNullList<Object> getInput()
     {
         return this.input;
     }
