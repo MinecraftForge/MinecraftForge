42a7036393c9e77bc8c28e80bc8fb7ba6a1175b7
LexManos <LexManos@gmail.com>
2016-11-16 16:06:52 -0800

    Fix ItemStackHandler's constructor ignoring size. Closes #3386

--- src/main/java/net/minecraftforge/items/ItemStackHandler.java
+++ src/main/java/net/minecraftforge/items/ItemStackHandler.java
@@ -39,7 +39,7 @@ public class ItemStackHandler implements IItemHandler, IItemHandlerModifiable, I
 
     public ItemStackHandler(int size)
     {
-        stacks = NonNullList.func_191196_a();
+        stacks = NonNullList.func_191197_a(size, ItemStack.field_190927_a);
     }
 
     public ItemStackHandler(NonNullList<ItemStack> stacks)
