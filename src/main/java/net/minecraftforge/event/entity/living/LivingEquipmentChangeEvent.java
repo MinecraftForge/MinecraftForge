0d42c961a613216e8f8bc11e32f3c718bbb8eb74
TechnicianLP <TechnicianLP@users.noreply.github.com>
2016-11-27 12:42:55 +0100

    Added information about joining world

--- src/main/java/net/minecraftforge/event/entity/living/LivingEquipmentChangeEvent.java
+++ src/main/java/net/minecraftforge/event/entity/living/LivingEquipmentChangeEvent.java
@@ -28,6 +28,7 @@ import net.minecraftforge.fml.common.eventhandler.Cancelable;
 /**
  * {@link LivingEquipmentChangeEvent} is fired when the Equipment of a Entity changes. <br>
  * This event is fired whenever changes in Equipment are detected in {@link EntityLivingBase#onUpdate()}. <br>
+ * This also includes entities joining the World, as well as being cloned. <br>
  * This event is fired on server-side only. <br>
  * <br>
  * {@link #slot} contains the affected {@link EntityEquipmentSlot}. <br>
