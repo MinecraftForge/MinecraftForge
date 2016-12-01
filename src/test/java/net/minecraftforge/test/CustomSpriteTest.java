fab8761b010cabf5c56bdac65b8da5760a8b6c7e
liach <liach@users.noreply.github.com>
2016-11-30 18:01:49 -0800

    Adopted @mezz's test since I am a noob on client

--- src/test/java/net/minecraftforge/test/CustomSpriteTest.java
+++ src/test/java/net/minecraftforge/test/CustomSpriteTest.java
@@ -4,17 +4,20 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.resources.IResource;
 import net.minecraft.client.resources.IResourceManager;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
+import javax.annotation.Nonnull;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -22,12 +25,13 @@ import java.io.IOException;
 /**
  * Test for {@link TextureStitchEvent.Pre}.
  */
-@Mod(modid = CustomSpriteTest.MOD_ID, name = CustomSpriteTest.NAME)
+@Mod(modid = CustomSpriteTest.MOD_ID, name = CustomSpriteTest.NAME, version = "1.0")
 public class CustomSpriteTest
 {
     static final String MOD_ID = "custom_sprite_test";
     static final String NAME = "Custom sprite test";
 
+    @Mod.EventHandler
     public void init(FMLPreInitializationEvent event)
     {
         MinecraftForge.EVENT_BUS.register(this);
@@ -38,45 +42,51 @@ public class CustomSpriteTest
     @SubscribeEvent
     public void textureStitch(TextureStitchEvent.Pre event)
     {
-        event.getMap().setTextureEntry(DelegateSprite.make("bottom", "blocks/diamond_block"));
-        event.getMap().setTextureEntry(DelegateSprite.make("top", "blocks/tnt_side"));
+        DelegateSprite bottom = DelegateSprite.make("bottom", new ResourceLocation("textures/blocks/diamond_block.png"));
+        DelegateSprite top = DelegateSprite.make("top", new ResourceLocation("textures/blocks/tnt_side.png"));
+
+        TextureMap textureMap = event.getMap();
+        textureMap.setTextureEntry(bottom);
+        textureMap.setTextureEntry(top);
     }
 
     static final class DelegateSprite extends TextureAtlasSprite
     {
-        final ResourceLocation location;
         final ResourceLocation delegate;
 
         private DelegateSprite(ResourceLocation loc, ResourceLocation delegate)
         {
             super(loc.toString());
-            this.location = loc;
             this.delegate = delegate;
         }
 
-        static DelegateSprite make(String name, String delegate)
+        static DelegateSprite make(String name, ResourceLocation delegate)
         {
-            return new DelegateSprite(new ResourceLocation(MOD_ID, name), new ResourceLocation("minecraft", delegate));
+            return new DelegateSprite(new ResourceLocation(MOD_ID, name), delegate);
         }
 
         @Override
-        public boolean hasCustomLoader(IResourceManager manager, ResourceLocation location)
+        public boolean hasCustomLoader(@Nonnull IResourceManager manager, @Nonnull ResourceLocation location)
         {
-            return location.equals(this.location);
+            return true;
         }
 
         @Override
-        public boolean load(IResourceManager manager, ResourceLocation location)
+        public boolean load(@Nonnull IResourceManager manager, @Nonnull ResourceLocation location)
         {
             BufferedImage image;
             try
             {
-                image = ImageIO.read(manager.getResource(delegate).getInputStream());
+                IResource resource = manager.getResource(delegate);
+                image = ImageIO.read(resource.getInputStream());
             }
             catch (IOException ioe)
             {
-                return false;
+                FMLLog.getLogger().error("Could not find resource", ioe);
+                return true;
             }
+            this.width = image.getWidth();
+            this.height = image.getHeight();
             int[][] pixels = new int[Minecraft.getMinecraft().gameSettings.mipmapLevels + 1][];
             pixels[0] = new int[image.getWidth() * image.getHeight()];
             image.getRGB(0, 0, image.getWidth(), image.getHeight(), pixels[0], 0, image.getWidth());
