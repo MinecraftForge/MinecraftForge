6ffb7cb5a34cff5eecccbba067b5ecfc7216a27a
Mathe172 <lukas172@outlook.com>
2016-12-17 21:09:35 +0100

    Allow custom selector types to be defined
    Rerouted EntitySelector.matchEntities(ICommandSender, String, Class), .isSelector(String) and .matchesMultiplePlayers(String) over SelectorHandlerManager
    Added SelectorHandlerManger to handle calls to EntitySelector methods and choose appropriate SelectorHandler
    Added SelectorHandler
    The vanilla version of SelectorHandler uses the old implementations, which are renamed to *Default(...) and is registered for prefixes @p,@a,@r,@e and as fallback
    Adjusted forge.exc to correctly obfuscate parameters of the new *Default(...) methods
    Added test mod to demonstrate the capabilities
    Test mod registers the "self"-selector, which returns the entity executing the command (as @self)

---
+++ src/test/java/net/minecraftforge/test/SelectorHandlerTest.java
@@ -0,0 +1,50 @@
+package net.minecraftforge.test;
+
+import java.util.Collections;
+import java.util.List;
+
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.Entity;
+import net.minecraftforge.common.command.SelectorHandler;
+import net.minecraftforge.common.command.SelectorHandlerManager;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = "selectorhandlertest", name = "Selector Handler Test", version = "0.0.0")
+public class SelectorHandlerTest
+{
+    @EventHandler
+    public void init(final FMLInitializationEvent event)
+    {
+        SelectorHandlerManager.register(Handler.name, new Handler());
+    }
+
+    private static class Handler implements SelectorHandler
+    {
+        protected static final String name = "@self";
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T extends Entity> List<T> matchEntities(final ICommandSender sender, final String token, final Class<? extends T> targetClass) throws CommandException
+        {
+            final Entity senderEntity = sender.getCommandSenderEntity();
+            return senderEntity != null && targetClass.isAssignableFrom(senderEntity.getClass()) && name.equals(token)
+                ? Collections.singletonList((T) sender.getCommandSenderEntity())
+                : Collections.<T> emptyList();
+        }
+
+        @Override
+        public boolean matchesMultiplePlayers(final String selectorStr) throws CommandException
+        {
+            return false;
+        }
+
+        @Override
+        public boolean isSelector(final String selectorStr)
+        {
+            return name.equals(selectorStr);
+        }
+    }
+}
