45b97a4ffeab6d358f46f13685dbceb20a054194
Ben Staddon <quadraxis@hotmail.co.uk>
2016-12-22 22:12:55 +0000

    fix braces >_>

--- src/test/java/net/minecraftforge/test/IsBookEnchantableTest.java
+++ src/test/java/net/minecraftforge/test/IsBookEnchantableTest.java
@@ -33,7 +33,8 @@ public class IsBookEnchantableTest
         }
 
         @Override
-        public int getItemEnchantability() {
+        public int getItemEnchantability()
+        {
             return 15;
         }
 
