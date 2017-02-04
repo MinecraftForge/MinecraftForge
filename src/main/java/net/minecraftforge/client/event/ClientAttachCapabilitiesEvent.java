51303ee46c960f8d134bc0ccf4e51d8c60e5fbc3
elix-x <Elix-x@users.noreply.github.com>
2017-02-04 20:53:26 +0100

    Updated capabilities events to match new standard
    Added type params, using new constructors instead.

--- src/main/java/net/minecraftforge/client/event/ClientAttachCapabilitiesEvent.java
+++ src/main/java/net/minecraftforge/client/event/ClientAttachCapabilitiesEvent.java
@@ -22,99 +22,107 @@ package net.minecraftforge.client.event;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 
 /**
- * Fired whenever client object with Capabilities support {currently JSON Model Hierarchy)
- * is created. Allowing for the attachment of arbitrary capability providers.
+ * Fired whenever client object with Capabilities support (currently JSON Model Hierarchy) is created. Allowing for the attachment of arbitrary capability providers.
  *
- * Please note that as this is fired for ALL object creations efficient code is recommended.
- * And if possible use one of the sub-classes to filter your intended objects.
+ * Please note that as this is fired for ALL object creations efficient code is recommended. And if possible use one of the sub-classes to filter your intended objects.
  */
-public class ClientAttachCapabilitiesEvent extends AttachCapabilitiesEvent
+public class ClientAttachCapabilitiesEvent<T> extends AttachCapabilitiesEvent<T>
 {
 
-    public ClientAttachCapabilitiesEvent(Object obj)
+    public ClientAttachCapabilitiesEvent(Class<T> type, T obj)
     {
-        super(obj);
+        super(type, obj);
     }
-    
+
     /**
      * A version of the parent event which is only fired for VanillaModelWrappers.
      */
-    public static class VanillaModelWrapper extends ClientAttachCapabilitiesEvent
+    public static class VanillaModelWrapper extends ClientAttachCapabilitiesEvent<net.minecraftforge.client.model.ModelLoader.VanillaModelWrapper>
     {
         private final net.minecraftforge.client.model.ModelLoader.VanillaModelWrapper modelWrapper;
+
         public VanillaModelWrapper(net.minecraftforge.client.model.ModelLoader.VanillaModelWrapper modelWrapper)
         {
-            super(modelWrapper);
+            super(net.minecraftforge.client.model.ModelLoader.VanillaModelWrapper.class, modelWrapper);
             this.modelWrapper = modelWrapper;
         }
+
         public net.minecraftforge.client.model.ModelLoader.VanillaModelWrapper getModelWrapper()
         {
             return this.modelWrapper;
         }
     }
-    
+
     /**
      * A version of the parent event which is only fired for ItemLayerModels.
      */
-    public static class ItemLayerModel extends ClientAttachCapabilitiesEvent
+    public static class ItemLayerModel extends ClientAttachCapabilitiesEvent<net.minecraftforge.client.model.ItemLayerModel>
     {
         private final net.minecraftforge.client.model.ItemLayerModel modelWrapper;
+
         public ItemLayerModel(net.minecraftforge.client.model.ItemLayerModel modelWrapper)
         {
-            super(modelWrapper);
+            super(net.minecraftforge.client.model.ItemLayerModel.class, modelWrapper);
             this.modelWrapper = modelWrapper;
         }
+
         public net.minecraftforge.client.model.ItemLayerModel getModelWrapper()
         {
             return this.modelWrapper;
         }
     }
-    
+
     /**
      * A version of the parent event which is only fired for ModelBlocks.
      */
-    public static class ModelBlock extends ClientAttachCapabilitiesEvent
+    public static class ModelBlock extends ClientAttachCapabilitiesEvent<net.minecraft.client.renderer.block.model.ModelBlock>
     {
         private final net.minecraft.client.renderer.block.model.ModelBlock modelWrapper;
+
         public ModelBlock(net.minecraft.client.renderer.block.model.ModelBlock modelWrapper)
         {
-            super(modelWrapper);
+            super(net.minecraft.client.renderer.block.model.ModelBlock.class, modelWrapper);
             this.modelWrapper = modelWrapper;
         }
+
         public net.minecraft.client.renderer.block.model.ModelBlock getModelWrapper()
         {
             return this.modelWrapper;
         }
     }
-    
+
     /**
      * A version of the parent event which is only fired for BlockParts.
      */
-    public static class BlockPart extends ClientAttachCapabilitiesEvent
+    public static class BlockPart extends ClientAttachCapabilitiesEvent<net.minecraft.client.renderer.block.model.BlockPart>
     {
         private final net.minecraft.client.renderer.block.model.BlockPart modelWrapper;
+
         public BlockPart(net.minecraft.client.renderer.block.model.BlockPart modelWrapper)
         {
-            super(modelWrapper);
+            super(net.minecraft.client.renderer.block.model.BlockPart.class, modelWrapper);
             this.modelWrapper = modelWrapper;
         }
+
         public net.minecraft.client.renderer.block.model.BlockPart getModelWrapper()
         {
             return this.modelWrapper;
         }
     }
-    
+
     /**
      * A version of the parent event which is only fired for BlockPartFaces.
      */
-    public static class BlockPartFace extends ClientAttachCapabilitiesEvent
+    public static class BlockPartFace extends ClientAttachCapabilitiesEvent<net.minecraft.client.renderer.block.model.BlockPartFace>
     {
         private final net.minecraft.client.renderer.block.model.BlockPartFace modelWrapper;
+
         public BlockPartFace(net.minecraft.client.renderer.block.model.BlockPartFace modelWrapper)
         {
-            super(modelWrapper);
+            super(net.minecraft.client.renderer.block.model.BlockPartFace.class, modelWrapper);
             this.modelWrapper = modelWrapper;
         }
+
         public net.minecraft.client.renderer.block.model.BlockPartFace getModelWrapper()
         {
             return this.modelWrapper;
