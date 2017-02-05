807146faa586031b5b0e5f683ba4e37d50e22e4a
LogicTechCorp <logictechcorp@gmail.com>
2017-02-04 21:26:07 -0700

    Added in UseShovelEvent

---
+++ src/main/java/net/minecraftforge/event/entity/player/UseShovelEvent.java
@@ -0,0 +1,72 @@
+/*
+ * Minecraft Forge
+ * Copyright (c) 2016.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package net.minecraftforge.event.entity.player;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.eventhandler.Cancelable;
+import net.minecraftforge.fml.common.eventhandler.Event;
+
+import javax.annotation.Nonnull;
+
+/**
+ * This event is fired when a player attempts to use a Shovel on a block, it
+ * can be canceled to completely prevent any further processing.
+ *
+ * You can also set the result to ALLOW to mark the event as processed
+ * and damage the shovel.
+ *
+ * setResult(ALLOW) is the same as the old setHandled();
+ */
+@Cancelable
+@Event.HasResult
+public class UseShovelEvent extends PlayerEvent
+{
+
+    private final ItemStack current;
+    private final World world;
+    private final BlockPos pos;
+
+    public UseShovelEvent(EntityPlayer player, @Nonnull ItemStack current, World world, BlockPos pos)
+    {
+        super(player);
+        this.current = current;
+        this.world = world;
+        this.pos = pos;
+    }
+
+    @Nonnull
+    public ItemStack getCurrent()
+    {
+        return current;
+    }
+
+    public World getWorld()
+    {
+        return world;
+    }
+
+    public BlockPos getPos()
+    {
+        return pos;
+    }
+}
