bd4fe5475339c6063c9c7188e50fdbddb6c4f3bb
ichttt <ichttt@users.noreply.github.com>
2017-02-24 02:15:11 +0100

    Improve exception messages during initialization.

--- src/main/java/net/minecraftforge/fml/client/GuiModsMissing.java
+++ src/main/java/net/minecraftforge/fml/client/GuiModsMissing.java
@@ -19,69 +19,19 @@
 
 package net.minecraftforge.fml.client;
 
-import java.awt.*;
-import java.io.File;
-import java.io.IOException;
-
-import net.minecraft.client.gui.GuiButton;
-import net.minecraft.client.gui.GuiErrorScreen;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.util.text.TextFormatting;
-import net.minecraftforge.fml.common.FMLLog;
-import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.MissingModsException;
 import net.minecraftforge.fml.common.versioning.ArtifactVersion;
 import net.minecraftforge.fml.common.versioning.DefaultArtifactVersion;
-import org.apache.logging.log4j.Level;
 
-public class GuiModsMissing extends GuiErrorScreen
+public class GuiModsMissing extends GuiErrorBase
 {
-    private File minecraftDir = new File(Loader.instance().getConfigDir().getParent());
-    private File clientLog = new File(minecraftDir, "logs/fml-client-latest.log");
     private MissingModsException modsMissing;
 
     public GuiModsMissing(MissingModsException modsMissing)
     {
-        super(null,null);
         this.modsMissing = modsMissing;
-    }
-
-    @Override
-    public void initGui()
-    {
-        super.initGui();
-        this.buttonList.clear();
-        this.buttonList.add(new GuiButton(1, 50, this.height - 38, this.width/2 -55, 20, I18n.format("fml.button.open.mods.folder")));
-        String openFileText = I18n.format("fml.button.open.file", clientLog.getName());
-        this.buttonList.add(new GuiButton(2, this.width / 2 + 5, this.height - 38, this.width / 2 - 55, 20, openFileText));
-    }
-
-    @Override
-    protected void actionPerformed(GuiButton button) throws IOException
-    {
-        if (button.id == 1)
-        {
-            try
-            {
-                File modsDir = new File(minecraftDir, "mods");
-                Desktop.getDesktop().open(modsDir);
-            }
-            catch (Exception e)
-            {
-                FMLLog.log(Level.ERROR, e, "Problem opening mods folder");
-            }
-        }
-        else if (button.id == 2)
-        {
-            try
-            {
-                Desktop.getDesktop().open(clientLog);
-            }
-            catch (Exception e)
-            {
-                FMLLog.log(Level.ERROR, e, "Problem opening log file " + clientLog);
-            }
-        }
     }
 
     @Override
@@ -108,15 +58,11 @@ public class GuiModsMissing extends GuiErrorScreen
                     continue;
                 }
             }
-            this.drawCenteredString(this.fontRendererObj, String.format("%s : %s", v.getLabel(), v.getRangeString()), this.width / 2, offset, 0xEEEEEE);
+            this.drawCenteredString(this.fontRendererObj, String.format(TextFormatting.BOLD + "%s" + TextFormatting.RESET + " : %s", v.getLabel(), v.getRangeString()), this.width / 2, offset, 0xEEEEEE);
         }
         offset+=20;
-        String seeLogText = I18n.format("fml.messages.mod.missing.dependencies.see.log", clientLog.getName());
+        String seeLogText = I18n.format("fml.messages.mod.missing.dependencies.see.log", GuiErrorBase.clientLog.getName());
         this.drawCenteredString(this.fontRendererObj, seeLogText, this.width / 2, offset, 0xFFFFFF);
-
-        for (int i = 0; i < this.buttonList.size(); ++i)
-        {
-            this.buttonList.get(i).drawButton(this.mc, mouseX, mouseY);
-        }
+        super.drawScreen(mouseX, mouseY, partialTicks);
     }
 }
