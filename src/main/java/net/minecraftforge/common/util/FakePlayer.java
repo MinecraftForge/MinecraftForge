646a8977d6a78962519a8d1b5f4cb9a3928007e3
diesieben07 <diesieben07@gmail.com>
2017-04-08 01:52:52 +0200

    Ignore calls to sendMessage for fake players. Fixes #3796. (#3798)

--- src/main/java/net/minecraftforge/common/util/FakePlayer.java
+++ src/main/java/net/minecraftforge/common/util/FakePlayer.java
@@ -45,6 +45,7 @@ public class FakePlayer extends EntityPlayerMP
     @Override public Vec3d getPositionVector(){ return new Vec3d(0, 0, 0); }
     @Override public boolean canUseCommand(int i, String s){ return false; }
     @Override public void sendStatusMessage(ITextComponent chatComponent, boolean actionBar){}
+    @Override public void sendMessage(ITextComponent component) {}
     @Override public void addStat(StatBase par1StatBase, int par2){}
     @Override public void openGui(Object mod, int modGuiId, World world, int x, int y, int z){}
     @Override public boolean isEntityInvulnerable(DamageSource source){ return true; }
