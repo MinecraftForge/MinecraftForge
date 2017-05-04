8f6a16166d48c4e13ed56ef2ce38911f326b8f4a
PhiPro95 <PProvenzano@web.de>
2017-05-04 19:31:27 +0200

    Fixed lighting bugs caused inside Chunk.setBlockState
    Added LightingHooks
    Skylight for sections is now handled by LightingHooks instead of
    Chunk.generateSkylightMap
    Chunk.setBlockState no longer suppresses call to relightBlock when a new
    section is created

---
+++ src/main/java/net/minecraftforge/common/lighting/LightingHooks.java
@@ -0,0 +1,48 @@
+/*
+ * Minecraft Forge
+ * Copyright (c) 2016.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package net.minecraftforge.common.lighting;
+
+import net.minecraft.world.EnumSkyBlock;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.storage.ExtendedBlockStorage;
+
+public class LightingHooks
+{
+    public static void initSkylightForSection(final World world, final Chunk chunk, final ExtendedBlockStorage section)
+    {
+        if (world.provider.hasSkyLight())
+        {
+            for (int x = 0; x < 16; ++x)
+            {
+                for (int z = 0; z < 16; ++z)
+                {
+                    if (chunk.getHeightValue(x, z) <= section.getYLocation())
+                    {
+                        for (int y = 0; y < 16; ++y)
+                        {
+                            section.setExtSkylightValue(x, y, z, EnumSkyBlock.SKY.defaultLightValue);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
