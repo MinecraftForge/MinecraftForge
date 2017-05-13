c4c6c83ae702e5a90bc71ff1a09e2544b446a57c
Nullcaller <nullcaller@gmail.com>
2017-05-13 20:02:25 +0300

    Specify a side

--- src/main/java/net/minecraftforge/common/IParticleColorControlling.java
+++ src/main/java/net/minecraftforge/common/IParticleColorControlling.java
@@ -5,5 +5,6 @@ import net.minecraft.util.math.BlockPos;
 
 public interface IParticleColorControlling 
 {
+    @SideOnly(Side.CLIENT)
     public boolean preventColoring(IBlockState blockState, BlockPos pos);
 }
