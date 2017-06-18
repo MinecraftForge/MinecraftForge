c36abceb46321285a7a3899d74bd3bf0c81d9710
Jaredlll08 <jaredlll08@gmail.com>
2017-06-18 16:08:01 +0200

    Fixed requests changes

--- src/test/java/net/minecraftforge/debug/FuelHandlerTest.java
+++ src/test/java/net/minecraftforge/debug/FuelHandlerTest.java
@@ -1,16 +1,14 @@
 package net.minecraftforge.debug;
 
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.fml.common.*;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
-import java.util.OptionalInt;
 
 /**
  * Debug mod that sets the burn time of every item to 0
  */
-@Mod(modid = "fuelhandlertest", name = "Guel Handler Test", version = "0.0.0")
+@Mod(modid = "fuelhandlertest", name = "Fuel Handler Test", version = "0.0.0", acceptableRemoteVersions = "*")
 public class FuelHandlerTest {
     
     @Mod.EventHandler
