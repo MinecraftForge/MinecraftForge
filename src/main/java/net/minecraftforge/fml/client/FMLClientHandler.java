5bba2e89f0421d1159166f6749acc3e05cb44218
mezz <tehgeek@gmail.com>
2017-06-19 00:00:12 -0700

    Remove the call to client refreshResources during loading

--- src/main/java/net/minecraftforge/fml/client/FMLClientHandler.java
+++ src/main/java/net/minecraftforge/fml/client/FMLClientHandler.java
@@ -361,8 +361,6 @@ public class FMLClientHandler implements IFMLSidedHandler
             return;
         }
 
-        // Reload resources
-        client.refreshResources();
         RenderingRegistry.loadEntityRenderers(Minecraft.getMinecraft().getRenderManager().entityRenderMap);
         guiFactories = HashBiMap.create();
         for (ModContainer mc : Loader.instance().getActiveModList())
