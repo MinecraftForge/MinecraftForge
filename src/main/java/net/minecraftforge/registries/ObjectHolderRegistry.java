84fe03c65f701e1b2de2e3c8dc640b71c851e50e
Marvin Rösch <marvinroesch99@gmail.com>
2017-06-25 17:03:26 +0200

    Start transition of ObjectHolder, not yet functional

--- src/main/java/net/minecraftforge/registries/ObjectHolderRegistry.java
+++ src/main/java/net/minecraftforge/registries/ObjectHolderRegistry.java
@@ -27,7 +27,9 @@ import java.util.Set;
 
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.FMLLog;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.ModContainer;
 import net.minecraftforge.fml.common.discovery.ASMDataTable;
 import net.minecraftforge.fml.common.discovery.ASMDataTable.ASMData;
 import net.minecraftforge.fml.common.registry.GameRegistry;
