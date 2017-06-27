77ec1f9b6971eb3505963c5efa4e606fe3daee94
covers1624 <laughlan.cov@internode.on.net>
2017-06-27 18:02:08 +0930

    FIx formating and minor derp.

--- src/test/java/net/minecraftforge/debug/FogColorInsideMaterialTest.java
+++ src/test/java/net/minecraftforge/debug/FogColorInsideMaterialTest.java
@@ -24,7 +24,8 @@ import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
 
 @EventBusSubscriber
 @Mod (modid = FogColorInsideMaterialTest.MOD_ID, name = "FogColor inside material debug.", version = "1.0", acceptableRemoteVersions = "*")
-public class FogColorInsideMaterialTest {
+public class FogColorInsideMaterialTest
+{
 
     public static final String MOD_ID = "fogcolorinsidematerialtest";
 
@@ -34,10 +35,13 @@ public class FogColorInsideMaterialTest {
     public static final Item FLUID_ITEM = null;
 
     @SubscribeEvent
-    public static void registerBlocks(RegistryEvent.Register<Block> event) {
-        Block fluid = new BlockFluidClassic(FluidRegistry.WATER, Material.WATER) {
+    public static void registerBlocks(RegistryEvent.Register<Block> event)
+    {
+        Block fluid = new BlockFluidClassic(FluidRegistry.WATER, Material.WATER)
+        {
             @Override
-            public Vec3d getFogColor(World world, BlockPos pos, IBlockState state, Entity entity, Vec3d originalColor, float partialTicks) {
+            public Vec3d getFogColor(World world, BlockPos pos, IBlockState state, Entity entity, Vec3d originalColor, float partialTicks)
+            {
                 return new Vec3d(0.6F, 0.1F, 0.0F);
             }
         };
@@ -48,18 +52,22 @@ public class FogColorInsideMaterialTest {
     }
 
     @SubscribeEvent
-    public static void registerItems(RegistryEvent.Register<Item> event) {
+    public static void registerItems(RegistryEvent.Register<Item> event)
+    {
         event.getRegistry().register(new ItemBlock(FLUID_BLOCK).setRegistryName(FLUID_BLOCK.getRegistryName()));
     }
 
     @SubscribeEvent
-    public static void registerModels(ModelRegistryEvent event) {
+    public static void registerModels(ModelRegistryEvent event)
+    {
         ModelResourceLocation fluidLocation = new ModelResourceLocation(MOD_ID.toLowerCase() + ":test_fluid", "fluid");
         ModelLoader.registerItemVariants(FLUID_ITEM);
         ModelLoader.setCustomMeshDefinition(FLUID_ITEM, stack -> fluidLocation);
-        ModelLoader.setCustomStateMapper(FLUID_BLOCK, new StateMapperBase() {
+        ModelLoader.setCustomStateMapper(FLUID_BLOCK, new StateMapperBase()
+        {
             @Override
-            protected ModelResourceLocation getModelResourceLocation(IBlockState state) {
+            protected ModelResourceLocation getModelResourceLocation(IBlockState state)
+            {
                 return fluidLocation;
             }
         });
