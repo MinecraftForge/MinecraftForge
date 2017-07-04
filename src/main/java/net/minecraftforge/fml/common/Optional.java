95ba6b80a28ac8bda9cd9a155c76817130db023d
Thanh-Phuong <python0429@gmail.com>
2017-07-04 17:47:24 -0400

    Add import statement

--- src/main/java/net/minecraftforge/fml/common/Optional.java
+++ src/main/java/net/minecraftforge/fml/common/Optional.java
@@ -20,6 +20,7 @@
 package net.minecraftforge.fml.common;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
