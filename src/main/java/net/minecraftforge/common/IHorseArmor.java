b414172dd3b0ca5b2261c50f85570183ae5fbc51
Leviathan143 <Leviathan143@users.noreply.github.com>
2017-07-17 18:41:16 +1200

    Use imports instead of fully qualified names in Forge classes

--- src/main/java/net/minecraftforge/common/IHorseArmor.java
+++ src/main/java/net/minecraftforge/common/IHorseArmor.java
@@ -1,5 +1,7 @@
 package net.minecraftforge.common;
 
+import net.minecraft.entity.passive.EntityHorse;
+import net.minecraft.entity.passive.HorseArmorType;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
@@ -9,12 +11,12 @@ public interface IHorseArmor
      * @param stack - The armor stack
      * @return The HorseArmorType of the armor stack 
      */
-    public net.minecraft.entity.passive.HorseArmorType getHorseArmorType(ItemStack stack);
+    public HorseArmorType getHorseArmorType(ItemStack stack);
     
     /**
      * @param world - The world the horse is in
      * @param horse - The horse wearing the armor
      * @param itemStack - the armor itemstack
      */
-    public void onHorseArmorTick(World world, net.minecraft.entity.passive.EntityHorse horse, ItemStack itemStack);
+    public void onHorseArmorTick(World world, EntityHorse horse, ItemStack itemStack);
 }
