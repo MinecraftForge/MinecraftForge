2202c786719a254b273134c45ebb57ec1d42f8f1
CreativeMD <Creative-Multimedia-Design@gmx.de>
2017-07-26 14:57:53 +0200

    Fixed formatting issues

--- src/main/java/net/minecraftforge/common/util/ChunkedTileEntityList.java
+++ src/main/java/net/minecraftforge/common/util/ChunkedTileEntityList.java
@@ -1,3 +1,21 @@
+/*
+ * Minecraft Forge
+ * Copyright (c) 2017.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 package net.minecraftforge.common.util;
 
 import java.util.ArrayList;
@@ -11,9 +29,9 @@ import java.util.Set;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
-import net.minecraft.world.chunk.Chunk;
 
-public class ChunkedTileEntityList {
+public class ChunkedTileEntityList
+{
 	
 	private static ChunkPos getChunkPos(TileEntity te)
 	{
