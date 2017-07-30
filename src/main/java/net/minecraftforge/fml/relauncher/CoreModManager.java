24eae384f6fcee30f4d764667b9d5bcc73ddc296
ChristoCoder <ChristoCoder@users.noreply.github.com>
2017-07-30 10:47:38 -0400

    Fix typo in CoreModManager

--- src/main/java/net/minecraftforge/fml/relauncher/CoreModManager.java
+++ src/main/java/net/minecraftforge/fml/relauncher/CoreModManager.java
@@ -603,9 +603,9 @@ public class CoreModManager {
                 }
                 else if (deobfuscatedEnvironment && location == null) // This is probably a mod dev workspace
                 {
-                    FMLLog.log.info("Ignoring missing certificate for coremod {} ({}), as this is a probably dev workspace", coreModName, coreModClass);
+                    FMLLog.log.info("Ignoring missing certificate for coremod {} ({}), as this is probably a dev workspace", coreModName, coreModClass);
                 }
-                else // This is a probably a normal minecraft workspace - log at warn
+                else // This is probably a a normal minecraft workspace - log at warn
                 {
                     FMLLog.log.warn("The coremod {} ({}) is not signed!", coreModName, coreModClass);
                 }
