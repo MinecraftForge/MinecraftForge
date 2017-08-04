f73c7cb764713c3c76077dd84c1d6a0bc8df446c
LexManos <LexManos@gmail.com>
2017-08-03 17:30:43 -0700

    1.12.1 Update, Not much modder facing changes, 1.12 mods should load and run fine on this version.

--- src/main/java/net/minecraftforge/fml/common/FMLModContainer.java
+++ src/main/java/net/minecraftforge/fml/common/FMLModContainer.java
@@ -264,6 +264,8 @@ public class FMLModContainer implements ModContainer
                 mcVersionString = "[1.9.4,1.10.2]";
         if ("[1.11]".equals(mcVersionString))
             mcVersionString = "[1.11,1.11.2]";
+        if ("[1.12]".equals(mcVersionString))
+            mcVersionString = "[1.12,1.12.1]";
         if (!Strings.isNullOrEmpty(mcVersionString))
         {
             minecraftAccepted = VersionParser.parseRange(mcVersionString);
