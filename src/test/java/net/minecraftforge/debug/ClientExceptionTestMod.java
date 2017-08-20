78fba2ac85a89484902517d2f71f7c277fede406
darkevilmac <bk1325@gmail.com>
2017-08-19 19:26:03 -0700

    Minor changes to the client exception test mod.

--- src/test/java/net/minecraftforge/debug/ClientExceptionTestMod.java
+++ src/test/java/net/minecraftforge/debug/ClientExceptionTestMod.java
@@ -6,10 +6,8 @@ import net.minecraftforge.fml.client.CustomModLoadingErrorDisplayException;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
-@Mod(modid = "clientexceptiontest", version = "0", name = "Client Exception Test")
+@Mod(modid = "clientexceptiontest", version = "0", name = "Client Exception Test", clientSideOnly = true)
 public class ClientExceptionTestMod
 {
 
@@ -18,44 +16,36 @@ public class ClientExceptionTestMod
     private static String LOREM_IPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 
     @Mod.EventHandler
-    @SideOnly(Side.CLIENT)
     public void onPreInit(FMLPreInitializationEvent e)
     {
         if (ENABLE_PREINIT)
         {
-            throw new CustomModLoadingErrorDisplayException("Custom Test Exception", new RuntimeException("Thrown in Pre-Init"))
-            {
-                @Override
-                public void initGui(GuiErrorScreen parent, FontRenderer fontRenderer) {}
-
-                @Override
-                public void drawScreen(GuiErrorScreen parent, FontRenderer fontRenderer, int mouseRelX, int mouseRelY, float tickTime)
-                {
-                    parent.drawCenteredString(parent.mc.fontRenderer, "Custom Test Exception", parent.width / 2, 90, 16777215);
-                    parent.drawCenteredString(parent.mc.fontRenderer, LOREM_IPSUM, parent.width / 2, 110, 16777215);
-                }
-            };
+            throwException("Thrown in Pre-Init");
         }
     }
 
     @Mod.EventHandler
-    @SideOnly(Side.CLIENT)
     public void onInit(FMLInitializationEvent e)
     {
         if (ENABLE_INIT)
         {
-            throw new CustomModLoadingErrorDisplayException("Custom Test Exception", new RuntimeException("Thrown in Initialization"))
-            {
-                @Override
-                public void initGui(GuiErrorScreen parent, FontRenderer fontRenderer) {}
-
-                @Override
-                public void drawScreen(GuiErrorScreen parent, FontRenderer fontRenderer, int mouseRelX, int mouseRelY, float tickTime)
-                {
-                    parent.drawCenteredString(parent.mc.fontRenderer, "Custom Test Exception", parent.width / 2, 90, 16777215);
-                    parent.drawCenteredString(parent.mc.fontRenderer, LOREM_IPSUM, parent.width / 2, 110, 16777215);
-                }
-            };
+            throwException("Thrown in Init");
         }
+    }
+
+    private void throwException(String runtimeMessage)
+    {
+        throw new CustomModLoadingErrorDisplayException("Custom Test Exception", new RuntimeException(runtimeMessage))
+        {
+            @Override
+            public void initGui(GuiErrorScreen parent, FontRenderer fontRenderer) {}
+
+            @Override
+            public void drawScreen(GuiErrorScreen parent, FontRenderer fontRenderer, int mouseRelX, int mouseRelY, float tickTime)
+            {
+                parent.drawCenteredString(parent.mc.fontRenderer, "Custom Test Exception", parent.width / 2, 90, 16777215);
+                parent.drawCenteredString(parent.mc.fontRenderer, LOREM_IPSUM, parent.width / 2, 110, 16777215);
+            }
+        };
     }
 }
