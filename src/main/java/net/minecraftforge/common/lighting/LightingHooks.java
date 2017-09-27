f874d5b76093283fd9e6ccc33fdbf834a0463ff9
PhiPro95 <pprovenzano@web.de>
2017-09-28 01:55:19 +0200

    Initialize blocklight properly
    Cleanup Chunk.checkLight()

---
+++ src/main/java/net/minecraftforge/common/lighting/LightingHooks.java
@@ -0,0 +1,67 @@
+/*
+ * Minecraft Forge
+ * Copyright (c) 2016.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package net.minecraftforge.common.lighting;
+
+import net.minecraft.util.math.BlockPos.PooledMutableBlockPos;
+import net.minecraft.world.EnumSkyBlock;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.storage.ExtendedBlockStorage;
+
+public class LightingHooks
+{
+    public static void initChunkLighting(final Chunk chunk, final World world)
+    {
+        final int xBase = chunk.xPosition << 4;
+        final int zBase = chunk.zPosition << 4;
+
+        final PooledMutableBlockPos pos = PooledMutableBlockPos.retain(xBase, 0, zBase);
+
+        if (world.isAreaLoaded(pos.add(-16, 0, -16), pos.add(31, 255, 31), false))
+        {
+            final ExtendedBlockStorage[] extendedBlockStorage = chunk.getBlockStorageArray();
+
+            for (int j = 0; j < extendedBlockStorage.length; ++j)
+            {
+                if (extendedBlockStorage[j] == Chunk.NULL_BLOCK_STORAGE)
+                    continue;
+
+                for (int x = 0; x < 16; ++x)
+                {
+                    for (int z = 0; z < 16; ++z)
+                    {
+                        for (int y = j << 4; y < (j + 1) << 4; ++y)
+                        {
+                            if (chunk.getBlockState(x, y, z).getLightValue(world, pos.setPos(xBase + x, y, zBase + z)) > 0)
+                                world.checkLightFor(EnumSkyBlock.BLOCK, pos);
+                        }
+                    }
+                }
+            }
+
+            if (world.provider.hasSkyLight())
+                chunk.setSkylightUpdated();
+
+            chunk.setLightPopulated(true);
+        }
+
+        pos.release();
+    }
+}
