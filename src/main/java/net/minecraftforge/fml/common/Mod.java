020c9793d093c93f1e0f6ff59eb90f61b9060915
mezz <tehgeek@gmail.com>
2017-10-02 21:37:35 -0700

    Fix up javadoc

--- src/main/java/net/minecraftforge/fml/common/Mod.java
+++ src/main/java/net/minecraftforge/fml/common/Mod.java
@@ -101,8 +101,9 @@ public @interface Mod
     /**
      * A dependency string for this mod, which specifies which mod(s) it depends on in order to run.
      *
-     * A dependency string can start with a combination of these prefixes, separated by "-":
+     * A dependency string must start with a combination of these prefixes, separated by "-":
      *     [before, after], [required], [client, server]
+     *     At least one "before", "after", or "required" must be specified.
      * Then ":" and the mod id.
      * Then a version range should be specified for the mod by adding "@" and the version range.
      *     The version range format is described in the javadoc here:
@@ -115,22 +116,22 @@ public @interface Mod
      * Example:
      *     Our example mod:
      *      * depends on Forge and uses new features that were introduced in Forge version 14.21.1.2395
-     *         "forge@[14.21.1.2395,);"
+     *         "required:forge@[14.21.1.2395,);"
      *      * is a dedicated addon to mod1 and has to have its event handlers run after mod1's are run,
      *         "required-after:mod1;"
      *      * has optional integration with mod2 which depends on features introduced in mod2 version 4.7.0,
-     *         "mod2@[4.7.0,);"
+     *         "after:mod2@[4.7.0,);"
      *      * depends on a client-side-only rendering library called rendermod
      *         "required-client:rendermod;"
      *
      *     The full dependencies string is all of those combined:
-     *         "required:forge@[14.21.1.2395,);required-after:mod1;mod2@[4.7.0,);required-client:rendermod;"
+     *         "required:forge@[14.21.1.2395,);required-after:mod1;after:mod2@[4.7.0,);required-client:rendermod;"
      *
      *     This will stop the game and display an error message if any of these is true:
      *         The installed forge is too old,
      *         mod1 is missing,
-     *         rendermod is missing on the client,
-     *         an old version of mod2 is present.
+     *         an old version of mod2 is present,
+     *         rendermod is missing on the client.
      */
     String dependencies() default "";
 
