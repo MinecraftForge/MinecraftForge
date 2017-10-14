7b9887d528d3e6c1ff2f42250dca5d760a3af5f1
jabelar <julian_abelar@hotmail.com>
2017-10-13 22:54:32 -0700

    disabled the test mod

--- src/test/java/net/minecraftforge/debug/FluidFogColorTest.java
+++ src/test/java/net/minecraftforge/debug/FluidFogColorTest.java
@@ -1,19 +1,17 @@
 package net.minecraftforge.debug;
 
-import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
-
 @net.minecraftforge.fml.common.Mod(modid=FluidFogColorTest.MODID, name="Test Mod", version="1.0.0", acceptedMinecraftVersions="*") 
 @net.minecraftforge.fml.common.Mod.EventBusSubscriber 
 public class FluidFogColorTest{
-	static final boolean ENABLED = true; // enable mod
-	static int color = 0xFFd742f4; // change value for testing
+	static final boolean ENABLED = false; // <-- enable mod
+	static int color = 0xFFd742f4;        // <-- change value for testing
 
 	static final String MODID = "fluidfogcolor";
 	static final net.minecraft.util.ResourceLocation RL = new net.minecraft.util.ResourceLocation(MODID, "slime");
     static {if (ENABLED) {net.minecraftforge.fluids.FluidRegistry.enableUniversalBucket();}}
 	public static final MF SF = new MF("slime", new net.minecraft.util.ResourceLocation(MODID,"slime_still"), new net.minecraft.util.ResourceLocation(MODID, "slime_flow"));
 	public static class MF extends net.minecraftforge.fluids.Fluid{public MF(String name, net.minecraft.util.ResourceLocation st, net.minecraft.util.ResourceLocation flowing) {super(name, st, flowing);} @Override public int getColor() {return color;}}
-    @ObjectHolder("slime") public static final net.minecraftforge.fluids.BlockFluidBase SB = null;
+    @net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder("slime") public static final net.minecraftforge.fluids.BlockFluidBase SB = null;
     @net.minecraftforge.fml.common.Mod.EventHandler public void ev(net.minecraftforge.fml.common.event.FMLPreInitializationEvent ev) {if (ENABLED) {net.minecraftforge.fluids.FluidRegistry.registerFluid(SF); net.minecraftforge.fluids.FluidRegistry.addBucketForFluid(SF);}}
     @net.minecraftforge.fml.common.eventhandler.SubscribeEvent public static void evb(final net.minecraftforge.event.RegistryEvent.Register<net.minecraft.block.Block> ev) {if (ENABLED) ev.getRegistry().register((new net.minecraftforge.fluids.BlockFluidClassic(SF, net.minecraft.block.material.Material.WATER)).setRegistryName(RL).setUnlocalizedName(RL.toString()));}
     @net.minecraftforge.fml.common.eventhandler.SubscribeEvent public static void evib(final net.minecraftforge.event.RegistryEvent.Register<net.minecraft.item.Item> ev) {if (ENABLED) {ev.getRegistry().register((new net.minecraft.item.ItemBlock(SB)).setRegistryName(RL));}} 
