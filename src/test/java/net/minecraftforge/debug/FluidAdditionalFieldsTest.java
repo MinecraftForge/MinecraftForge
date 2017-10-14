28cf0710d9552ccf30b9f86e560acbbbc7ac0538
jabelar <julian_abelar@hotmail.com>
2017-10-14 11:25:06 -0700

    Expanded the imports in the test mod as requested.

--- src/test/java/net/minecraftforge/debug/FluidAdditionalFieldsTest.java
+++ src/test/java/net/minecraftforge/debug/FluidAdditionalFieldsTest.java
@@ -1,18 +1,40 @@
 package net.minecraftforge.debug;
 
-@net.minecraftforge.fml.common.Mod(modid=FluidAdditionalFieldsTest.MODID, name="Test Mod", version="1.0.0", acceptedMinecraftVersions="*") 
-@net.minecraftforge.fml.common.Mod.EventBusSubscriber 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fluids.BlockFluidBase;
+import net.minecraftforge.fluids.BlockFluidClassic;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@Mod(modid=FluidAdditionalFieldsTest.MODID, name="Test Mod", version="1.0.0", acceptedMinecraftVersions="*") 
+@EventBusSubscriber 
 public class FluidAdditionalFieldsTest{
 	static final boolean ENABLED = false; // <-- enable mod
 	static final int COLOR = 0xFFd742f4;  // <-- change value for testing
 
 	static final String MODID = "fluidadditionalfields";
-	static final net.minecraft.util.ResourceLocation RL = new net.minecraft.util.ResourceLocation(MODID, "slime");
-    static {if (ENABLED) {net.minecraftforge.fluids.FluidRegistry.enableUniversalBucket();}}
-	public static final net.minecraftforge.fluids.Fluid SF = new net.minecraftforge.fluids.Fluid("slime", new net.minecraft.util.ResourceLocation(MODID,"slime_still"), new net.minecraft.util.ResourceLocation(MODID, "slime_flow")).setColor(COLOR);
-    @net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder("slime") public static final net.minecraftforge.fluids.BlockFluidBase SB = null;
-    @net.minecraftforge.fml.common.Mod.EventHandler public void ev(net.minecraftforge.fml.common.event.FMLPreInitializationEvent ev) {if (ENABLED) {net.minecraftforge.fluids.FluidRegistry.registerFluid(SF); net.minecraftforge.fluids.FluidRegistry.addBucketForFluid(SF);}}
-    @net.minecraftforge.fml.common.eventhandler.SubscribeEvent public static void evb(final net.minecraftforge.event.RegistryEvent.Register<net.minecraft.block.Block> ev) {if (ENABLED) ev.getRegistry().register((new net.minecraftforge.fluids.BlockFluidClassic(SF, net.minecraft.block.material.Material.WATER)).setRegistryName(RL).setUnlocalizedName(RL.toString()));}
-    @net.minecraftforge.fml.common.eventhandler.SubscribeEvent public static void evib(final net.minecraftforge.event.RegistryEvent.Register<net.minecraft.item.Item> ev) {if (ENABLED) {ev.getRegistry().register((new net.minecraft.item.ItemBlock(SB)).setRegistryName(RL));}} 
-    @net.minecraftforge.fml.common.eventhandler.SubscribeEvent @net.minecraftforge.fml.relauncher.SideOnly(net.minecraftforge.fml.relauncher.Side.CLIENT) public static void evm(final net.minecraftforge.client.event.ModelRegistryEvent ev) {if (ENABLED) {net.minecraftforge.client.model.ModelLoader.setCustomModelResourceLocation(net.minecraft.item.Item.getItemFromBlock(SB), 0, new net.minecraft.client.renderer.block.model.ModelResourceLocation(RL, "inventory")); net.minecraftforge.client.model.ModelLoader.setCustomModelResourceLocation((new net.minecraft.item.ItemBlock(SB)), 0, new net.minecraft.client.renderer.block.model.ModelResourceLocation(RL, "inventory"));}}
+	static final ResourceLocation RL = new ResourceLocation(MODID, "slime");
+    static {if (ENABLED) {FluidRegistry.enableUniversalBucket();}}
+	public static final Fluid SF = new Fluid("slime", new ResourceLocation(MODID,"slime_still"), new ResourceLocation(MODID, "slime_flow")).setColor(COLOR);
+    @ObjectHolder("slime") public static final BlockFluidBase SB = null;
+    @EventHandler public void ev(FMLPreInitializationEvent ev) {if (ENABLED) {FluidRegistry.registerFluid(SF); FluidRegistry.addBucketForFluid(SF);}}
+    @SubscribeEvent public static void evb(final RegistryEvent.Register<Block> ev) {if (ENABLED) ev.getRegistry().register((new BlockFluidClassic(SF, Material.WATER)).setRegistryName(RL).setUnlocalizedName(RL.toString()));}
+    @SubscribeEvent public static void evib(final RegistryEvent.Register<Item> ev) {if (ENABLED) {ev.getRegistry().register((new ItemBlock(SB)).setRegistryName(RL));}} 
+    @SubscribeEvent @SideOnly(Side.CLIENT) public static void evm(final ModelRegistryEvent ev) {if (ENABLED) {ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(SB), 0, new ModelResourceLocation(RL, "inventory")); ModelLoader.setCustomModelResourceLocation((new ItemBlock(SB)), 0, new ModelResourceLocation(RL, "inventory"));}}
 }
