85632770f96fdaf521f285df457c5bfed10272ca
Ben Staddon <quadraxis@hotmail.co.uk>
2017-10-18 12:35:04 +0100

    Clean up and fix another test mod

--- src/test/java/net/minecraftforge/debug/FogColorInsideMaterialTest.java
+++ src/test/java/net/minecraftforge/debug/FogColorInsideMaterialTest.java
@@ -7,6 +7,7 @@ import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.renderer.block.statemap.StateMapperBase;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.util.ResourceLocation;
@@ -17,6 +18,7 @@ import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fluids.BlockFluidClassic;
+import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -41,7 +43,9 @@ public class FogColorInsideMaterialTest
     @SubscribeEvent
     public static void registerBlocks(RegistryEvent.Register<Block> event)
     {
-        Block fluid = new BlockFluidClassic(FluidRegistry.WATER, Material.WATER)
+        Fluid fluid = new Fluid("fog_test", Blocks.WATER.getRegistryName(), Blocks.FLOWING_WATER.getRegistryName());
+        FluidRegistry.registerFluid(fluid);
+        Block fluidBlock = new BlockFluidClassic(fluid, Material.WATER)
         {
             @Override
             public Vec3d getFogColor(World world, BlockPos pos, IBlockState state, Entity entity, Vec3d originalColor, float partialTicks)
@@ -49,10 +53,10 @@ public class FogColorInsideMaterialTest
                 return new Vec3d(0.6F, 0.1F, 0.0F);
             }
         };
-        fluid.setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
-        fluid.setUnlocalizedName(testFluidRegistryName.toString());
-        fluid.setRegistryName(testFluidRegistryName);
-        event.getRegistry().register(fluid);
+        fluidBlock.setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+        fluidBlock.setUnlocalizedName(MOD_ID + ".test_fluid");
+        fluidBlock.setRegistryName(testFluidRegistryName);
+        event.getRegistry().register(fluidBlock);
     }
 
     @SubscribeEvent
