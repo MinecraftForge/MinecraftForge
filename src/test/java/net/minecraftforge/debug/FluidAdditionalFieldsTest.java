431531426d2380c10c60bdc452fff977f7482869
jabelar <julian_abelar@hotmail.com>
2017-10-26 02:19:14 -0700

    Simplified the test mod as requested by liach

--- src/test/java/net/minecraftforge/debug/FluidAdditionalFieldsTest.java
+++ src/test/java/net/minecraftforge/debug/FluidAdditionalFieldsTest.java
@@ -4,12 +4,7 @@ import java.awt.Color;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fluids.BlockFluidBase;
 import net.minecraftforge.fluids.BlockFluidClassic;
@@ -21,8 +16,6 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod(modid = FluidAdditionalFieldsTest.MODID, name = "Test Mod", version = "1.0.0", acceptedMinecraftVersions = "*")
 @EventBusSubscriber
@@ -58,26 +51,8 @@ public class FluidAdditionalFieldsTest
     public static void eventBlockRegistry(final RegistryEvent.Register<Block> event)
     {
         if (ENABLED)
+        {
             event.getRegistry().register((new BlockFluidClassic(SLIME, Material.WATER)).setRegistryName(RES_LOC).setUnlocalizedName(RES_LOC.toString()));
-    }
-
-    @SubscribeEvent
-    public static void eventItemRegistry(final RegistryEvent.Register<Item> event)
-    {
-        if (ENABLED)
-        {
-            event.getRegistry().register((new ItemBlock(SLIME_BLOCK)).setRegistryName(RES_LOC));
-        }
-    }
-
-    @SubscribeEvent
-    @SideOnly(Side.CLIENT)
-    public static void eventModelRegistry(final ModelRegistryEvent event)
-    {
-        if (ENABLED)
-        {
-            ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(SLIME_BLOCK), 0, new ModelResourceLocation(RES_LOC, "inventory"));
-            ModelLoader.setCustomModelResourceLocation((new ItemBlock(SLIME_BLOCK)), 0, new ModelResourceLocation(RES_LOC, "inventory"));
         }
     }
 }
