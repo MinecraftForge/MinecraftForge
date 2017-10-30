db5ecaaed1e347124efd275785a483c3fac3e6bc
Marcel Mundl <marcel.mundl@lustitia.de>
2017-10-31 00:47:23 +0100

    remove wildcard import

--- src/main/java/net/minecraftforge/fml/common/FMLModContainer.java
+++ src/main/java/net/minecraftforge/fml/common/FMLModContainer.java
@@ -29,7 +29,6 @@ import java.lang.reflect.Modifier;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.cert.Certificate;
-import java.util.*;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Config;
@@ -57,6 +56,12 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
