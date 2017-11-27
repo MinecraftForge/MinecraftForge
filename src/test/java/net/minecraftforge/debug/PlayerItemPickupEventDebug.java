4cf01f4a7a164852deea20182add3c25ee5feb5b
Draco18s <draco18s@gmail.com>
2017-11-27 14:03:58 -0500

    Test mod disabled by default

--- src/test/java/net/minecraftforge/debug/PlayerItemPickupEventDebug.java
+++ src/test/java/net/minecraftforge/debug/PlayerItemPickupEventDebug.java
@@ -14,7 +14,7 @@ import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 public class PlayerItemPickupEventDebug
 {
 
-    private static final boolean ENABLED = true;
+    private static final boolean ENABLED = false;
     public static final String MODID = "playeritempickupeventdebug";
     public static final String NAME = "Player.ItemPickup Event Debug";
     public static final String VERSION = "1.0.0";
