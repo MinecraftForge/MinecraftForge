415eacbd6615ee654bfd8c3dac7792133b169766
Matthew Prenger <matt@prenger.co>
2017-12-16 19:02:24 -0600

    Fix startIndex for /forge generate position parsing (#4572)

--- src/main/java/net/minecraftforge/server/command/CommandGenerate.java
+++ src/main/java/net/minecraftforge/server/command/CommandGenerate.java
@@ -65,7 +65,7 @@ class CommandGenerate extends CommandBase
             throw new WrongUsageException("commands.forge.gen.usage");
         }
 
-        BlockPos blockpos = parseBlockPos(sender, args, 1, false);
+        BlockPos blockpos = parseBlockPos(sender, args, 0, false);
         int count = parseInt(args[3], 10);
         int dim = args.length >= 5 ? parseInt(args[4]) : sender.getEntityWorld().provider.getDimension();
         int interval = args.length >= 6 ? parseInt(args[5]) : -1;
