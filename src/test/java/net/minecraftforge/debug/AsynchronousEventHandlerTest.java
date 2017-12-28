9e0de82ffb0a2e8e616159d737c73e12c089e42f
Marcel Mundl <Marcel@Mundlhome.de>
2017-12-28 01:29:14 +0100

    added example mod

---
+++ src/test/java/net/minecraftforge/debug/AsynchronousEventHandlerTest.java
@@ -0,0 +1,67 @@
+package net.minecraftforge.debug;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static net.minecraftforge.fml.common.Mod.EventHandler;
+
+@Mod(
+        modid = AsynchronousEventHandlerTest.MOD_ID,
+        name = "Asynchronous event handlers test mod",
+        version = "1.0",
+        acceptableRemoteVersions = "*"
+)
+public class AsynchronousEventHandlerTest
+{
+
+    public static final String MOD_ID = "Asynchronous_event_handlers_test";
+
+    private static void doSomeStuffThatTakesALongTime()
+    {
+        try
+        {
+            // simulate workload
+            Thread.sleep(100);
+        }
+        catch (InterruptedException e)
+        {
+            // we don't need to handle interruptions here
+        }
+    }
+
+    /**
+     * A synchronous event handler to test backwards compatibility
+     * @param e unused but necessary for FML to know what kind of handler this is and when to call it
+     */
+    @EventHandler
+    public void synchronousPreInitializationEventHandler(FMLPreInitializationEvent e)
+    {
+        // This will block the thread for a total of about one second.
+        for (int i = 0; i < 10; i++)
+        {
+            doSomeStuffThatTakesALongTime();
+        }
+    }
+
+    /**
+     * An asynchronous event handler to show how the support for them can be used
+     * @param e unused but necessary for FML to know what kind of handler this is and when to call it
+     * @return A future that will resolve as soon as all work inside this handler is done
+     */
+    @EventHandler
+    public Future<Void> asynchronousPreInitializationEventHandler(FMLPreInitializationEvent e)
+    {
+        // This won't block the thread and will run in parallel to the other event handler
+        CompletableFuture[] futures = Stream.of(1,2,3,4,5,6,7,8,9,10)
+                .map(i -> CompletableFuture.runAsync(AsynchronousEventHandlerTest::doSomeStuffThatTakesALongTime))
+                .collect(Collectors.toList())
+                .toArray(new CompletableFuture[10]);
+        return CompletableFuture.allOf(futures);
+    }
+
+}
