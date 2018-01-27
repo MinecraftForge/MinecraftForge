bbc055aedf9ceb4b1a346bef80b3355fa5fd2806
Shadows_of_Fire <Bward7864@gmail.com>
2018-01-27 03:40:38 -0500

    Cleaner TEISRs

--- src/main/java/net/minecraftforge/client/ForgeHooksClient.java
+++ src/main/java/net/minecraftforge/client/ForgeHooksClient.java
@@ -581,7 +581,7 @@ public class ForgeHooksClient
     }
 
     /**
-     * @deprecated Will be removed as soon as possible, hopefully 1.9.
+     * @deprecated Will be removed as soon as possible, hopefully 1.9.  See TileEntityItemStackRenderer.ITEM_TO_RENDERER.
      */
     @Deprecated
     public static void registerTESRItemStack(Item item, int metadata, Class<? extends TileEntity> TileClass)
