4811590668a503813156a3190908b3378ff292e2
Shadows_of_Fire <Bward7864@gmail.com>
2018-02-04 20:13:11 -0500

    update jdoc

--- src/main/java/net/minecraftforge/client/ForgeHooksClient.java
+++ src/main/java/net/minecraftforge/client/ForgeHooksClient.java
@@ -581,7 +581,7 @@ public class ForgeHooksClient
     }
 
     /**
-     * @deprecated Will be removed as soon as possible, hopefully 1.9.  See {@link Item#getTEISR()}.
+     * @deprecated Will be removed as soon as possible, hopefully 1.9.  See {@link Item#getTileEntityItemStackRenderer()}.
      */
     @Deprecated
     public static void registerTESRItemStack(Item item, int metadata, Class<? extends TileEntity> TileClass)
