0cf8ff5d19cfea13cec555f11b814843bd083228
Ben Staddon <quadraxis@hotmail.co.uk>
2018-02-15 05:12:48 +0000

    Change composite models to fetch quads from submodels using full context

--- src/main/java/net/minecraftforge/client/model/MultiLayerModel.java
+++ src/main/java/net/minecraftforge/client/model/MultiLayerModel.java
@@ -131,48 +131,33 @@ public final class MultiLayerModel implements IModel
         private final ImmutableMap<TransformType, TRSRTransformation> cameraTransforms;
         private final IBakedModel base;
         private final IBakedModel missing;
-        private final ImmutableMap<Optional<EnumFacing>, ImmutableList<BakedQuad>> quads;
 
         public MultiLayerBakedModel(ImmutableMap<Optional<BlockRenderLayer>, IBakedModel> models, IBakedModel missing, ImmutableMap<TransformType, TRSRTransformation> cameraTransforms)
         {
             this.models = models;
             this.cameraTransforms = cameraTransforms;
             this.missing = missing;
-            base = models.getOrDefault(Optional.empty(), missing);
-            ImmutableMap.Builder<Optional<EnumFacing>, ImmutableList<BakedQuad>> quadBuilder = ImmutableMap.builder();
-            quadBuilder.put(Optional.empty(), buildQuads(models, Optional.empty()));
-            for(EnumFacing side: EnumFacing.values())
-            {
-                quadBuilder.put(Optional.of(side), buildQuads(models, Optional.of(side)));
-            }
-            quads = quadBuilder.build();
-        }
-
-        private static ImmutableList<BakedQuad> buildQuads(ImmutableMap<Optional<BlockRenderLayer>, IBakedModel> models, Optional<EnumFacing> side)
-        {
-            ImmutableList.Builder<BakedQuad> builder = ImmutableList.builder();
-            for(IBakedModel model : models.values())
-            {
-                builder.addAll(model.getQuads(null, side.orElse(null), 0));
-            }
-            return builder.build();
+            this.base = models.getOrDefault(Optional.empty(), missing);
         }
 
         @Override
         public List<BakedQuad> getQuads(@Nullable IBlockState state, @Nullable EnumFacing side, long rand)
         {
-            IBakedModel model;
             BlockRenderLayer layer = MinecraftForgeClient.getRenderLayer();
-            if(layer == null)
+            if (layer == null)
             {
-                return quads.get(Optional.ofNullable(side));
+                ImmutableList.Builder<BakedQuad> builder = ImmutableList.builder();
+                for (IBakedModel model : models.values())
+                {
+                    builder.addAll(model.getQuads(state, side, rand));
+                }
+                return builder.build();
             }
             else
             {
-                model = models.getOrDefault(Optional.of(layer), missing);
+                // assumes that child model will handle this state properly. FIXME?
+                return models.getOrDefault(Optional.of(layer), missing).getQuads(state, side, rand);
             }
-            // assumes that child model will handle this state properly. FIXME?
-            return model.getQuads(state, side, rand);
         }
 
         @Override
