3529bf62191137d9eaf9d3af2e26a9acb833fcf2
Minecraftschurli <minecraftschurli@gmail.com>
2018-02-21 09:31:53 +0100

    added a way to add a custom elytra
    added IElytra which is checked for in EntityLivingBasse for elytra flight
    added implements IElytra to ItemElytra

---
+++ src/main/java/net/minecraftforge/items/IElytra.java
@@ -0,0 +1,38 @@
+package net.minecraftforge.items;
+
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+
+public interface IElytra 
+{
+	public boolean getIsRepairable(ItemStack toRepair, ItemStack repair);
+	
+	default public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+	{
+        ItemStack itemstack = playerIn.getHeldItem(handIn);
+        EntityEquipmentSlot entityequipmentslot = EntityLiving.getSlotForItemStack(itemstack);
+        ItemStack itemstack1 = playerIn.getItemStackFromSlot(entityequipmentslot);
+
+        if (itemstack1.isEmpty())
+        {
+            playerIn.setItemStackToSlot(entityequipmentslot, itemstack.copy());
+            itemstack.setCount(0);
+            return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, itemstack);
+        }
+        else
+        {
+            return new ActionResult<ItemStack>(EnumActionResult.FAIL, itemstack);
+        }
+    }
+	
+	public static boolean isUsable(ItemStack stack)
+	{
+		return false;
+	}
+}
