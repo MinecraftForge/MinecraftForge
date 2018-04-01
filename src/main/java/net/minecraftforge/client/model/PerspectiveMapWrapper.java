0b5a6a3b031816ca46d4bc405a0062781dec914b
Ben Staddon <bs2609@users.noreply.github.com>
2018-04-01 08:24:37 +0100

    Reduce memory usage of model transforms (#4753)

--- src/main/java/net/minecraftforge/client/model/PerspectiveMapWrapper.java
+++ src/main/java/net/minecraftforge/client/model/PerspectiveMapWrapper.java
@@ -1,5 +1,6 @@
 package net.minecraftforge.client.model;
 
+import java.util.EnumMap;
 import java.util.Optional;
 import com.google.common.collect.ImmutableMap;
 import net.minecraft.block.state.IBlockState;
@@ -35,41 +36,46 @@ public class PerspectiveMapWrapper implements IBakedModel
 
     public static ImmutableMap<ItemCameraTransforms.TransformType, TRSRTransformation> getTransforms(IModelState state)
     {
-        ImmutableMap.Builder<ItemCameraTransforms.TransformType, TRSRTransformation> builder = ImmutableMap.builder();
+        EnumMap<ItemCameraTransforms.TransformType, TRSRTransformation> map = new EnumMap<>(ItemCameraTransforms.TransformType.class);
         for(ItemCameraTransforms.TransformType type : ItemCameraTransforms.TransformType.values())
         {
             Optional<TRSRTransformation> tr = state.apply(Optional.of(type));
             if(tr.isPresent())
             {
-                builder.put(type, tr.get());
+                map.put(type, tr.get());
             }
         }
-        return builder.build();
+        return ImmutableMap.copyOf(map);
     }
 
     @SuppressWarnings("deprecation")
     public static ImmutableMap<ItemCameraTransforms.TransformType, TRSRTransformation> getTransforms(ItemCameraTransforms transforms)
     {
-        ImmutableMap.Builder<ItemCameraTransforms.TransformType, TRSRTransformation> builder = ImmutableMap.builder();
+        EnumMap<ItemCameraTransforms.TransformType, TRSRTransformation> map = new EnumMap<>(ItemCameraTransforms.TransformType.class);
         for(ItemCameraTransforms.TransformType type : ItemCameraTransforms.TransformType.values())
         {
-            builder.put(type, TRSRTransformation.blockCenterToCorner(new TRSRTransformation(transforms.getTransform(type))));
+            if (transforms.hasCustomTransform(type))
+            {
+                map.put(type, TRSRTransformation.blockCenterToCorner(TRSRTransformation.from(transforms.getTransform(type))));
+            }
         }
-        return builder.build();
+        return ImmutableMap.copyOf(map);
     }
 
     public static Pair<? extends IBakedModel, Matrix4f> handlePerspective(IBakedModel model, ImmutableMap<ItemCameraTransforms.TransformType, TRSRTransformation> transforms, ItemCameraTransforms.TransformType cameraTransformType)
     {
-        TRSRTransformation tr = transforms.get(cameraTransformType);
-        Matrix4f mat = null;
-        if(tr != null && !tr.equals(TRSRTransformation.identity())) mat = TRSRTransformation.blockCornerToCenter(tr).getMatrix();
-        return Pair.of(model, mat);
+        TRSRTransformation tr = transforms.getOrDefault(cameraTransformType, TRSRTransformation.identity());
+        if (!tr.isIdentity())
+        {
+            return Pair.of(model, TRSRTransformation.blockCornerToCenter(tr).getMatrix());
+        }
+        return Pair.of(model, null);
     }
 
     public static Pair<? extends IBakedModel, Matrix4f> handlePerspective(IBakedModel model, IModelState state, ItemCameraTransforms.TransformType cameraTransformType)
     {
         TRSRTransformation tr = state.apply(Optional.of(cameraTransformType)).orElse(TRSRTransformation.identity());
-        if(tr != TRSRTransformation.identity())
+        if (!tr.isIdentity())
         {
             return Pair.of(model, TRSRTransformation.blockCornerToCenter(tr).getMatrix());
         }
