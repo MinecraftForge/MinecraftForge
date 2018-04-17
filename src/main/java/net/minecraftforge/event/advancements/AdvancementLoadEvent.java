0b76cf22cc195c66e82a68152bb8ec018e24a585
kindlich <kindlich@gmx.net>
2018-04-17 10:31:45 +0200

    Braces on newLine...

--- src/main/java/net/minecraftforge/event/advancements/AdvancementLoadEvent.java
+++ src/main/java/net/minecraftforge/event/advancements/AdvancementLoadEvent.java
@@ -1,32 +1,35 @@
 package net.minecraftforge.event.advancements;
 
+import java.util.Map;
+
 import net.minecraft.advancements.Advancement;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.Event;
-
-import java.util.Map;
 
 /**
  * This Event is fired when Forge starts doing its thing with Advancements, like on server start and on reload.
  * The provided Map is a direct reference to the Advancement Builder map, so you can directly modify it to inject custom Advancement Builders.
  * This event is fired on the {@link MinecraftForge#EVENT_BUS}.
  */
-public class AdvancementLoadEvent extends Event {
+public class AdvancementLoadEvent extends Event
+{
 
     public final Map<ResourceLocation, Advancement.Builder> advancementMap;
 
-
-    public AdvancementLoadEvent(Map<ResourceLocation, Advancement.Builder> advancementMap) {
+    public AdvancementLoadEvent(Map<ResourceLocation, Advancement.Builder> advancementMap)
+    {
         this.advancementMap = advancementMap;
     }
 
     /**
      * This Event is fired before Forge loads mods' advancements but after Vanilla loaded its advancements
      */
-    public static class Pre extends AdvancementLoadEvent {
+    public static class Pre extends AdvancementLoadEvent
+    {
 
-        public Pre(Map<ResourceLocation, Advancement.Builder> advancementMap) {
+        public Pre(Map<ResourceLocation, Advancement.Builder> advancementMap)
+        {
             super(advancementMap);
         }
     }
@@ -34,9 +37,11 @@ public class AdvancementLoadEvent extends Event {
     /**
      * This Event is fired after Forge has completed loading mods' advancements
      */
-    public static class Post extends AdvancementLoadEvent {
+    public static class Post extends AdvancementLoadEvent
+    {
 
-        public Post(Map<ResourceLocation, Advancement.Builder> advancementMap) {
+        public Post(Map<ResourceLocation, Advancement.Builder> advancementMap)
+        {
             super(advancementMap);
         }
     }
