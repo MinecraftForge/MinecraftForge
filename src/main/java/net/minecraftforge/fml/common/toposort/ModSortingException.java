979797a2a6651857decf05f5a878cf4e7efd271f
mezz <tehgeek@gmail.com>
2018-05-12 22:39:42 -0700

    Give immediate and complete error messages when there is a crash during startup (#4869)

--- src/main/java/net/minecraftforge/fml/common/toposort/ModSortingException.java
+++ src/main/java/net/minecraftforge/fml/common/toposort/ModSortingException.java
@@ -21,10 +21,15 @@ package net.minecraftforge.fml.common.toposort;
 
 import java.util.Set;
 
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.fml.client.GuiSortingProblem;
+import net.minecraftforge.fml.client.IDisplayableError;
 import net.minecraftforge.fml.common.EnhancedRuntimeException;
 import net.minecraftforge.fml.common.ModContainer;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class ModSortingException extends EnhancedRuntimeException
+public class ModSortingException extends EnhancedRuntimeException implements IDisplayableError
 {
     private static final long serialVersionUID = 1L;
 
@@ -76,4 +81,11 @@ public class ModSortingException extends EnhancedRuntimeException
         }
     }
 
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public GuiScreen createGui()
+    {
+        return new GuiSortingProblem(this);
+    }
 }
