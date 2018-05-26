0400b0db0799a87ef38d8f693af53e3523e6a756
Ben Staddon <quadraxis@hotmail.co.uk>
2018-05-26 11:01:58 +0100

    Update test mod

--- src/test/java/net/minecraftforge/debug/fluid/ColoredFluidTest.java
+++ src/test/java/net/minecraftforge/debug/fluid/ColoredFluidTest.java
@@ -24,19 +24,25 @@ import java.awt.Color;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fluids.BlockFluidBase;
 import net.minecraftforge.fluids.BlockFluidClassic;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.IFluidColor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
+import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = ColoredFluidTest.MODID, name = "Test Mod", version = "1.0.0", acceptedMinecraftVersions = "*", acceptableRemoteVersions = "*")
+@Mod(modid = ColoredFluidTest.MODID, name = "Colored Fluid Test Mod", version = "1.0.0", acceptedMinecraftVersions = "*", acceptableRemoteVersions = "*")
 @EventBusSubscriber
 public class ColoredFluidTest
 {
@@ -52,7 +58,9 @@ public class ColoredFluidTest
             FluidRegistry.enableUniversalBucket();
         }
     }
-    public static final Fluid SLIME = new Fluid("slime", new ResourceLocation(MODID, "slime_still"), new ResourceLocation(MODID, "slime_flow")).setColor(COLOR);
+
+    public static final Fluid SLIME = new ColoredFluid("slime", new ResourceLocation(MODID, "slime_still"), new ResourceLocation(MODID, "slime_flow"));
+
     @ObjectHolder("slime")
     public static final BlockFluidBase SLIME_BLOCK = null;
 
@@ -63,15 +71,55 @@ public class ColoredFluidTest
         {
             FluidRegistry.registerFluid(SLIME);
             FluidRegistry.addBucketForFluid(SLIME);
+            FluidRegistry.registerFluidColorHandler(new IFluidColor.Default(), SLIME);
         }
     }
 
     @SubscribeEvent
-    public static void eventBlockRegistry(final RegistryEvent.Register<Block> event)
+    public static void registerBlocks(final RegistryEvent.Register<Block> event)
     {
         if (ENABLED)
         {
-            event.getRegistry().register((new BlockFluidClassic(SLIME, Material.WATER)).setRegistryName(RES_LOC).setUnlocalizedName(RES_LOC.toString()));
+            event.getRegistry().register(new BlockFluidClassic(SLIME, Material.WATER)
+                    .setRegistryName(RES_LOC)
+                    .setUnlocalizedName(MODID + ".slime")
+            );
+        }
+    }
+
+    @EventBusSubscriber(modid = MODID, value = Side.CLIENT)
+    public static class ClientEventHandler
+    {
+        @SubscribeEvent
+        public static void registerBlockColors(ColorHandlerEvent.Block event)
+        {
+            event.getBlockColors().registerBlockColorHandler((state, worldIn, pos, tintIndex) -> COLOR.getRGB(), SLIME_BLOCK);
+        }
+    }
+
+    static class ColoredFluid extends Fluid
+    {
+        ColoredFluid(String fluidName, ResourceLocation still, ResourceLocation flowing)
+        {
+            super(fluidName, still, flowing);
+        }
+
+        @Override
+        public int getColor()
+        {
+            return -1;
+        }
+
+        @Override
+        public int getColor(FluidStack stack)
+        {
+            return COLOR.getRGB();
+        }
+
+        @Override
+        public int getColor(World world, BlockPos pos)
+        {
+            return COLOR.getRGB();
         }
     }
 }
