a11037ee0ec1b496f515e590765d40ec88fbe353
Alex Baker <alexbaker@wustl.edu>
2018-05-25 21:41:25 -0500

    Changed Mapping to Lower Case

--- src/main/java/net/minecraftforge/common/ForgeModContainer.java
+++ src/main/java/net/minecraftforge/common/ForgeModContainer.java
@@ -404,7 +404,7 @@ public class ForgeModContainer extends DummyModContainer implements WorldAccessC
                 ResourceLocation newTotem = new ResourceLocation("minecraft:totem_of_undying");
                 entry.remap(ForgeRegistries.ITEMS.getValue(newTotem));
             }
-            if (entry.key.toString().equals("forge:bucketFilled"))
+            if (entry.key.toString().equals("forge:bucketfilled"))
             {
                 ResourceLocation newBucket = new ResourceLocation("forge:bucket_filled");
                 entry.remap(ForgeRegistries.ITEMS.getValue(newBucket));
