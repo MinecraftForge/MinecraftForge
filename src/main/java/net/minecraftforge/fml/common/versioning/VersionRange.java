538a9cd72b66931c369c5ef05a56a7e09e5c30e0
Minecrell <minecrell@minecrell.net>
2018-07-01 20:39:10 +0200

    Restore original license headers in Maven versioning package

--- src/main/java/net/minecraftforge/fml/common/versioning/VersionRange.java
+++ src/main/java/net/minecraftforge/fml/common/versioning/VersionRange.java
@@ -22,6 +22,25 @@ package net.minecraftforge.fml.common.versioning;
  * Modifications by cpw under LGPL 2.1 or later
  */
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
@@ -75,7 +94,7 @@ public class VersionRange
 
         return new VersionRange( recommendedVersion, copiedRestrictions );
     }
-    
+
     /**
      * Factory method, for custom versioning schemes
      * @param version version
