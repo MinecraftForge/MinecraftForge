2491482a0ea6555cecac4e2988318441820ca74f
Ben Staddon <quadraxis@hotmail.co.uk>
2018-07-11 19:26:17 +0100

    Relocate test mod

--- src/test/java/net/minecraftforge/debug/world/WorldProviderEventTest.java
+++ src/test/java/net/minecraftforge/debug/world/WorldProviderEventTest.java
@@ -17,7 +17,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-package net.minecraftforge.debug;
+package net.minecraftforge.debug.world;
 
 import net.minecraft.world.WorldProviderSurface;
 import net.minecraftforge.event.world.CreateWorldProviderEvent;
