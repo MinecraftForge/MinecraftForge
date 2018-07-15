e09edcaad32df0888d61cb79d4478275efac1512
TechnicianLP <technicianlp@users.noreply.github.com>
2018-07-15 23:36:41 +0200

    Add TestMod

---
+++ src/test/java/net/minecraftforge/debug/gameplay/CustomSmeltingTest.java
@@ -0,0 +1,71 @@
+/*
+ * Minecraft Forge
+ * Copyright (c) 2016-2018.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package net.minecraftforge.debug.gameplay;
+
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionUtils;
+import net.minecraftforge.common.smelting.ISmeltingRecipe;
+import net.minecraftforge.common.smelting.SmeltingHandler;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = CustomSmeltingTest.MODID, version = "1.0", acceptableRemoteVersions = "*")
+public class CustomSmeltingTest
+{
+    public static final String MODID = "furnace_customsmelt_test";
+
+    private static final boolean ENABLED = false;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        if (ENABLED)
+            SmeltingHandler.instance().addSmeltingRecipe(new ItemStack(Items.POTIONITEM), new PotionSmeltingRecipe());
+    }
+
+    private static class PotionSmeltingRecipe implements ISmeltingRecipe
+    {
+
+        private static final ItemStack generic = new ItemStack(Items.LINGERING_POTION);
+
+        @Override
+        public ItemStack getSmeltingResult(ItemStack input)
+        {
+            // set the output item to be a lingering potion with the same effects as the input potion
+            ItemStack out = new ItemStack(Items.LINGERING_POTION);
+            PotionUtils.addPotionToItemStack(out, PotionUtils.getPotionFromItem(input));
+            PotionUtils.appendEffects(out, PotionUtils.getEffectsFromStack(input));
+            return out;
+        }
+
+        @Override
+        public float getExperience(ItemStack input)
+        {
+            return input.getItem().getSmeltingExperience(input);
+        }
+
+        @Override
+        public ItemStack getGenericOutput()
+        {
+            return generic;
+        }
+    }
+}
