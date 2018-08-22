4aaa0370abc26b023ceebe9df5f0d5c326ab0aa9
LexManos <LexManos@gmail.com>
2018-08-22 11:56:35 -0700

    Fix more apache exclusions.

--- src/main/java/net/minecraftforge/fml/relauncher/FMLLaunchHandler.java
+++ src/main/java/net/minecraftforge/fml/relauncher/FMLLaunchHandler.java
@@ -67,6 +67,8 @@ public class FMLLaunchHandler
         this.classLoader.addTransformerExclusion("net.minecraftforge.fml.common.asm.transformers.");
         this.classLoader.addTransformerExclusion("net.minecraftforge.fml.common.patcher.");
         this.classLoader.addTransformerExclusion("net.minecraftforge.fml.repackage.");
+        this.classLoader.addClassLoaderExclusion("org.apache.commons.");
+        this.classLoader.addClassLoaderExclusion("org.apache.http.");
         this.classLoader.addClassLoaderExclusion("org.apache.maven.");
         this.classLoader.addClassLoaderExclusion("com.google.common.");
         this.classLoader.addClassLoaderExclusion("org.objectweb.asm.");
