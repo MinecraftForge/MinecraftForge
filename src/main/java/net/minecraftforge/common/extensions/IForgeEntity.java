8073e91f28e51756b55963e6a278b02fb00e1ca1
tterrag <tterrag1098@gmail.com>
2018-09-08 22:44:21 -0400

    Add capability patch to Entity, not everything yet

---
+++ src/main/java/net/minecraftforge/common/extensions/IForgeEntity.java
@@ -0,0 +1,9 @@
+package net.minecraftforge.common.extensions;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.common.capabilities.ICapabilitySerializable;
+
+public interface IForgeEntity extends ICapabilitySerializable<NBTTagCompound>
+{
+
+}
