e16b77393cd42193048dec1f09333d4c225fc0b0
malte0811 <malte0811@web.de>
2018-11-07 21:23:04 +0100

    Completely removed the generics from SidedInvWrapper#create since they are kind of useless here

--- src/main/java/net/minecraftforge/items/wrapper/SidedInvWrapper.java
+++ src/main/java/net/minecraftforge/items/wrapper/SidedInvWrapper.java
@@ -34,11 +34,11 @@ public class SidedInvWrapper implements IItemHandlerModifiable
     protected final EnumFacing side;
 
     @SuppressWarnings("unchecked")
-    public static <T extends IItemHandlerModifiable> OptionalCapabilityInstance<T>[] create(ISidedInventory inv, EnumFacing... sides) {
-        OptionalCapabilityInstance<T>[] ret = new OptionalCapabilityInstance[sides.length];
+    public static OptionalCapabilityInstance<IItemHandlerModifiable>[] create(ISidedInventory inv, EnumFacing... sides) {
+        OptionalCapabilityInstance<IItemHandlerModifiable>[] ret = new OptionalCapabilityInstance[sides.length];
         for (int x = 0; x < sides.length; x++) {
             final EnumFacing side = sides[x];
-            ret[x] = OptionalCapabilityInstance.of(() -> (T)new SidedInvWrapper(inv, side));
+            ret[x] = OptionalCapabilityInstance.of(() -> new SidedInvWrapper(inv, side));
         }
         return ret;
     }
