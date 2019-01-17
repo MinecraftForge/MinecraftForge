e8ef11385d09497f25c302878fdc9b2e40038239
mezz <tehgeek@gmail.com>
2019-01-16 22:50:28 -0800

    Only fire the event when the recipes are synced from the server

--- src/main/java/net/minecraftforge/client/event/RecipesUpdatedEvent.java
+++ src/main/java/net/minecraftforge/client/event/RecipesUpdatedEvent.java
@@ -24,10 +24,7 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.Event;
 
 /**
- * Fired when {@link RecipeManager} has all of its recipes updated.
- *
- * This happens on {@link Dist#CLIENT} when recipes are synced from the server to the client (just after a client has connected),
- * and on both {@link Dist#DEDICATED_SERVER} and {@link Dist#CLIENT} when datapacks are loaded or reloaded.
+ * Fired on {@link Dist#CLIENT} when {@link RecipeManager} has all of its recipes synced from the server to the client (just after a client has connected),
  */
 public class RecipesUpdatedEvent extends Event
 {
