554da2f72bf22d010279d5c199d64957abcfa062
tterrag <tterrag1098@gmail.com>
2019-03-14 23:42:25 -0400

    Address some minor feedback

--- src/main/java/net/minecraftforge/client/model/data/IModelData.java
+++ src/main/java/net/minecraftforge/client/model/data/IModelData.java
@@ -1,10 +1,14 @@
 package net.minecraftforge.client.model.data;
 
+import javax.annotation.Nullable;
+
 public interface IModelData
 {
     boolean hasProperty(ModelProperty<?> prop);
     
+    @Nullable
     <T> T getData(ModelProperty<T> prop);
     
+    @Nullable
     <T> T setData(ModelProperty<T> prop, T data);
 }
