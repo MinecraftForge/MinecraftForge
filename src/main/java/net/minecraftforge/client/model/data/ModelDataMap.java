09a18c738240a64231a5682b5daf181869bdbfb0
tterrag <tterrag1098@gmail.com>
2019-03-14 23:50:46 -0400

    Change how hasProperty functions, add some docs for it

--- src/main/java/net/minecraftforge/client/model/data/ModelDataMap.java
+++ src/main/java/net/minecraftforge/client/model/data/ModelDataMap.java
@@ -2,27 +2,22 @@ package net.minecraftforge.client.model.data;
 
 import java.util.IdentityHashMap;
 import java.util.Map;
-import java.util.Set;
 
 import com.google.common.base.Preconditions;
-import com.google.common.collect.Sets;
 
 public class ModelDataMap implements IModelData
 {
-    private final Set<ModelProperty<?>> validProperties;
     private final Map<ModelProperty<?>, Object> backingMap;
     
     private ModelDataMap(Map<ModelProperty<?>, Object> map)
     {
-        this.validProperties = Sets.newIdentityHashSet();
-        this.validProperties.addAll(map.keySet());
         this.backingMap = new IdentityHashMap<>(map);
     }
-
+    
     @Override
     public boolean hasProperty(ModelProperty<?> prop)
     {
-        return validProperties.contains(prop);
+        return backingMap.containsKey(prop);
     }
 
     @SuppressWarnings("unchecked")
@@ -36,7 +31,6 @@ public class ModelDataMap implements IModelData
     @Override
     public <T> T setData(ModelProperty<T> prop, T data)
     {
-        Preconditions.checkArgument(hasProperty(prop), "Cannot set model property here as it does not exist");
         Preconditions.checkArgument(prop.test(data), "Value is invalid for this property");
         return (T) backingMap.put(prop, data);
     }
