00dee62878be3b615b96e50e496abeb8a4369660
Brennan Ward <Bward7864@gmail.com>
2019-03-25 15:31:54 -0400

    Add class to wrap IItemHandler as IInventory for easier recipe interaction. (#5522)

--- src/main/java/net/minecraftforge/common/extensions/ForgeRecipeManager.java
+++ src/main/java/net/minecraftforge/common/extensions/ForgeRecipeManager.java
@@ -28,6 +28,7 @@ import javax.annotation.Nullable;
 import com.google.common.collect.Maps;
 
 import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.resources.IResourceManager;
@@ -36,24 +37,42 @@ import net.minecraft.util.NonNullList;
 import net.minecraft.world.World;
 import net.minecraftforge.common.crafting.CraftingHelper;
 import net.minecraftforge.common.crafting.RecipeType;
+import net.minecraftforge.items.wrapper.RecipeWrapper;
 
 @SuppressWarnings("deprecation")
 public abstract class ForgeRecipeManager implements IResourceManagerReloadListener
 {
+    /**
+     * Master list of sorted recipes.
+     */
     protected final Map<RecipeType<? extends IRecipe>, List<? extends IRecipe>> sortedRecipes = Maps.newHashMap();
 
     @Override
-    public void onResourceManagerReload(IResourceManager resourceManager) {
+    public void onResourceManagerReload(IResourceManager resourceManager)
+    {
         this.sortedRecipes.clear();
         CraftingHelper.reloadConstants(resourceManager);
     }
 
+    /**
+     * Returns all the recipes that match this type.
+     * @param type A recipe type.
+     * @return All recipes that are of the passed type.  This list may be empty.
+     */
     @SuppressWarnings("unchecked")
     public <T extends IRecipe> List<T> getRecipes(RecipeType<T> type)
     {
         return (List<T>) this.sortedRecipes.computeIfAbsent(type, t -> new ArrayList<>());
     }
 
+    /**
+     * Automatically returns a result from the first recipe that matches this inv and type.
+     * Modders should probably not use this, and instead use {@link #getRecipe(IInventory, World, RecipeType)} since you get the recipe object itself.
+     * @param input An inventory.
+     * @param world The world.
+     * @param type The type of recipe to match against.
+     * @return The output of the first matching recipe, or {@link ItemStack#EMPTY} if no recipes match.
+     */
     public ItemStack getResult(IInventory input, World world, RecipeType<?> type)
     {
         for(IRecipe irecipe : getRecipes(type))
@@ -61,7 +80,16 @@ public abstract class ForgeRecipeManager implements IResourceManagerReloadListen
         return ItemStack.EMPTY;
     }
 
-	@Nullable
+    /**
+     * Used to find a matching recipe for the given inv, world, and type.
+     * Since this requires an IInventory, modders can use {@link RecipeWrapper} to avoid direct implementation.
+     * For crafting recipes, it is advised to still use {@link InventoryCrafting}.  The dynamic vanilla recipes require they be crafted in one.
+     * @param input An inventory.
+     * @param world The world.
+     * @param type The type of recipe to match against.
+     * @return The first matching recipe.
+     */
+    @Nullable
     public <T extends IRecipe> T getRecipe(IInventory input, World world, RecipeType<T> type)
     {
         for(T irecipe : getRecipes(type))
@@ -71,7 +99,9 @@ public abstract class ForgeRecipeManager implements IResourceManagerReloadListen
 
     /**
      * Modders should not use this if possible.  In the context that this is used, you should already have the recipe.  Do not run extra lookups using this method.
+     * Use {@link IRecipe#getRemainingItems(IInventory)}.
      */
+    @Deprecated
     public NonNullList<ItemStack> getRemainingItems(IInventory input, World world, RecipeType<?> type)
     {
         for(IRecipe irecipe : getRecipes(type))
