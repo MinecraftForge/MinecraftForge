8773f5651951c83795f75ce6a845d537a440be70
rootmenu <rootmenu@web.de>
2019-04-18 02:30:51 +0200

    Added /summon command patch file missing from previous commit

--- src/test/java/net/minecraftforge/debug/entity/living/ExtendedSpecialSpawnTest.java
+++ src/test/java/net/minecraftforge/debug/entity/living/ExtendedSpecialSpawnTest.java
@@ -1,5 +1,7 @@
 package net.minecraftforge.debug.entity.living;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.monster.EntityCreeper;
 import net.minecraft.entity.monster.EntitySkeleton;
 import net.minecraft.entity.monster.EntityZombie;
@@ -9,6 +11,8 @@ import net.minecraft.util.EnumHand;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.List;
 
 @Mod(
         modid = ExtendedSpecialSpawnTest.MOD_ID,
@@ -29,24 +33,43 @@ public class ExtendedSpecialSpawnTest
             return;
         }
 
+        if (event.getEntity() != null)
+            if (cancelEventRec(event.getEntity()))
+                event.setCanceled(true);
+    }
+
+    /** Handles the entity and all it's passengers recursively
+     *
+     * @param entity
+     * @return true, if the event should be cancelled, false otherwise
+     */
+    public static boolean cancelEventRec(Entity entity)
+    {
         // Rename all skeletons
-        if (event.getEntity() instanceof EntitySkeleton)
+        if (entity instanceof EntitySkeleton)
         {
-            event.getEntity().setCustomNameTag("Dinnerbone");
+            entity.setCustomNameTag("Dinnerbone");
         }
 
         // Prevents Creepers from spawning
-        if (event.getEntity() instanceof EntityCreeper)
+        if (entity instanceof EntityCreeper)
         {
-            event.setCanceled(true);
+            return true;
         }
 
         // Give Zombies a diamond sword
-        if (event.getEntity() instanceof EntityZombie)
+        if (entity instanceof EntityZombie)
         {
-            EntityZombie zombie = (EntityZombie) event.getEntity();
+            EntityZombie zombie = (EntityZombie) entity;
 
             zombie.setHeldItem(EnumHand.MAIN_HAND, new ItemStack(Item.getByNameOrId("minecraft:diamond_sword")));
         }
+
+        for(Entity passenger : entity.getPassengers())
+        {
+            if (cancelEventRec(passenger))
+                return true;
+        }
+        return false;
     }
 }
