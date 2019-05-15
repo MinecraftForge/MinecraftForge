6bfaa408989544832b965eddc03d8ec4987a2de2
mcenderdragon <goto.minecraft@googlemail.com>
2019-05-15 23:15:31 +0200

    calling ForgeEventFactory.getItemBurnTime everytime again

--- src/main/java/net/minecraftforge/common/crafting/FurnaceHelper.java
+++ src/main/java/net/minecraftforge/common/crafting/FurnaceHelper.java
@@ -10,16 +10,11 @@ import net.minecraft.tileentity.TileEntityFurnace;
 
 public class FurnaceHelper 
 {
-    private static Map<Item, Integer> burnTimes, vanillaBurnTimes;
+    private static Map<Item, Integer> burnTimes;
     
     public static Map<Item, Integer> getBurnTimes()
     {
-        return burnTimes != null ? burnTimes : (burnTimes = Maps.newLinkedHashMap());
-    }
-    
-    public static Map<Item, Integer> getVanillaBurnTimes()
-    {
-        return vanillaBurnTimes != null ? vanillaBurnTimes : (vanillaBurnTimes = TileEntityFurnace.getBurnTimes());
+        return burnTimes != null ? burnTimes : (burnTimes = TileEntityFurnace.getBurnTimes());
     }
     
     public static int getItemBurnTime(ItemStack stack) 
@@ -33,13 +28,12 @@ public class FurnaceHelper
              Item item = stack.getItem();
              int base = stack.getBurnTime();
              //I changed the original logic here. Previously every time ForgeEventFactory#getItemBurnTime was called, now the result is cahced aswell.
-             return getBurnTimes().computeIfAbsent(item, i -> net.minecraftforge.event.ForgeEventFactory.getItemBurnTime(stack, base == -1 ? getVanillaBurnTimes().getOrDefault(i, 0) : base));
+             return net.minecraftforge.event.ForgeEventFactory.getItemBurnTime(stack, base == -1 ? getBurnTimes().getOrDefault(item, 0) : base);
           }
     }
     
     public static void invalidate()
     {
         burnTimes = null;
-        vanillaBurnTimes = null;
     }
 }
