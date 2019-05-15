568decc5dbd91d78caf579fca34f637ac0a69bdf
mcenderdragon <goto.minecraft@googlemail.com>
2019-05-15 22:50:45 +0200

    fixed formating, added invalidation & fixed FurnaceRecipeBookGui

--- src/main/java/net/minecraftforge/common/crafting/FurnaceHelper.java
+++ src/main/java/net/minecraftforge/common/crafting/FurnaceHelper.java
@@ -10,30 +10,36 @@ import net.minecraft.tileentity.TileEntityFurnace;
 
 public class FurnaceHelper 
 {
-	private static Map<Item, Integer> burnTimes, vanillaBurnTimes;
-	
-	public static Map<Item, Integer> getBurnTimes()
-	{
-		return burnTimes != null ? burnTimes : (burnTimes = Maps.newLinkedHashMap());
-	}
-	
-	public static Map<Item, Integer> getVanillaBurnTimes()
-	{
-		return vanillaBurnTimes != null ? vanillaBurnTimes : (vanillaBurnTimes = TileEntityFurnace.getBurnTimes());
-	}
-	
-	public static int getItemBurnTime(ItemStack stack) 
-	{
-	      if (stack.isEmpty()) 
-	      {
-	         return 0;
-	      }
-	      else
-	      {
-	         Item item = stack.getItem();
-	         int base = stack.getBurnTime();
-	         //I changed the original logic here. Previously every time ForgeEventFactory#getItemBurnTime was called, now the result is cahced aswell.
-	         return getBurnTimes().computeIfAbsent(item, i -> net.minecraftforge.event.ForgeEventFactory.getItemBurnTime(stack, base == -1 ? getVanillaBurnTimes().getOrDefault(i, 0) : base));
-	      }
-	   }
+    private static Map<Item, Integer> burnTimes, vanillaBurnTimes;
+    
+    public static Map<Item, Integer> getBurnTimes()
+    {
+        return burnTimes != null ? burnTimes : (burnTimes = Maps.newLinkedHashMap());
+    }
+    
+    public static Map<Item, Integer> getVanillaBurnTimes()
+    {
+        return vanillaBurnTimes != null ? vanillaBurnTimes : (vanillaBurnTimes = TileEntityFurnace.getBurnTimes());
+    }
+    
+    public static int getItemBurnTime(ItemStack stack) 
+    {
+          if (stack.isEmpty()) 
+          {
+             return 0;
+          }
+          else
+          {
+             Item item = stack.getItem();
+             int base = stack.getBurnTime();
+             //I changed the original logic here. Previously every time ForgeEventFactory#getItemBurnTime was called, now the result is cahced aswell.
+             return getBurnTimes().computeIfAbsent(item, i -> net.minecraftforge.event.ForgeEventFactory.getItemBurnTime(stack, base == -1 ? getVanillaBurnTimes().getOrDefault(i, 0) : base));
+          }
+    }
+    
+    public static void invalidate()
+    {
+        burnTimes = null;
+        vanillaBurnTimes = null;
+    }
 }
