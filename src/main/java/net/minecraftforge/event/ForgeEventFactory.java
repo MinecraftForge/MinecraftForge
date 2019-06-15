ace5decf8fe2dbdd945ca80c7602278a750a50fb
Tslat <tslatmail@gmail.com>
2019-06-16 00:15:18 +1000

    Revert "Make Nether Star protection configurable"
    This reverts commit 32236de3e72c1b1f2728b89aac7ee61f21a4d1a8.

--- src/main/java/net/minecraftforge/event/ForgeEventFactory.java
+++ src/main/java/net/minecraftforge/event/ForgeEventFactory.java
@@ -42,7 +42,6 @@ import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.entity.projectile.EntityFireball;
 import net.minecraft.entity.projectile.EntityThrowable;
 import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.MinecraftServer;
@@ -512,7 +511,6 @@ public class ForgeEventFactory
     public static float onItemDamage(EntityItem entity, DamageSource source, float amount)
     {
         ItemDamageEvent event = new ItemDamageEvent(entity, source, amount);
-        if (!entity.getItem().isEmpty() && entity.getItem().getItem() == Items.NETHER_STAR && source.isExplosion()) event.setCanceled(true);
         return !MinecraftForge.EVENT_BUS.post(event) ? event.getAmount() : 0F;
     }
 
