7383e3e40490676fa8e0ae788e33fbf3e51712b4
Lanse <simon_stalnabb@hotmail.com>
2019-07-01 23:11:11 +0200

    Fixed typo in JavaDocs for LivingTotemEvent

--- src/main/java/net/minecraftforge/event/entity/living/LivingTotemEvent.java
+++ src/main/java/net/minecraftforge/event/entity/living/LivingTotemEvent.java
@@ -16,7 +16,7 @@ import net.minecraftforge.fml.common.eventhandler.Event;
  * This event has a result. {@link HasResult}
  *  Result.Allow = Respawns the Player regardless of them having a Totem.
  *  Result.Default = Runs the default Totem of Undying checks and balances.
- *  Result.Deny = Don't save the player regardless of them holding the Totem.
+ *  Result.Deny = Do not save the player regardless of them holding the Totem.
  * <br>
  * This event is fired on the {@link MinecraftForge#EVENT_BUS}
  */
