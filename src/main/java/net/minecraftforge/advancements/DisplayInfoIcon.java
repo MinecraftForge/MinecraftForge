2238c6749f08687c4977dde6dd8f0377e4ca39cf
Nightenom <17338378+Nightenom@users.noreply.github.com>
2019-08-16 00:30:28 +0200

    Pretty easier solution, failsafe

--- src/main/java/net/minecraftforge/advancements/DisplayInfoIcon.java
+++ src/main/java/net/minecraftforge/advancements/DisplayInfoIcon.java
@@ -19,64 +19,30 @@
 
 package net.minecraftforge.advancements;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonSyntaxException;
 import com.mojang.blaze3d.platform.GlStateManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.AbstractGui;
 import net.minecraft.client.renderer.texture.AtlasTexture;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.ResourceLocationException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.lwjgl.opengl.GL11;
 
 /**
  * Used for rendering of a texture as an advancement icon instead of need of providing a proxy item.
  */
-public class DisplayInfoIcon {
+public class DisplayInfoIcon
+{
+    private static final Logger LOGGER = LogManager.getLogger();
     private static final String NBT_KEY = "forgeAdvIcon";
-    private static final AdvancementForgeIconItem ICON_INSTANCE = new AdvancementForgeIconItem(new Item.Properties());
 
     /**
-     * Creates a fake itemStack with reference to texture given from advancement json.
-     * Existence of "forge" json object is checked in {@link net.minecraft.advancements.DisplayInfo#deserializeIcon()}.
-     *
-     * @param object advancement json object
-     * @return fake itemStack with texture reference
-     */
-    public static ItemStack deserializeForgeIcon(final JsonObject object)
-    {
-        final String path = object.get("forge").getAsString();
-
-        try
-        {
-            new ResourceLocation(path);
-        }
-        catch (ResourceLocationException e)
-        {
-            throw new JsonSyntaxException("Invalid resource location for forge advancement icon: " + path, e);
-        }
-
-        // TODO: used registered instance instead
-        ItemStack itemStack = new ItemStack(ICON_INSTANCE);
-        CompoundNBT compound = new CompoundNBT();
-        compound.putString(NBT_KEY, path);
-        itemStack.setTag(compound);
-        return itemStack;
-    }
-
-    public static JsonObject serializeForgeIcon(final ItemStack itemStack)
-    {
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("forge", getForgeIconTexturePath(itemStack));
-        return jsonObject;
-    }
-
-    /**
-     * Tries to render given itemStack as an advancement icon if item was created by {@link #createForgeIcon()}.
+     * Tries to render given itemStack as an advancement icon if itemstack passes through {@link #isForgeIcon()}.
      * 
-     * @return true if forge icon item, false if not
+     * @return true if forge icon item, false if not or provided resource location is not parsable
      */
     public static boolean renderForgeIcon(final ItemStack itemStack, final int x, final int y)
     {
@@ -85,15 +51,31 @@ public class DisplayInfoIcon {
             return false;
         }
 
+        final CompoundNBT iconCompound = itemStack.getTag().getCompound(NBT_KEY);
+        final String texturePath = iconCompound.getString("location");
+        final ResourceLocation textureLocation;
+
+        try
+        {
+            textureLocation = new ResourceLocation(texturePath);
+        }
+        catch (ResourceLocationException e)
+        {
+            LOGGER.error("Invalid resource location for forge advancement icon: " + texturePath, e);
+            // fall back to render the default icon
+            return false;
+        }
+
+        // copied from net.minecraft.client.renderer.ItemRenderer.renderItemModelIntoGUI(ItemStack, int, int, IBakedModel)
         GlStateManager.pushMatrix();
         GlStateManager.enableRescaleNormal();
         GlStateManager.enableAlphaTest();
-        GlStateManager.alphaFunc(516, 0.1F);
+        GlStateManager.alphaFunc(GL11.GL_GREATER, 0.1F);
         GlStateManager.enableBlend();
         GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
         GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-        Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation(getForgeIconTexturePath(itemStack)));
-        AbstractGui.blit(x, y, 0, 0, 0, 16, 16, 16, 16);
+        Minecraft.getInstance().getTextureManager().bindTexture(textureLocation);
+        AbstractGui.blit(x, y, 0, 0, 0, iconCompound.getInt("width"), iconCompound.getInt("height"), 16, 16);
         Minecraft.getInstance().getTextureManager().bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
         GlStateManager.disableBlend();
         GlStateManager.disableAlphaTest();
@@ -103,31 +85,11 @@ public class DisplayInfoIcon {
         return true;
     }
 
+    /**
+     * Checks if given itemStack has nbt compound tag named according to value of NBT_KEY
+     */
     public static boolean isForgeIcon(final ItemStack itemStack)
     {
-        return itemStack.getItem() == ICON_INSTANCE;
-    }
-
-    public static String getForgeIconTexturePath(final ItemStack itemStack)
-    {
-        if(!(isForgeIcon(itemStack) && itemStack.hasTag()))
-        {
-            return "";
-        }
-
-        return itemStack.getTag().getString(NBT_KEY);
-    }
-
-    /**
-     * Item class to ensure an itemStack was made by this class can be rendered by this class.
-     * Transfers texture resource location.
-     * TODO: register item properly for advancement network sync
-     */
-    private static class AdvancementForgeIconItem extends Item
-    {
-        private AdvancementForgeIconItem(Item.Properties properties)
-        {
-            super(properties);
-        }
+        return itemStack.hasTag() && itemStack.getTag().contains(NBT_KEY, 10);
     }
 }
