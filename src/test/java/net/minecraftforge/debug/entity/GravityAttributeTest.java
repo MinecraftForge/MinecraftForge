2c8c022034ba9967affc7ecf02dc6583f4f3652a
LexManos <LexManos@gmail.com>
2019-09-03 15:38:15 -0700

    Add a bunch of tags from the omnibus. Fix test imports. And update patches for line numbers.

--- src/test/java/net/minecraftforge/debug/entity/GravityAttributeTest.java
+++ src/test/java/net/minecraftforge/debug/entity/GravityAttributeTest.java
@@ -22,7 +22,6 @@ package net.minecraftforge.debug.entity;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -41,96 +40,104 @@ import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Rarity;
-import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraft.world.ServerWorld;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome.Category;
-import net.minecraft.world.chunk.ServerChunkProvider;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("gravity_attribute_test")
 public class GravityAttributeTest
 {
     public static final boolean ENABLE = true;
-    private static Logger logger;
+    private static Logger logger = LogManager.getLogger();
     private int ticks;
     private static final UUID REDUCED_GRAVITY_ID = UUID.fromString("DEB06000-7979-4242-8888-00000DEB0600");
     private static final AttributeModifier REDUCED_GRAVITY = (new AttributeModifier(REDUCED_GRAVITY_ID, "Reduced gravity", (double)-0.80, Operation.MULTIPLY_TOTAL)).setSaved(false);
 
-    
+
     public GravityAttributeTest() {
-        if (ENABLE) {
-            logger = LogManager.getLogger();
+        if (ENABLE)
+        {
             MinecraftForge.EVENT_BUS.register(this);
             FMLJavaModLoadingContext.get().getModEventBus().register(this);
         }
     }
 
     @SubscribeEvent
-    public void worldTick(TickEvent.WorldTickEvent event) {
-        if (ENABLE && !event.world.isRemote) {
-            if (ticks++ > 60) {
+    public void worldTick(TickEvent.WorldTickEvent event)
+    {
+        if (ENABLE && !event.world.isRemote)
+        {
+            if (ticks++ > 60)
+            {
                 ticks = 0;
                 World w = event.world;
                 List<LivingEntity> list;
-                if(w.isRemote) {
-                    ClientWorld cw = (ClientWorld) w;
+                if(w.isRemote)
+                {
+                    ClientWorld cw = (ClientWorld)w;
                     list = new ArrayList<>(100);
-                    for(Entity e : cw.func_217416_b()) {
-                        if(e.isAlive())
-                        {
-                            if(e instanceof LivingEntity) {
-                                list.add((LivingEntity) e);
-                            }
-                        }
+                    for(Entity e : cw.getAllEntities())
+                    {
+                        if(e.isAlive() && e instanceof LivingEntity)
+                            list.add((LivingEntity)e);
                     }
-                } else {
-                    ServerWorld sw = (ServerWorld) w;
+                }
+                else
+                {
+                    ServerWorld sw = (ServerWorld)w;
                     Stream<LivingEntity> s = sw.getEntities().filter(Entity::isAlive).filter(e -> e instanceof LivingEntity).map(e -> (LivingEntity)e);
                     list = s.collect(Collectors.toList());
                 }
-                for(LivingEntity liv : list)  {
+
+                for(LivingEntity liv : list)
+                {
                     IAttributeInstance grav = liv.getAttribute(LivingEntity.ENTITY_GRAVITY);
                     boolean inPlains = liv.world.getBiome(liv.getPosition()).getCategory() == Category.PLAINS;
-                    if (inPlains && !grav.hasModifier(REDUCED_GRAVITY)) {
+                    if (inPlains && !grav.hasModifier(REDUCED_GRAVITY))
+                    {
                         logger.info("Granted low gravity to Entity: {}", liv);
                         grav.applyModifier(REDUCED_GRAVITY);
-                    } else if (!inPlains && grav.hasModifier(REDUCED_GRAVITY)) {
+                    }
+                    else if (!inPlains && grav.hasModifier(REDUCED_GRAVITY))
+                    {
                         logger.info("Removed low gravity from Entity: {}", liv);
                         grav.removeModifier(REDUCED_GRAVITY);
                     }
                 }
             }
-            
+
         }
     }
-    
+
     @SubscribeEvent
-    public void registerItems(RegistryEvent.Register<Item> event) {
+    public void registerItems(RegistryEvent.Register<Item> event)
+    {
         event.getRegistry().register(new ItemGravityStick(new Item.Properties().group(ItemGroup.TOOLS).rarity(Rarity.RARE)).setRegistryName("gravity_attribute_test:gravity_stick"));
     }
-    
-    public static class ItemGravityStick extends Item 
+
+    public static class ItemGravityStick extends Item
     {
         private static final UUID GRAVITY_MODIFIER = UUID.fromString("DEB06001-7979-4242-8888-10000DEB0601");
-        
-        public ItemGravityStick(Properties properties)  {
+
+        public ItemGravityStick(Properties properties)
+        {
             super(properties);
         }
-        
+
         @Override
-        public Multimap<String, AttributeModifier> getAttributeModifiers(EquipmentSlotType slot) {
+        public Multimap<String, AttributeModifier> getAttributeModifiers(EquipmentSlotType slot)
+        {
             @SuppressWarnings("deprecation")
             Multimap<String, AttributeModifier> multimap = super.getAttributeModifiers(slot);
-            if (slot == EquipmentSlotType.MAINHAND) {
+            if (slot == EquipmentSlotType.MAINHAND)
                 multimap.put(LivingEntity.ENTITY_GRAVITY.getName(), new AttributeModifier(GRAVITY_MODIFIER, "More Gravity", 1.0D, Operation.ADDITION));
-            }
-            
+
             return multimap;
         }
     }
