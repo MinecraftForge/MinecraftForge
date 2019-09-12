e919a312922a283c4e115a12174766148ab14f2e
cheaterpaul <thecheaterpaul@gmail.com>
2019-09-12 18:51:26 +0200

    rearranging stuff

--- src/main/java/net/minecraftforge/event/VillageStructureInitEvent.java
+++ src/main/java/net/minecraftforge/event/VillageStructureInitEvent.java
@@ -19,19 +19,57 @@
 
 package net.minecraftforge.event;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Maps;
 import com.mojang.datafixers.util.Pair;
 
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.gen.feature.jigsaw.JigsawPiece;
 import net.minecraftforge.eventbus.api.Event;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * This event is called after all village
  * {@link net.minecraft.world.gen.feature.jigsaw.JigsawPattern} and {@link net.minecraft.world.gen.feature.jigsaw.JigsawPiece} are initialized.
  *
- * If you want to add custom JigsawPieces to an existing village JigsawPattern, catch this event and
- * get {@link net.minecraft.world.gen.feature.jigsaw.JigsawPatternRegistry#get(ResourceLocation)} from {@link net.minecraft.world.gen.feature.jigsaw.JigsawManager#field_214891_a}
- * and call {@link net.minecraft.world.gen.feature.jigsaw.JigsawPattern#addBuilding(Pair)
  */
 public class VillageStructureInitEvent extends Event {
+    private Map<ResourceLocation,ImmutableList.Builder<Pair<JigsawPiece,Integer>>> buildings = Maps.newConcurrentMap();
 
+    /**
+     * add single JigsawPiece with weight to a pool
+     * @param type The structure pool where the building should be added
+     * @param piece The structure that should be added
+     * @param weight The weight of the structure
+     */
+    public void addBuilding(ResourceLocation type, JigsawPiece piece, int weight)
+    {
+        if(buildings.containsKey(type))
+            buildings.get(type).add(new Pair<>(piece, weight));
+        else
+            buildings.put(type,new ImmutableList.Builder<Pair<JigsawPiece, Integer>>().add(new Pair<>(piece, weight)));
+    }
+
+    /**
+     * Add multiple JigsawPieces with weight to a pool at the same time
+     * @param type The structure pool where the buildings should be added
+     * @param pieces A list of JigsawPieces and weights that should be added to the pool
+     */
+    public void addBuildings(ResourceLocation type, List<Pair<JigsawPiece, Integer>> pieces)
+    {
+        if(buildings.containsKey(type))
+            buildings.get(type).addAll(pieces);
+        else
+            buildings.put(type,new ImmutableList.Builder<Pair<JigsawPiece, Integer>>().addAll(pieces));
+    }
+
+    Map<ResourceLocation, ImmutableList<Pair<JigsawPiece, Integer>>> getBuildings(){
+        Map<ResourceLocation, ImmutableList<Pair<JigsawPiece, Integer>>> builds = Maps.newConcurrentMap();
+        for (Map.Entry<ResourceLocation,ImmutableList.Builder<Pair<JigsawPiece, Integer>>> entry: buildings.entrySet()){
+            builds.put(entry.getKey(),entry.getValue().build());
+        }
+        return builds;
+    }
 }
