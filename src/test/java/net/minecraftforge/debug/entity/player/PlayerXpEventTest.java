3eb839716e0de350c39717a915eb5f2f578c1797
FireController#1847 <usfirepilot123@gmail.com>
2019-09-19 17:27:47 -0600

    Reorganize Events
    * Merged all XP events into one overarching file (`PlayerXpEvent`)
    * Moved `PayerGiveXpEvent` to `PlayerXpEvent#XpChange`
    * Moved  `PlayerChangeLevelEvent` to `PlayerXpEvent#LevelChange`
    * Added `PlayerXpEvent#PickupXp` and deprecated `PlayerPickupXpEvent`

--- src/test/java/net/minecraftforge/debug/entity/player/PlayerXpEventTest.java
+++ src/test/java/net/minecraftforge/debug/entity/player/PlayerXpEventTest.java
@@ -3,9 +3,8 @@ package net.minecraftforge.debug.entity.player;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import net.minecraftforge.event.entity.player.PlayerGiveXpEvent;
-import net.minecraftforge.event.entity.player.PlayerChangeLevelEvent;
 import net.minecraftforge.event.entity.player.PlayerPickupXpEvent;
+import net.minecraftforge.event.entity.player.PlayerXpEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -14,23 +13,40 @@ import net.minecraftforge.fml.common.Mod;
 public class PlayerXpEventTest
 {
 
-    private static final boolean ENABLE = false;
+    private static final boolean ENABLE = true;
     private static Logger logger = LogManager.getLogger(PlayerXpEventTest.class);
     
     @SubscribeEvent
-    public static void onPlayerPickupXp(PlayerPickupXpEvent event) {
-        if (!ENABLE) return;
-        logger.info("{} picked up an experience orb worth {}", event.getPlayer().getName().getString(), event.getOrb().getXpValue());
+    public static void onPlayerXpEvent(PlayerXpEvent event)
+    {
+    	if (!ENABLE) return;
+    	logger.info("The PlayerXpEvent has been called!");
     }
     
     @SubscribeEvent
-    public static void onPlayerGiveXp(PlayerGiveXpEvent event) {
+    public static void onPlayerPickupXpOld(PlayerPickupXpEvent event)
+    {
+    	if (!ENABLE) return;
+    	logger.info("The deprecated PlayerPickupXpEvent has been called!");
+    }
+    
+    @SubscribeEvent
+    public static void onPlayerPickupXp(PlayerXpEvent.PickupXp event)
+    {
+    	if (!ENABLE) return;
+    	logger.info("{} picked up an experience orb worth {}", event.getPlayer().getName().getString(), event.getOrb().getXpValue());
+    }
+    
+    @SubscribeEvent
+    public static void onPlayerXpChange(PlayerXpEvent.XpChange event)
+    {
         if (!ENABLE) return;
         logger.info("{} has been given {} experience", event.getPlayer().getName().getString(), event.getAmount());
     }
     
     @SubscribeEvent
-    public static void onPlayerLevelChange(PlayerChangeLevelEvent event) {
+    public static void onPlayerLevelChange(PlayerXpEvent.LevelChange event)
+    {
         if (!ENABLE) return;
         logger.info("{} has changed {} levels", event.getPlayer().getName().getString(), event.getLevels());
     }
