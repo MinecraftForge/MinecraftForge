eb3fe33416c6e7dbb60cfa29192548b85d43ad19
Christian Voß <cech12@gmail.com>
2019-10-07 12:40:29 +0200

    Change the Test mod to mushroom things

--- src/test/java/net/minecraftforge/debug/block/RandomTickEventTest.java
+++ src/test/java/net/minecraftforge/debug/block/RandomTickEventTest.java
@@ -1,37 +1,57 @@
 package net.minecraftforge.debug.block;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.SaplingBlock;
+import net.minecraft.block.*;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Items;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.world.IWorld;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 /**
  * This test mod:
- * - Will print before random tick of saplings, their blockstate and the position to all players wearing leather helmets in the same world
- * - Will prevent oak saplings from growing to trees automatically via random tick
+ * - Will print before random tick of mushroom and saplings blocks, their blockstate and the position to all players wearing iron boots in the same world
+ * - Will grow brown mushrooms at random tick to a huge mushroom
+ * - Will prevent red mushrooms from spreading at random tick
  */
 @Mod("random_tick_event_test")
 @Mod.EventBusSubscriber
 public class RandomTickEventTest {
 
+    private static boolean hasDebugItem(PlayerEntity player) {
+        return player.getItemStackFromSlot(EquipmentSlotType.FEET).getItem() == Items.IRON_BOOTS;
+    }
+
+    private static void sendMessageToAllPlayersWithDebugItem(IWorld world, String message) {
+        world.getPlayers().forEach(player -> {
+            if (hasDebugItem(player)) {
+                player.sendMessage(new StringTextComponent(message));
+            }
+        });
+    }
+
     @SubscribeEvent
     public static void onRandomTick(BlockEvent.RandomTickEvent event) {
-        Block block = event.getState().getBlock();
-        if (block instanceof SaplingBlock) {
-            event.getWorld().getPlayers().forEach(player -> {
-                if (player.getItemStackFromSlot(EquipmentSlotType.HEAD).getItem() == Items.LEATHER_HELMET) {
-                    player.sendMessage(new StringTextComponent("Random tick of " + event.getState() + " at position " + event.getPos().toString()));
-                    if (block == Blocks.OAK_SAPLING) {
-                        player.sendMessage(new StringTextComponent("Canceled random tree growing of " + event.getState() + " at position " + event.getPos().toString()));
-                    }
+        IWorld world = event.getWorld();
+        BlockPos pos = event.getPos();
+        if (!world.isAreaLoaded(pos, 7)) return; // Forge: prevent loading unloaded chunks
+        BlockState state = event.getState();
+        Block block = state.getBlock();
+        if (block instanceof MushroomBlock || block instanceof SaplingBlock) {
+            sendMessageToAllPlayersWithDebugItem(world, "Random tick of " + event.getState() + " at position " + event.getPos().toString());
+            if (block instanceof MushroomBlock) {
+                if (block == Blocks.BROWN_MUSHROOM) {
+                    event.setCanceled(true);
+                    ((MushroomBlock) block).grow(world.getWorld(), event.getRandom(), pos, state);
+                    sendMessageToAllPlayersWithDebugItem(world, "Grow huge mushroom of " + event.getState() + " at position " + event.getPos().toString());
+                } else if (block == Blocks.RED_MUSHROOM) {
+                    event.setCanceled(true);
+                    sendMessageToAllPlayersWithDebugItem(world, "Cancel random tick behaviour of " + event.getState() + " at position " + event.getPos().toString());
                 }
-            });
-            event.setCanceled(block == Blocks.OAK_SAPLING);
+            }
         }
     }
 
