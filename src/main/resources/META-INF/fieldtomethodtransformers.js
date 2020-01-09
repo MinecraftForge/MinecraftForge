d5b94aa9c8c99942017976a838822d7bd88d303b
tterrag <tterrag1098@gmail.com>
2020-01-09 17:22:17 -0500

    Add constructor for FishBucketItem that accepts suppliers

--- src/main/resources/META-INF/fieldtomethodtransformers.js
+++ src/main/resources/META-INF/fieldtomethodtransformers.js
@@ -61,6 +61,18 @@ function initializeCoreMod() {
                 asmapi.redirectFieldToMethod(classNode, fn, asmapi.mapMethod('func_220276_d'))
                 return classNode;
             }
+        },
+        'fishbucketitem': {
+            'target': {
+                'type': 'CLASS',
+                'name': 'net.minecraft.item.FishBucketItem'
+            },
+            'transformer': function(classNode) {
+                var asmapi=Java.type('net.minecraftforge.coremod.api.ASMAPI')
+                var fn = asmapi.mapField('field_203794_a') // fishType - remap to mcp if necessary
+                asmapi.redirectFieldToMethod(classNode, fn, asmapi.mapMethod('getFishType'))
+                return classNode;
+            }
         }
     }
 }
