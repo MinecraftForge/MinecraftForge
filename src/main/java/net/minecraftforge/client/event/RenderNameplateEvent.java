23464f5c626a8b223cd7fb97677a5f8e0e07ae6d
ScottehBoeh <scottehboeh@gmail.com>
2020-01-11 22:27:25 +0000

    - RenderNameplateEvent now extends EntityEvent - Removed unnecessary entity getter

--- src/main/java/net/minecraftforge/client/event/RenderNameplateEvent.java
+++ src/main/java/net/minecraftforge/client/event/RenderNameplateEvent.java
@@ -3,42 +3,33 @@ package net.minecraftforge.client.event;
 import com.google.common.base.Strings;
 import net.minecraft.entity.Entity;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.EntityEvent;
 import net.minecraftforge.eventbus.api.Event;
 
 /**
  * RenderNameplateEvent is fired whenever the entity renderer attempts to render a name plate/tag of an entity.
  * <br>
- * {@link #entity} contains the entity in which the plate is being rendered to. This cannot be changed by mods.<br>
  * {@link #nameplateContent} contains the content being rendered on the name plate/tag. This can be changed by mods.<br>
  * {@link #originalContent} contains the original content being rendered on the name plate/tag. This cannot be changed by mods.<br>
  * <br>
- * This event {@link HasResult}. <br>
- * This event has a result. ALLOW will force-render name plate/tag, DEFAULT will ignore the hook and continue using the vanilla check
+ * This event has a result. {@link HasResult}. <br>
+ * ALLOW will force-render name plate/tag, DEFAULT will ignore the hook and continue using the vanilla check
  * & DENY will prevent name plate/tag from rendering<br>
  * <br>
  * This event is fired on the {@link MinecraftForge#EVENT_BUS}.
  **/
 @Event.HasResult
-public class RenderNameplateEvent extends Event
+public class RenderNameplateEvent extends EntityEvent
 {
 
-    private final Entity entity;
     private String nameplateContent;
     private final String originalContent;
 
     public RenderNameplateEvent(Entity entity, String content)
     {
-        this.entity = entity;
+        super(entity);
         this.originalContent = content;
         this.setContent(this.originalContent);
-    }
-
-    /**
-     * The entity in which the name plate/tag is being rendered to
-     */
-    public Entity getEntity()
-    {
-        return this.entity;
     }
 
     /**
