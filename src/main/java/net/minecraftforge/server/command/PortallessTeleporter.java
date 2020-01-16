b95ac359e635a11cb063e38b1f39d8381faaaacc
mcenderdragon <goto.minecraft@googlemail.com>
2020-01-16 12:35:50 +0100

    Revert "deleted PortallessTeleporterand replaced with lambda" - ITeleporter is not a functional interface
    This reverts commit a54ee7417b896e7f040ad86c07a959d4fa6e7dd5.

---
+++ src/main/java/net/minecraftforge/server/command/PortallessTeleporter.java
@@ -0,0 +1,37 @@
+/*
+ * Minecraft Forge
+ * Copyright (c) 2016-2019.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package net.minecraftforge.server.command;
+
+import java.util.function.Function;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.common.util.ITeleporter;
+
+public class PortallessTeleporter implements ITeleporter 
+{
+    public static final PortallessTeleporter INSTANCE = new PortallessTeleporter();
+
+    @Override
+    public Entity placeEntity(Entity entity, ServerWorld currentWorld, ServerWorld destWorld, float yaw, Function<Boolean, Entity> repositionEntity) 
+    {
+        return repositionEntity.apply(false);
+    }
+}
