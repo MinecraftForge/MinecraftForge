f7cef5bcb83ce76f954ebac1d14acf7b887606d5
TheSilkMiner <thesilkminer@outlook.com>
2020-03-21 10:52:44 +0100

    Fix merge and code style issues
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- src/test/java/net/minecraftforge/debug/item/MusicDiscTest.java
+++ src/test/java/net/minecraftforge/debug/item/MusicDiscTest.java
@@ -21,16 +21,16 @@ public class MusicDiscTest
     private static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, MOD_ID);
     private static final DeferredRegister<SoundEvent> SOUND_EVENTS = new DeferredRegister<>(ForgeRegistries.SOUND_EVENTS, MOD_ID);
 
+    private static final RegistryObject<SoundEvent> TEST_SOUND_EVENT = SOUND_EVENTS.register("test_sound_event",
+            () -> new SoundEvent(new ResourceLocation(MOD_ID, "test_sound_event")));
+
+    private static final RegistryObject<Item> TEST_MUSIC_DISC = ITEMS.register("test_music_disc",
+            () -> new MusicDiscItem(1, TEST_SOUND_EVENT, new Item.Properties().maxStackSize(1).rarity(Rarity.EPIC)));
+
     public MusicDiscTest()
     {
         final IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
         ITEMS.register(modBus);
         SOUND_EVENTS.register(modBus);
-    }
-
-    static
-    {
-        final RegistryObject<SoundEvent> testEvent = SOUND_EVENTS.register("test_sound_event", () -> new SoundEvent(new ResourceLocation(MOD_ID, "test_sound_event")));
-        ITEMS.register("test_music_disc", () -> new MusicDiscItem(1, testEvent, new Item.Properties().maxStackSize(1).rarity(Rarity.EPIC)));
     }
 }
