617d12d263a219a5a58fa76a3bb67ee9cf496827
Henne <henne90gen@gmail.com>
2020-03-24 16:57:09 +0100

    Added proper logging

--- src/main/java/net/minecraftforge/command/ForgeCommands.java
+++ src/main/java/net/minecraftforge/command/ForgeCommands.java
@@ -2,11 +2,15 @@ package net.minecraftforge.command;
 
 import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.command.CommandSource;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ForgeCommands {
+
+	private static final Logger LOGGER = LogManager.getLogger();
 
 	private static List<ModCommand> COMMANDS = new ArrayList<>();
 
@@ -20,9 +24,10 @@ public class ForgeCommands {
 		// 		- look through all classes annotated with @ModCommand and execute the register method
 		//  	- create a list of ModCommands and have the user add their command to it
 
-		System.err.println("Registering commands");
+		LOGGER.info("Registering commands");
 		for (ModCommand command : COMMANDS) {
 			command.register(dispatcher, isDedicatedServer);
+			LOGGER.info("Registered command " + command.getClass());
 		}
 	}
 }
--- src/main/java/net/minecraftforge/command/ModCommand.java
+++ src/main/java/net/minecraftforge/command/ModCommand.java
@@ -3,6 +3,7 @@ package net.minecraftforge.command;
 import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.command.CommandSource;
 
+@FunctionalInterface
 public interface ModCommand {
 	void register(CommandDispatcher<CommandSource> dispatcher, boolean isDedicatedServer);
 }
