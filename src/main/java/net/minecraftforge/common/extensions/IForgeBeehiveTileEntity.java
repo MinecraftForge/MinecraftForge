af8620f1979b342e192777635c928a7dc728157b
Jais Djurhuus-Kempel <jdk@combine.dk>
2020-03-31 04:35:57 +0300

    Use fully qualified names

---
+++ src/main/java/net/minecraftforge/common/extensions/IForgeBeehiveTileEntity.java
@@ -0,0 +1,9 @@
+package net.minecraftforge.common.extensions;
+
+import net.minecraft.entity.Entity;
+
+public interface IForgeBeehiveTileEntity {
+    boolean func_226968_d_();
+    boolean func_226970_h_();
+    void func_226961_a_(Entity entity, boolean hasNectar);
+}
