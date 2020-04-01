62ab07356dc3dc7cb4fd6b4c1b93c4f7edada5ae
LexManos <LexManos@gmail.com>
2020-04-01 11:34:36 -0700

    Cleanup IForgeTagBuilder code style.

--- src/main/java/net/minecraftforge/common/data/IOptionalTagEntry.java
+++ src/main/java/net/minecraftforge/common/data/IOptionalTagEntry.java
@@ -2,7 +2,7 @@ package net.minecraftforge.common.data;
 
 import net.minecraft.tags.Tag.ITagEntry;
 
-public interface IOptionalTagEntry<T> extends ITagEntry<T>
-{
-
-}
+/**
+ * Marker class used by Forge to make a tag entry optional at runtime
+ */
+public interface IOptionalTagEntry<T> extends ITagEntry<T> {}
