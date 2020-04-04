5131656c7ab482fc0c4727725a74519f06d0a8e4
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2020-04-03 17:16:52 -0700

    Fix compile error

--- src/main/java/net/minecraftforge/registries/DataSerializerEntry.java
+++ src/main/java/net/minecraftforge/registries/DataSerializerEntry.java
@@ -21,7 +21,7 @@ package net.minecraftforge.registries;
 
 import net.minecraft.network.datasync.IDataSerializer;
 
-public final class DataSerializerEntry<T> extends ForgeRegistryEntry<DataSerializerEntry>
+public final class DataSerializerEntry<T> extends ForgeRegistryEntry<DataSerializerEntry<T>>
 {
     private final IDataSerializer<T> serializer;
 
