398bc2d238b326235c9e43285d0846420af6b4ce
pupnewfster <richard@freimer.com>
2020-04-17 12:40:41 -0400

    Fix precheck for if an ItemHandler is full

--- src/main/java/net/minecraftforge/items/VanillaInventoryCodeHooks.java
+++ src/main/java/net/minecraftforge/items/VanillaInventoryCodeHooks.java
@@ -225,7 +225,7 @@ public class VanillaInventoryCodeHooks
         for (int slot = 0; slot < itemHandler.getSlots(); slot++)
         {
             ItemStack stackInSlot = itemHandler.getStackInSlot(slot);
-            if (stackInSlot.isEmpty() || stackInSlot.getCount() != stackInSlot.getMaxStackSize())
+            if (stackInSlot.isEmpty() || stackInSlot.getCount() < itemHandler.getSlotLimit(slot))
             {
                 return false;
             }
