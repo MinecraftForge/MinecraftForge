746d702058b03d2c098f17b14bc78b4b42cc7dc0
LexManos <LexManos@gmail.com>
2020-04-26 16:50:42 -0700

    Fix resource packs not being sorted properly. Closes #6287

--- src/main/java/net/minecraftforge/fml/server/ServerLifecycleHooks.java
+++ src/main/java/net/minecraftforge/fml/server/ServerLifecycleHooks.java
@@ -207,7 +207,7 @@ public class ServerLifecycleHooks
             IModInfo mod = e.getKey().getModInfos().get(0);
             if (Objects.equals(mod.getModId(), "minecraft")) continue; // skip the minecraft "mod"
             final String name = "mod:" + mod.getModId();
-            final T packInfo = ResourcePackInfo.createResourcePack(name, true, e::getValue, factory, ResourcePackInfo.Priority.TOP);
+            final T packInfo = ResourcePackInfo.createResourcePack(name, true, e::getValue, factory, ResourcePackInfo.Priority.BOTTOM);
             if (packInfo == null) {
                 // Vanilla only logs an error, instead of propagating, so handle null and warn that something went wrong
                 ModLoader.get().addWarning(new ModLoadingWarning(mod, ModLoadingStage.ERROR, "fml.modloading.brokenresources", e.getKey()));
