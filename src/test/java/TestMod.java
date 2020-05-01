0e7dc6cb0410c6c7623fc38750e393f04d6493ce
niprow <prog@nprommi.de>
2020-05-01 16:40:10 +0200

    just a little smaller and some not needed stuff

--- src/test/java/TestMod.java
+++ src/test/java/TestMod.java
@@ -3,7 +3,6 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.crafting.IRecipeSerializer;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -11,13 +10,12 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("testmod")
+@Mod.EventBusSubscriber(bus= Mod.EventBusSubscriber.Bus.MOD)
 public class TestMod {
     public static RecipeBookCategories test_category = new RecipeBookCategories(new ItemStack(Items.BELL));
 
     public TestMod() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-
-        MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void doClientStuff(final FMLClientSetupEvent t) {
@@ -27,16 +25,13 @@ public class TestMod {
         });
     }
 
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onRecipeSerializerRegistry(final RegistryEvent.Register<IRecipeSerializer<?>> event) {
-            event.getRegistry().register(new TestRecipe.Serializer().setRegistryName("testmod:crafting_test"));
-        }
+    @SubscribeEvent
+    public static void onRecipeSerializerRegistry(RegistryEvent.Register<IRecipeSerializer<?>> event) {
+        event.getRegistry().register(new TestRecipe.Serializer().setRegistryName("testmod:crafting_test"));
+    }
 
-        @SubscribeEvent
-        public static void onItemRegistry(final RegistryEvent.Register<Item> event) {
-            event.getRegistry().register(new TestItem().setRegistryName("testmod:test_item"));
-        }
+    @SubscribeEvent
+    public static void onItemRegistry(RegistryEvent.Register<Item> event) {
+        event.getRegistry().register(new TestItem().setRegistryName("testmod:test_item"));
     }
 }
