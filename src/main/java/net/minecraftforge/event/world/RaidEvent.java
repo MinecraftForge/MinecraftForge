f1ba5caa1f8336336cfa9437bbf3857ffd10be16
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2020-05-09 14:45:18 -0700

    stuff

---
+++ src/main/java/net/minecraftforge/event/world/RaidEvent.java
@@ -0,0 +1,24 @@
+package net.minecraftforge.event.world;
+
+import net.minecraft.world.raid.Raid;
+import net.minecraftforge.eventbus.api.Cancelable;
+import net.minecraftforge.eventbus.api.Event;
+/*
+ * This event is called right before a raid starts.
+ */
+@Cancelable
+public class RaidEvent extends Event
+{
+   public final Raid raid;
+   
+   public RaidEvent(Raid raid)
+   {
+	  this.raid = raid;
+   }
+   
+   public Raid getRaid()
+   {
+	   return raid;
+   }
+   
+}
---
+++ src/test/java/net/minecraftforge/debug/world/RaidEventTest.java
@@ -0,0 +1,35 @@
+package net.minecraftforge.debug.world;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.world.raid.Raid.WaveMember;
+import net.minecraftforge.event.world.RaidEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("raid_event_and_more_test")
+@Mod.EventBusSubscriber
+public class RaidEventTest 
+{
+	public static final boolean ENABLE = true;
+	private static Logger LOGGER = LogManager.getLogger(RaidEventTest.class);
+	
+    public RaidEventTest()
+    {
+        if (ENABLE)
+        {
+           WaveMember.create("ILLUSIONER", EntityType.ILLUSIONER, new int[]{0, 4, 3, 3, 4, 4, 4, 2});	
+        }
+    }
+    
+    @SubscribeEvent
+    public static void onRaid(RaidEvent event)
+    {
+       if (event.getRaid().isActive() && ENABLE)
+       {
+    	   LOGGER.info("if you see this, this means the event works");
+       }
+    }
+}
