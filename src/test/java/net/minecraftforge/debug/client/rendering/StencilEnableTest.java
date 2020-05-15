797830f9be548f7ad8f8651a29c096b59178861a
ichttt <ichttt@users.noreply.github.com>
2020-05-15 20:25:57 +0200

    fix test mod and add warning comment to framebuffer (#6698)

--- src/test/java/net/minecraftforge/debug/client/rendering/StencilEnableTest.java
+++ src/test/java/net/minecraftforge/debug/client/rendering/StencilEnableTest.java
@@ -2,6 +2,7 @@ package net.minecraftforge.debug.client.rendering;
 
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.fml.DeferredWorkQueue;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -13,6 +14,6 @@ public class StencilEnableTest {
     }
 
     private void clientSetup(FMLClientSetupEvent event) {
-        Minecraft.getInstance().getFramebuffer().enableStencil();
+        DeferredWorkQueue.runLater(() -> Minecraft.getInstance().getFramebuffer().enableStencil());
     }
 }
