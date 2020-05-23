f784f7a0e50d2e0d26d9d2276ca655759d45e1c4
Cyborgmas <thpetuaudletang@gmail.com>
2020-05-23 12:07:21 -0400

    Fix glowing Extended Button + test mod

---
+++ src/test/java/net/minecraftforge/debug/client/gui/ExtendedButtonGlowOnHoverTest.java
@@ -0,0 +1,37 @@
+package net.minecraftforge.debug.client.gui;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.MainMenuScreen;
+import net.minecraft.client.gui.screen.OptionsScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.Widget;
+import net.minecraft.client.resources.I18n;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.gui.widget.ExtendedButton;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
+
+@Mod("extended_button_glow_on_hover_test")
+@Mod.EventBusSubscriber(value = Dist.CLIENT, modid = "extended_button_glow_on_hover_test")
+public class ExtendedButtonGlowOnHoverTest {
+   private static final boolean ENABLED = true;
+
+   @SubscribeEvent
+   public static void replaceButton(GuiScreenEvent.InitGuiEvent.Post event){
+      if(ENABLED && event.getGui() instanceof MainMenuScreen) {
+         List<Widget> widgets = event.getWidgetList();
+         Screen screen = event.getGui();
+         event.removeWidget(widgets.get(5));
+         ExtendedButton extButton = new ExtendedButton(
+                 screen.width / 2 - 100,
+                 screen.height / 4 + 48 + 72 + 12,
+                 98, 20, I18n.format("menu.options"),
+                 (button) -> Minecraft.getInstance().displayGuiScreen(new OptionsScreen(screen, Minecraft.getInstance().gameSettings)));
+         event.addWidget(extButton);
+         extButton.active = false;
+      }
+   }
+}
