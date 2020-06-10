aaa3c37b6cd7b8a269a6a94e13999a7edbbf23d3
Mysterious Devs <mysteriousdevs@gmail.com>
2020-06-10 22:55:50 +0200

    Add Get Entity Function for Event

--- src/main/java/net/minecraftforge/event/entity/EndermanAttackEvent.java
+++ src/main/java/net/minecraftforge/event/entity/EndermanAttackEvent.java
@@ -1,5 +1,6 @@
 package net.minecraftforge.event.entity;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.monster.EndermanEntity;
 import net.minecraft.entity.monster.ZombieEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -9,11 +10,18 @@ import net.minecraftforge.eventbus.api.Cancelable;
 public class EndermanAttackEvent extends EntityEvent {
 
     private final PlayerEntity player;
+    private final Entity entity;
 
     public EndermanAttackEvent(EndermanEntity entity, PlayerEntity player)
     {
         super(entity);
         this.player = player;
+        this.entity = entity;
+    }
+
+    @Override
+    public Entity getEntity() {
+        return entity;
     }
 
     public PlayerEntity getPlayer() {
