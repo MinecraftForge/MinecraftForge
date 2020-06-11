6d5139f27f5ba123b9a580555608fa0de94a7124
Mysterious Devs <mysteriousdevs@gmail.com>
2020-06-11 11:18:00 +0200

    Add Documentation for this Event - Change extends to LivingEvent - Change getEntity return to EndermanEntity

--- src/main/java/net/minecraftforge/event/entity/EndermanAttackEvent.java
+++ src/main/java/net/minecraftforge/event/entity/EndermanAttackEvent.java
@@ -1,16 +1,27 @@
 package net.minecraftforge.event.entity;
 
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.monster.EndermanEntity;
-import net.minecraft.entity.monster.ZombieEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.Cancelable;
 
-@net.minecraftforge.eventbus.api.Cancelable
-public class EndermanAttackEvent extends EntityEvent {
+/**
+ * This event gets fired whenever a player look an Enderman in the eye<br>
+ * <br>
+ * This event is {@link net.minecraftforge.eventbus.api.Cancelable}.<br>
+ * If this event is canceled, the enderman will not attack the player who looks into the eyes<br>
+ * <br>
+ * This event does not have a result. {@link HasResult}<br>
+ *<br>
+ * This event is fired on the {@link MinecraftForge#EVENT_BUS}.
+ *
+ */
+@Cancelable
+public class EndermanAttackEvent extends LivingEvent {
 
     private final PlayerEntity player;
-    private final Entity entity;
+    private final EndermanEntity entity;
 
     public EndermanAttackEvent(EndermanEntity entity, PlayerEntity player)
     {
@@ -20,7 +31,7 @@ public class EndermanAttackEvent extends EntityEvent {
     }
 
     @Override
-    public Entity getEntity() {
+    public EndermanEntity getEntity() {
         return entity;
     }
 
