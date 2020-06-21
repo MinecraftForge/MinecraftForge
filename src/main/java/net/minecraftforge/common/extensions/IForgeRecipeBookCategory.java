c44be68366cb4c14f0248c368235bffcf13c8087
Jan Lalis <lalis.jan@gmail.com>
2020-06-21 11:41:00 +0200

    Code fixes, added doc

--- src/main/java/net/minecraftforge/common/extensions/IForgeRecipeBookCategory.java
+++ src/main/java/net/minecraftforge/common/extensions/IForgeRecipeBookCategory.java
@@ -9,8 +9,27 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public interface IForgeRecipeBookCategory<T extends IRecipeType<?>> extends IForgeRegistryEntry<IForgeRecipeBookCategory<?>> {
+    /**
+     * If tab is search - does not filter recipes and always is on most top position
+     * @return if tab is search
+     */
     boolean isSearch();
+
+    /**
+     * Filter for displayed recipes
+     * @return filtering function
+     */
     Predicate<IRecipe<?>> getPredicate();
+
+    /**
+     * Recipe type of recipe book category
+     * @return recipe type
+     */
     T getRecipeType();
+
+    /**
+     * Recipe tab icons, can be 1 or 2 ItemStack
+     * @return recipe tab icons
+     */
     List<ItemStack> getIcon();
 }
