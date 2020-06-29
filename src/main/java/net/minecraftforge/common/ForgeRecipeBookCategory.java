dd506270e0675e7d2415626d1021032a2bc05835
Yanny7 <lalis.jan@gmail.com>
2020-06-29 19:45:42 +0200

    Added missing license entries, Renamed isSearch to isUnfiltered

--- src/main/java/net/minecraftforge/common/ForgeRecipeBookCategory.java
+++ src/main/java/net/minecraftforge/common/ForgeRecipeBookCategory.java
@@ -1,3 +1,22 @@
+/*
+ * Minecraft Forge
+ * Copyright (c) 2016-2021.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package net.minecraftforge.common;
 
 import com.google.common.collect.ImmutableList;
@@ -13,35 +32,35 @@ import java.util.function.Predicate;
 
 public class ForgeRecipeBookCategory<T extends IRecipeType<?>> extends ForgeRegistryEntry<IForgeRecipeBookCategory<?>> implements IForgeRecipeBookCategory<T> {
     private final List<ItemStack> icons;
-    private final boolean isSearch;
+    private final boolean isUnfiltered;
     private final T recipeType;
     private final Predicate<IRecipe<?>> predicate;
 
     /**
-     * @param isSearch If tab is search
+     * @param isUnfiltered If tab is without filter
      * @param recipeType Recipe type
      * @param icons Recipe tab icons
      */
-    public ForgeRecipeBookCategory(boolean isSearch, T recipeType, ItemStack... icons) {
-        this(isSearch, recipeType, recipe -> true, icons);
+    public ForgeRecipeBookCategory(boolean isUnfiltered, T recipeType, ItemStack... icons) {
+        this(isUnfiltered, recipeType, recipe -> true, icons);
     }
 
     /**
-     * @param isSearch If tab is search
+     * @param isUnfiltered If tab is without filter
      * @param recipeType Recipe type
      * @param predicate Filtering function
      * @param icons Recipe tab icons
      */
-    public ForgeRecipeBookCategory(boolean isSearch, T recipeType, Predicate<IRecipe<?>> predicate, ItemStack... icons) {
-        this.isSearch = isSearch;
+    public ForgeRecipeBookCategory(boolean isUnfiltered, T recipeType, Predicate<IRecipe<?>> predicate, ItemStack... icons) {
+        this.isUnfiltered = isUnfiltered;
         this.recipeType = recipeType;
         this.predicate = predicate;
         this.icons = ImmutableList.copyOf(icons);
     }
 
     @Override
-    public boolean isSearch() {
-        return this.isSearch;
+    public boolean isUnfiltered() {
+        return this.isUnfiltered;
     }
 
     @Override
--- src/main/java/net/minecraftforge/registries/ForgeRegistries.java
+++ src/main/java/net/minecraftforge/registries/ForgeRegistries.java
@@ -114,6 +114,7 @@ public class ForgeRegistries
     public static final IForgeRegistry<DataSerializerEntry> DATA_SERIALIZERS = RegistryManager.ACTIVE.getRegistry(DataSerializerEntry.class);
     public static final IForgeRegistry<GlobalLootModifierSerializer<?>> LOOT_MODIFIER_SERIALIZERS = RegistryManager.ACTIVE.getRegistry(GlobalLootModifierSerializer.class);
     public static final IForgeRegistry<ForgeWorldType> WORLD_TYPES = RegistryManager.ACTIVE.getRegistry(ForgeWorldType.class);
+    public static final IForgeRegistry<IForgeRecipeBookCategory<?>> RECIPE_BOOK_CATEGORIES = RegistryManager.ACTIVE.getRegistry(IForgeRecipeBookCategory.class);
 
     public static final class Keys {
         //Vanilla
