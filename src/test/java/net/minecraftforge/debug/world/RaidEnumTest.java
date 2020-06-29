52b5b78e46863897305f91749e5e63629b359dfc
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2020-06-28 20:10:05 -0700

    did stuff

---
+++ src/test/java/net/minecraftforge/debug/world/RaidEnumTest.java
@@ -0,0 +1,19 @@
+package net.minecraftforge.debug.world;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.world.raid.Raid;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("raid_enum_test")
+public class RaidEnumTest 
+{
+	private static final boolean ENABLE = false;
+	
+	public RaidEnumTest()
+	{
+		if (ENABLE)
+		{
+			Raid.WaveMember.create("thebluemengroup", EntityType.ILLUSIONER, new int[]{0, 10, 1, 1, 1, 1, 1});
+		}
+	}
+}
