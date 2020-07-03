8e5138da2757c1ac71d104180bd0984fbcfaffb7
David Quintana <gigaherz@gmail.com>
2020-07-03 14:40:21 +0200

    Attempt to use a more compatible method to initialize stencil support. In case the separate attachments don't work for everyone, there's a new setting to choose the combined attachment.

--- src/main/java/net/minecraftforge/common/ForgeConfig.java
+++ src/main/java/net/minecraftforge/common/ForgeConfig.java
@@ -139,6 +139,8 @@ public class ForgeConfig
 
         public final BooleanValue showLoadWarnings;
 
+        public final BooleanValue useCombinedDepthStencilAttachment;
+
         Client(ForgeConfigSpec.Builder builder) {
             builder.comment("Client only settings, mostly things related to rendering")
                    .push("client");
@@ -183,6 +185,11 @@ public class ForgeConfig
                 .comment("When enabled, Forge will show any warnings that occurred during loading.")
                 .translation("forge.configgui.showLoadWarnings")
                 .define("showLoadWarnings", true);
+
+            useCombinedDepthStencilAttachment = builder
+                    .comment("Set to true to use a combined DEPTH_STENCIL attachment instead of two separate ones.")
+                    .translation("forge.configgui.useCombinedDepthStencilAttachment")
+                    .define("useCombinedDepthStencilAttachment", false);
 
             builder.pop();
         }
