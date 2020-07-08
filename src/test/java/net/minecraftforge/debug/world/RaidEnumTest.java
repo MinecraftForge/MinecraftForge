6bb0c33ef9d7917b975c2e4dcc50e7b38bb5c65e
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2020-07-08 09:58:16 -0700

    fixed boolean

--- src/test/java/net/minecraftforge/debug/world/RaidEnumTest.java
+++ src/test/java/net/minecraftforge/debug/world/RaidEnumTest.java
@@ -7,7 +7,7 @@ import net.minecraftforge.fml.common.Mod;
 @Mod("raid_enum_test")
 public class RaidEnumTest 
 {
-    private static final boolean ENABLE = true;
+    private static final boolean ENABLE = false;
 	
     public RaidEnumTest()
     {
