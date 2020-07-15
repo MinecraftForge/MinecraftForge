da6ae4db4c23a20cc46f281d6504594e20f51c78
Jared <jaredlll08@gmail.com>
2020-07-15 21:30:41 +0200

    Added an event for registering commands. closes #6968 (#6969)

--- src/main/java/net/minecraftforge/fml/event/server/FMLServerStartingEvent.java
+++ src/main/java/net/minecraftforge/fml/event/server/FMLServerStartingEvent.java
@@ -19,14 +19,13 @@
 
 package net.minecraftforge.fml.event.server;
 
-import com.mojang.brigadier.CommandDispatcher;
-import net.minecraft.command.CommandSource;
 import net.minecraft.server.MinecraftServer;
 
 /**
  * Called after {@link FMLServerAboutToStartEvent} and before {@link FMLServerStartedEvent}.
- * This event allows for customizations of the server, such as loading custom commands, perhaps customizing recipes or
- * other activities.
+ * This event allows for customizations of the server.
+ *
+ * If you need to add commands use {@link net.minecraftforge.event.RegisterCommandsEvent}.
  *
  * @author cpw
  */
@@ -37,7 +36,4 @@ public class FMLServerStartingEvent extends ServerLifecycleEvent
         super(server);
     }
 
-    public CommandDispatcher<CommandSource> getCommandDispatcher() {
-        return server.getCommandManager().getDispatcher();
-    }
 }
