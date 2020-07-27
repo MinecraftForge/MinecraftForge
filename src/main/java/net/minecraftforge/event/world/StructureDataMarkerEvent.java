455ad4d42839661557df63764c8a392e100e1bb9
PssbleTrngle <nik@somethingcatchy.net>
2020-07-27 13:19:23 +0200

    Documentation & field getters

--- src/main/java/net/minecraftforge/event/world/StructureDataMarkerEvent.java
+++ src/main/java/net/minecraftforge/event/world/StructureDataMarkerEvent.java
@@ -3,22 +3,31 @@ package net.minecraftforge.event.world;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MutableBoundingBox;
 import net.minecraft.world.IWorld;
+import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.structure.TemplateStructurePiece;
 import net.minecraftforge.eventbus.api.Cancelable;
 import net.minecraftforge.eventbus.api.Event;
 
 import java.util.Random;
 
+/**
+ * Fired before a {@link TemplateStructurePiece#handleDataMarker}
+ * The {@link #function} is saved metadata of the marker
+ * The marker is is saved at {@link #pos} and generated in {@link #world} using {@link #rand}
+ * The generated structure is {@link #structurePiece}
+ *
+ * Cancelling the event will prevent the vanilla logic to be executed
+ */
 @Cancelable
 public class StructureDataMarkerEvent extends Event
 {
 
-    public final String function;
-    public final BlockPos pos;
-    public final IWorld world;
-    public final Random rand;
-    public final MutableBoundingBox sbb;
-    public final TemplateStructurePiece structurePiece;
+    private final String function;
+    private final BlockPos pos;
+    private final IWorld world;
+    private final Random rand;
+    private final MutableBoundingBox sbb;
+    private final TemplateStructurePiece structurePiece;
 
     public StructureDataMarkerEvent(TemplateStructurePiece structurePiece, String function, BlockPos pos, IWorld world, Random rand, MutableBoundingBox sbb)
     {
@@ -28,5 +37,70 @@ public class StructureDataMarkerEvent extends Event
         this.rand = rand;
         this.sbb = sbb;
         this.structurePiece = structurePiece;
+    }
+
+
+    /**
+     * Get the bounding box
+     *
+     * @return the piece bounding box
+     */
+    public MutableBoundingBox getBoundingBox()
+    {
+        return sbb;
+    }
+
+    /**
+     * Get the generated piece instance
+     *
+     * @return the generated piece instance
+     */
+    public TemplateStructurePiece getStructurePiece()
+    {
+        return structurePiece;
+    }
+
+
+    /**
+     * The used random
+     *
+     * @return the random
+     */
+    public Random getRandom()
+    {
+        return rand;
+    }
+
+
+    /**
+     * Get the world
+     *
+     * @return the world
+     */
+    public IWorld getWorld()
+    {
+        return world;
+    }
+
+
+    /**
+     * Get position
+     *
+     * @return the position
+     */
+    public BlockPos getPos()
+    {
+        return pos;
+    }
+
+
+    /**
+     * Get metadata
+     *
+     * @return the metadata
+     */
+    public String getFunction()
+    {
+        return function;
     }
 }
--- src/test/java/net/minecraftforge/debug/world/StructureDataMarkerTest.java
+++ src/test/java/net/minecraftforge/debug/world/StructureDataMarkerTest.java
@@ -20,20 +20,20 @@ public class StructureDataMarkerTest
     @SubscribeEvent
     public static void onDataMarker(StructureDataMarkerEvent event)
     {
-        if (event.structurePiece instanceof EndCityPieces.CityTemplate && event.function.startsWith("Chest"))
+        if (event.getStructurePiece() instanceof EndCityPieces.CityTemplate && event.getFunction().startsWith("Chest"))
         {
             // Prevent generation of loot in end cities
             event.setCanceled(true);
         }
-        else if (event.structurePiece instanceof WoodlandMansionPieces.MansionTemplate && event.function.equals("Warrior"))
+        else if (event.getStructurePiece() instanceof WoodlandMansionPieces.MansionTemplate && event.getFunction().equals("Warrior"))
         {
             // Replace naturally generated vindicators in mansion with wither skeletons
             event.setCanceled(true);
-            WitherSkeletonEntity warrior = EntityType.WITHER_SKELETON.create(event.world.getWorld());
+            WitherSkeletonEntity warrior = EntityType.WITHER_SKELETON.create(event.getWorld().getWorld());
             warrior.enablePersistence();
-            warrior.moveToBlockPosAndAngles(event.pos, 0.0F, 0.0F);
-            warrior.onInitialSpawn(event.world, event.world.getDifficultyForLocation(warrior.func_233580_cy_()), SpawnReason.STRUCTURE, null, null);
-            event.world.addEntity(warrior);
+            warrior.moveToBlockPosAndAngles(event.getPos(), 0.0F, 0.0F);
+            warrior.onInitialSpawn(event.getWorld(), event.getWorld().getDifficultyForLocation(warrior.func_233580_cy_()), SpawnReason.STRUCTURE, null, null);
+            event.getWorld().addEntity(warrior);
         }
     }
 
