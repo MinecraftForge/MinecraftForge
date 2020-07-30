abfea7a180b427e1a3a549cecf949b94ab8b98c2
Mysterious Devs <mysteriousdevs@gmail.com>
2020-07-30 18:14:40 +0200

    Add possibility to create custom GameRule Category

--- src/test/java/net/minecraftforge/debug/misc/GameRulesTest.java
+++ src/test/java/net/minecraftforge/debug/misc/GameRulesTest.java
@@ -6,7 +6,9 @@ import net.minecraftforge.fml.common.Mod;
 @Mod("gamerulestest")
 public class GameRulesTest {
 
-    public static final GameRules.RuleKey<GameRules.BooleanValue> TEST_BOOLEAN_GAMERULE = GameRules.func_234903_a_("test_boolean_gamerule", GameRules.Category.MISC, GameRules.BooleanValue.create(true));
-    public static final GameRules.RuleKey<GameRules.IntegerValue> TEST_INTEGER_GAMERULE = GameRules.func_234903_a_("test_integer_gamerule", GameRules.Category.MISC, GameRules.IntegerValue.create(60));
+    public static GameRules.Category TEST_CATEGORY = GameRules.Category.create("TEST_CATEGORY", "gamerule.category.test_category");
+
+    public static final GameRules.RuleKey<GameRules.BooleanValue> TEST_BOOLEAN_GAMERULE = GameRules.func_234903_a_("test_boolean_gamerule", TEST_CATEGORY, GameRules.BooleanValue.create(true));
+    public static final GameRules.RuleKey<GameRules.IntegerValue> TEST_INTEGER_GAMERULE = GameRules.func_234903_a_("test_integer_gamerule", TEST_CATEGORY, GameRules.IntegerValue.create(60));
 
 }
