33712cefe96509da7e9e5b1b83771d7ff66b42c2
DaemonUmbra <daemon.umbra@gmail.com>
2020-08-01 16:34:05 -0400

    Added Class-level SAS capability to checkSAS

--- buildSrc/src/main/groovy/net/minecraftforge/forge/tasks/CheckSAS.groovy
+++ buildSrc/src/main/groovy/net/minecraftforge/forge/tasks/CheckSAS.groovy
@@ -21,29 +21,39 @@ public class CheckSAS extends DefaultTask {
 		sass.each { f -> 
 			def lines = []
 			f.eachLine { line ->
-				if (line[0] == '\t') return //Skip any tabed lines, those are ones we add
+				if (line[0] == '\t') return //Skip any tabbed lines, those are ones we add
 				def idx = line.indexOf('#')
 				if (idx == 0 || line.isEmpty()) {
 					lines.add(line)
 					return
 				}
-				
+
 				def comment = idx == -1 ? null : line.substring(idx)
 				if (idx != -1) line = line.substring(0, idx - 1)
-				
+
 				def (cls, desc) = (line.trim() + '    ').split(' ', -1)
 				cls = cls.replaceAll('\\.', '/')
 				desc = desc.replace('(', ' (')
-				if (desc.isEmpty() || json[cls] == null || json[cls]['methods'] == null || json[cls]['methods'][desc] == null) {
+
+				if (json[cls] == null || (!desc.isEmpty() && (json[cls]['methods'] == null || json[cls]['methods'][desc] == null))) {
 					println('Invalid: ' + line)
 					return
 				}
-				
-				def mtd = json[cls]['methods'][desc]
+
+				//Class SAS
+				if (desc.isEmpty()) {
+					lines.add(cls + (comment == null ? '' : ' ' + comment))
+					if (json[cls]['methods'] != null)
+						(json[cls]['methods'] as TreeMap).each {
+							lines.add('\t' + cls + ' ' + it.key.replace(' ', ''))
+							findChildMethods(json, cls, it.key).each { lines.add('\t' + it) }
+						}
+					return
+				}
+
+				//Method SAS
 				lines.add(cls + ' ' + desc.replace(' ', '') + (comment == null ? '' : ' ' + comment))
-				def children = json.values().findAll{ it.methods != null && it.methods[desc] != null && it.methods[desc].override == cls}
-				.collect { it.name + ' ' + desc.replace(' ', '') } as TreeSet
-				children.each { lines.add('\t' + it) }
+				findChildMethods(json, cls, desc).each { lines.add('\t' + it) }
 			}
 			f.text = lines.join('\n')
 		}
