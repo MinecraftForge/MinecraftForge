2ca7bfd1576113be6dfa339cc5c6e173847c2ae2
GirafiStudios <faj10@msn.com>
2020-09-15 11:07:25 +0200

    Added tags to Bamboo biomes Added Modified Jungle Edge to wet

--- src/main/java/net/minecraftforge/common/data/BiomeTagsProvider.java
+++ src/main/java/net/minecraftforge/common/data/BiomeTagsProvider.java
@@ -47,7 +47,8 @@ public class BiomeTagsProvider extends ForgeRegistryTagsProvider<Biome>
         func_240522_a_(Tags.Biomes.HOT).add(Biomes.DESERT, Biomes.field_235254_j_, Biomes.field_235252_ay_, Biomes.field_235253_az_, Biomes.field_235250_aA_, Biomes.field_235251_aB_,
                 Biomes.DESERT_HILLS, Biomes.JUNGLE, Biomes.JUNGLE_HILLS, Biomes.JUNGLE_EDGE,
                 Biomes.SAVANNA, Biomes.SAVANNA_PLATEAU, Biomes.WARM_OCEAN, Biomes.DEEP_WARM_OCEAN, Biomes.DESERT_LAKES, Biomes.MODIFIED_JUNGLE, Biomes.MODIFIED_JUNGLE_EDGE,
-                Biomes.SHATTERED_SAVANNA, Biomes.SHATTERED_SAVANNA_PLATEAU, Biomes.ERODED_BADLANDS, Biomes.MODIFIED_WOODED_BADLANDS_PLATEAU, Biomes.MODIFIED_BADLANDS_PLATEAU);
+                Biomes.SHATTERED_SAVANNA, Biomes.SHATTERED_SAVANNA_PLATEAU, Biomes.ERODED_BADLANDS, Biomes.MODIFIED_WOODED_BADLANDS_PLATEAU, Biomes.MODIFIED_BADLANDS_PLATEAU,
+                Biomes.BAMBOO_JUNGLE, Biomes.BAMBOO_JUNGLE_HILLS);
         func_240522_a_(Tags.Biomes.COLD).add(Biomes.TAIGA, Biomes.THE_END, Biomes.SMALL_END_ISLANDS, Biomes.END_MIDLANDS, Biomes.END_HIGHLANDS, Biomes.END_BARRENS,
                 Biomes.FROZEN_OCEAN, Biomes.FROZEN_RIVER, Biomes.SNOWY_TUNDRA, Biomes.SNOWY_MOUNTAINS, Biomes.TAIGA_HILLS, Biomes.SNOWY_BEACH, Biomes.SNOWY_TAIGA,
                 Biomes.SNOWY_TAIGA_HILLS, Biomes.GIANT_TREE_TAIGA, Biomes.GIANT_TREE_TAIGA_HILLS, Biomes.COLD_OCEAN, Biomes.DEEP_COLD_OCEAN, Biomes.TAIGA_MOUNTAINS,
@@ -57,7 +58,8 @@ public class BiomeTagsProvider extends ForgeRegistryTagsProvider<Biome>
                 Biomes.MODIFIED_WOODED_BADLANDS_PLATEAU, Biomes.MODIFIED_BADLANDS_PLATEAU);
         func_240522_a_(Tags.Biomes.DENSE).add(Biomes.JUNGLE, Biomes.JUNGLE_HILLS, Biomes.DARK_FOREST, Biomes.MODIFIED_JUNGLE, Biomes.TALL_BIRCH_FOREST, Biomes.TALL_BIRCH_HILLS,
                 Biomes.DARK_FOREST_HILLS, Biomes.GIANT_SPRUCE_TAIGA, Biomes.GIANT_SPRUCE_TAIGA_HILLS);
-        func_240522_a_(Tags.Biomes.WET).add(Biomes.SWAMP, Biomes.JUNGLE, Biomes.JUNGLE_HILLS, Biomes.JUNGLE_EDGE, Biomes.SWAMP_HILLS, Biomes.MODIFIED_JUNGLE);
+        func_240522_a_(Tags.Biomes.WET).add(Biomes.SWAMP, Biomes.JUNGLE, Biomes.JUNGLE_HILLS, Biomes.JUNGLE_EDGE, Biomes.SWAMP_HILLS, Biomes.MODIFIED_JUNGLE,
+                Biomes.MODIFIED_JUNGLE_EDGE, Biomes.BAMBOO_JUNGLE, Biomes.BAMBOO_JUNGLE_HILLS);
         func_240522_a_(Tags.Biomes.DRY).add(Biomes.DESERT, Biomes.field_235254_j_, Biomes.field_235252_ay_, Biomes.field_235253_az_, Biomes.field_235250_aA_, Biomes.field_235251_aB_,
                 Biomes.THE_END, Biomes.SMALL_END_ISLANDS, Biomes.END_MIDLANDS, Biomes.END_HIGHLANDS,
                 Biomes.END_BARRENS, Biomes.DESERT_HILLS, Biomes.BADLANDS, Biomes.WOODED_BADLANDS_PLATEAU, Biomes.BADLANDS_PLATEAU, Biomes.DESERT_LAKES,
@@ -65,7 +67,8 @@ public class BiomeTagsProvider extends ForgeRegistryTagsProvider<Biome>
         func_240522_a_(Tags.Biomes.SAVANNA).add(Biomes.SAVANNA, Biomes.SAVANNA_PLATEAU, Biomes.SHATTERED_SAVANNA, Biomes.SHATTERED_SAVANNA_PLATEAU);
         func_240522_a_(Tags.Biomes.CONIFEROUS).add(Biomes.TAIGA, Biomes.TAIGA_HILLS, Biomes.SNOWY_TAIGA, Biomes.SNOWY_TAIGA_HILLS, Biomes.GIANT_TREE_TAIGA,
                 Biomes.GIANT_TREE_TAIGA_HILLS, Biomes.TAIGA_MOUNTAINS, Biomes.SNOWY_TAIGA_MOUNTAINS);
-        func_240522_a_(Tags.Biomes.JUNGLE).add(Biomes.JUNGLE, Biomes.JUNGLE_HILLS, Biomes.JUNGLE_EDGE, Biomes.MODIFIED_JUNGLE, Biomes.MODIFIED_JUNGLE_EDGE);
+        func_240522_a_(Tags.Biomes.JUNGLE).add(Biomes.JUNGLE, Biomes.JUNGLE_HILLS, Biomes.JUNGLE_EDGE, Biomes.MODIFIED_JUNGLE, Biomes.MODIFIED_JUNGLE_EDGE, Biomes.BAMBOO_JUNGLE,
+                Biomes.BAMBOO_JUNGLE_HILLS);
         func_240522_a_(Tags.Biomes.SPOOKY).add(Biomes.DARK_FOREST, Biomes.DARK_FOREST_HILLS);
         func_240522_a_(Tags.Biomes.MUSHROOM).add(Biomes.MUSHROOM_FIELDS, Biomes.MUSHROOM_FIELD_SHORE);
         func_240522_a_(Tags.Biomes.RARE).add(Biomes.MUSHROOM_FIELDS, Biomes.MUSHROOM_FIELD_SHORE, Biomes.JUNGLE_EDGE, Biomes.SAVANNA_PLATEAU, Biomes.SUNFLOWER_PLAINS,
@@ -91,7 +94,7 @@ public class BiomeTagsProvider extends ForgeRegistryTagsProvider<Biome>
                 Biomes.SHATTERED_SAVANNA, Biomes.ERODED_BADLANDS, Biomes.MODIFIED_BADLANDS_PLATEAU);
         func_240522_a_(Tags.Biomes.HILLS).add(Biomes.MOUNTAINS, Biomes.DESERT_HILLS, Biomes.WOODED_HILLS, Biomes.TAIGA_HILLS, Biomes.JUNGLE_HILLS, Biomes.BIRCH_FOREST_HILLS,
                 Biomes.SNOWY_TAIGA_HILLS, Biomes.GIANT_TREE_TAIGA_HILLS, Biomes.FLOWER_FOREST, Biomes.SWAMP_HILLS, Biomes.ICE_SPIKES, Biomes.MODIFIED_JUNGLE_EDGE,
-                Biomes.TALL_BIRCH_FOREST, Biomes.GIANT_SPRUCE_TAIGA_HILLS, Biomes.SHATTERED_SAVANNA_PLATEAU, Biomes.MODIFIED_WOODED_BADLANDS_PLATEAU);
+                Biomes.TALL_BIRCH_FOREST, Biomes.GIANT_SPRUCE_TAIGA_HILLS, Biomes.SHATTERED_SAVANNA_PLATEAU, Biomes.MODIFIED_WOODED_BADLANDS_PLATEAU, Biomes.BAMBOO_JUNGLE_HILLS);
         func_240522_a_(Tags.Biomes.SWAMP).add(Biomes.SWAMP, Biomes.SWAMP_HILLS);
         func_240522_a_(Tags.Biomes.SANDY).add(Biomes.DESERT, Biomes.DESERT_HILLS, Biomes.BADLANDS, Biomes.WOODED_BADLANDS_PLATEAU, Biomes.BADLANDS_PLATEAU, Biomes.DESERT_LAKES);
         func_240522_a_(Tags.Biomes.SNOWY).add(Biomes.FROZEN_OCEAN, Biomes.FROZEN_RIVER, Biomes.SNOWY_TUNDRA, Biomes.SNOWY_MOUNTAINS, Biomes.SNOWY_BEACH, Biomes.SNOWY_TAIGA,
@@ -109,7 +112,8 @@ public class BiomeTagsProvider extends ForgeRegistryTagsProvider<Biome>
                 Biomes.SUNFLOWER_PLAINS, Biomes.DESERT_LAKES, Biomes.GRAVELLY_MOUNTAINS, Biomes.FLOWER_FOREST, Biomes.TAIGA_MOUNTAINS, Biomes.SWAMP_HILLS,
                 Biomes.ICE_SPIKES, Biomes.MODIFIED_JUNGLE, Biomes.MODIFIED_JUNGLE_EDGE, Biomes.TALL_BIRCH_FOREST, Biomes.TALL_BIRCH_HILLS, Biomes.DARK_FOREST_HILLS,
                 Biomes.SNOWY_TAIGA_MOUNTAINS, Biomes.GIANT_SPRUCE_TAIGA, Biomes.GIANT_SPRUCE_TAIGA_HILLS, Biomes.MODIFIED_GRAVELLY_MOUNTAINS, Biomes.SHATTERED_SAVANNA,
-                Biomes.SHATTERED_SAVANNA_PLATEAU, Biomes.ERODED_BADLANDS, Biomes.MODIFIED_WOODED_BADLANDS_PLATEAU, Biomes.MODIFIED_BADLANDS_PLATEAU);
+                Biomes.SHATTERED_SAVANNA_PLATEAU, Biomes.ERODED_BADLANDS, Biomes.MODIFIED_WOODED_BADLANDS_PLATEAU, Biomes.MODIFIED_BADLANDS_PLATEAU,
+                Biomes.BAMBOO_JUNGLE, Biomes.BAMBOO_JUNGLE_HILLS);
         func_240522_a_(Tags.Biomes.NETHER).add(Biomes.field_235254_j_, Biomes.field_235252_ay_, Biomes.field_235253_az_, Biomes.field_235250_aA_, Biomes.field_235251_aB_);
         func_240522_a_(Tags.Biomes.END).add(Biomes.THE_END, Biomes.SMALL_END_ISLANDS, Biomes.END_MIDLANDS, Biomes.END_HIGHLANDS, Biomes.END_BARRENS);
     }
