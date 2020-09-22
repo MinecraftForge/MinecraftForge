341e61415b08cef3f9b5d002433737bbb68569fc
Alexiy <alexiy.ov@gmail.com>
2020-09-22 15:09:04 +0300

    Entry extraction

--- src/main/java/net/minecraftforge/fml/client/gui/screen/DefaultConfigScreen.java
+++ src/main/java/net/minecraftforge/fml/client/gui/screen/DefaultConfigScreen.java
@@ -4,6 +4,8 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.common.ForgeConfigSpec;
 
+import java.util.Map;
+
 public class DefaultConfigScreen extends Screen {
     protected Screen previousScreen;
     protected ForgeConfigSpec configSpec;
@@ -17,6 +19,21 @@ public class DefaultConfigScreen extends Screen {
     @Override
     protected void init() {
         super.init();
+        Map<String, Object> specValues = configSpec.getSpec().valueMap();
+        Map<String, Object> valueValues = configSpec.getValues().valueMap();
+        ;
+        int spacing = 30;
+        for (String key : specValues.keySet()) {
+            Object value = specValues.get(key);
+            if (value instanceof ForgeConfigSpec.ValueSpec) {
+                ForgeConfigSpec.ValueSpec valueSpec = (ForgeConfigSpec.ValueSpec) value;
+                Object specValue = valueValues.get(key);
+                if (specValue instanceof ForgeConfigSpec.BooleanValue) {
+                    ForgeConfigSpec.BooleanValue booleanValue = (ForgeConfigSpec.BooleanValue) specValue;
+                    Boolean b = booleanValue.get();
+                }
+            }
+        }
     }
 
     @Override
