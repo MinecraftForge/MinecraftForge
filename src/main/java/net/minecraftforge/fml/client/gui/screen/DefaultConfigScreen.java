35788d61fa6261bc8f68e428966bbc68e927ed49
Alexiy <alexiy.ov@gmail.com>
2020-09-22 15:59:29 +0300

    Handle boolean values

--- src/main/java/net/minecraftforge/fml/client/gui/screen/DefaultConfigScreen.java
+++ src/main/java/net/minecraftforge/fml/client/gui/screen/DefaultConfigScreen.java
@@ -1,8 +1,11 @@
 package net.minecraftforge.fml.client.gui.screen;
 
+import com.electronwill.nightconfig.core.UnmodifiableConfig;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.client.gui.IPressHandler;
+import net.minecraftforge.fml.client.gui.widget.ExtendedButton;
 
 import java.util.Map;
 
@@ -19,18 +22,33 @@ public class DefaultConfigScreen extends Screen {
     @Override
     protected void init() {
         super.init();
-        Map<String, Object> specValues = configSpec.getSpec().valueMap();
-        Map<String, Object> valueValues = configSpec.getValues().valueMap();
-        ;
+        Map<String, Object> specMap = configSpec.getSpec().valueMap();
+        Map<String, Object> valueMap = configSpec.getValues().valueMap();
         int spacing = 30;
-        for (String key : specValues.keySet()) {
-            Object value = specValues.get(key);
-            if (value instanceof ForgeConfigSpec.ValueSpec) {
-                ForgeConfigSpec.ValueSpec valueSpec = (ForgeConfigSpec.ValueSpec) value;
-                Object specValue = valueValues.get(key);
-                if (specValue instanceof ForgeConfigSpec.BooleanValue) {
-                    ForgeConfigSpec.BooleanValue booleanValue = (ForgeConfigSpec.BooleanValue) specValue;
-                    Boolean b = booleanValue.get();
+        int x = 6;
+        for (String key : specMap.keySet()) {
+            Object value = specMap.get(key);
+            if (value instanceof UnmodifiableConfig) {
+                UnmodifiableConfig unmodifiableConfig = (UnmodifiableConfig) value;
+                Map<String, Object> entries = unmodifiableConfig.valueMap();
+                UnmodifiableConfig actualValues = (UnmodifiableConfig) valueMap.get(key);
+                for (String k : entries.keySet()) {
+                    Object v = entries.get(k);
+                    if (v instanceof ForgeConfigSpec.ValueSpec) {
+                        ForgeConfigSpec.ValueSpec valueSpec = (ForgeConfigSpec.ValueSpec) v;
+                        Object specValue = actualValues.get(k);
+                        if (specValue instanceof ForgeConfigSpec.BooleanValue) {
+                            ForgeConfigSpec.BooleanValue booleanValue = (ForgeConfigSpec.BooleanValue) specValue;
+                            Boolean b = booleanValue.get();
+                            ExtendedButton switchButton = new ExtendedButton(x, spacing, font.getStringWidth(k + b.toString()) + 30, 18, k + ": " + b.toString(), (IPressHandler<ExtendedButton>) button -> {
+                                booleanValue.set(!booleanValue.get());
+                                button.setMessage(k + ": " + booleanValue.get());
+                                booleanValue.save();
+                            });
+                            addButton(switchButton);
+                            spacing += 20;
+                        }
+                    }
                 }
             }
         }
@@ -41,6 +59,7 @@ public class DefaultConfigScreen extends Screen {
         renderBackground();
         drawCenteredString(font, title.getFormattedText(), width / 2, 6, 0xffffff);
         super.render(p_render_1_, p_render_2_, p_render_3_);
+
     }
 
     @Override
