1abe5083ee56217a9c5ee1eff7beb5887645d925
Tobias Hotz <tobias.hotz@hotmail.de>
2020-09-24 09:07:10 +0200

    move to fml.loading subpackage

--- src/fmllauncher/java/net/minecraftforge/fml/loading/log4j/ForgeHighlight.java
+++ src/fmllauncher/java/net/minecraftforge/fml/loading/log4j/ForgeHighlight.java
@@ -17,7 +17,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-package net.minecraftforge.fml.log4j;
+package net.minecraftforge.fml.loading.log4j;
 
 import net.minecrell.terminalconsole.HighlightErrorConverter;
 import net.minecrell.terminalconsole.TerminalConsoleAppender;
