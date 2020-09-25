ebb014bb4bdec51c165415b8381a66be7015ec85
Ocelot <FinntheRaider@gmail.com>
2020-09-24 22:48:40 -0700

    Add test mod

---
+++ src/test/java/net/minecraftforge/debug/entity/player/PlayerGameModeEventTest.java
@@ -0,0 +1,42 @@
+/*
+ * Minecraft Forge
+ * Copyright (c) 2016-2020.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package net.minecraftforge.debug.entity.player;
+
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("player_game_mode_event_test")
+@Mod.EventBusSubscriber()
+public class PlayerGameModeEventTest
+{
+    private static final boolean ENABLE = true;
+    private static final Logger LOGGER = LogManager.getLogger(PlayerGameModeEventTest.class);
+
+    @SubscribeEvent
+    public static void onPlayerChangeGameModeEvent(PlayerEvent.PlayerChangeGameModeEvent event)
+    {
+        if (!ENABLE) return;
+        LOGGER.info("{} changed game mode. Old GameType: {}. Current Game Type: {}", event.getPlayer(), event.getPreviousGameMode(), event.getCurrentGameMode());
+    }
+}
