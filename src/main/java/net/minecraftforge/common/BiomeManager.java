736bdc1cfd34131786ee8ab6a2e0d017bdd61fe9
maxanier <dev@maxgb.de>
2020-09-26 13:25:50 +0200

    Prevent duplicate entries

--- src/main/java/net/minecraftforge/common/BiomeManager.java
+++ src/main/java/net/minecraftforge/common/BiomeManager.java
@@ -101,8 +101,11 @@ public class BiomeManager
     /**
      * Add biomes that you add to the overworld without using {@link BiomeManager#addBiome(BiomeType, BiomeEntry)}
      */
-    public static void addAdditionalOverworldBiomes(List<RegistryKey<Biome>> biomes){
-        additionalOverworldBiomes.addAll(biomes);
+    public static void addAdditionalOverworldBiomes(RegistryKey<Biome> biome){
+        if (!"minecraft".equals(biome.func_240901_a_().getNamespace()) && additionalOverworldBiomes.stream().noneMatch(entry -> entry.compareTo(biome) == 0))
+        {
+            additionalOverworldBiomes.add(biome);
+        }
     }
 
     public static boolean addBiome(BiomeType type, BiomeEntry entry)
