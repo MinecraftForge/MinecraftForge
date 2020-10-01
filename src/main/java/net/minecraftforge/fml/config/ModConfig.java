5a0f0ce42809fe704cfaba596e4cb42772361659
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2020-10-01 22:26:07 +1000

    3rd

--- src/main/java/net/minecraftforge/fml/config/ModConfig.java
+++ src/main/java/net/minecraftforge/fml/config/ModConfig.java
@@ -86,7 +86,7 @@ public class ModConfig
         this.spec.setConfig(this.configData);
     }
 
-    public void fireEvent(final ModConfigEvent configEvent) {
+    void fireEvent(final ModConfigEvent configEvent) {
         this.container.dispatchConfigEvent(configEvent);
     }
 
