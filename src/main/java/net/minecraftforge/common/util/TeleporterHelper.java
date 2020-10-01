1dbdae0dddbf50f097780bf2f3c6b040d4b0f62c
Silver_David <djbake101@gmail.com>
2020-09-30 20:36:07 -0400

    Removed getPortalInfo from TeleporterHelper as it's unneeded

--- src/main/java/net/minecraftforge/common/util/TeleporterHelper.java
+++ src/main/java/net/minecraftforge/common/util/TeleporterHelper.java
@@ -1,17 +1,10 @@
 package net.minecraftforge.common.util;
 
-import net.minecraft.block.PortalInfo;
-import net.minecraft.block.PortalSize;
-import net.minecraft.entity.Entity;
-import net.minecraft.util.Direction;
-import net.minecraft.util.TeleportationRepositioner;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.World;
 import net.minecraft.world.border.WorldBorder;
-import net.minecraft.world.server.ServerWorld;
 
 public class TeleporterHelper
 {
@@ -29,18 +22,6 @@ public class TeleporterHelper
         return new BlockPos(MathHelper.clamp(originalPos.getX() * dimensionScaling, minX, maxX), originalPos.getY(), MathHelper.clamp(originalPos.getZ() * dimensionScaling, minZ, maxZ));
     }
 
-    /**
-     * An overload of {@link ITeleporter#getPortalInfo} that takes the entity being teleported as well as the
-     * destination world to create a complete {@link PortalInfo}.
-     */
-    public static PortalInfo getPortalInfo(TeleportationRepositioner.Result tpResult, Entity entity, ServerWorld destWorld)
-    {
-        Direction.Axis axis1 = Direction.Axis.X;
-        Vector3d vector3d = new Vector3d(0.5D, 0.0D, 0.0D);
-        return PortalSize.func_242963_a(destWorld, tpResult, axis1, vector3d,
-                entity.getSize(entity.getPose()), entity.getMotion(), entity.rotationYaw, entity.rotationPitch);
-    }
-    
     /**
      * Gets the search radius to look for a portal based on dimension scaling.
      */
