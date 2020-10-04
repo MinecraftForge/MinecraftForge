74698ea0ca0434fb80056cb6b5dc3242559a2929
Cyborgmas <cyborgmas18@gmail.com>
2020-10-03 22:36:45 -0400

    test

--- src/main/java/net/minecraftforge/common/world/ICodecExtra.java
+++ src/main/java/net/minecraftforge/common/world/ICodecExtra.java
@@ -1,17 +1,31 @@
 package net.minecraftforge.common.world;
 
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.ForgeRegistryEntry;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public interface ICodecExtra
 {
-    Codec<CodecExtraType<?>> FROM_REGISTRY = ResourceLocation.field_240908_a_.xmap(ForgeRegistries.CODEC_EXTRA_TYPES::getValue, ForgeRegistryEntry::getRegistryName);
+    Codec<CodecExtraType<?>> FROM_REGISTRY = ResourceLocation.field_240908_a_.xmap(ForgeRegistries.CODEC_EXTRA_TYPES::getValue, ForgeRegistries.CODEC_EXTRA_TYPES::getKey);
     Codec<ICodecExtra> GENERIC_CODEC = FROM_REGISTRY.dispatch(ICodecExtra::getType, CodecExtraType::getCodec);
-    Codec<List<ICodecExtra>> LIST_EXTRAS = GENERIC_CODEC.listOf().fieldOf("forge_extras").codec();
+    Codec<List<ICodecExtra>> LIST_CODEC = GENERIC_CODEC.listOf().fieldOf("forge_extras").codec().flatXmap(ICodecExtra::ensureUnique, ICodecExtra::ensureUnique);
+
+    static DataResult<List<ICodecExtra>> ensureUnique(List<ICodecExtra> extras)
+    {
+        Set<ResourceLocation> toTest = new HashSet<>();
+        for(ICodecExtra extra : extras)
+        {
+            if(!toTest.add(extra.getType().getRegistryName()))
+                return DataResult.error("Duplicate codec extra entries were found: " + extra.getType().getRegistryName(), extras);
+        }
+
+        return DataResult.success(extras);
+    }
 
     CodecExtraType<?> getType();
 }
--- src/main/java/net/minecraftforge/common/world/ICodecExtraHolder.java
+++ src/main/java/net/minecraftforge/common/world/ICodecExtraHolder.java
@@ -12,11 +12,14 @@ import java.util.Optional;
 
 public interface ICodecExtraHolder
 {
-    IdentityHashMap<ICodecExtraHolder, List<ICodecExtra>> EXTRA_HOLDER = new IdentityHashMap<>();
+    class PrivateFieldHolder
+    {
+        private static IdentityHashMap<ICodecExtraHolder, List<ICodecExtra>> EXTRA_HOLDER = new IdentityHashMap<>();
+    }
 
     default void setExtra(ICodecExtra extra)
     {
-        List<ICodecExtra> extras = EXTRA_HOLDER.computeIfAbsent(this, s -> new ArrayList<>());
+        List<ICodecExtra> extras = PrivateFieldHolder.EXTRA_HOLDER.computeIfAbsent(this, s -> new ArrayList<>());
         if(extras.stream().anyMatch(e -> e.getType().equals(extra.getType())))
             throw new IllegalArgumentException("Can not add twice the same extra type to the same object");
         extras.add(extra);
@@ -24,7 +27,7 @@ public interface ICodecExtraHolder
 
     default void setupExtras(List<ICodecExtra> extras)
     {
-        if(EXTRA_HOLDER.put(this, extras) != null)
+        if(PrivateFieldHolder.EXTRA_HOLDER.put(this, extras) != null)
         {
             throw new RuntimeException("Setup extras on the same object twice!");
         }
@@ -32,22 +35,26 @@ public interface ICodecExtraHolder
 
     default <A extends ICodecExtra> Optional<A> getExtra(CodecExtraType<A> type)
     {
-        return EXTRA_HOLDER.get(this).stream().filter(e -> e.getType().equals(type)).findAny().map(e -> (A) e);
+        return PrivateFieldHolder.EXTRA_HOLDER.get(this).stream().filter(e -> e.getType().equals(type)).findAny().map(e -> (A) e);
     }
 
     class ExtraParser<E extends ICodecExtraHolder> implements Codec<E>
     {
         private final Codec<E> base;
 
-        public ExtraParser(Codec<E> base) {
+        public ExtraParser(Codec<E> base)
+        {
             this.base = base;
         }
 
         @Override
-        public <T> DataResult<Pair<E, T>> decode(DynamicOps<T> ops, T input) {
-            return this.base.decode(ops, input).flatMap(baseResult -> {
-                DataResult<List<ICodecExtra>> extrasResult = ICodecExtra.LIST_EXTRAS.parse(ops, input);
-                if(extrasResult.result().isPresent()) {
+        public <T> DataResult<Pair<E, T>> decode(DynamicOps<T> ops, T input)
+        {
+            return this.base.decode(ops, input).flatMap(baseResult ->
+            {
+                DataResult<List<ICodecExtra>> extrasResult = ICodecExtra.LIST_CODEC.parse(ops, input);
+                if(extrasResult.result().isPresent())
+                {
                     baseResult.getFirst().setupExtras(extrasResult.result().get());
                     return DataResult.success(baseResult);
                 }
@@ -56,9 +63,10 @@ public interface ICodecExtraHolder
         }
 
         @Override
-        public <T> DataResult<T> encode(E input, DynamicOps<T> ops, T prefix) {
+        public <T> DataResult<T> encode(E input, DynamicOps<T> ops, T prefix)
+        {
             return this.base.encode(input, ops, prefix)
-                    .flatMap(t -> ICodecExtra.LIST_EXTRAS.encode(EXTRA_HOLDER.get(input), ops, t));
+                    .flatMap(t -> ICodecExtra.LIST_CODEC.encode(PrivateFieldHolder.EXTRA_HOLDER.get(input), ops, t));
         }
     }
 }
