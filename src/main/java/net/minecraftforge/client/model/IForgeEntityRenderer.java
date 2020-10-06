548ff038d055f6028cb618ed1b6132d51b9feb63
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2020-10-06 14:09:50 -0400

    Update IForgeEntityRenderer

--- src/main/java/net/minecraftforge/client/model/IForgeEntityRenderer.java
+++ src/main/java/net/minecraftforge/client/model/IForgeEntityRenderer.java
@@ -26,8 +26,11 @@ import java.util.List;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.entity.Entity;
 
+@SuppressWarnings("rawtypes")
 public interface IForgeEntityRenderer<T extends Entity, M extends EntityModel<T>> {
-
-    public List<LayerRenderer<T, M>> getLayers();
+	
+	public boolean removeLayer(LayerRenderer layer);
+	
+	public List<LayerRenderer> getLayers();
 	
 }
