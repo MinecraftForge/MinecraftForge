a5590f4c11007826832315e87d28ad6f491ab31a
Kovak <kovac1066@gmail.com>
2020-10-17 10:59:16 -0700

    fix a few import changes

--- src/main/java/net/minecraftforge/fml/ModLoadingStage.java
+++ src/main/java/net/minecraftforge/fml/ModLoadingStage.java
@@ -41,6 +41,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
