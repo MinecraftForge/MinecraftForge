061aa30cccda153c7002dc76710f74b611cf9697
TheSilkMiner <thesilkminer@outlook.com>
2020-10-26 12:58:36 +0100

    Expose ILootCondition registration and remove LCM patch
    The loot condition type gets statically initialized only when
    registering it, which in turn means it should work properly just fine in
    regards to registration order.
    
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- src/main/java/net/minecraftforge/common/loot/LootTableIdCondition.java
+++ src/main/java/net/minecraftforge/common/loot/LootTableIdCondition.java
@@ -26,55 +26,67 @@ import net.minecraft.loot.ILootSerializer;
 import net.minecraft.loot.LootConditionType;
 import net.minecraft.loot.LootContext;
 import net.minecraft.loot.conditions.ILootCondition;
-import net.minecraft.loot.conditions.LootConditionManager;
 import net.minecraft.util.JSONUtils;
 import net.minecraft.util.ResourceLocation;
 
-public class LootTableIdCondition implements ILootCondition {
-    public static final ResourceLocation UNKNOWN_LOOT_TABLE = new ResourceLocation("forge", "unknown");
+public class LootTableIdCondition implements ILootCondition
+{
+    // TODO Forge Registry at some point?
+    public static final LootConditionType LOOT_TABLE_ID = new LootConditionType(new LootTableIdCondition.Serializer());
+    public static final ResourceLocation UNKNOWN_LOOT_TABLE = new ResourceLocation("forge", "unknown_loot_table");
 
     private final ResourceLocation targetLootTableId;
 
-    private LootTableIdCondition(final ResourceLocation targetLootTableId) {
+    private LootTableIdCondition(final ResourceLocation targetLootTableId)
+    {
         this.targetLootTableId = targetLootTableId;
     }
 
-    public static Builder builder(final ResourceLocation targetLootTableId) {
-        return new Builder(targetLootTableId);
+    @Override
+    public LootConditionType func_230419_b_()
+    {
+        return LOOT_TABLE_ID;
     }
 
     @Override
-    public LootConditionType func_230419_b_() {
-        return LootConditionManager.LOOT_TABLE_ID;
-    }
-
-    @Override
-    public boolean test(LootContext lootContext) {
+    public boolean test(LootContext lootContext)
+    {
         return lootContext.getQueriedLootTableId().equals(this.targetLootTableId);
     }
 
-    public static class Builder implements ILootCondition.IBuilder {
+    public static Builder builder(final ResourceLocation targetLootTableId)
+    {
+        return new Builder(targetLootTableId);
+    }
+
+    public static class Builder implements ILootCondition.IBuilder
+    {
         private final ResourceLocation targetLootTableId;
 
-        public Builder(ResourceLocation targetLootTableId) {
+        public Builder(ResourceLocation targetLootTableId)
+        {
             if (targetLootTableId == null) throw new IllegalArgumentException("Target loot table must not be null");
             this.targetLootTableId = targetLootTableId;
         }
 
         @Override
-        public ILootCondition build() {
+        public ILootCondition build()
+        {
             return new LootTableIdCondition(this.targetLootTableId);
         }
     }
 
-    public static class Serializer implements ILootSerializer<LootTableIdCondition> {
+    public static class Serializer implements ILootSerializer<LootTableIdCondition>
+    {
         @Override
-        public void func_230424_a_(JsonObject object, LootTableIdCondition instance, JsonSerializationContext serializationContext) {
+        public void serialize(JsonObject object, LootTableIdCondition instance, JsonSerializationContext ctx)
+        {
             object.addProperty("loot_table_id", instance.targetLootTableId.toString());
         }
 
         @Override
-        public LootTableIdCondition func_230423_a_(JsonObject object, JsonDeserializationContext deserializationContext) {
+        public LootTableIdCondition deserialize(JsonObject object, JsonDeserializationContext ctx)
+        {
             return new LootTableIdCondition(new ResourceLocation(JSONUtils.getString(object, "loot_table_id")));
         }
     }
