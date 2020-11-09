367a23e94a1ad1b6ea106f7cb07c1853f8ed07c6
Kovak <kovac1066@gmail.com>
2020-11-09 10:39:57 -0800

    change to the mod loading stage api, fix mods.toml

---
+++ src/test/java/net/minecraftforge/debug/entity/AddEntityAttributeTest.java
@@ -0,0 +1,32 @@
+package net.minecraftforge.debug.entity;
+
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.entity.ai.attributes.RangedAttribute;
+import net.minecraftforge.event.entity.EntityAttributeSetupEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+
+@Mod("add_entity_attribute_test")
+public class AddEntityAttributeTest {
+    public static final boolean ENABLE = true;
+    private static final DeferredRegister<Attribute> ATTRIBUTES = DeferredRegister.create(Attribute.class, "add_entity_attribute_test");
+
+    public static final RegistryObject<Attribute> TEST_ATTR = ATTRIBUTES.register("test_attr",
+            () -> new RangedAttribute("forge.test_attr", 1.0D, 0.0D, 1024.0D).func_233753_a_(true));
+
+
+    public AddEntityAttributeTest() {
+        if (ENABLE) {
+            ATTRIBUTES.register(FMLJavaModLoadingContext.get().getModEventBus());
+            FMLJavaModLoadingContext.get().getModEventBus().register(this);
+        }
+    }
+
+    @SubscribeEvent
+    public void entityAttributeSetup(EntityAttributeSetupEvent event){
+        event.getEntityAttrbiutes().forEach((type, map) -> map.func_233814_a_(TEST_ATTR.get()));
+    }
+}
