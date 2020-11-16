b830375c00b7569dc6ce855cde2aa2b2c076ac39
mrp-v2 <matthrewp@gmail.com>
2020-11-15 19:26:30 -0500

    fix test mod formatting

--- src/test/java/net/minecraftforge/debug/block/CustomRedstoneWireTest.java
+++ src/test/java/net/minecraftforge/debug/block/CustomRedstoneWireTest.java
@@ -14,26 +14,30 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod(CustomRedstoneWireTest.MODID)
-public class CustomRedstoneWireTest {
-
+public class CustomRedstoneWireTest
+{
 	static final String MODID = "custom_redstone_wire_test";
 	static final String BLOCK_ID = "custom_redstone_wire";
 
 	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 
-	public static final RegistryObject<RedstoneWireBlock> CUSTOM_REDSTONE_WIRE = BLOCKS.register(BLOCK_ID, () -> new RedstoneWireBlock(AbstractBlock.Properties.from(Blocks.REDSTONE_WIRE)) {
+	public static final RegistryObject<RedstoneWireBlock> CUSTOM_REDSTONE_WIRE = BLOCKS.register(BLOCK_ID, () -> new RedstoneWireBlock(AbstractBlock.Properties.from(Blocks.REDSTONE_WIRE))
+	{
 		@Override
-		public boolean canConnectToOther(BlockState thisState, BlockState otherState) {
+		public boolean canConnectToOther(BlockState thisState, BlockState otherState)
+		{
 			return true;
 		}
 	});
 
-	static {
+	static
+	{
 		ITEMS.register(BLOCK_ID, () -> new BlockItem(CUSTOM_REDSTONE_WIRE.get(), new Item.Properties()));
 	}
 
-	public CustomRedstoneWireTest() {
+	public CustomRedstoneWireTest()
+	{
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 		bus.addListener(this::clientSetup);
 
@@ -41,9 +45,8 @@ public class CustomRedstoneWireTest {
 		ITEMS.register(bus);
 	}
 
-	private void clientSetup(final FMLClientSetupEvent event) {
+	private void clientSetup(final FMLClientSetupEvent event)
+	{
 		RenderTypeLookup.setRenderLayer(CUSTOM_REDSTONE_WIRE.get(), RenderType.getCutout());
 	}
-
-
 }
