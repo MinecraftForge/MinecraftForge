1717d5904668fa2448e5ebb31ff8e50d9d33b528
sciwhiz12 <arnoldnunag12@gmail.com>
2020-12-08 12:58:14 +0800

    Add SoundEvent casts to null parameters in superclass constructor call
    This makes the constructor error if the superclass constructor's parameters changes.

--- src/main/java/net/minecraftforge/common/util/ForgeSoundType.java
+++ src/main/java/net/minecraftforge/common/util/ForgeSoundType.java
@@ -57,7 +57,7 @@ public class ForgeSoundType extends SoundType
 
     public ForgeSoundType(float volumeIn, float pitchIn, Supplier<SoundEvent> breakSoundIn, Supplier<SoundEvent> stepSoundIn, Supplier<SoundEvent> placeSoundIn, Supplier<SoundEvent> hitSoundIn, Supplier<SoundEvent> fallSoundIn)
     {
-        super(volumeIn, pitchIn, null, null, null, null, null);
+        super(volumeIn, pitchIn, (SoundEvent) null, (SoundEvent) null, (SoundEvent) null, (SoundEvent) null, (SoundEvent) null);
         this.breakSound = breakSoundIn;
         this.stepSound = stepSoundIn;
         this.placeSound = placeSoundIn;
