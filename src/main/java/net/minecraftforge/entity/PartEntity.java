9bfab932bdee998cd33eab0dd13ac39b9cc1cde7
brandon3055 <brandon4261@gmail.com>
2020-12-23 05:49:48 +1100

    Removed auto generated "created by" line.

--- src/main/java/net/minecraftforge/entity/PartEntity.java
+++ src/main/java/net/minecraftforge/entity/PartEntity.java
@@ -3,9 +3,6 @@ package net.minecraftforge.entity;
 import net.minecraft.entity.Entity;
 import net.minecraft.network.IPacket;
 
-/**
- * Created by brandon3055 on 16/12/20
- */
 public abstract class PartEntity<T extends Entity> extends Entity {
     private final T parent;
 
