7d40ed20510cd6c3b8912e261b09787efe83e270
Unbekannt <unbekannt-gitlab@protonmail.com>
2021-02-19 20:41:50 +0100

    Address covers comments added License Header and some spaces

--- src/main/java/net/minecraftforge/common/data/AdvancementBuilderHelper.java
+++ src/main/java/net/minecraftforge/common/data/AdvancementBuilderHelper.java
@@ -1,3 +1,22 @@
+/*
+ * Minecraft Forge
+ * Copyright (c) 2016-2020.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package net.minecraftforge.common.data;
 
 import com.google.common.collect.Maps;
@@ -7,9 +26,9 @@ import net.minecraft.resources.ResourcePackType;
 
 public class AdvancementBuilderHelper {
 
-    public static boolean build(ExistingFileHelper fileHelper, Advancement.Builder builder){
+    public static boolean build(ExistingFileHelper fileHelper, Advancement.Builder builder) {
         return builder.resolveParent((advancementId) -> {
-            if(fileHelper.exists(advancementId, ResourcePackType.SERVER_DATA, ".json", "advancements")) {
+            if (fileHelper.exists(advancementId, ResourcePackType.SERVER_DATA, ".json", "advancements")) {
                 return new Advancement(advancementId, null, null, AdvancementRewards.EMPTY, Maps.newHashMap(), null);
             }
             return null;
