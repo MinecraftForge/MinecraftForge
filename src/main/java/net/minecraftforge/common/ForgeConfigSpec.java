49374658a1bd169552f2573915f4a7142349247d
Curle <curle@gemwire.uk>
2021-03-01 17:06:51 +0000

    More attempts - ConfigBuilder wants empty to start, so let's take this up a level

--- src/main/java/net/minecraftforge/common/ForgeConfigSpec.java
+++ src/main/java/net/minecraftforge/common/ForgeConfigSpec.java
@@ -528,11 +528,20 @@ public class ForgeConfigSpec extends UnmodifiableConfigWrapper<UnmodifiableConfi
 
         public Builder comment(String comment)
         {
+            if(comment == null || comment.length() < 1)
+            {
+                comment = "No comment";
+            }
             context.setComment(comment);
             return this;
         }
         public Builder comment(String... comment)
         {
+            if(comment == null || comment.length < 1 || comment[comment.length - 1].length() < 1)
+            {
+                comment = new String[] {"No comment"};
+            }
+
             context.setComment(comment);
             return this;
         }
@@ -605,7 +614,7 @@ public class ForgeConfigSpec extends UnmodifiableConfigWrapper<UnmodifiableConfi
         private boolean worldRestart = false;
         private Class<?> clazz;
 
-        public void setComment(String... value) { this.comment = value == null ? new String[] {"No comment"} : value; } // TODO 1.17: this should throw an IllegalStateException in future
+        public void setComment(String... value) { this.comment = value == null ? new String[] {""} : value; } // TODO 1.17: this should throw an IllegalStateException in future
         public boolean hasComment() { return this.comment.length > 0; }
         public String[] getComment() { return this.comment; }
         public String buildComment() { return LINE_JOINER.join(comment); }
