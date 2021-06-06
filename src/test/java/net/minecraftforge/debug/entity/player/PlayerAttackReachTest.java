12b2ff63a88d3987cc8d74462de7ee8930b2ec50
Thelnfamous1 <miguel.garay60622@gmail.com>
2021-06-05 22:41:23 -0500

    Based off the previous attack reach PR, instead opts to utilize the existing reach distance attribute with various additions/subtractions

--- src/test/java/net/minecraftforge/debug/entity/player/PlayerAttackReachTest.java
+++ src/test/java/net/minecraftforge/debug/entity/player/PlayerAttackReachTest.java
@@ -16,22 +16,21 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.UUID;
 /**
- * Tests if the patch to PlayerEntity to make it utilize ForgeMod.ATTACK_REACH works
- * The Longsword adds a modifier to the user's attack reach
- * The goal of this patch is to allow modders to more easily add equipment or potion effects that affect the attack reach of players
- * As well as allow command users to modify this attribute for a player
+ * Tests if the various patches made to utilize values associated with Forge.REACH_DISTANCE works
+ * The Longsword adds a modifier to the user's reach distance
+ * The goal of these patches are to allow modders to more easily add equipment or potion effects that affect the attack reach of players
  */
 @Mod(PlayerAttackReachTest.MODID)
 public class PlayerAttackReachTest {
 
     static final String MODID = "player_attack_reach_test";
 
-    static final float ATTACK_REACH_VALUE = 2.0F; // Change this value for testing, but note that the attribute is capped at 1024.0D
+    static final float REACH_VALUE = 2.0F; // Change this value for testing, but note that the attribute is capped at 1024.0D
 
     static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 
     static RegistryObject<Item> LONGSWORD = ITEMS.register("longsword", () ->
-            new LongswordItem(ItemTier.IRON, 3, -2.4F, ATTACK_REACH_VALUE, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT))
+            new LongswordItem(ItemTier.IRON, 3, -2.4F, REACH_VALUE, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT))
     );
 
     public PlayerAttackReachTest()
@@ -41,13 +40,13 @@ public class PlayerAttackReachTest {
     }
 
     static class LongswordItem extends SwordItem {
-        private final float attackReach;
+        private final float reach;
         private final Multimap<Attribute, AttributeModifier> defaultModifiers = ArrayListMultimap.create(); // initialize as empty
-        protected static final UUID BASE_ATTACK_REACH_UUID = UUID.fromString("f1797f2f-f02c-44f9-951f-27c9e5446916");
+        protected static final UUID BASE_REACH_DISTANCE_UUID = UUID.fromString("f1797f2f-f02c-44f9-951f-27c9e5446916");
 
-        LongswordItem(IItemTier itemTier, int attackDamageIn, float attackSpeedIn, float attackReach, Properties properties) {
+        LongswordItem(IItemTier itemTier, int attackDamageIn, float attackSpeedIn, float reachIn, Properties properties) {
             super(itemTier, attackDamageIn, attackSpeedIn, properties);
-            this.attackReach = attackReach;
+            this.reach = reachIn;
         }
 
         @Override
@@ -56,7 +55,7 @@ public class PlayerAttackReachTest {
                 if(this.defaultModifiers.isEmpty()){
                     Multimap<Attribute, AttributeModifier> oldAttributeModifiers = super.getDefaultAttributeModifiers(equipmentSlotType);
                     this.defaultModifiers.putAll(oldAttributeModifiers);
-                    this.defaultModifiers.put(ForgeMod.ATTACK_REACH.get(), new AttributeModifier(BASE_ATTACK_REACH_UUID, "Weapon modifier", (double)this.attackReach, AttributeModifier.Operation.ADDITION));
+                    this.defaultModifiers.put(ForgeMod.REACH_DISTANCE.get(), new AttributeModifier(BASE_REACH_DISTANCE_UUID, "Weapon modifier", (double)this.reach, AttributeModifier.Operation.ADDITION));
                 }
                 return this.defaultModifiers;
             }
