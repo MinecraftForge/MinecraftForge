c9f250c517d588487a78bc08ff7b98501e5defb4
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-06-07 23:02:42 +0200

    Add Breathe, ConsumeAir and RefillAir events

---
+++ src/main/java/net/minecraftforge/event/entity/living/LivingBreatheEvent.java
@@ -0,0 +1,23 @@
+package net.minecraftforge.event.entity.living;
+
+import net.minecraft.world.entity.LivingEntity;
+
+public class LivingBreatheEvent extends LivingEvent
+{
+   private boolean canBreathe;
+   public LivingBreatheEvent(LivingEntity entity, boolean canBreathe)
+   {
+      super(entity);
+      this.canBreathe = canBreathe;
+   }
+
+   public boolean isCanBreathe()
+   {
+      return canBreathe;
+   }
+
+   public void setCanBreathe(boolean canBreathe)
+   {
+      this.canBreathe = canBreathe;
+   }
+}
---
+++ src/main/java/net/minecraftforge/event/entity/living/LivingRefillAirEvent.java
@@ -0,0 +1,23 @@
+package net.minecraftforge.event.entity.living;
+
+import net.minecraft.world.entity.LivingEntity;
+
+public class LivingRefillAirEvent extends LivingEvent
+{
+   private int amount;
+   public LivingRefillAirEvent(LivingEntity entity, int amount)
+   {
+      super(entity);
+      this.amount = amount;
+   }
+
+   public int getAmount()
+   {
+      return amount;
+   }
+
+   public void setAmount(int amount)
+   {
+      this.amount = amount;
+   }
+}
