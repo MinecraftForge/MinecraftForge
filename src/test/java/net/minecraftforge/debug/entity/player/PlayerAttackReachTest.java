a7283f78eb0bc9d0fefd8ba187daba936db02220
Thelnfamous1 <miguel.garay60622@gmail.com>
2021-06-07 20:03:08 -0500

    Made some patches single-lines, removed the attack reach test assets as they're not needed, fixed formatting issues

--- src/test/java/net/minecraftforge/debug/entity/player/PlayerAttackReachTest.java
+++ src/test/java/net/minecraftforge/debug/entity/player/PlayerAttackReachTest.java
@@ -1,3 +1,22 @@
+/*
+ * Minecraft Forge
+ * Copyright (c) 2016-2021.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package net.minecraftforge.debug.entity.player;
 
 import com.google.common.collect.ArrayListMultimap;
@@ -22,8 +41,8 @@ import java.util.UUID;
  * and will work to attack from a distance further than the one you can break blocks from
  */
 @Mod(PlayerAttackReachTest.MODID)
-public class PlayerAttackReachTest {
-
+public class PlayerAttackReachTest
+{
     static final String MODID = "player_attack_reach_test";
 
     static final float ATTACK_REACH = 4.0F; // Change this value for testing - default attack reach attribute value is 3.0D and ranges from 0.0D-1024.0D
@@ -40,18 +59,21 @@ public class PlayerAttackReachTest {
         ITEMS.register(modEventBus);
     }
 
-    static class LongswordItem extends SwordItem {
+    static class LongswordItem extends SwordItem
+    {
         private final float reach;
         private final Multimap<Attribute, AttributeModifier> defaultModifiers = ArrayListMultimap.create(); // initialize as empty
         protected static final UUID BASE_REACH_DISTANCE_UUID = UUID.fromString("f1797f2f-f02c-44f9-951f-27c9e5446916");
 
-        LongswordItem(IItemTier itemTier, int attackDamageIn, float attackSpeedIn, float reachIn, Properties properties) {
+        LongswordItem(IItemTier itemTier, int attackDamageIn, float attackSpeedIn, float reachIn, Properties properties)
+        {
             super(itemTier, attackDamageIn, attackSpeedIn, properties);
             this.reach = reachIn;
         }
 
         @Override
-        public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlotType equipmentSlotType) {
+        public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlotType equipmentSlotType)
+        {
             if(equipmentSlotType == EquipmentSlotType.MAINHAND){
                 if(this.defaultModifiers.isEmpty()){
                     Multimap<Attribute, AttributeModifier> oldAttributeModifiers = super.getDefaultAttributeModifiers(equipmentSlotType);
