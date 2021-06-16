5405616f43a877ce42cac5183e62e098f3310a06
Evan Steinkerchner <esteinkerchner@gmail.com>
2021-06-16 13:42:21 -0400

    Addressed review comments

--- src/main/java/net/minecraftforge/client/event/ToastAddEvent.java
+++ src/main/java/net/minecraftforge/client/event/ToastAddEvent.java
@@ -1,21 +1,55 @@
+/*
+ * Minecraft Forge
+ * Copyright (c) 2016-2021.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package net.minecraftforge.client.event;
 
 import net.minecraft.client.gui.toasts.IToast;
 import net.minecraftforge.eventbus.api.Cancelable;
 import net.minecraftforge.eventbus.api.Event;
 
+/**
+ * Fired whenever the client is about to queue up a toast message to be rendered on-screen.
+ * Canceling the event will prevent the toast from being rendered.
+ */
 @Cancelable
-public class ToastAddEvent extends Event {
+public class ToastAddEvent extends Event
+{
     private IToast toast;
-    public ToastAddEvent(IToast toast) {
+
+    public ToastAddEvent(IToast toast)
+    {
         setToast(toast);
     }
 
-    public IToast getToast() {
+    /**
+     * @return the toast that will be rendered on-screen
+     */
+    public IToast getToast()
+    {
         return toast;
     }
 
-    public void setToast(IToast toast) {
+    /**
+     * @param toast the replacement toast to show instead of the original
+     */
+    public void setToast(IToast toast)
+    {
         this.toast = toast;
     }
 }
