7d133fc06629749c291cab6fd010ddb7c1574b43
Thelnfamous1 <miguel.garay60622@gmail.com>
2021-06-20 16:07:13 -0500

    Reduced patch size for LivingEntity, added flight boots to the test mod and made the whole test mod toggleable

--- src/test/java/net/minecraftforge/debug/entity/living/ExpandedElytraFlightTest.java
+++ src/test/java/net/minecraftforge/debug/entity/living/ExpandedElytraFlightTest.java
@@ -19,15 +19,20 @@
 
 package net.minecraftforge.debug.entity.living;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnchantmentType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.attributes.Attribute;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.ai.attributes.ModifiableAttributeInstance;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.*;
 import net.minecraft.potion.*;
 import net.minecraftforge.common.ForgeMod;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -43,16 +48,24 @@ import java.util.UUID;
 public class ExpandedElytraFlightTest
 {
     public static final String MOD_ID = "expanded_elytra_flight_test";
-    public static final boolean ENABLE_INTRINSIC_BOOST_FOR_EFFECT = true;
-    public static final double FLIGHT_EFFECT_MODIFIER_VALUE_ADDITION = 1.0D;
-    public static final double FLIGHT_ENCHANTMENT_MODIFIER_VALUE_ADDITION = 1.0D;
-    public static final double FLIGHT_SPEED_EFFECT_MODIFIER_VALUE_ADDITION = 1.0D;
-    public static final double FLIGHT_SPEED_ENCHANTMENT_MODIFIER_VALUE_ADDITION = 1.0D;
+    public static final boolean ENABLE = true;
+
+    static final boolean ENABLE_INTRINSIC_BOOST_FOR_EFFECT = true;
+    static final boolean ENABLE_INTRINSIC_BOOST_FOR_ARMOR = true;
+
+    static final double FLIGHT_ARMOR_MODIFIER_VALUE_ADDITION = 1.0D;
+    static final double FLIGHT_EFFECT_MODIFIER_VALUE_ADDITION = 1.0D;
+    static final double FLIGHT_ENCHANTMENT_MODIFIER_VALUE_ADDITION = 1.0D;
+    static final double FLIGHT_SPEED_ARMOR_MODIFIER_VALUE_ADDITION = 1.0D;
+    static final double FLIGHT_SPEED_EFFECT_MODIFIER_VALUE_ADDITION = 1.0D;
+    static final double FLIGHT_SPEED_ENCHANTMENT_MODIFIER_VALUE_ADDITION = 1.0D;
+    static final int FLIGHT_POTION_EFFECT_DURATION = 180 * 20;
 
     // Deferred Registers
     private static final DeferredRegister<Effect> EFFECTS = DeferredRegister.create(ForgeRegistries.POTIONS, MOD_ID);
     private static final DeferredRegister<Potion> POTIONS = DeferredRegister.create(ForgeRegistries.POTION_TYPES, MOD_ID);
     private static final DeferredRegister<Enchantment> ENCHANTMENTS = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, MOD_ID);
+    private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
 
     // Registry Utilities
     private static final String FLIGHT_EFFECT_MODIFIER_UUID_STRING = "53992b1d-4f7c-4479-a00b-f2fdf57dfcdf";
@@ -60,13 +73,15 @@ public class ExpandedElytraFlightTest
     private static final EquipmentSlotType[] ARMOR_SLOTS = new EquipmentSlotType[]{EquipmentSlotType.HEAD, EquipmentSlotType.CHEST, EquipmentSlotType.LEGS, EquipmentSlotType.FEET};
 
     // Effects
-    private static final RegistryObject<Effect> FLIGHT_EFFECT = EFFECTS.register("flight_effect",
+    private static final RegistryObject<Effect> FLIGHT_EFFECT = EFFECTS.register("flight",
             () -> new FallFlightEffect(EffectType.BENEFICIAL, 0x0000FF)
                     .addAttributeModifier(ForgeMod.FALL_FLIGHT.get(), FLIGHT_EFFECT_MODIFIER_UUID_STRING, FLIGHT_EFFECT_MODIFIER_VALUE_ADDITION, AttributeModifier.Operation.ADDITION)
     );
 
     // Potions
-    private static final RegistryObject<Potion> FLIGHT_POTION = POTIONS.register("flight_potion", () -> new Potion(new EffectInstance(FLIGHT_EFFECT.get(), 180 * 20)));
+    private static final RegistryObject<Potion> FLIGHT_POTION = POTIONS.register("flight",
+            () -> new Potion(new EffectInstance(FLIGHT_EFFECT.get(), FLIGHT_POTION_EFFECT_DURATION))
+    );
 
     // Enchantments
     private static final RegistryObject<Enchantment> FLIGHT_ENCHANTMENT = ENCHANTMENTS.register("flight",
@@ -77,15 +92,59 @@ public class ExpandedElytraFlightTest
             () -> new FlightEnchantment(Enchantment.Rarity.VERY_RARE, EnchantmentType.ARMOR, ARMOR_SLOTS)
     );
 
+    // Items
+    private static final RegistryObject<Item> FLIGHT_BOOTS = ITEMS.register("flight_boots",
+            () -> new FlightArmorItem(ArmorMaterial.IRON, EquipmentSlotType.FEET, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+
     public ExpandedElytraFlightTest()
     {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        EFFECTS.register(modEventBus);
-        POTIONS.register(modEventBus);
-        ENCHANTMENTS.register(modEventBus);
+        if(ENABLE){
+            IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+            EFFECTS.register(modEventBus);
+            POTIONS.register(modEventBus);
+            ENCHANTMENTS.register(modEventBus);
+            ITEMS.register(modEventBus);
+            MinecraftForge.EVENT_BUS.register(this);
+        }
     }
 
-    public static class FallFlightEffect extends Effect
+    @SubscribeEvent
+    public void onLivingUpdate(LivingEvent.LivingUpdateEvent event)
+    {
+        LivingEntity living = event.getEntityLiving();
+        if(living.level.isClientSide) return;
+
+        int flightEnchantmentLevel = EnchantmentHelper.getEnchantmentLevel(FLIGHT_ENCHANTMENT.get(), living);
+
+        ModifiableAttributeInstance fallFlyingAttribute = living.getAttribute(ForgeMod.FALL_FLIGHT.get());
+        if (fallFlyingAttribute != null)
+        {
+            if (fallFlyingAttribute.getModifier(FlightEnchantment.FLIGHT_ENCHANTMENT_MODIFIER_UUID) != null)
+            {
+                fallFlyingAttribute.removeModifier(FlightEnchantment.FLIGHT_ENCHANTMENT_MODIFIER_UUID);
+            }
+            if(flightEnchantmentLevel > 0)
+            {
+                fallFlyingAttribute.addTransientModifier(FlightEnchantment.ELYTRA_ENCHANTMENT_MODIFIER);
+            }
+        }
+
+        int flightSpeedEnchantmentLevel = EnchantmentHelper.getEnchantmentLevel(FLIGHT_SPEED_ENCHANTMENT.get(), living);
+        ModifiableAttributeInstance fallFlyingSpeedAttribute = living.getAttribute(ForgeMod.FALL_FLYING_SPEED.get());
+        if (fallFlyingSpeedAttribute != null)
+        {
+            if (fallFlyingSpeedAttribute.getModifier(FlightEnchantment.FLIGHT_SPEED_ENCHANTMENT_MODIFIER_UUID) != null)
+            {
+                fallFlyingSpeedAttribute.removeModifier(FlightEnchantment.FLIGHT_SPEED_ENCHANTMENT_MODIFIER_UUID);
+            }
+            if(flightSpeedEnchantmentLevel > 0)
+            {
+                fallFlyingSpeedAttribute.addTransientModifier(FlightEnchantment.ELYTRA_SPEED_ENCHANTMENT_MODIFIER);
+            }
+        }
+    }
+
+    static class FallFlightEffect extends Effect
     {
         public FallFlightEffect(EffectType effectType, int color)
         {
@@ -98,8 +157,7 @@ public class ExpandedElytraFlightTest
 
     }
 
-    @Mod.EventBusSubscriber(modid = MOD_ID)
-    public static class FlightEnchantment extends Enchantment
+    static class FlightEnchantment extends Enchantment
     {
         private static final UUID FLIGHT_ENCHANTMENT_MODIFIER_UUID = UUID.fromString("f0d64484-f532-4786-963f-e9456ba20d41");
         private static final AttributeModifier ELYTRA_ENCHANTMENT_MODIFIER = new AttributeModifier(FLIGHT_ENCHANTMENT_MODIFIER_UUID, "Flight Enchantment", FLIGHT_ENCHANTMENT_MODIFIER_VALUE_ADDITION, AttributeModifier.Operation.ADDITION);
@@ -112,41 +170,35 @@ public class ExpandedElytraFlightTest
         {
             super(rarity, enchantmentType, slotTypes);
         }
+    }
 
-        @SubscribeEvent
-        public static void onLivingUpdate(LivingEvent.LivingUpdateEvent event)
+    static class FlightArmorItem extends ArmorItem
+    {
+        private static final UUID[] ARMOR_MODIFIER_UUID_PER_SLOT = new UUID[]{UUID.fromString("845DB27C-C624-495F-8C9F-6020A9A58B6B"), UUID.fromString("D8499B04-0E66-4726-AB29-64469D734E0D"), UUID.fromString("9F3D476D-C118-4544-8365-64846904B48E"), UUID.fromString("2AD3F246-FEE1-4E67-B886-69FD380BB150")};
+
+        private final Multimap<Attribute, AttributeModifier> defaultModifiers = ArrayListMultimap.create(); // initialize as empty
+
+        public FlightArmorItem(IArmorMaterial armorMaterial, EquipmentSlotType slotType, Properties properties) {
+            super(armorMaterial, slotType, properties);
+        }
+
+        @Override
+        public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlotType equipmentSlotType)
         {
-            LivingEntity living = event.getEntityLiving();
-            if(living.level.isClientSide) return;
-
-            int elytraEnchantmentLevel = EnchantmentHelper.getEnchantmentLevel(FLIGHT_ENCHANTMENT.get(), living);
-
-            ModifiableAttributeInstance fallFlyingAttribute = living.getAttribute(ForgeMod.FALL_FLIGHT.get());
-            if (fallFlyingAttribute != null)
-            {
-                if (fallFlyingAttribute.getModifier(FLIGHT_ENCHANTMENT_MODIFIER_UUID) != null)
-                {
-                    fallFlyingAttribute.removeModifier(FLIGHT_ENCHANTMENT_MODIFIER_UUID);
+            if(equipmentSlotType == this.slot){
+                if(this.defaultModifiers.isEmpty()){
+                    Multimap<Attribute, AttributeModifier> oldAttributeModifiers = super.getDefaultAttributeModifiers(equipmentSlotType);
+                    this.defaultModifiers.putAll(oldAttributeModifiers);
+                    UUID uuid = ARMOR_MODIFIER_UUID_PER_SLOT[equipmentSlotType.getIndex()];
+                    this.defaultModifiers.put(ForgeMod.FALL_FLIGHT.get(), new AttributeModifier(uuid, "Armor fall flight modifier", FLIGHT_ARMOR_MODIFIER_VALUE_ADDITION, AttributeModifier.Operation.ADDITION));
+                    if(ENABLE_INTRINSIC_BOOST_FOR_ARMOR)
+                    {
+                        this.defaultModifiers.put(ForgeMod.FALL_FLYING_SPEED.get(), new AttributeModifier(uuid, "Armor fall flight speed modifier", FLIGHT_SPEED_ARMOR_MODIFIER_VALUE_ADDITION, AttributeModifier.Operation.ADDITION));
+                    }
                 }
-                if(elytraEnchantmentLevel > 0)
-                {
-                    fallFlyingAttribute.addTransientModifier(ELYTRA_ENCHANTMENT_MODIFIER);
-                }
+                return this.defaultModifiers;
             }
-
-            int elytraSpeedEnchantmentLevel = EnchantmentHelper.getEnchantmentLevel(FLIGHT_SPEED_ENCHANTMENT.get(), living);
-            ModifiableAttributeInstance fallFlyingSpeedAttribute = living.getAttribute(ForgeMod.FALL_FLYING_SPEED.get());
-            if (fallFlyingSpeedAttribute != null)
-            {
-                if (fallFlyingSpeedAttribute.getModifier(FLIGHT_SPEED_ENCHANTMENT_MODIFIER_UUID) != null)
-                {
-                    fallFlyingSpeedAttribute.removeModifier(FLIGHT_SPEED_ENCHANTMENT_MODIFIER_UUID);
-                }
-                if(elytraSpeedEnchantmentLevel > 0)
-                {
-                    fallFlyingSpeedAttribute.addTransientModifier(ELYTRA_SPEED_ENCHANTMENT_MODIFIER);
-                }
-            }
+            else return super.getDefaultAttributeModifiers(equipmentSlotType);
         }
     }
 }
