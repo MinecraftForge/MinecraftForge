0d422faf7cd823926eeec52976ca8432b62ff85f
ferriarnus <ferri.arnus@gmail.com>
2021-07-11 23:15:01 +0200

    Initial commit
    Added a Grindstone recipe type and (default) serializer, making it possible to use json recipes and custom serializers for the grindstone

---
+++ src/test/java/net/minecraftforge/debug/item/crafting/GrindStoneRecipeTest.java
@@ -0,0 +1,35 @@
+/*
+ * Minecraft Forge
+ * Copyright (c) 2016-2021.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package net.minecraftforge.debug.item.crafting;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(GrindStoneRecipeTest.MOD_ID)
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = GrindStoneRecipeTest.MOD_ID)
+public class GrindStoneRecipeTest
+{
+    public static final String MOD_ID = "grindstonerecipe_test";
+
+    public GrindStoneRecipeTest()
+    {
+        
+    }
+
+}
