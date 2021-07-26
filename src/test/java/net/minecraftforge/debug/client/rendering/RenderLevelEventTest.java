06cc2fe7e73ad96583ffee2ad6c8e02cb99fbd0b
RogueLogix <roguelogix@roguelogix.net>
2021-07-26 02:16:04 -0700

    update test mod with performance testing

--- src/test/java/net/minecraftforge/debug/client/rendering/RenderLevelEventTest.java
+++ src/test/java/net/minecraftforge/debug/client/rendering/RenderLevelEventTest.java
@@ -19,8 +19,10 @@
 
 package net.minecraftforge.debug.client.rendering;
 
+import com.google.common.base.Stopwatch;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RenderLevelEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent.Phase;
 import net.minecraftforge.event.TickEvent.RenderTickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -28,13 +30,34 @@ import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.concurrent.TimeUnit;
+
 @Mod(RenderLevelEventTest.MODID)
 @Mod.EventBusSubscriber(value = Dist.CLIENT)
 public class RenderLevelEventTest {
     private static final Logger LOGGER = LogManager.getLogger();
     public static final String MODID = "render_level_event_test";
     public static final boolean ENABLED = false;
+    public static final boolean REGISTER_EVENTS = true;
+    public static final boolean DO_LOG = false;
+    private static final int EVENT_BLOCK_COUNT = 50;
+    private static final int WAIT_UNTIL_FRAME = 5000;
+    private static final int FRAME_COUNT = 5000;
+    private static int frameCount = 0;
     private static int eventCount = 0;
+    
+    private static final Stopwatch timer = Stopwatch.createUnstarted();
+    
+    public RenderLevelEventTest() {
+        if (!ENABLED || !REGISTER_EVENTS) {
+            return;
+        }
+    
+        for (int i = 0; i < EVENT_BLOCK_COUNT; i++) {
+            MinecraftForge.EVENT_BUS.register(new EventBlock());
+        }
+        
+    }
     
     @SubscribeEvent
     public static void onBegin(RenderLevelEvent.BeginEvent event) {
@@ -43,7 +66,11 @@ public class RenderLevelEventTest {
         }
         
         eventCount++;
-        LOGGER.info("[RLE] begin");
+        if (DO_LOG) {
+            LOGGER.info("[RLE] begin");
+        }
+        
+        timer.start();
     }
     
     @SubscribeEvent
@@ -51,61 +78,56 @@ public class RenderLevelEventTest {
         if (!ENABLED) {
             return;
         }
+        timer.stop();
         
         eventCount++;
-        LOGGER.info("[RLE] end");
+        if (DO_LOG) {
+            LOGGER.info("[RLE] end");
+        }
     }
     
-    @SubscribeEvent
-    public static void onTerrainUpdate(RenderLevelEvent.TerrainUpdateEvent event) {
-        if (!ENABLED) {
-            return;
+    private static class EventBlock {
+        
+        @SubscribeEvent
+        public void onTerrainUpdate(RenderLevelEvent.TerrainUpdateEvent event) {
+            eventCount++;
+            if (DO_LOG) {
+                LOGGER.info("[RLE] terrain update");
+            }
         }
         
-        eventCount++;
-        LOGGER.info("[RLE] terrain update");
-    }
-    
-    @SubscribeEvent
-    public static void onBlockLayer(RenderLevelEvent.BlockLayerEvent event) {
-        if (!ENABLED) {
-            return;
+        @SubscribeEvent
+        public void onBlockLayer(RenderLevelEvent.BlockLayerEvent event) {
+            eventCount++;
+            if (DO_LOG) {
+                LOGGER.info("[RLE] block layer: " + event.getRenderType().toString());
+            }
         }
         
-        eventCount++;
-        LOGGER.info("[RLE] block layer: " + event.getRenderType().toString());
-    }
-    
-    @SubscribeEvent
-    public static void onRenderTypeFinish(RenderLevelEvent.RenderTypeFinishEvent event) {
-        if (!ENABLED) {
-            return;
+        @SubscribeEvent
+        public void onRenderTypeFinish(RenderLevelEvent.RenderTypeFinishEvent event) {
+            eventCount++;
+            if (DO_LOG) {
+                LOGGER.info("[RLE] render type: " + event.getRenderType().toString());
+            }
         }
         
-        eventCount++;
-        LOGGER.info("[RLE] render type: " + event.getRenderType().toString());
-    }
-    
-    @SubscribeEvent
-    public static void onTerrainAndEntitiesDone(RenderLevelEvent.TerrainAndEntitiesDoneEvent event) {
-        if (!ENABLED) {
-            return;
+        @SubscribeEvent
+        public void onTerrainAndEntitiesDone(RenderLevelEvent.TerrainAndEntitiesDoneEvent event) {
+            eventCount++;
+            if (DO_LOG) {
+                LOGGER.info("[RLE] terrain and tile entities done");
+            }
         }
         
-        eventCount++;
-        LOGGER.info("[RLE] terrain and tile entities done");
-    }
-    
-    
-    
-    @SubscribeEvent
-    public static void onWeatherDone(RenderLevelEvent.WeatherDoneEvent event) {
-        if (!ENABLED) {
-            return;
-        }
         
-        eventCount++;
-        LOGGER.info("[RLE] weather done");
+        @SubscribeEvent
+        public void onWeatherDone(RenderLevelEvent.WeatherDoneEvent event) {
+            eventCount++;
+            if (DO_LOG) {
+                LOGGER.info("[RLE] weather done");
+            }
+        }
     }
     
     @SubscribeEvent
@@ -115,8 +137,26 @@ public class RenderLevelEventTest {
         }
         
         if (event.phase == Phase.END && eventCount != 0) {
-            LOGGER.info("[RLE] event count: " + eventCount);
+            
+            if(frameCount >= WAIT_UNTIL_FRAME && frameCount % FRAME_COUNT == 0){
+                final double timeElapsed = timer.elapsed(TimeUnit.NANOSECONDS) / (double)FRAME_COUNT;
+                final double potentialFPS = 1_000_000_000.0 / timeElapsed;
+                
+                LOGGER.info("[RLE] NS spent rendering level: " + timeElapsed);
+                LOGGER.info("[RLE] Theoretical peak FPS:     " + potentialFPS);
+                
+                timer.reset();
+                if(!DO_LOG){
+                    LOGGER.info("[RLE] event count: " + eventCount);
+                }
+            }
+            
+            if(DO_LOG){
+                LOGGER.info("[RLE] event count: " + eventCount);
+            }
             eventCount = 0;
+            frameCount++;
         }
+        
     }
 }
