1bc898128f020cffc69100cc7a56173968b25f3f
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2021-07-28 20:05:00 -0400

    Expose the world's default seed

---
+++ src/main/java/net/minecraftforge/common/world/DefaultWorldSeed.java
@@ -0,0 +1,42 @@
+/*
+ * Minecraft Forge
+ * Copyright (c) 2016-2021.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package net.minecraftforge.common.world;
+
+public class DefaultWorldSeed {
+
+    private static long SEED = 0;
+
+    /**
+     * Call this in a mod's BiomeSource, ChunkGenerator, etc to have access to the default seed given to the server.
+     * @return The server's default seed if it exist. Otherwise, returns 0
+     */
+    public static long getSeed() {
+        return SEED;
+    }
+
+    /**
+     * DO NOT USE THIS METHOD
+     * For internal Forge use only
+     */
+    public static long setSeedInternal(long seed) {
+        SEED = seed;
+        return seed;
+    }
+}
