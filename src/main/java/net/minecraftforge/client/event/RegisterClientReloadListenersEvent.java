4f470bf5fbe0fd91c1563495db5f0f8af0a2f44d
sciwhiz12 <arnoldnunag12@gmail.com>
2021-08-11 04:44:07 +0800

    Add docs for RegisterClientReloadListenersEvent

--- src/main/java/net/minecraftforge/client/event/RegisterClientReloadListenersEvent.java
+++ src/main/java/net/minecraftforge/client/event/RegisterClientReloadListenersEvent.java
@@ -19,11 +19,27 @@
 
 package net.minecraftforge.client.event;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.server.packs.resources.PreparableReloadListener;
 import net.minecraft.server.packs.resources.ReloadableResourceManager;
+import net.minecraftforge.event.AddReloadListenerEvent;
+import net.minecraftforge.eventbus.api.Cancelable;
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.event.IModBusEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
+/**
+ * Fired to allow mods to register their reload listeners on the client-side resource manager.
+ * This event is fired once during the construction of the {@link Minecraft} instance.
+ *
+ * <p>For registering reload listeners on the server-side resource manager, see {@link AddReloadListenerEvent}. </p>
+ *
+ * <p>This event is not {@linkplain Cancelable cancelable}, and does not {@linkplain HasResult have a result}. </p>
+ *
+ * <p>This event is fired on the {@linkplain FMLJavaModLoadingContext#getModEventBus() mod-specific event bus},
+ * only on the {@linkplain LogicalSide#CLIENT logical client}. </p>
+ */
 public class RegisterClientReloadListenersEvent extends Event implements IModBusEvent
 {
     private final ReloadableResourceManager resourceManager;
@@ -33,8 +49,13 @@ public class RegisterClientReloadListenersEvent extends Event implements IModBus
         this.resourceManager = resourceManager;
     }
 
+    /**
+     * Registers the given reload listener to the client-side resource manager.
+     *
+     * @param reloadListener the reload listener
+     */
     public void registerReloadListener(PreparableReloadListener reloadListener)
     {
-        resourceManager.registerReloadListener(reloadListener);
+        resourceManager.registerReloadListener(reloadListener); // TODO: does this need to be wrapped and made state-aware?
     }
 }
