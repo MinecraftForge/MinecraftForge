b91ac868d612c41c3aae4a43e7cc7a28e8bb5607
sciwhiz12 <arnoldnunag12@gmail.com>
2021-08-11 04:21:15 +0800

    Add docs for EntityRenderersEvent

--- src/main/java/net/minecraftforge/client/event/EntityRenderersEvent.java
+++ src/main/java/net/minecraftforge/client/event/EntityRenderersEvent.java
@@ -23,6 +23,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.EntityModelSet;
 import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
@@ -30,6 +31,7 @@ import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
@@ -37,24 +39,48 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.client.ForgeHooksClient;
+import net.minecraftforge.eventbus.api.Cancelable;
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.event.IModBusEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import javax.annotation.Nullable;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Supplier;
 
+/**
+ * Fired for on different events/actions relating to {@linkplain EntityRenderer entity renderers}.
+ * See the various subclasses for listening to different events.
+ *
+ * <p>These events are fired on the {@linkplain FMLJavaModLoadingContext#getModEventBus() mod-specific event bus},
+ * only on the {@linkplain LogicalSide#CLIENT logical client}. </p>
+ *
+ * @see EntityRenderersEvent.RegisterLayerDefinitions
+ * @see EntityRenderersEvent.RegisterRenderers
+ * @see EntityRenderersEvent.AddLayers
+ */
 public class EntityRenderersEvent extends Event implements IModBusEvent
 {
+    /**
+     * Fired for registering layer definitions at the appropriate time.
+     *
+     * <p>This event is not {@linkplain Cancelable cancelable}, and does not {@linkplain HasResult have a result}. </p>
+     *
+     * <p>This event is fired on the {@linkplain FMLJavaModLoadingContext#getModEventBus() mod-specific event bus},
+     * only on the {@linkplain LogicalSide#CLIENT logical client}. </p>
+     */
     public static class RegisterLayerDefinitions extends EntityRenderersEvent
     {
         /**
          * Registers a layer definition supplier with the given {@link ModelLayerLocation}.
          * These will be inserted into the main layer definition map for entity model layers at the appropriate time.
          *
-         * @param layerLocation The model layer location, which should be used in conjunction with {@link EntityRendererProvider.Context#bakeLayer(ModelLayerLocation)}
-         * @param supplier A supplier to create a {@link LayerDefinition}, generally a static method reference in the entity model class
+         * @param layerLocation the model layer location, which should be used in conjunction with
+         *                      {@link EntityRendererProvider.Context#bakeLayer(ModelLayerLocation)}
+         * @param supplier      a supplier to create a {@link LayerDefinition}, generally a static method reference in
+         *                      the entity model class
          */
         public void registerLayerDefinition(ModelLayerLocation layerLocation, Supplier<LayerDefinition> supplier)
         {
@@ -63,15 +89,22 @@ public class EntityRenderersEvent extends Event implements IModBusEvent
     }
 
     /**
-     * Provides a safe place to register entity renderers and layer renderers.
+     * Fired for registering entity and block entity renderers at the appropriate time.
+     * For registering entity renderer layers to existing entity renderers (whether vanilla or registered through this
+     * event), listen for the {@link AddLayers} event instead.
+     *
+     * <p>This event is not {@linkplain Cancelable cancelable}, and does not {@linkplain HasResult have a result}. </p>
+     *
+     * <p>This event is fired on the {@linkplain FMLJavaModLoadingContext#getModEventBus() mod-specific event bus},
+     * only on the {@linkplain LogicalSide#CLIENT logical client}. </p>
      */
     public static class RegisterRenderers extends EntityRenderersEvent
     {
         /**
-         * Registers an entity renderer.
+         * Registers an entity renderer for the given block entity type.
          *
-         * @param entityType The EntityType to register a renderer for
-         * @param entityRendererProvider The renderer provider, can be a lambda like MyRenderer::new
+         * @param entityType the entity type to register a renderer for
+         * @param entityRendererProvider the renderer provider
          */
         public <T extends Entity> void registerEntityRenderer(EntityType<? extends T> entityType, EntityRendererProvider<T> entityRendererProvider)
         {
@@ -79,10 +112,10 @@ public class EntityRenderersEvent extends Event implements IModBusEvent
         }
 
         /**
-         * Registers a block entity renderer.
+         * Registers a block entity renderer for the given block entity type.
          *
-         * @param blockEntityType The BlockEntityType to register a renderer for
-         * @param blockEntityRendererProvider The renderer provider, can be a lambda like MyRenderer::new
+         * @param blockEntityType The block entity type to register a renderer for
+         * @param blockEntityRendererProvider The renderer provider
          */
         public <T extends BlockEntity> void registerBlockEntityRenderer(BlockEntityType<? extends T> blockEntityType, BlockEntityRendererProvider<T> blockEntityRendererProvider)
         {
@@ -91,7 +124,13 @@ public class EntityRenderersEvent extends Event implements IModBusEvent
     }
 
     /**
-     * Provides a safe place to register entity renderer layers.
+     * Fired for registering entity renderer layers at the appropriate time, after the entity and player renderers maps
+     * have been created.
+     *
+     * <p>This event is not {@linkplain Cancelable cancelable}, and does not {@linkplain HasResult have a result}. </p>
+     *
+     * <p>This event is fired on the {@linkplain FMLJavaModLoadingContext#getModEventBus() mod-specific event bus},
+     * only on the {@linkplain LogicalSide#CLIENT logical client}. </p>
      */
     public static class AddLayers extends EntityRenderersEvent
     {
@@ -106,7 +145,11 @@ public class EntityRenderersEvent extends Event implements IModBusEvent
         }
 
         /**
-         * Returns the list of skins.
+         * {@return the set of player skin names which have a renderer}
+         *
+         * Minecraft provides two default skin names: {@code default} for the
+         * {@linkplain ModelLayers#PLAYER regular player model} and {@code slim} for the
+         * {@linkplain ModelLayers#PLAYER_SLIM slim player model}.
          */
         public Set<String> getSkins()
         {
@@ -114,10 +157,12 @@ public class EntityRenderersEvent extends Event implements IModBusEvent
         }
 
         /**
-         * Returns a player skin renderer. Vanilla skins are "default" and "slim".
-         * @param skinName The name of the skin renderer to return
-         * @param <R> The type of the skin renderer, usually PlayerRenderer
-         * @return The skin renderer, or null if not found
+         * Returns a player skin renderer for the given skin name.
+         *
+         * @param skinName the name of the skin to get the renderer for
+         * @param <R>      the type of the skin renderer, usually {@link PlayerRenderer}
+         * @return the skin renderer, or {@code null} if no renderer is registered for that skin name
+         * @see #getSkins()
          */
         @Nullable
         @SuppressWarnings("unchecked")
@@ -128,10 +173,11 @@ public class EntityRenderersEvent extends Event implements IModBusEvent
 
         /**
          * Returns an entity renderer for the given entity type.
-         * @param entityType The entity to return a renderer for
-         * @param <T> The type of entity the renderer is for
-         * @param <R> The renderer type
-         * @return The renderer
+         *
+         * @param entityType the entity type to return a renderer for
+         * @param <T> the type of entity the renderer is for
+         * @param <R> the type of the renderer
+         * @return the renderer, or {@code null} if no renderer is registered for that entity type
          */
         @Nullable
         @SuppressWarnings("unchecked")
@@ -139,6 +185,9 @@ public class EntityRenderersEvent extends Event implements IModBusEvent
             return (R) renderers.get(entityType);
         }
 
+        /**
+         * {@return the set of entity models}
+         */
         public EntityModelSet getEntityModels()
         {
             return entityModels;
