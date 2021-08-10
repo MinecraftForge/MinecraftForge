63d8bec4e9a27d63ab64ad0ca35e3468c3946716
sciwhiz12 <arnoldnunag12@gmail.com>
2021-08-11 04:54:29 +0800

    Add docs for RegisterShadersEvent

--- src/main/java/net/minecraftforge/client/event/RegisterShadersEvent.java
+++ src/main/java/net/minecraftforge/client/event/RegisterShadersEvent.java
@@ -22,12 +22,24 @@ package net.minecraftforge.client.event;
 import com.mojang.datafixers.util.Pair;
 import net.minecraft.client.renderer.ShaderInstance;
 import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraftforge.eventbus.api.Cancelable;
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.event.IModBusEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import java.util.List;
 import java.util.function.Consumer;
 
+/**
+ * Fired to allow mods to register custom {@linkplain ShaderInstance shaders}.
+ * This event is fired after the default Minecraft shaders have been registered.
+ *
+ * <p>This event is not {@linkplain Cancelable cancelable}, and does not {@linkplain HasResult have a result}. </p>
+ *
+ * <p>This event is fired on the {@linkplain FMLJavaModLoadingContext#getModEventBus() mod-specific event bus},
+ * only on the {@linkplain LogicalSide#CLIENT logical client}. </p>
+ */
 public class RegisterShadersEvent extends Event implements IModBusEvent
 {
     private final ResourceManager resourceManager;
@@ -39,11 +51,26 @@ public class RegisterShadersEvent extends Event implements IModBusEvent
         this.shaderList = shaderList;
     }
 
+    /**
+     * {@return the client-side resource manager}
+     */
     public ResourceManager getResourceManager()
     {
         return resourceManager;
     }
 
+    /**
+     * Registers a shader, and a callback for when the shader is loaded.
+     *
+     * <p>When creating a {@link ShaderInstance}, pass in the {@linkplain #getResourceManager()
+     * client-side resource manager} as the resource provider. </p>
+     *
+     * <p>Mods should not store the shader instance passed into this method. Instead, mods should store the shader
+     * passed into the registered load callback. </p>
+     *
+     * @param shaderInstance a shader
+     * @param onLoaded       a callback for when the shader is loaded
+     */
     public void registerShader(ShaderInstance shaderInstance, Consumer<ShaderInstance> onLoaded)
     {
         shaderList.add(Pair.of(shaderInstance, onLoaded));
