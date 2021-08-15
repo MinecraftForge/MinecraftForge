0d654af4e09b9235b09794a38a05b54ba9691579
malte0811 <malte0811@web.de>
2021-08-15 15:06:40 +0200

    Apply suggestion from #7978

--- src/test/java/net/minecraftforge/debug/client/rendering/StencilEnableTest.java
+++ src/test/java/net/minecraftforge/debug/client/rendering/StencilEnableTest.java
@@ -19,7 +19,6 @@
 
 package net.minecraftforge.debug.client.rendering;
 
-import com.mojang.blaze3d.pipeline.RenderTarget;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.fml.DeferredWorkQueue;
@@ -37,6 +36,6 @@ public class StencilEnableTest {
 
     private void clientSetup(FMLClientSetupEvent event) {
         if (ENABLED)
-            event.enqueueWork(RenderTarget::enableStencil);
+            event.enqueueWork(() -> Minecraft.getInstance().getMainRenderTarget().enableStencil());
     }
 }
