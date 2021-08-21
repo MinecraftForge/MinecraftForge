e76bf2ff7fb23f6c2bc31804e9a9a879c8409b2f
Minecraftschurli <minecraftschurli@gmail.com>
2021-08-21 22:12:23 +0200

    reformat

--- src/main/java/net/minecraftforge/common/crafting/VanillaAnvilRecipe.java
+++ src/main/java/net/minecraftforge/common/crafting/VanillaAnvilRecipe.java
@@ -51,16 +51,25 @@ public record VanillaAnvilRecipe(ResourceLocation id) implements IAnvilRecipe
         int k = 0;
         boolean flag = false;
 
-        if (!itemstack2.isEmpty()) {
-            if (!net.minecraftforge.common.ForgeHooks.onAnvilChange(container.getMenu(), itemstack, itemstack2, container.getResultSlots(), container.getItemName(), j, container.getPlayer())) return false;
+        if (!itemstack2.isEmpty())
+        {
+            if (!net.minecraftforge.common.ForgeHooks.onAnvilChange(container.getMenu(), itemstack, itemstack2, container.getResultSlots(), container.getItemName(), j, container.getPlayer()))
+            {
+                return false;
+            }
             flag = itemstack2.is(Items.ENCHANTED_BOOK) && !EnchantedBookItem.getEnchantments(itemstack2).isEmpty();
-            if (itemstack.isDamageableItem() && itemstack.getItem().isValidRepairItem(itemstack, itemstack2)) {
+            if (itemstack.isDamageableItem() && itemstack.getItem().isValidRepairItem(itemstack, itemstack2))
+            {
                 int l2 = Math.min(itemstack.getDamageValue(), itemstack.getMaxDamage() / 4);
-                if (l2 <= 0) {
+                if (l2 <= 0)
+                {
                     return false;
                 }
-            } else {
-                if (!flag && (!itemstack.is(itemstack2.getItem()) || !itemstack.isDamageableItem())) {
+            }
+            else
+            {
+                if (!flag && (!itemstack.is(itemstack2.getItem()) || !itemstack.isDamageableItem()))
+                {
                     return false;
                 }
 
@@ -68,34 +77,44 @@ public record VanillaAnvilRecipe(ResourceLocation id) implements IAnvilRecipe
                 boolean flag2 = false;
                 boolean flag3 = false;
 
-                for(Enchantment enchantment1 : map1.keySet()) {
-                    if (enchantment1 != null) {
+                for (Enchantment enchantment1 : map1.keySet())
+                {
+                    if (enchantment1 != null)
+                    {
                         int i2 = map.getOrDefault(enchantment1, 0);
                         int j2 = map1.get(enchantment1);
                         j2 = i2 == j2 ? j2 + 1 : Math.max(j2, i2);
                         boolean flag1 = enchantment1.canEnchant(itemstack);
-                        if (container.getPlayer().getAbilities().instabuild || itemstack.is(Items.ENCHANTED_BOOK)) {
+                        if (container.getPlayer().getAbilities().instabuild || itemstack.is(Items.ENCHANTED_BOOK))
+                        {
                             flag1 = true;
                         }
 
-                        for(Enchantment enchantment : map.keySet()) {
-                            if (enchantment != enchantment1 && !enchantment1.isCompatibleWith(enchantment)) {
+                        for (Enchantment enchantment : map.keySet())
+                        {
+                            if (enchantment != enchantment1 && !enchantment1.isCompatibleWith(enchantment))
+                            {
                                 flag1 = false;
                                 ++i;
                             }
                         }
 
-                        if (!flag1) {
+                        if (!flag1)
+                        {
                             flag3 = true;
-                        } else {
+                        }
+                        else
+                        {
                             flag2 = true;
-                            if (j2 > enchantment1.getMaxLevel()) {
+                            if (j2 > enchantment1.getMaxLevel())
+                            {
                                 j2 = enchantment1.getMaxLevel();
                             }
 
                             map.put(enchantment1, j2);
                             int k3 = 0;
-                            switch(enchantment1.getRarity()) {
+                            switch (enchantment1.getRarity())
+                            {
                                 case COMMON:
                                     k3 = 1;
                                     break;
@@ -109,39 +128,53 @@ public record VanillaAnvilRecipe(ResourceLocation id) implements IAnvilRecipe
                                     k3 = 8;
                             }
 
-                            if (flag) {
+                            if (flag)
+                            {
                                 k3 = Math.max(1, k3 / 2);
                             }
 
                             i += k3 * j2;
-                            if (itemstack.getCount() > 1) {
+                            if (itemstack.getCount() > 1)
+                            {
                                 i = 40;
                             }
                         }
                     }
                 }
 
-                if (flag3 && !flag2) {
+                if (flag3 && !flag2)
+                {
                     return false;
                 }
             }
         }
 
-        if (StringUtils.isBlank(container.getItemName())) {
-            if (itemstack.hasCustomHoverName()) {
+        if (StringUtils.isBlank(container.getItemName()))
+        {
+            if (itemstack.hasCustomHoverName())
+            {
                 k = 1;
                 i += k;
             }
-        } else if (!container.getItemName().equals(itemstack.getHoverName().getString())) {
+        }
+        else if (!container.getItemName().equals(itemstack.getHoverName().getString()))
+        {
             k = 1;
             i += k;
         }
-        if (flag && !itemstack.isBookEnchantable(itemstack2)) return false;
+        if (flag && !itemstack.isBookEnchantable(itemstack2))
+        {
+            return false;
+        }
 
         int cost = j + i;
-        if (i <= 0) return false;
+        if (i <= 0)
+        {
+            return false;
+        }
 
-        if (k == i && cost >= 40) {
+        if (k == i && cost >= 40)
+        {
             cost = 39;
         }
 
@@ -162,16 +195,20 @@ public record VanillaAnvilRecipe(ResourceLocation id) implements IAnvilRecipe
         container.setItemCost(1, 0);
         boolean flag = false;
 
-        if (!itemstack2.isEmpty()) {
+        if (!itemstack2.isEmpty())
+        {
             flag = itemstack2.getItem() == Items.ENCHANTED_BOOK && !EnchantedBookItem.getEnchantments(itemstack2).isEmpty();
-            if (itemstack1.isDamageableItem() && itemstack1.getItem().isValidRepairItem(itemstack, itemstack2)) {
+            if (itemstack1.isDamageableItem() && itemstack1.getItem().isValidRepairItem(itemstack, itemstack2))
+            {
                 int l2 = Math.min(itemstack1.getDamageValue(), itemstack1.getMaxDamage() / 4);
-                if (l2 <= 0) {
+                if (l2 <= 0)
+                {
                     return ItemStack.EMPTY;
                 }
 
                 int i3;
-                for(i3 = 0; l2 > 0 && i3 < itemstack2.getCount(); ++i3) {
+                for (i3 = 0; l2 > 0 && i3 < itemstack2.getCount(); ++i3)
+                {
                     int j3 = itemstack1.getDamageValue() - l2;
                     itemstack1.setDamageValue(j3);
                     ++i;
@@ -179,22 +216,28 @@ public record VanillaAnvilRecipe(ResourceLocation id) implements IAnvilRecipe
                 }
 
                 container.setItemCost(1, i3);
-            } else {
-                if (!flag && (!itemstack1.is(itemstack2.getItem()) || !itemstack1.isDamageableItem())) {
+            }
+            else
+            {
+                if (!flag && (!itemstack1.is(itemstack2.getItem()) || !itemstack1.isDamageableItem()))
+                {
                     return ItemStack.EMPTY;
                 }
 
-                if (itemstack1.isDamageableItem() && !flag) {
+                if (itemstack1.isDamageableItem() && !flag)
+                {
                     int l = itemstack.getMaxDamage() - itemstack.getDamageValue();
                     int i1 = itemstack2.getMaxDamage() - itemstack2.getDamageValue();
                     int j1 = i1 + itemstack1.getMaxDamage() * 12 / 100;
                     int k1 = l + j1;
                     int l1 = itemstack1.getMaxDamage() - k1;
-                    if (l1 < 0) {
+                    if (l1 < 0)
+                    {
                         l1 = 0;
                     }
 
-                    if (l1 < itemstack1.getDamageValue()) {
+                    if (l1 < itemstack1.getDamageValue())
+                    {
                         itemstack1.setDamageValue(l1);
                         i += 2;
                     }
@@ -204,98 +247,116 @@ public record VanillaAnvilRecipe(ResourceLocation id) implements IAnvilRecipe
                 boolean flag2 = false;
                 boolean flag3 = false;
 
-                for(Enchantment enchantment1 : map1.keySet()) {
-                    if (enchantment1 != null) {
+                for (Enchantment enchantment1 : map1.keySet())
+                {
+                    if (enchantment1 != null)
+                    {
                         int i2 = map.getOrDefault(enchantment1, 0);
                         int j2 = map1.get(enchantment1);
                         j2 = i2 == j2 ? j2 + 1 : Math.max(j2, i2);
                         boolean flag1 = enchantment1.canEnchant(itemstack);
-                        if (container.getPlayer().getAbilities().instabuild || itemstack.is(Items.ENCHANTED_BOOK)) {
+                        if (container.getPlayer().getAbilities().instabuild || itemstack.is(Items.ENCHANTED_BOOK))
+                        {
                             flag1 = true;
                         }
 
-                        for(Enchantment enchantment : map.keySet()) {
-                            if (enchantment != enchantment1 && !enchantment1.isCompatibleWith(enchantment)) {
+                        for (Enchantment enchantment : map.keySet())
+                        {
+                            if (enchantment != enchantment1 && !enchantment1.isCompatibleWith(enchantment))
+                            {
                                 flag1 = false;
                                 ++i;
                             }
                         }
 
-                        if (!flag1) {
+                        if (!flag1)
+                        {
                             flag3 = true;
-                        } else {
+                        }
+                        else
+                        {
                             flag2 = true;
-                            if (j2 > enchantment1.getMaxLevel()) {
+                            if (j2 > enchantment1.getMaxLevel())
+                            {
                                 j2 = enchantment1.getMaxLevel();
                             }
 
                             map.put(enchantment1, j2);
-                            int k3 = 0;
-                            switch(enchantment1.getRarity()) {
-                                case COMMON:
-                                    k3 = 1;
-                                    break;
-                                case UNCOMMON:
-                                    k3 = 2;
-                                    break;
-                                case RARE:
-                                    k3 = 4;
-                                    break;
-                                case VERY_RARE:
-                                    k3 = 8;
-                            }
+                            int k3 = switch (enchantment1.getRarity())
+                                    {
+                                        case COMMON -> 1;
+                                        case UNCOMMON -> 2;
+                                        case RARE -> 4;
+                                        case VERY_RARE -> 8;
+                                    };
 
-                            if (flag) {
+                            if (flag)
+                            {
                                 k3 = Math.max(1, k3 / 2);
                             }
 
                             i += k3 * j2;
-                            if (itemstack.getCount() > 1) {
+                            if (itemstack.getCount() > 1)
+                            {
                                 i = 40;
                             }
                         }
                     }
                 }
 
-                if (flag3 && !flag2) {
+                if (flag3 && !flag2)
+                {
                     return ItemStack.EMPTY;
                 }
             }
         }
 
-        if (StringUtils.isBlank(container.getItemName())) {
-            if (itemstack.hasCustomHoverName()) {
+        if (StringUtils.isBlank(container.getItemName()))
+        {
+            if (itemstack.hasCustomHoverName())
+            {
                 k = 1;
                 i += k;
                 itemstack1.resetHoverName();
             }
-        } else if (!container.getItemName().equals(itemstack.getHoverName().getString())) {
+        }
+        else if (!container.getItemName().equals(itemstack.getHoverName().getString()))
+        {
             k = 1;
             i += k;
             itemstack1.setHoverName(new TextComponent(container.getItemName()));
         }
-        if (flag && !itemstack1.isBookEnchantable(itemstack2)) itemstack1 = ItemStack.EMPTY;
-
-        container.setXpCost(j + i);
-        if (i <= 0) {
+        if (flag && !itemstack1.isBookEnchantable(itemstack2))
+        {
             itemstack1 = ItemStack.EMPTY;
         }
 
-        if (k == i && k > 0 && container.getXpCost() >= 40) {
+        container.setXpCost(j + i);
+        if (i <= 0)
+        {
+            itemstack1 = ItemStack.EMPTY;
+        }
+
+        if (k == i && k > 0 && container.getXpCost() >= 40)
+        {
             container.setXpCost(39);
         }
 
-        if (container.getXpCost() >= 40 && !container.getPlayer().getAbilities().instabuild) {
+        if (container.getXpCost() >= 40 && !container.getPlayer().getAbilities().instabuild)
+        {
             itemstack1 = ItemStack.EMPTY;
         }
 
-        if (!itemstack1.isEmpty()) {
+        if (!itemstack1.isEmpty())
+        {
             int k2 = itemstack1.getBaseRepairCost();
-            if (!itemstack2.isEmpty() && k2 < itemstack2.getBaseRepairCost()) {
+            if (!itemstack2.isEmpty() && k2 < itemstack2.getBaseRepairCost())
+            {
                 k2 = itemstack2.getBaseRepairCost();
             }
 
-            if (k != i || k == 0) {
+            if (k != i || k == 0)
+            {
                 k2 = AnvilMenu.calculateIncreasedRepairCost(k2);
             }
 
