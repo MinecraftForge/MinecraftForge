c73c01024e918cf39139db35a18383ef6f9e4aa0
Ian Rehwinkel <ian.rehwinkel@tutanota.com>
2021-09-03 12:25:30 +0200

    redoing changes to be more like globallootmodifiers

---
+++ src/test/java/net/minecraftforge/debug/world/StructurePoolModifierTest.java
@@ -0,0 +1,17 @@
+package net.minecraftforge.debug.world;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(StructurePoolModifierTest.MODID)
+public class StructurePoolModifierTest {
+
+    public static final String MODID = "structure_pool_modifier_test";
+    private static final Logger LOGGER = LogManager.getLogger(MODID);
+
+    public StructurePoolModifierTest() {
+        //TODO
+    }
+
+}
