4cd4d184a4d0885ccc6ba9e9cbf09892db30bb7f
noeppi_noeppi <noeppinoeppi@gmail.com>
2021-09-03 16:05:30 +0200

    Propagate all arguments passed to the server start scripts to the game

--- server_files/run.sh
+++ server_files/run.sh
@@ -2,4 +2,4 @@
 # Forge requires a configured set of both JVM and program arguments.
 # Add custom JVM arguments to the user_jvm_args.txt
 # Add custom program arguments {such as nogui} to this file at the end of the next line
-java @user_jvm_args.txt @libraries/@MAVEN_PATH@/unix_args.txt
+java @user_jvm_args.txt @libraries/@MAVEN_PATH@/unix_args.txt "$@"
