2e81bc831c558be1dda7979df4c6f6ef256bab3b
lobbienjonsji <36815592+lobbienjonsji@users.noreply.github.com>
2021-09-17 08:09:28 +0200

    Improved Imports, removed some redundant code and updated licences

--- src/main/java/net/minecraftforge/items/ItemDecoratorHandler.java
+++ src/main/java/net/minecraftforge/items/ItemDecoratorHandler.java
@@ -1,3 +1,22 @@
+/*
+ * Minecraft Forge
+ * Copyright (c) 2016-2021.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package net.minecraftforge.items;
 
 import net.minecraft.client.gui.Font;
@@ -5,12 +24,12 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.client.ItemDecorator;
 
 import javax.annotation.Nullable;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ItemDecoratorHandler implements IItemDecoratorHandler
 {
     private Map<String, ItemDecorator> ItemDecoratorCache = new HashMap();
-    private static final String DECORATORS = "CapabilityItemDecoratorHandler";
     
     @Override
     public void addDecorator(ItemDecorator decorator)
