771df292c815e1ab4affbfad7ec84824e667a3d6
diesieben07 <diesieben07@gmail.com>
2021-10-25 12:19:42 +0200

    Update FMLNETVERSION

--- src/main/java/net/minecraftforge/fmllegacy/network/FMLNetworkConstants.java
+++ src/main/java/net/minecraftforge/fmllegacy/network/FMLNetworkConstants.java
@@ -33,7 +33,7 @@ import java.util.List;
 public class FMLNetworkConstants
 {
     public static final String FMLNETMARKER = "FML";
-    public static final int FMLNETVERSION = 2;
+    public static final int FMLNETVERSION = 3;
     public static final String NETVERSION = FMLNETMARKER + FMLNETVERSION;
     public static final String NOVERSION = "NONE";
 
