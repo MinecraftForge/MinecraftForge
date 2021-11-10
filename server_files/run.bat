3ab9abb62309feab71ece5d974f9c6432245b8af
DaemonUmbra <daemon.umbra@gmail.com>
2021-11-10 00:49:45 -0500

    Added JVM Version Checker
    This should hopefully allow us to get warnings out to users on old JVMs

--- server_files/run.bat
+++ server_files/run.bat
@@ -1,6 +1,13 @@
+@ECHO OFF
+
+REM Some versions of Java lack features necessary for Forge to function.
+REM CheckVersion.java will be invoked to check for these versions.
+REM To skip this check, simply comment out the following line:
+java CheckVersion.java || EXIT
+
 REM Forge requires a configured set of both JVM and program arguments.
 REM Add custom JVM arguments to the user_jvm_args.txt
 REM Add custom program arguments {such as nogui} to this file in the next line before the %* or
-REM  pass them to this script directly
+REM pass them to this script directly (i.e. 'run.bat nogui')
 java @user_jvm_args.txt @libraries/@MAVEN_PATH@/win_args.txt %*
 pause
--- server_files/run.sh
+++ server_files/run.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
+
+# Some versions of Java lack features necessary for Forge to function.
+# CheckVersion.java will be invoked to check for these versions.
+# To skip this check, simply comment out the following line:
+if ! java CheckVersion.java ; then exit 0; fi
+
 # Forge requires a configured set of both JVM and program arguments.
 # Add custom JVM arguments to the user_jvm_args.txt
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
-#  pass them to this script directly
+# pass them to this script directly (i.e. 'run.sh nogui')
 java @user_jvm_args.txt @libraries/@MAVEN_PATH@/unix_args.txt "$@"
