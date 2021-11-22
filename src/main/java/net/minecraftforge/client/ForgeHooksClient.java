ae7609979e8781aab5c20628b7c08a9b185ce1ef
Sara Freimer <sara@freimer.com>
2021-11-22 16:19:02 -0500

    Backport #8207 to 1.16 (#8209)

--- src/main/java/net/minecraftforge/client/ForgeHooksClient.java
+++ src/main/java/net/minecraftforge/client/ForgeHooksClient.java
@@ -139,6 +139,11 @@ public class ForgeHooksClient
      */
     private static final Stack<Screen> guiLayers = new Stack<>();
 
+    public static void resizeGuiLayers(Minecraft minecraft, int width, int height)
+    {
+        guiLayers.forEach(screen -> screen.resize(minecraft, width, height));
+    }
+
     public static void clearGuiLayers(Minecraft minecraft)
     {
         while(guiLayers.size() > 0)
