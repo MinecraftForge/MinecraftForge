6890b8a31ade38e1d2c5010e488c38a09e9876fd
Ian Rehwinkel <ian.rehwinkel@tutanota.com>
2021-11-25 08:51:17 +0100

    Fixed license, javadoc and constructor parameter name in ForgeSerializerTypeRegistryEntry.
    Signed-off-by: Ian Rehwinkel <ian.rehwinkel@tutanota.com>

--- src/main/java/net/minecraftforge/registries/ForgeSerializerTypeRegistryEntry.java
+++ src/main/java/net/minecraftforge/registries/ForgeSerializerTypeRegistryEntry.java
@@ -1,3 +1,22 @@
+/*
+ * Minecraft Forge
+ * Copyright (c) 2016-2021.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package net.minecraftforge.registries;
 
 import com.google.common.reflect.TypeToken;
@@ -7,12 +26,19 @@ import net.minecraft.world.level.storage.loot.SerializerType;
 
 import javax.annotation.Nullable;
 
+/**
+ * This class acts as a superclass for all classes that would usually extend {@link SerializerType} to make them Forge-Registry compatible.
+ * Having this class means {@link net.minecraft.world.level.storage.loot.entries.LootPoolEntryType},
+ * {@link net.minecraft.world.level.storage.loot.functions.LootItemFunctionType} and others don't need to implement {@link IForgeRegistryEntry} themselves.
+ * @param <V> The type extending this class
+ * @param <T> The type to be serialized
+ */
 public class ForgeSerializerTypeRegistryEntry<V extends IForgeRegistryEntry<V>, T> extends SerializerType<T> implements IForgeRegistryEntry<V>
 {
 
-    public ForgeSerializerTypeRegistryEntry(Serializer<? extends T> p_79330_)
+    public ForgeSerializerTypeRegistryEntry(Serializer<? extends T> serializer)
     {
-        super(p_79330_);
+        super(serializer);
     }
 
     @SuppressWarnings("serial")
