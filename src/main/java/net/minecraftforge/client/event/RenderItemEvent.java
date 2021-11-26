790d59b9a99670715684a6c4b6994e7ecddecabd
MrScautHD <MrScautHD5@gmail.com>
2021-11-27 00:52:37 +0100

    Update RenderItemEvent.java

--- src/main/java/net/minecraftforge/client/event/RenderItemEvent.java
+++ src/main/java/net/minecraftforge/client/event/RenderItemEvent.java
@@ -17,7 +17,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-package net.minecraftforge.event;
+package net.minecraftforge.client.event;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
