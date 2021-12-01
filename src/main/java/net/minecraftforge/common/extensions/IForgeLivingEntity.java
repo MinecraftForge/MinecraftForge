19d25e123abd846ce7ac00ee983b4df9604409ca
Bailey Hollingsworth <baileyholl@gmail.com>
2021-12-01 13:04:22 -0600

    Restore missing getExperienceDrop hook in getExperienceReward, make method an extension from IForgeLivingEntity

---
+++ src/main/java/net/minecraftforge/common/extensions/IForgeLivingEntity.java
@@ -0,0 +1,20 @@
+package net.minecraftforge.common.extensions;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+
+public interface IForgeLivingEntity
+{
+
+    private LivingEntity self() { return (LivingEntity) this; }
+
+    /**
+     * Returns the experience value of the entity after the LivingExperienceDropEvent is fired.
+     * Returns 0 if the entity should not drop experience.
+     *
+     * @param player The attacking player involved in obtaining this experience, typically lastHurtByPlayer.
+     * @return The amount of experience
+     */
+    int getTotalExperienceValue(Player player);
+}
