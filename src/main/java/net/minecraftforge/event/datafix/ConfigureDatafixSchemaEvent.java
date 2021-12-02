def0b2f124240a05a478f1079fdde9c6b7067acb
Marc Hermans <marc.hermans@ldtteam.com>
2021-12-02 19:14:52 +0100

    Add some initial api for the configure event.

--- src/main/java/net/minecraftforge/event/datafix/ConfigureDatafixSchemaEvent.java
+++ src/main/java/net/minecraftforge/event/datafix/ConfigureDatafixSchemaEvent.java
@@ -1,12 +1,18 @@
 package net.minecraftforge.event.datafix;
 
 import com.google.common.collect.Maps;
+import com.mojang.datafixers.DSL;
 import com.mojang.datafixers.schemas.Schema;
 import com.mojang.datafixers.types.templates.TypeTemplate;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.datafix.fixes.References;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.fml.event.IModBusEvent;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class ConfigureDatafixSchemaEvent extends Event implements IModBusEvent
@@ -16,6 +22,9 @@ public class ConfigureDatafixSchemaEvent extends Event implements IModBusEvent
 
     private final Map<String, Supplier<TypeTemplate>> entityTypes = Maps.newConcurrentMap();
     private final Map<String, Supplier<TypeTemplate>> blockEntityTypes = Maps.newConcurrentMap();
+
+    private final Map<String, Supplier<TypeTemplate>> entityTypesView = Collections.unmodifiableMap(entityTypes);
+    private final Map<String, Supplier<TypeTemplate>> blockEntityTypesView = Collections.unmodifiableMap(blockEntityTypes);
 
     public ConfigureDatafixSchemaEvent(final Schema schema)
     {
@@ -27,13 +36,45 @@ public class ConfigureDatafixSchemaEvent extends Event implements IModBusEvent
         return schema;
     }
 
+    public int getVersion() {
+        return getSchema().getVersionKey();
+    }
+
+    public void registerSimpleBlockEntity(final ResourceLocation registryName) {
+        registerBlockEntity(registryName, DSL::remainder);
+    }
+
+    public void registerSimpleItemInventory(final ResourceLocation registryName, final String nbtKey) {
+        registerBlockEntity(registryName, () -> DSL.optionalFields(nbtKey, DSL.list(References.ITEM_STACK.in(getSchema()))));
+    }
+
+    public void registerBlockEntity(final ResourceLocation registryName, final Function<String, TypeTemplate> template) {
+        registerBlockEntity(registryName, () -> template.apply(registryName.toString()));
+    }
+
+    public void registerBlockEntity(final ResourceLocation registryName, final Supplier<TypeTemplate> template) {
+        blockEntityTypes.put(registryName.toString(), template);
+    }
+
+    public void registerSimpleEntity(final ResourceLocation registryName) {
+        registerEntity(registryName, DSL::remainder);
+    }
+
+    public void registerEntity(final ResourceLocation registryName, final Function<String, TypeTemplate> template) {
+        registerEntity(registryName, () -> template.apply(registryName.toString()));
+    }
+
+    public void registerEntity(final ResourceLocation registryName, final Supplier<TypeTemplate> template) {
+        entityTypes.put(registryName.toString(), template);
+    }
+
     public Map<String, Supplier<TypeTemplate>> getEntityTypes()
     {
-        return entityTypes;
+        return entityTypesView;
     }
 
     public Map<String, Supplier<TypeTemplate>> getBlockEntityTypes()
     {
-        return blockEntityTypes;
+        return blockEntityTypesView;
     }
 }
