8938696ebd1925d4c303ee179e25cc987f444c9b
Marc Hermans <marc.hermans@ldtteam.com>
2021-12-03 18:36:41 +0100

    Document the event.

--- src/main/java/net/minecraftforge/event/datafix/ConfigureDatafixSchemaEvent.java
+++ src/main/java/net/minecraftforge/event/datafix/ConfigureDatafixSchemaEvent.java
@@ -9,12 +9,22 @@ import net.minecraft.util.datafix.fixes.References;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.fml.event.IModBusEvent;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+/**
+ * Event fired in parallel on the mod event bus to allow for the modification
+ * of a DFU Schemas type signature.
+ *
+ * If used to register a new entity or block entity to a particular version,
+ * then this also will need to be registered to all newer versions to persist properly!
+ * As opposed to vanilla which automatically transfers the entities and block entities from older
+ * schemas to newer ones, this event does not do that and the modder is on his own to implement this properly.
+ *
+ * We suggest instead of checking for version equality to check for a greater or equal version.
+ */
 public class ConfigureDatafixSchemaEvent extends Event implements IModBusEvent
 {
 
@@ -23,6 +33,7 @@ public class ConfigureDatafixSchemaEvent extends Event implements IModBusEvent
     private final Map<String, Supplier<TypeTemplate>> entityTypes = Maps.newConcurrentMap();
     private final Map<String, Supplier<TypeTemplate>> blockEntityTypes = Maps.newConcurrentMap();
 
+    //Some unmodifiable views of the above maps
     private final Map<String, Supplier<TypeTemplate>> entityTypesView = Collections.unmodifiableMap(entityTypes);
     private final Map<String, Supplier<TypeTemplate>> blockEntityTypesView = Collections.unmodifiableMap(blockEntityTypes);
 
@@ -31,48 +42,112 @@ public class ConfigureDatafixSchemaEvent extends Event implements IModBusEvent
         this.schema = schema;
     }
 
+    /**
+     * The schema which you can configure.
+     *
+     * @return The schema
+     */
     public Schema getSchema()
     {
         return schema;
     }
 
+    /**
+     * The version of the schema that is about to be configured.
+     *
+     * @return The version.
+     */
     public int getVersion() {
         return getSchema().getVersionKey();
     }
 
+    /**
+     * Registers a new simple (catch-all-nbt) block entity to the CURRENT schema.
+     *
+     * @param registryName The registry name of the block entity.
+     */
     public void registerSimpleBlockEntity(final ResourceLocation registryName) {
         registerBlockEntity(registryName, DSL::remainder);
     }
 
+    /**
+     * Registers a new simple (catch-all-nbt) block entity with an item inventory array on the nbt field with the given name to the CURRENT schema.
+     *
+     * @param registryName The registry name of the block entity.
+     * @param nbtKey The name of the nbt field to where the item array is located.
+     */
     public void registerSimpleItemInventory(final ResourceLocation registryName, final String nbtKey) {
         registerBlockEntity(registryName, () -> DSL.optionalFields(nbtKey, DSL.list(References.ITEM_STACK.in(getSchema()))));
     }
 
+    /**
+     * Allows for the custom definition of a new block entity type and registration of it to the CURRENT schema.
+     * The function is given the registry name of the block entity type template to produce.
+     *
+     * @param registryName The registry name of the block entity type.
+     * @param template The function to produce the block entity type template.
+     */
     public void registerBlockEntity(final ResourceLocation registryName, final Function<String, TypeTemplate> template) {
         registerBlockEntity(registryName, () -> template.apply(registryName.toString()));
     }
 
+    /**
+     * Allows for the custom definition of a new block entity type and registration of it to the CURRENT schema.
+     * The supplier is invoked to produce the block entity type template.
+     *
+     * @param registryName The registry name of the block entity type.
+     * @param template The supplier of the type template.
+     */
     public void registerBlockEntity(final ResourceLocation registryName, final Supplier<TypeTemplate> template) {
         blockEntityTypes.put(registryName.toString(), template);
     }
 
+    /**
+     * Registers a new simple (catch-all-nbt) entity to the CURRENT schema.
+     *
+     * @param registryName The registry name of the entity.
+     */
     public void registerSimpleEntity(final ResourceLocation registryName) {
         registerEntity(registryName, DSL::remainder);
     }
 
+    /**
+     * Allows for the custom definition of a new entity type and registration of it to the CURRENT schema.
+     * The function is given the registry name of the entity type template to produce.
+     *
+     * @param registryName The registry name of the entity type.
+     * @param template The function to produce the entity type template.
+     */
     public void registerEntity(final ResourceLocation registryName, final Function<String, TypeTemplate> template) {
         registerEntity(registryName, () -> template.apply(registryName.toString()));
     }
 
+    /**
+     * Allows for the custom definition of a new entity type and registration of it to the CURRENT schema.
+     * The supplier is invoked to produce the entity type template.
+     *
+     * @param registryName The registry name of the entity type.
+     * @param template The supplier of the type template.
+     */
     public void registerEntity(final ResourceLocation registryName, final Supplier<TypeTemplate> template) {
         entityTypes.put(registryName.toString(), template);
     }
 
+    /**
+     * An unmodifiable map of all registered additional modded block entity types.
+     *
+     * @return The already registered additional modded block entity types.
+     */
     public Map<String, Supplier<TypeTemplate>> getEntityTypes()
     {
         return entityTypesView;
     }
 
+    /**
+     * An unmodifiable map of all registered additional modded entity types.
+     *
+     * @return The already registered additional modded entity types.
+     */
     public Map<String, Supplier<TypeTemplate>> getBlockEntityTypes()
     {
         return blockEntityTypesView;
