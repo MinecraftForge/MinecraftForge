975580ade1ac09722b0e7d939a98dedf2a6e88c4
Won-Ton <wonton@terraforged.com>
2021-12-13 12:57:29 +0000

    Added test cases for: - data override of builtin entry - data-only entries referencing data-only entries - data-only entries referencing lists of mixed builtin and data-only entries

--- src/test/java/net/minecraftforge/debug/world/RegistryAccessExtensionTest.java
+++ src/test/java/net/minecraftforge/debug/world/RegistryAccessExtensionTest.java
@@ -22,11 +22,11 @@ package net.minecraftforge.debug.world;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.Registry;
-import net.minecraft.data.worldgen.placement.VegetationPlacements;
+import net.minecraft.resources.RegistryFileCodec;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Biomes;
-import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.world.RegistryAccessExtension;
 import net.minecraftforge.event.RegistryEvent;
@@ -38,6 +38,7 @@ import net.minecraftforge.registries.ForgeRegistryEntry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 @Mod(RegistryAccessExtensionTest.MODID)
@@ -45,16 +46,22 @@ public class RegistryAccessExtensionTest
 {
     public static final String MODID = "registry_access_extension_test";
     public static final Logger LOGGER = LogManager.getLogger(MODID);
-    public static final ResourceLocation REGISTRY_NAME = new ResourceLocation(MODID, "worldgen/example");
-    public static final RegistryAccessExtension<ExampleComponent> EXAMPLE_COMPONENT = new RegistryAccessExtension<>(REGISTRY_NAME, ExampleComponent.class, ExampleComponent.CODEC);
+
+    public static final ResourceLocation TEST_A_NAME = new ResourceLocation(MODID, "worldgen/test_a");
+    public static final ResourceLocation TEST_B_NAME = new ResourceLocation(MODID, "worldgen/test_b");
+    public static final RegistryAccessExtension<TestA> TEST_A = new RegistryAccessExtension<>(TEST_A_NAME, TestA.class, TestA.DIRECT_CODEC);
+    public static final RegistryAccessExtension<TestB> TEST_B = new RegistryAccessExtension<>(TEST_B_NAME, TestB.class, TestB.DIRECT_CODEC);
 
     public RegistryAccessExtensionTest()
     {
         var modBus = FMLJavaModLoadingContext.get().getModEventBus();
         modBus.addGenericListener(RegistryAccessExtension.class, this::registerExtension);
 
-        EXAMPLE_COMPONENT.getRegister().register(modBus);
-        EXAMPLE_COMPONENT.getRegister().register("builtin_example", builtinSupplier());
+        TEST_A.getRegister().register(modBus);
+        TEST_B.getRegister().register(modBus);
+
+        TEST_A.getRegister().register("test_a_builtin", builtinASupplier());
+        TEST_A.getRegister().register("test_a_builtin_overridden", builtinASupplier());
 
         MinecraftForge.EVENT_BUS.addListener(this::onServerStart);
     }
@@ -63,62 +70,119 @@ public class RegistryAccessExtensionTest
     {
         var registries = event.getServer().registryAccess();
 
-        var biomes = registries.registryOrThrow(Registry.BIOME_REGISTRY);
-        var features = registries.registryOrThrow(Registry.PLACED_FEATURE_REGISTRY);
-        var biomeFeatures = registries.registryOrThrow(EXAMPLE_COMPONENT.getRegistryKey());
+        var biomeRegistry = registries.registryOrThrow(Registry.BIOME_REGISTRY);
+        var testARegistry = registries.ownedRegistryOrThrow(TEST_A.getRegistryKey());
+        var testBRegistry = registries.ownedRegistryOrThrow(TEST_B.getRegistryKey());
 
-        LOGGER.info("RegistryAccessExtensionTest - ExampleComponent registry:");
-        for (var entry : biomeFeatures.entrySet())
+        LOGGER.info("RegistryAccessExtension TestA:");
+        for (var entry : testARegistry.entrySet())
         {
-            var name = entry.getKey().location();
-            var pair = entry.getValue();
+            var registryName = entry.getKey().location();
 
-            // Check that pair holds valid Biome and PlacedFeature references by attempting to
-            // retrieve their key from the RegistryAccess instance. Will return null if invalid.
-            var biomeName = biomes.getKey(pair.getBiome().get());
-            var featureName = features.getKey(pair.getFeature().get());
+            var value = entry.getValue();
+            var name = value.getStringValue();
+            int intValue = value.getIntValue();
+            var biomeName = biomeRegistry.getKey(value.getBiome().get());
 
-            LOGGER.info(" - Name: {}, Biome: {}, Feature: {}", name, biomeName, featureName);
+            LOGGER.info(" - Registry Name: {}, String: {}, Int: {}, Biome: {} (valid={})", registryName, name, intValue, biomeName, biomeName != null);
+        }
+
+        LOGGER.info("RegistryAccessExtension TestB:");
+        for (var entry : testBRegistry.entrySet())
+        {
+            var registryName = entry.getKey().location();
+            var value = entry.getValue();
+
+            var testAName = testARegistry.getKey(value.getTestA().get());
+            LOGGER.info(" - Registry Name: {}, TestA: {} (valid={}), TestAList:", registryName, testAName, testAName != null);
+
+            for (int i = 0; i < value.getTestAList().size(); i++)
+            {
+                var element = value.getTestAList().get(i);
+                var elementName = testARegistry.getKey(element.get());
+                LOGGER.info("   [{}] {} (valid={})", i, elementName, elementName != null);
+            }
         }
     }
 
     private void registerExtension(RegistryEvent.Register<RegistryAccessExtension<?>> event)
     {
-        event.getRegistry().register(EXAMPLE_COMPONENT);
+        event.getRegistry().register(TEST_A);
+        event.getRegistry().register(TEST_B);
     }
 
-    private static Supplier<ExampleComponent> builtinSupplier()
+    private static Supplier<TestA> builtinASupplier()
     {
-        return () -> new ExampleComponent(
-                () -> ForgeRegistries.BIOMES.getValue(Biomes.TAIGA.location()),
-                () -> VegetationPlacements.BAMBOO_VEGETATION
+        return () -> new TestA(
+                "This is the default string value",
+                12345,
+                () -> ForgeRegistries.BIOMES.getValue(Biomes.PLAINS.location())
         );
     }
 
-    public static class ExampleComponent extends ForgeRegistryEntry<ExampleComponent>
+    public static class TestA extends ForgeRegistryEntry<TestA>
     {
-        public static final Codec<ExampleComponent> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-                Biome.CODEC.fieldOf("biome").forGetter(p -> p.biome),
-                PlacedFeature.CODEC.fieldOf("feature").forGetter(p -> p.feature)
-        ).apply(instance, ExampleComponent::new));
+        public static final Codec<TestA> DIRECT_CODEC = RecordCodecBuilder.create(instance -> instance.group(
+                Codec.STRING.fieldOf("string_value").forGetter(TestA::getStringValue),
+                Codec.INT.fieldOf("int_value").forGetter(TestA::getIntValue),
+                Biome.CODEC.fieldOf("biome").forGetter(TestA::getBiome)
+        ).apply(instance, TestA::new));
 
+        public static final ResourceKey<Registry<TestA>> REGISTRY_KEY = ResourceKey.createRegistryKey(TEST_A_NAME);
+        public static final Codec<Supplier<TestA>> CODEC = RegistryFileCodec.create(REGISTRY_KEY, DIRECT_CODEC);
+        public static final Codec<List<Supplier<TestA>>> LIST_CODEC = RegistryFileCodec.homogeneousList(REGISTRY_KEY, DIRECT_CODEC);
+
+        private final String stringValue;
+        private final int intValue;
         private final Supplier<Biome> biome;
-        private final Supplier<PlacedFeature> feature;
 
-        public ExampleComponent(Supplier<Biome> biome, Supplier<PlacedFeature> feature)
+        public TestA(String stringValue, int intValue, Supplier<Biome> biome)
         {
+            this.stringValue = stringValue;
+            this.intValue = intValue;
             this.biome = biome;
-            this.feature = feature;
+        }
+
+        public String getStringValue()
+        {
+            return stringValue;
+        }
+
+        public int getIntValue()
+        {
+            return intValue;
         }
 
         public Supplier<Biome> getBiome()
         {
             return biome;
         }
+    }
 
-        public Supplier<PlacedFeature> getFeature()
+    public static class TestB extends ForgeRegistryEntry<TestB>
+    {
+        public static final Codec<TestB> DIRECT_CODEC = RecordCodecBuilder.create(instance -> instance.group(
+                TestA.CODEC.fieldOf("test_a").forGetter(TestB::getTestA),
+                TestA.LIST_CODEC.fieldOf("test_a_list").forGetter(TestB::getTestAList)
+        ).apply(instance, TestB::new));
+
+        private final Supplier<TestA> testA;
+        private final List<Supplier<TestA>> testAList;
+
+        public TestB(Supplier<TestA> refB, List<Supplier<TestA>> refsB)
         {
-            return feature;
+            this.testA = refB;
+            this.testAList = refsB;
+        }
+
+        public Supplier<TestA> getTestA()
+        {
+            return testA;
+        }
+
+        public List<Supplier<TestA>> getTestAList()
+        {
+            return testAList;
         }
     }
 }
