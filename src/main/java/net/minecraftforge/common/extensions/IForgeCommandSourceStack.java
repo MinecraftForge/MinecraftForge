8e7bc549dbbf2cc53246020445e24366f80e4cba
coehlrich <coehlrich@users.noreply.github.com>
2021-12-28 11:10:57 +1300

    add javadocs to source stack classes

--- src/main/java/net/minecraftforge/common/extensions/IForgeCommandSourceStack.java
+++ src/main/java/net/minecraftforge/common/extensions/IForgeCommandSourceStack.java
@@ -26,6 +26,9 @@ import net.minecraft.world.item.crafting.RecipeManager;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.scores.Scoreboard;
 
+/**
+ * Additional methods for {@link CommandSourceStack} so that commands and arguments can access various things without directly referencing using server specific classes
+ */
 public interface IForgeCommandSourceStack
 {
 
@@ -34,21 +37,33 @@ public interface IForgeCommandSourceStack
         return (CommandSourceStack) this;
     }
 
+    /**
+     * @return the scoreboard
+     */
     default Scoreboard getScoreboard()
     {
         return self().getServer().getScoreboard();
     }
 
+    /**
+     * @return the advancement from the id
+     */
     default Advancement getAdvancement(ResourceLocation id)
     {
         return self().getServer().getAdvancements().getAdvancement(id);
     }
 
+    /**
+     * @return the recipe manager
+     */
     default RecipeManager getRecipeManager()
     {
         return self().getServer().getRecipeManager();
     }
 
+    /**
+     * @return the level but without being specifically the server side level
+     */
     default Level getUnsidedLevel()
     {
         return self().getLevel();
