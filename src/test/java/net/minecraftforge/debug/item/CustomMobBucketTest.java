c03c20556aa6e2d7101dab81ba4814d5952976aa
EERussianguy <eerussianguy@gmail.com>
2021-12-27 10:03:15 -0500

    cleanup test mod class

--- src/test/java/net/minecraftforge/debug/item/CustomMobBucketTest.java
+++ src/test/java/net/minecraftforge/debug/item/CustomMobBucketTest.java
@@ -1,3 +1,22 @@
+/*
+ * Minecraft Forge
+ * Copyright (c) 2016-2021.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package net.minecraftforge.debug.item;
 
 import net.minecraft.sounds.SoundEvents;
@@ -19,13 +38,17 @@ public class CustomMobBucketTest
     public static final String MODID = "custom_mob_bucket_test";
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 
-    public static final boolean ENABLE = true;
+    public static final boolean ENABLED = true;
 
-    public static final RegistryObject<Item> COW_BUCKET = ITEMS.register("cow_bucket", () -> new MobBucketItem(() -> EntityType.COW, () -> Fluids.WATER, () -> SoundEvents.BUCKET_EMPTY_FISH, (new Item.Properties()).stacksTo(1).tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> COW_BUCKET = ITEMS.register("cow_bucket", () -> new MobBucketItem(
+        () -> EntityType.COW,
+        () -> Fluids.WATER,
+        () -> SoundEvents.BUCKET_EMPTY_FISH,
+        (new Item.Properties()).stacksTo(1).tab(CreativeModeTab.TAB_MISC)));
 
     public CustomMobBucketTest()
     {
-        if (ENABLE)
+        if (ENABLED)
         {
             IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
             ITEMS.register(modEventBus);
