cef8c1c1c795d2f40895a9ad79f019faf3cdaf55
Commoble <josephbettendorff@yahoo.com>
2021-12-27 18:23:54 -0600

    Make RegistryAccessCodec singletonny and clean up javadocs

--- src/main/java/net/minecraftforge/common/RegistryAccessCodec.java
+++ src/main/java/net/minecraftforge/common/RegistryAccessCodec.java
@@ -28,22 +28,27 @@ import com.mojang.serialization.MapLike;
 import com.mojang.serialization.RecordBuilder;
 
 import net.minecraft.core.RegistryAccess;
+import net.minecraft.resources.RegistryLookupCodec;
 import net.minecraft.resources.RegistryReadOps;
 
 /**
  * Unit codec for getting access to dynamic registries during dimension loading.
- * This is only safe to use while loading dimensions (LevelStems, ChunkGenerators, and BiomeSources);
- * when access to registries is needed while loading dynamic registry objects such as configured features,
- * then RegistryLookupCodec should be used instead.
+ * This is only safe to use while loading dimensions (LevelStems, ChunkGenerators, and BiomeSources).
+ * When access to registries is needed while loading dynamic registry objects such as configured features,
+ * then {@link RegistryLookupCodec} should be used instead.
  */
 public final class RegistryAccessCodec extends MapCodec<RegistryAccess>
 {
     public static final RegistryAccessCodec INSTANCE = new RegistryAccessCodec();
+    
+    private RegistryAccessCodec()
+    {
+    }
 
     @Override
     public <T> DataResult<RegistryAccess> decode(DynamicOps<T> ops, MapLike<T> input)
     {
-        return ops instanceof RegistryReadOps ? DataResult.success(((RegistryReadOps)ops).registryAccess) : DataResult.error("Not a registry ops");
+        return ops instanceof RegistryReadOps ? DataResult.success(((RegistryReadOps) ops).registryAccess) : DataResult.error("Not a registry ops");
     }
 
     @Override
