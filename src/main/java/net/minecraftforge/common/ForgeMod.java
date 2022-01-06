cb6805e0f05b7067dc221cccb2e899ff1b038be9
Take Weiland <diesieben07@gmail.com>
2022-01-06 19:14:05 +0100

    Fix ingredients not being invalidated at all

--- src/main/java/net/minecraftforge/common/ForgeMod.java
+++ src/main/java/net/minecraftforge/common/ForgeMod.java
@@ -167,6 +167,7 @@ public class ForgeMod implements WorldPersistenceHooks.WorldPersistenceHook
 
         MinecraftForge.EVENT_BUS.addListener(VillagerTradingManager::loadTrades);
         MinecraftForge.EVENT_BUS.register(MinecraftForge.INTERNAL_HANDLER);
+        MinecraftForge.EVENT_BUS.addListener(this::mappingChanged);
         BiomeDictionary.init();
     }
 
