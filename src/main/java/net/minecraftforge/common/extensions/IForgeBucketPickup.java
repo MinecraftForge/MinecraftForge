2cdfc1669bc2ecf3006d030eafd426f3d181e600
Sara Freimer <sara@freimer.com>
2022-01-08 15:56:39 -0500

    Improve javadoc

--- src/main/java/net/minecraftforge/common/extensions/IForgeBucketPickup.java
+++ src/main/java/net/minecraftforge/common/extensions/IForgeBucketPickup.java
@@ -35,6 +35,8 @@ public interface IForgeBucketPickup
     /**
      * State sensitive variant of {@link BucketPickup#getPickupSound()}.
      *
+     * Override to change the pickup sound based on the {@link BlockState} of the object being picked up.
+     *
      * @param state State
      *
      * @return Sound event for pickup sound or empty if there isn't a pickup sound.
