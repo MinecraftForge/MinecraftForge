f81708296d514c53ca915f54cf14918868b989e5
Marc Hermans <marc.hermans@ldtteam.com>
2022-01-15 17:35:45 +0100

    Fix the derp in the build configuration.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -47,12 +47,10 @@ project {
         text("env.PUBLISHED_JAVA_FML_ARTIFACT_VERSION", "0.0.0-SNAPSHOT", label = "Published fmlonly artifact version", description = "The version for fml only that has been published by this build. Can not be empty.", allowEmpty = false)
         text("env.PUBLISHED_JAVA_GROUP", "net.minecraftforge", label = "Published group", description = "The maven coordinate group that has been published by this build. Can not be empty.", allowEmpty = false)
 
-        params {
-            checkbox("should_execute_build", "false", label = "Should build", description = "Indicates if the build task should be executed.", display = ParameterDisplay.HIDDEN,
-                checked = "true", unchecked = "false")
-            checkbox("should_execute_test", "false", label = "Should build", description = "Indicates if the build task should be executed.", display = ParameterDisplay.HIDDEN,
-                checked = "true", unchecked = "false")
-        }
+        checkbox("should_execute_build", "false", label = "Should build", description = "Indicates if the build task should be executed.", display = ParameterDisplay.HIDDEN,
+            checked = "true", unchecked = "false")
+        checkbox("should_execute_test", "false", label = "Should build", description = "Indicates if the build task should be executed.", display = ParameterDisplay.HIDDEN,
+            checked = "true", unchecked = "false")
     }
 
     features {
