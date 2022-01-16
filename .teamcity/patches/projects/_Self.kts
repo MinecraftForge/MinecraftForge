fe7dcdf637baabb254ea72a8ee8939e26c27767a
cpw <cpw@weeksfamily.ca>
2022-01-16 23:26:35 +0000

    TeamCity change in 'MinecraftForge / MinecraftForge' project: project parameters were changed

--- .teamcity/patches/projects/_Self.kts
+++ .teamcity/patches/projects/_Self.kts
@@ -17,7 +17,7 @@ changeProject(DslContext.projectId) {
             password("env.KEYSTORE_URL", "credentialsJSON:b5b933de-054e-4503-b6ea-d5939a500356", display = ParameterDisplay.HIDDEN)
         }
         add {
-            password("env.KEYSTORE_PASSWORD", "credentialsJSON:6c5b69bf-cf7f-4979-a124-ccafc0df2623", display = ParameterDisplay.HIDDEN)
+            password("env.KEYSTORE_PASSWORD", "credentialsJSON:d7b964e3-a1fd-47a8-b892-6f601fe47479", display = ParameterDisplay.HIDDEN)
         }
     }
 
