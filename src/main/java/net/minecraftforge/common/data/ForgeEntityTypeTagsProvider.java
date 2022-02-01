ab0622f58b468490ff6a4a13827fdb198a31d641
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2022-02-01 02:14:41 -0500

    Added datagen provider

---
+++ src/main/java/net/minecraftforge/common/data/ForgeEntityTypeTagsProvider.java
@@ -0,0 +1,46 @@
+/*
+ * Minecraft Forge
+ * Copyright (c) 2016-2021.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package net.minecraftforge.common.data;
+
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.tags.EntityTypeTagsProvider;
+import net.minecraft.world.entity.EntityType;
+import net.minecraftforge.common.Tags;
+
+public class ForgeEntityTypeTagsProvider extends EntityTypeTagsProvider {
+
+    public ForgeEntityTypeTagsProvider(DataGenerator generator, String modId, ExistingFileHelper existingFileHelper)
+    {
+        super(generator, modId, existingFileHelper);
+    }
+
+    @Override
+    public void addTags()
+    {
+        tag(Tags.EntityTypes.BOSSES).add(EntityType.ENDER_DRAGON, EntityType.WITHER);
+        tag(Tags.EntityTypes.CONSTRUCTS).add(EntityType.ELDER_GUARDIAN, EntityType.GUARDIAN, EntityType.IRON_GOLEM, EntityType.SNOW_GOLEM);
+    }
+
+    @Override
+    public String getName()
+    {
+        return "Forge EntityType Tags";
+    }
+}
