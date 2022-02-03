7a1622293e460eaa9d89fb6b9f4d8a28fa6b55c7
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2022-02-02 19:08:43 -0500

    Added creepers, draconic mobs, elementals, and ghosts to the EntityType tags

--- src/main/java/net/minecraftforge/common/data/ForgeEntityTypeTagsProvider.java
+++ src/main/java/net/minecraftforge/common/data/ForgeEntityTypeTagsProvider.java
@@ -31,11 +31,31 @@ public class ForgeEntityTypeTagsProvider extends EntityTypeTagsProvider {
         super(generator, "forge", existingFileHelper);
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public void addTags()
     {
         tag(Tags.EntityTypes.BOSSES).add(EntityType.ENDER_DRAGON, EntityType.WITHER);
+        tag(Tags.EntityTypes.CREEPERS).add(EntityType.CREEPER);
+        tag(Tags.EntityTypes.DRACONIC_MOBS).add(EntityType.ENDER_DRAGON);
+
+        /**
+         * Many of these mob types do not appear in vanilla Minecraft, and are thus left empty here
+         */
+        tag(Tags.EntityTypes.ELEMENTALS).addTags(Tags.EntityTypes.ELEMENTALS_AIR, Tags.EntityTypes.ELEMENTALS_DARK, Tags.EntityTypes.ELEMENTALS_EARTH, Tags.EntityTypes.ELEMENTALS_ELECTRIC, Tags.EntityTypes.ELEMENTALS_FIRE, Tags.EntityTypes.ELEMENTALS_ICE, Tags.EntityTypes.ELEMENTALS_LIGHT, Tags.EntityTypes.ELEMENTALS_METAL, Tags.EntityTypes.ELEMENTALS_PLANT, Tags.EntityTypes.ELEMENTALS_WATER);
+        tag(Tags.EntityTypes.ELEMENTALS_AIR);
+        tag(Tags.EntityTypes.ELEMENTALS_DARK);
+        tag(Tags.EntityTypes.ELEMENTALS_EARTH);
+        tag(Tags.EntityTypes.ELEMENTALS_ELECTRIC);
+        tag(Tags.EntityTypes.ELEMENTALS_FIRE).add(EntityType.BLAZE);
+        tag(Tags.EntityTypes.ELEMENTALS_ICE).add(EntityType.SNOW_GOLEM);
+        tag(Tags.EntityTypes.ELEMENTALS_LIGHT);
+        tag(Tags.EntityTypes.ELEMENTALS_METAL).add(EntityType.IRON_GOLEM);
+        tag(Tags.EntityTypes.ELEMENTALS_PLANT);
+        tag(Tags.EntityTypes.ELEMENTALS_WATER);
+
         tag(Tags.EntityTypes.FISH).add(EntityType.COD, EntityType.PUFFERFISH, EntityType.SALMON, EntityType.TROPICAL_FISH);
+        tag(Tags.EntityTypes.GHOSTS).add(EntityType.VEX);
     }
 
     @Override
