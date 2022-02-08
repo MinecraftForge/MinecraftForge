bedf5faab422f676329e33646768946e9c21c93a
MichaelTRoss0 <michaeltross0@gmail.com>
2022-02-08 13:23:45 -0500

    Added many animal subtags. Some parts still require some work.

--- src/main/java/net/minecraftforge/common/data/ForgeEntityTypeTagsProvider.java
+++ src/main/java/net/minecraftforge/common/data/ForgeEntityTypeTagsProvider.java
@@ -35,8 +35,27 @@ public class ForgeEntityTypeTagsProvider extends EntityTypeTagsProvider {
     @Override
     public void addTags()
     {
-        tag(Tags.EntityTypes.ANIMALS).addTag(Tags.EntityTypes.ANIMALS_FISH);
+        tag(Tags.EntityTypes.ANIMALS).addTags(Tags.EntityTypes.ANIMALS_AMPHIBIANS, Tags.EntityTypes.ANIMALS_AQUATIC_MAMMALS, Tags.EntityTypes.ANIMALS_FISH);
+        tag(Tags.EntityTypes.ANIMALS_AMPHIBIANS).add(EntityType.AXOLOTL); // TODO Add frogs and tadpoles when they come to Minecraft Java
+        tag(Tags.EntityTypes.ANIMALS_ARTHROPODS);
+        tag(Tags.EntityTypes.ANIMALS_AQUATIC_MAMMALS).add(EntityType.DOLPHIN);
+        tag(Tags.EntityTypes.ANIMALS_AVIANS).addTags(Tags.EntityTypes.ANIMALS_AVIANS_FLYING, Tags.EntityTypes.ANIMALS_AVIANS_FOWLS);
+        tag(Tags.EntityTypes.ANIMALS_AVIANS_FLYING).add(EntityType.PARROT);
+        tag(Tags.EntityTypes.ANIMALS_AVIANS_FOWLS).addTag(Tags.EntityTypes.ANIMALS_AVIANS_FOWLS_LAND);
+        tag(Tags.EntityTypes.ANIMALS_AVIANS_FOWLS_LAND).add(EntityType.CHICKEN);
+//        tag(Tags.EntityTypes.ANIMALS_AVIANS_GROUNDED).add(EntityType.OSTRICH); // Uncomment when the ostrich is added to vanilla Minecraft Java
+        tag(Tags.EntityTypes.ANIMALS_BOVINES).add(EntityType.COW, EntityType.MOOSHROOM, EntityType.SHEEP);
+        tag(Tags.EntityTypes.ANIMALS_CAMELIDS).add(EntityType.LLAMA, EntityType.TRADER_LLAMA);
+        tag(Tags.EntityTypes.ANIMALS_CANIDS).add(EntityType.FOX, EntityType.WOLF);
+        tag(Tags.EntityTypes.ANIMALS_CAPRINES).add(EntityType.GOAT);
+        tag(Tags.EntityTypes.ANIMALS_CEPHALOPODS).add(EntityType.GLOW_SQUID, EntityType.SQUID);
+        tag(Tags.EntityTypes.ANIMALS_EQUINES).add(EntityType.DONKEY, EntityType.HORSE, EntityType.MULE);
+        tag(Tags.EntityTypes.ANIMALS_FELIDS).addTag(Tags.EntityTypes.ANIMALS_FELIDS_SMALL);
+        tag(Tags.EntityTypes.ANIMALS_FELIDS_SMALL).add(EntityType.CAT, EntityType.OCELOT);
         tag(Tags.EntityTypes.ANIMALS_FISH).add(EntityType.COD, EntityType.PUFFERFISH, EntityType.SALMON, EntityType.TROPICAL_FISH);
+        tag(Tags.EntityTypes.ANIMALS_REPTILES).add(EntityType.TURTLE);
+        tag(Tags.EntityTypes.ANIMALS_SWINES).add(EntityType.HOGLIN, EntityType.PIG);
+        tag(Tags.EntityTypes.ANIMALS_URSIDS).add(EntityType.PANDA, EntityType.POLAR_BEAR);
 
         tag(Tags.EntityTypes.BOSSES).add(EntityType.ENDER_DRAGON, EntityType.WITHER);
         tag(Tags.EntityTypes.CREEPERS).add(EntityType.CREEPER);
