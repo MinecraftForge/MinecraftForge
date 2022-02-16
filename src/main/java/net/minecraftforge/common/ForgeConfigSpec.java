31b2054498624aa736fb8aaac1aff25401c7e8ab
sciwhiz12 <arnoldnunag12@gmail.com>
2022-02-16 08:05:54 +0800

    Improve invalid comment check to use proper path
    The previous position of the comment check being in the `comment` method meant that the logged config path is incorrect.
    
    This is because the actual config path is constructed from the current
    path and the path passed in to one of the `define*` methods, while the
    comment check at its previous position only has access to the current
    path (which is the current category containing the value).
    
    The comment check is now changed to flip an invalid comment flag. This
    is then checked later when defining a value or creating a config
    category, which has the full actual path of the value/category.

--- src/main/java/net/minecraftforge/common/ForgeConfigSpec.java
+++ src/main/java/net/minecraftforge/common/ForgeConfigSpec.java
@@ -293,6 +293,7 @@ public class ForgeConfigSpec extends UnmodifiableConfigWrapper<UnmodifiableConfi
         private Map<List<String>, String> levelComments = new HashMap<>();
         private List<String> currentPath = new ArrayList<>();
         private List<ConfigValue<?>> values = new ArrayList<>();
+        private boolean hasInvalidComment = false;
 
         //Object
         public <T> ConfigValue<T> define(String path, T defaultValue) {
@@ -326,6 +327,7 @@ public class ForgeConfigSpec extends UnmodifiableConfigWrapper<UnmodifiableConfi
                 path = tmp;
             }
             storage.set(path, value);
+            checkComment(path);
             context = new BuilderContext();
             return new ConfigValue<>(this, path, defaultSupplier);
         }
@@ -546,28 +548,20 @@ public class ForgeConfigSpec extends UnmodifiableConfigWrapper<UnmodifiableConfi
 
         public Builder comment(String comment)
         {
-            if(comment == null || comment.isEmpty())
+            hasInvalidComment = comment == null || comment.isEmpty();
+            if(hasInvalidComment)
             {
                 comment = "No comment";
-                if(!FMLEnvironment.production)
-                {
-                    LogManager.getLogger().error(CORE, "Null comment for config option {}, this is invalid and may be disallowed in the future.",
-                            DOT_JOINER.join(this.currentPath));
-                }
             }
             context.setComment(comment);
             return this;
         }
         public Builder comment(String... comment)
         {
-            if(comment == null || comment.length < 1 || (comment.length == 1 && comment[0].isEmpty()))
+            hasInvalidComment = comment == null || comment.length < 1 || (comment.length == 1 && comment[0].isEmpty());
+            if(hasInvalidComment)
             {
                 comment = new String[] {"No comment"};
-                if(!FMLEnvironment.production)
-                {
-                    LogManager.getLogger().error(CORE, "Null comment for config option {}, this is invalid and may be disallowed in the future.",
-                            DOT_JOINER.join(this.currentPath));
-                }
             }
 
             context.setComment(comment);
@@ -592,8 +586,8 @@ public class ForgeConfigSpec extends UnmodifiableConfigWrapper<UnmodifiableConfi
 
         public Builder push(List<String> path) {
             currentPath.addAll(path);
+            checkComment(currentPath);
             if (context.hasComment()) {
-
                 levelComments.put(new ArrayList<String>(currentPath), context.buildComment());
                 context.setComment(); // Set to empty
             }
@@ -627,6 +621,19 @@ public class ForgeConfigSpec extends UnmodifiableConfigWrapper<UnmodifiableConfi
             ForgeConfigSpec ret = new ForgeConfigSpec(storage, valueCfg, levelComments);
             values.forEach(v -> v.spec = ret);
             return ret;
+        }
+
+        private void checkComment(List<String> path)
+        {
+            if (hasInvalidComment)
+            {
+                hasInvalidComment = false;
+                if(!FMLEnvironment.production)
+                {
+                    LogManager.getLogger().error(CORE, "Null comment for config option {}, this is invalid and may be disallowed in the future.",
+                            DOT_JOINER.join(path));
+                }
+            }
         }
 
         public interface BuilderConsumer {
