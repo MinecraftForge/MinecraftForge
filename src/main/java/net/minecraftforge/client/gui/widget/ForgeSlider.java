559620bbc15fcd58584ac8f0cc3d0e4c96dd7320
PlatinPython <robert.alexander.kaempf@gmail.com>
2022-03-07 10:40:22 +0100

    Add docs for remaining constructor parameters.

--- src/main/java/net/minecraftforge/client/gui/widget/ForgeSlider.java
+++ src/main/java/net/minecraftforge/client/gui/widget/ForgeSlider.java
@@ -32,7 +32,18 @@ public class ForgeSlider extends AbstractSliderButton
     private final DecimalFormat format;
 
     /**
+     * @param x x position of upper left corner
+     * @param y y position of upper left corner
+     * @param width Width of the widget
+     * @param height Height of the widget
+     * @param prefix {@link Component} displayed before the value string
+     * @param suffix {@link Component} displayed after the value string
+     * @param minValue Minimum (left) value of slider
+     * @param maxValue Maximum (right) value of slider
+     * @param currentValue Starting value when widget is first displayed
+     * @param stepSize Size of step used. Precision will automatically be calculated based on this value if this value is not 0.
      * @param precision Only used when {@code stepSize} is 0. Limited to a maximum of 4 (inclusive).
+     * @param drawString Should text be displayed on the widget
      */
     public ForgeSlider(int x, int y, int width, int height, Component prefix, Component suffix, double minValue, double maxValue, double currentValue, double stepSize, int precision, boolean drawString)
     {
