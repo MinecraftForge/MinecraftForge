8a74fdcd2502e649588decd44720fc97261e81a3
SizableShrimp <sizableshrimp@sizableshrimp.me>
2022-03-13 16:03:05 -0500

    Update fmlonly license headers
    Signed-off-by: SizableShrimp <sizableshrimp@sizableshrimp.me>

--- fmlonly/src/main/java/net/minecraftforge/fmlonly/FMLOnlyBindings.java
+++ fmlonly/src/main/java/net/minecraftforge/fmlonly/FMLOnlyBindings.java
@@ -1,3 +1,8 @@
+/*
+ * Minecraft Forge - Forge Development LLC
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 package net.minecraftforge.fmlonly;
 
 import net.minecraftforge.eventbus.api.BusBuilder;
