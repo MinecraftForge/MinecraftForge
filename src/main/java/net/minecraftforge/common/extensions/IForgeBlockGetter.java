bc4231f7451939b266bd534e20adb82827a0bd67
XFactHD <xfacthd@gmx.de>
2022-03-13 22:34:56 +0100

    Mark IForgeBlockGetter#getExistingBlockEntity() as Nullable

--- src/main/java/net/minecraftforge/common/extensions/IForgeBlockGetter.java
+++ src/main/java/net/minecraftforge/common/extensions/IForgeBlockGetter.java
@@ -10,18 +10,21 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.chunk.*;
+import org.jetbrains.annotations.Nullable;
 
 public interface IForgeBlockGetter
 {
     private BlockGetter self() { return (BlockGetter) this; }
 
     /**
-     * Returns the {@link BlockEntity} at the given position if it exists.
+     * Get the {@link BlockEntity} at the given position if it exists.
      * <p>
      * {@link Level#getBlockEntity(BlockPos)} would create a new {@link BlockEntity} if the
      * {@link net.minecraft.world.level.block.Block} has one, but it has not been placed in the world yet
      * (This can happen on world load).
+     * @return The BlockEntity at the given position or null if it doesn't exist
      */
+    @Nullable
     default BlockEntity getExistingBlockEntity(BlockPos pos)
     {
         if (this instanceof Level level)
