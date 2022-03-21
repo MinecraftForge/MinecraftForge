82523fa9fa2f33a263b8ef62891c6d8974cee9cd
Minecraftschurli <minecraftschurli@gmail.com>
2022-03-21 15:37:59 +0100

    fix mistake in doc comment

--- src/main/java/net/minecraftforge/event/entity/LightningStrikeEvent.java
+++ src/main/java/net/minecraftforge/event/entity/LightningStrikeEvent.java
@@ -12,7 +12,7 @@ import net.minecraftforge.eventbus.api.Event;
 
 /**
  * Event that fires when a lightning strikes.
- * Cancel to prevent vanilla behaviour.
+ * Cancel to prevent vanilla behavior.
  */
 @Cancelable
 public class LightningStrikeEvent extends Event
