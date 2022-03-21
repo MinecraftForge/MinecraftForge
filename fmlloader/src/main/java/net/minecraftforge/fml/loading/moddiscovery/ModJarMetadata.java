3dbf5a13a9d36e47ed7e263974490ba0d1b51e90
sciwhiz12 <arnoldnunag12@gmail.com>
2022-03-21 23:39:39 +0800

    Apply and update license headers

--- fmlloader/src/main/java/net/minecraftforge/fml/loading/moddiscovery/ModJarMetadata.java
+++ fmlloader/src/main/java/net/minecraftforge/fml/loading/moddiscovery/ModJarMetadata.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) Forge Development LLC and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 package net.minecraftforge.fml.loading.moddiscovery;
 
 import cpw.mods.jarhandling.JarMetadata;
