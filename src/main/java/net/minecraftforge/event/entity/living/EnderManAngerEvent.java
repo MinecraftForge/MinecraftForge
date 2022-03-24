b88b9897d7652e6606e028ed49faa93160060281
Minecraftschurli <minecraftschurli@gmail.com>
2022-03-24 19:05:20 +0100

    improve doc comment

--- src/main/java/net/minecraftforge/event/entity/living/EnderManAngerEvent.java
+++ src/main/java/net/minecraftforge/event/entity/living/EnderManAngerEvent.java
@@ -10,8 +10,13 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.eventbus.api.Cancelable;
 
 /**
- * Event that is fired when an enderman checks if the player is looking at him.
- * If cancelled the enderman does not get angry at the player.
+ * This event is fired when an Enderman detects that a player is looking at them.
+ * It will not be fired if the detection is prevented by {@link IForgeItem#isEnderMask}
+ * <p>
+ * This event is {@link Cancelable}.
+ * If this event is canceled, the Enderman will not target the player.
+ * <p>
+ * This event does not have a {@link Result}.
  */
 @Cancelable
 public class EnderManAngerEvent extends LivingEvent {
