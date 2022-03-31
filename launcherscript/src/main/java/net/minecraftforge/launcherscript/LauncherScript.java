9cfbd74b2493120a0bd506c773525a592c7b23b7
Take Weiland <diesieben07@gmail.com>
2022-04-01 00:55:20 +0200

    Remove star import, add license header

--- launcherscript/src/main/java/net/minecraftforge/launcherscript/LauncherScript.java
+++ launcherscript/src/main/java/net/minecraftforge/launcherscript/LauncherScript.java
@@ -1,10 +1,19 @@
+/*
+ * Minecraft Forge - Forge Development LLC
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 package net.minecraftforge.launcherscript;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
 
 public class LauncherScript
 {
