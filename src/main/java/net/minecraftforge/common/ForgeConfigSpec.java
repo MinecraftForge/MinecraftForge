8f2a83bc719887f20970746d044576e86302f372
sciwhiz12 <arnoldnunag12@gmail.com>
2022-04-04 10:29:34 +0800

    Change check to dev-time only for now
    This prevents the change from breaking already published mods, while
    forcing mod developers to fix their incorrect usages in their
    development environment.

--- src/main/java/net/minecraftforge/common/ForgeConfigSpec.java
+++ src/main/java/net/minecraftforge/common/ForgeConfigSpec.java
@@ -834,7 +834,17 @@ public class ForgeConfigSpec extends UnmodifiableConfigWrapper<UnmodifiableConfi
         public T get()
         {
             Preconditions.checkNotNull(spec, "Cannot get config value before spec is built");
-            Preconditions.checkState(spec.childConfig != null, "Cannot get config value before config is loaded");
+            // TODO: Remove this dev-time check so this errors out on both production and dev
+            // This is dev-time-only in 1.18.2, to avoid breaking already published mods while forcing devs to fix their errors
+            if (!FMLEnvironment.production)
+            {
+                // When the above if-check is removed, change message to "Cannot get config value before config is loaded"
+                Preconditions.checkState(spec.childConfig != null, """
+                        Cannot get config value before config is loaded.
+                        This error is currently only thrown in the development environment, to avoid breaking published mods.
+                        In a future version, this will also throw in the production environment.
+                        """);
+            }
             return getRaw();
         }
 
