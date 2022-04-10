ef637889bbed7e3cef510f3812a6ac1a9ef58ef5
Filip Š <filip.stamcar@hotmail.com>
2022-04-10 19:47:47 +0200

    Update JigsawPieceEvent to 1.18.2 & Move event fields behind getters
    Note: Currently has some bugs

--- src/main/java/net/minecraftforge/event/level/JigsawPieceEvent.java
+++ src/main/java/net/minecraftforge/event/level/JigsawPieceEvent.java
@@ -1,10 +1,15 @@
+/*
+ * Minecraft Forge - Forge Development LLC
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 package net.minecraftforge.event.world;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.levelgen.feature.structures.StructurePoolElement;
-import net.minecraft.world.level.levelgen.feature.structures.StructureTemplatePool;
+import net.minecraft.world.level.levelgen.structure.pools.StructurePoolElement;
+import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
 import net.minecraftforge.eventbus.api.Cancelable;
 import net.minecraftforge.eventbus.api.Event;
 
@@ -13,10 +18,8 @@ import java.util.Map;
 import java.util.Random;
 
 /**
- * Is fired from {@link net.minecraft.world.level.levelgen.feature.structures.JigsawPlacement.Placer#tryPlacingChildren}
+ * Is fired from {@link net.minecraft.world.level.levelgen.structure.pools.JigsawPlacement.Placer#tryPlacingChildren}
  * for each generated Jigsaw block after it stores the pool of Jigsaw pieces in can attach to.
- * The list does NOT contain the pieces contained in {@link net.minecraft.world.level.levelgen.feature.structures.StructureTemplatePool#fallback}
- * to avoid infinite generation problems.
  * <br><br>
  * Adding a custom villager's house to plains village and zombie village can be just:
  *
@@ -25,32 +28,33 @@ import java.util.Random;
  *    if (pool.startsWith("village/plains/houses") || pool.startsWith("village/plains/zombie/houses"))
  *      event.pieces.add(StructurePoolElement.single("namespace:path"));
  * }
- *
- * For more detailed insight see <a href=https://mcforge.readthedocs.io/en/1.16.x/events/jigsawevent/>here.</a>
+ * <br><br>
+ * For more detailed insight see <a href=https://mcforge.readthedocs.io/en/1.18.x/events/jigsawevent/>here.</a>
  */
 @Cancelable
 public class JigsawPieceEvent extends Event
 {
-   public final List<StructurePoolElement> pieces;
-   public final ResourceLocation patternLocation;
-   public final BlockPos jigsawBlockPosition;
-   public final Map<ResourceLocation, Object> jigsawPlacerSettings;
-   public final Registry<StructureTemplatePool> patternRegistry;
-   public final Random random;
-   public final int currentDepth;
+   private final List<StructurePoolElement> pieces;
+   private final ResourceLocation patternLocation;
+   private final BlockPos jigsawBlockPosition;
+   private final Map<ResourceLocation, Object> jigsawPlacerSettings;
+   private final Registry<StructureTemplatePool> patternRegistry;
+   private final Random random;
+   private final int currentDepth;
 
    /**
-    * @param pieces                  The list of pieces that have a probability of being placed. Any addition to big pools should be shuffled for better results.
-    *                                The list does NOT contain the fallback pool of the current pool. See the appropriate pool for the structure to know what the fallback is.
+    * @param pieces                  The list of pieces that have a probability of being placed. Any addition to the pool should be shuffled for better results.
+    *                                The list does NOT contain the fallback pool pieces contained in {@link StructureTemplatePool#fallback} to avoid infinite
+    *                                generation problems. See the appropriate pool for the structure to know what the fallback is.
     *                                ({@link net.minecraft.data.worldgen.PlainVillagePools} for Plains Village)
-    * @param patternLocation         The resource location of the {@link net.minecraft.world.level.levelgen.feature.structures.StructureTemplatePool}.
+    * @param patternLocation         The resource location of the {@link StructureTemplatePool}.
     * @param jigsawBlockPosition     The block position of jigsaw block requesting the pieces.
-    * @param jigsawPlacerSettings    The map of generate-time settings for specific structure. It's unused for vanilla structures but can be used
-    *                                in modded ones to add specific settings or more or less randomness. If `forge:force_piece_placement` is set to `true`,
-    *                                all future pieces in that structure will always be placed, even if they overwrite existing pieces.
-    * @param patternRegistry         The access to Registry instance for structure pools. Can be used to get pools of other structures and re-use them.
-    * @param random                  The access to Random instance of specific structure. Can be used to shuffle custom pieces after they are added.
-    * @param currentDepth            The depth of the piece that is going to be placed. It will be 0 for the jigsaw blocks contained in the starting piece.
+    * @param jigsawPlacerSettings    The map of generate-time settings for specific structure. Unused for vanilla structures but can be used
+    *                                in modded ones to add specific settings or more or less randomness. If  {@code forge:force_piece_placement} is set
+    *                                to {@code true}, all future pieces in that structure will always be placed, even if they overwrite existing pieces.
+    * @param patternRegistry         The access to {@link Registry} instance for structure pools. Can be used to get pools of other structures and re-use them.
+    * @param random                  The access to {@link Random} instance of specific structure. Can be used to shuffle custom pieces after they are added.
+    * @param currentDepth            The current depth of the piece that is going to be placed. It will be 0 for the jigsaw blocks contained in the starting piece.
     */
    public JigsawPieceEvent(List<StructurePoolElement> pieces, ResourceLocation patternLocation, BlockPos jigsawBlockPosition, Map<ResourceLocation, Object> jigsawPlacerSettings, Registry<StructureTemplatePool> patternRegistry, Random random, int currentDepth)
    {
@@ -61,5 +65,69 @@ public class JigsawPieceEvent extends Event
       this.patternRegistry = patternRegistry;
       this.random = random;
       this.currentDepth = currentDepth;
+   }
+
+   /**
+    * Gets the list of pieces that have a probability of being placed.
+    * <br><br>
+    * Any addition to the pool should be shuffled for better results.
+    * <br><br>
+    * The list does NOT contain the fallback pool pieces contained in {@link StructureTemplatePool#fallback} to
+    * avoid infinite generation problems. See the appropriate pool for the structure to know what the fallback is.
+    * ({@link net.minecraft.data.worldgen.PlainVillagePools} for Plains Village)
+    */
+   public List<StructurePoolElement> getPieces()
+   {
+      return pieces;
+   }
+
+   /**
+    * Gets the resource location of the {@link StructureTemplatePool}.
+    */
+   public ResourceLocation getPatternLocation ()
+   {
+      return patternLocation;
+   }
+
+   /**
+    * Gets the block position of jigsaw block requesting the pieces.
+    */
+   public BlockPos getJigsawBlockPosition() {
+      return jigsawBlockPosition;
+   }
+
+   /**
+    * Gets map of generate-time settings for specific structure.
+    * <br><br>
+    * Unused for vanilla structures but can be used in modded ones to add specific settings or more or less randomness.
+    * <br><br>
+    * If  {@code forge:force_piece_placement} is set to {@code true}, all future pieces in that structure will always be placed, even if they overwrite existing pieces.
+    */
+   public Map<ResourceLocation, Object> getJigsawPlacerSettings() {
+      return jigsawPlacerSettings;
+   }
+
+   /**
+    * Gets the {@link Registry} instance for structure pools.
+    * Can be used to get pools of other structures and re-use them.
+    */
+   public Registry<StructureTemplatePool> getPatternRegistry() {
+      return patternRegistry;
+   }
+
+   /**
+    * Gets the {@link Random} instance of specific structure.
+    * Can be used to shuffle custom pieces after they are added.
+    */
+   public Random getRandom() {
+      return random;
+   }
+
+   /**
+    * Gets the current depth of the piece that is going to be placed.
+    * It will be 0 for the jigsaw blocks contained in the starting piece.
+    */
+   public int getCurrentDepth() {
+      return currentDepth;
    }
 }
--- src/test/java/net/minecraftforge/debug/world/JigsawPieceEventTest.java
+++ src/test/java/net/minecraftforge/debug/world/JigsawPieceEventTest.java
@@ -1,9 +1,14 @@
+/*
+ * Minecraft Forge - Forge Development LLC
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 package net.minecraftforge.debug.world;
 
 import net.minecraft.data.worldgen.ProcessorLists;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.levelgen.feature.structures.StructurePoolElement;
-import net.minecraft.world.level.levelgen.feature.structures.StructureTemplatePool;
+import net.minecraft.world.level.levelgen.structure.pools.StructurePoolElement;
+import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
 import net.minecraftforge.event.world.JigsawPieceEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -18,43 +23,43 @@ public class JigsawPieceEventTest
    public static void jigsawEvent(JigsawPieceEvent event)
    {
       // Replace iron golem with giant in all villages
-      if (event.patternLocation.getPath().equals("village/common/iron_golem"))
+      if (event.getPatternLocation().getPath().equals("village/common/iron_golem"))
       {
-         event.pieces.clear();
-         event.pieces.add(StructurePoolElement.single("jigsaw_event_test:giant", ProcessorLists.EMPTY).apply(StructureTemplatePool.Projection.RIGID));
+         event.getPieces().clear();
+         event.getPieces().add(StructurePoolElement.single("jigsaw_event_test:giant").apply(StructureTemplatePool.Projection.RIGID));
       }
 
       // Replace at most one house in every plains village with cube of diamond blocks
       // Force diamond house to always generate, even if this will overwrite existing piece
-      boolean isGenerated = (boolean) event.jigsawPlacerSettings.getOrDefault(new ResourceLocation("jigsaw_event_test:generated"), false);
-      if (event.patternLocation.getPath().equals("village/plains/houses") && !isGenerated)
+      boolean isGenerated = (boolean) event.getJigsawPlacerSettings().getOrDefault(new ResourceLocation("jigsaw_event_test:generated"), false);
+      if (event.getPatternLocation().getPath().equals("village/plains/houses") && !isGenerated)
       {
-         event.pieces.clear();
-         event.pieces.add(StructurePoolElement.single("jigsaw_event_test:diamond_house", ProcessorLists.EMPTY).apply(StructureTemplatePool.Projection.RIGID));
+         event.getPieces().clear();
+         event.getPieces().add(StructurePoolElement.single("jigsaw_event_test:diamond_house").apply(StructureTemplatePool.Projection.RIGID));
 
-         event.jigsawPlacerSettings.put(new ResourceLocation("jigsaw_event_test:generated"), true);
-         event.jigsawPlacerSettings.put(new ResourceLocation("forge:force_piece_placement"), true);
+         event.getJigsawPlacerSettings().put(new ResourceLocation("jigsaw_event_test:generated"), true);
+         event.getJigsawPlacerSettings().put(new ResourceLocation("forge:force_piece_placement"), true);
       } else {
-         event.jigsawPlacerSettings.put(new ResourceLocation("forge:force_piece_placement"), false);
+         event.getJigsawPlacerSettings().put(new ResourceLocation("forge:force_piece_placement"), false);
       }
 
       // After 5 villagers have been generated in taiga village, replace all future villagers in that village with zombie villagers
-      int numberOfVillagers = (int) event.jigsawPlacerSettings.getOrDefault(new ResourceLocation("jigsaw_event_test:villagers"), 0);
-      if (event.patternLocation.getPath().equals("village/taiga/villagers") && numberOfVillagers <= 5)
+      int numberOfVillagers = (int) event.getJigsawPlacerSettings().getOrDefault(new ResourceLocation("jigsaw_event_test:villagers"), 0);
+      if (event.getPatternLocation().getPath().equals("village/taiga/villagers") && numberOfVillagers <= 5)
       {
-         event.pieces.clear();
-         event.pieces.add(StructurePoolElement.single("village/plains/zombie/villagers/baby", ProcessorLists.EMPTY).apply(StructureTemplatePool.Projection.RIGID));
-         event.pieces.add(StructurePoolElement.single("village/plains/zombie/villagers/nitwit", ProcessorLists.EMPTY).apply(StructureTemplatePool.Projection.RIGID));
-         event.pieces.add(StructurePoolElement.single("village/plains/zombie/villagers/nitwit", ProcessorLists.EMPTY).apply(StructureTemplatePool.Projection.RIGID));
-         event.jigsawPlacerSettings.put(new ResourceLocation("jigsaw_event_test:villagers"), numberOfVillagers + 1);
+         event.getPieces().clear();
+         event.getPieces().add(StructurePoolElement.single("village/plains/zombie/villagers/baby").apply(StructureTemplatePool.Projection.RIGID));
+         event.getPieces().add(StructurePoolElement.single("village/plains/zombie/villagers/nitwit").apply(StructureTemplatePool.Projection.RIGID));
+         event.getPieces().add(StructurePoolElement.single("village/plains/zombie/villagers/nitwit").apply(StructureTemplatePool.Projection.RIGID));
+         event.getJigsawPlacerSettings().put(new ResourceLocation("jigsaw_event_test:villagers"), numberOfVillagers + 1);
       }
 
       // Replace savanna houses on y>=70 with desert ones
-      if (event.patternLocation.getPath().equals("village/savanna/houses") && event.jigsawBlockPosition.getY() >= 70)
+      if (event.getPatternLocation().getPath().equals("village/savanna/houses") && event.getJigsawBlockPosition().getY() >= 70)
       {
-         Optional<StructureTemplatePool> jigsawpattern = event.patternRegistry.getOptional(new ResourceLocation("village/desert/houses"));
-         event.pieces.clear();
-         event.pieces.addAll(jigsawpattern.get().getShuffledTemplates(event.random));
+         Optional<StructureTemplatePool> jigsawpattern = event.getPatternRegistry().getOptional(new ResourceLocation("village/desert/houses"));
+         event.getPieces().clear();
+         event.getPieces().addAll(jigsawpattern.get().getShuffledTemplates(event.getRandom()));
       }
    }
 }
