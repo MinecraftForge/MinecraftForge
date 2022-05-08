c491c9037e4dc3b02d1d16c541e9c71f5fd89cca
XFactHD <xfacthd@gmx.de>
2022-05-08 20:50:26 +0200

    Add callback after a BlockState was changed and the neighbors were updated

--- src/main/java/net/minecraftforge/common/extensions/IForgeBlockState.java
+++ src/main/java/net/minecraftforge/common/extensions/IForgeBlockState.java
@@ -696,4 +696,19 @@ public interface IForgeBlockState
     {
         return self().getBlock().supportsExternalFaceHiding(self());
     }
+
+    /**
+     * Called after the {@link BlockState} at the given {@link BlockPos} was changed and neighbors were updated.
+     * This method is called on the server and client side.
+     * Modifying the level is disallowed in this method.
+     * Useful for calculating additional data based on the new state and the neighbor's reactions to the state change.
+     *
+     * @param level The level the state was modified in
+     * @param pos The blocks position in the level
+     * @param oldState The previous state of the block at the given position, may be a different block than this one
+     */
+    default void onBlockStateChange(LevelReader level, BlockPos pos, BlockState oldState)
+    {
+        self().getBlock().onBlockStateChange(level, pos, oldState, self());
+    }
 }
