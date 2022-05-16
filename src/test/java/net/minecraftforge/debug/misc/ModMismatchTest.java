3ad039a52ddcb5ef10db45c4cc669cab55c424a7
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2022-05-16 16:51:09 +0200

    Changed mod id of test mod

--- src/test/java/net/minecraftforge/debug/misc/ModMismatchTest.java
+++ src/test/java/net/minecraftforge/debug/misc/ModMismatchTest.java
@@ -43,7 +43,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 @Mod(ModMismatchTest.MOD_ID)
 public class ModMismatchTest
 {
-    public static final String MOD_ID = "mod_mismatch";
+    public static final String MOD_ID = "mod_mismatch_test";
 
     private static final boolean ENABLED = false;
     // Enable these fields to register the channel for either the server, the client, or both.
