57d76e6b1ad53e95f33f3c4a1079c156fc39485c
lcy0x1 <arthurlcy0x1@gmail.com>
2022-06-06 16:20:19 -0700

    remove ItemStackEvent

--- src/main/java/net/minecraftforge/event/item/PlayerItemStackEvent.java
+++ src/main/java/net/minecraftforge/event/item/PlayerItemStackEvent.java
@@ -2,21 +2,22 @@ package net.minecraftforge.event.item;
 
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 
 /**
  * Base class for all events that modifies the behavior of ItemStack, and require Player as user.
  */
-public class PlayerItemStackEvent extends ItemStackEvent {
+public class PlayerItemStackEvent extends PlayerEvent {
 
-	private final Player user;
+	private final ItemStack stack;
 
 	public PlayerItemStackEvent(Player user, ItemStack stack) {
-		super(user, stack);
-		this.user = user;
+		super(user);
+		this.stack = stack;
 	}
 
-	public Player getPlayer() {
-		return user;
+	public ItemStack getStack(){
+		return stack;
 	}
 
 }
