1b95f65abf29bea921a25739900c905699c166e5
lcy0x1 <arthurlcy0x1@gmail.com>
2022-06-06 16:22:31 -0700

    reduce diff

--- src/main/java/net/minecraftforge/event/item/PlayerItemStackEvent.java
+++ src/main/java/net/minecraftforge/event/item/PlayerItemStackEvent.java
@@ -5,7 +5,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 
 /**
- * Base class for all events that modifies the behavior of ItemStack, and require Player as user.
+ * Base class for all events that modify the behavior of ItemStack, and require Player as user.
  */
 public class PlayerItemStackEvent extends PlayerEvent {
 
