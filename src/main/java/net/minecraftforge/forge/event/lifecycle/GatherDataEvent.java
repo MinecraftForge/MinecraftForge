b97e42435651ea294ab7724a840c1aa5a4c173f5
cpw <cpw+github@weeksfamily.ca>
2022-06-17 16:24:48 -0400

    Also preserve order in runAll
    Signed-off-by: cpw <cpw+github@weeksfamily.ca>

--- src/main/java/net/minecraftforge/forge/event/lifecycle/GatherDataEvent.java
+++ src/main/java/net/minecraftforge/forge/event/lifecycle/GatherDataEvent.java
@@ -14,11 +14,7 @@ import net.minecraftforge.fml.ModContainer;
 import net.minecraftforge.fml.event.IModBusEvent;
 
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -90,7 +86,7 @@ public class GatherDataEvent extends Event implements IModBusEvent
         }
 
         public void runAll() {
-            Map<Path, List<DataGenerator>> paths = generators.stream().collect(Collectors.groupingBy(DataGenerator::getOutputFolder));
+            Map<Path, List<DataGenerator>> paths = generators.stream().collect(Collectors.groupingBy(DataGenerator::getOutputFolder, LinkedHashMap::new, Collectors.toList()));
 
             paths.values().forEach(LamdbaExceptionUtils.rethrowConsumer(lst -> {
                 DataGenerator parent = lst.get(0);
