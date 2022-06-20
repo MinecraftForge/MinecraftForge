05240fecf7fd4391e7ff9ad4e6a0007d2a1df926
King Lemming <kinglemming@gmail.com>
2022-06-20 15:07:48 -0700

    Make Forge ConfigValues implement Supplier. (#8776)

--- src/main/java/net/minecraftforge/common/ForgeConfigSpec.java
+++ src/main/java/net/minecraftforge/common/ForgeConfigSpec.java
@@ -809,7 +809,7 @@ public class ForgeConfigSpec extends UnmodifiableConfigWrapper<UnmodifiableConfi
         }
     }
 
-    public static class ConfigValue<T>
+    public static class ConfigValue<T> implements Supplier<T>
     {
         private static boolean USE_CACHES = true;
 
@@ -842,6 +842,7 @@ public class ForgeConfigSpec extends UnmodifiableConfigWrapper<UnmodifiableConfi
          *                              not yet been built
          * @throws IllegalStateException if the associated config has not yet been loaded
          */
+        @Override
         public T get()
         {
             Preconditions.checkNotNull(spec, "Cannot get config value before spec is built");
