f2fc0649cb605aca9a6ef9f29200c73873f2e8d4
amadornes <amadornes@gmail.com>
2022-06-23 01:18:30 +0200

    HUD overlay refactors - Rename to IGuiOverlay match vanilla (instead of Ingame) - Add overlay manager and registration event - Move vanilla overlays to a standalone enum

---
+++ src/main/java/net/minecraftforge/client/gui/overlay/IGuiOverlay.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) Forge Development LLC and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+package net.minecraftforge.client.gui.overlay;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
+
+/**
+ * A HUD overlay.
+ *
+ * @see RegisterGuiOverlaysEvent
+ */
+@FunctionalInterface
+public interface IGuiOverlay
+{
+    void render(ForgeGui gui, PoseStack poseStack, float partialTick, int width, int height);
+}
