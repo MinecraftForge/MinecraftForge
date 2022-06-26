9024f47e5068161c368c1b81c86e7401fd54d3f8
Commoble <josephbettendorff@yahoo.com>
2022-06-26 10:14:04 -0400

    WIP

--- src/main/java/net/minecraftforge/common/util/ForgeHolderSets.java
+++ src/main/java/net/minecraftforge/common/util/ForgeHolderSets.java
@@ -4,15 +4,9 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
-import java.util.Set;
 import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.jetbrains.annotations.Nullable;
-
-import com.google.common.base.Suppliers;
 import com.mojang.datafixers.util.Either;
 
 import net.minecraft.core.Holder;
@@ -34,9 +28,9 @@ public final class ForgeHolderSets
 		
 		@Override
 		public Iterator<Holder<T>> iterator()
-		{
-			return this.stream().iterator();
-		}
+        {
+            return this.stream().iterator();
+        }
 
 		@Override
 		public Stream<Holder<T>> stream()
@@ -87,98 +81,46 @@ public final class ForgeHolderSets
 		}
 	}
 	
-	public static final class AndHolderSet<T> implements HolderSet<T>
-	{
-		private final List<HolderSet<T>> values;
-		
+	public static record AndHolderSet<T>(List<HolderSet<T>> values) implements CompositeHolderSet<T>
+	{		
 		public static <T> AndHolderSet<T> create(List<HolderSet<T>> values)
 		{
 			return new AndHolderSet<>(values);
 		}
-		
-		private AndHolderSet(List<HolderSet<T>> values)
-		{
-			this.values = values;
-		}
 
-		@Override
-		public Either<TagKey<T>, List<Holder<T>>> unwrap()
-		{
-			return Either.right(this.stream().toList());
-		}
+        @Override
+        public Stream<Holder<T>> stream()
+        {
+            final int subsetCount = this.values.size();
+            if (subsetCount <= 0)
+            {
+                return Stream.empty();
+            }
+            return this.values.get(0).stream().filter(holder ->
+            {
+                final int size = this.values.size();
+                for (int i=1; i<size; i++)
+                {
+                    if (!this.values.get(i).contains(holder))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            });
+        }
 
 		@Override
 		public boolean contains(Holder<T> holder)
 		{
-			for (HolderSet<T> holderSet : this.values)
+			for (final HolderSet<T> subset : this.values)
 			{
-				if (!holderSet.contains(holder))
+				if (!subset.contains(holder))
 				{
 					return false;
 				}
 			}
 			return true;
-		}
-
-		@Override
-		public Iterator<Holder<T>> iterator()
-		{
-			return this.stream().iterator();
-		}
-
-		@Override
-		public Stream<Holder<T>> stream()
-		{
-			return this.values.stream().flatMap(HolderSet::stream);
-		}
-
-		@Override
-		public int size()
-		{
-			int size = 0;
-			for (HolderSet<T> holderSet : this.values)
-			{
-				size += holderSet.size();
-			}
-			return size;
-		}
-
-		@Override
-		public Optional<Holder<T>> getRandomElement(RandomSource random)
-		{
-			int size = this.size();
-			if (size <= 0)
-			{
-				return Optional.empty();
-			}
-			return Optional.ofNullable(this.getNullable(size));
-		}
-		
-		@Nullable
-		public Holder<T> getNullable(int i)
-		{
-			for (HolderSet<T> subset : this.values)
-			{
-				int subsetSize = subset.size();
-				if (i < subset.size())
-				{
-					return subset.get(i);
-				}
-				else
-				{
-					i -= subsetSize;
-				}
-			}
-			return null;
-		}
-
-		@Override
-		public Holder<T> get(int i)
-		{
-			@Nullable Holder<T> holder = this.getNullable(i);
-			if (holder == null)
-				throw new IndexOutOfBoundsException("Index " + i + " exceeds bounds " + this.size() + " of " + this.toString());
-			return holder;
 		}
 
 		@Override
@@ -200,4 +142,122 @@ public final class ForgeHolderSets
 			return "AndHolderSet[" + this.values + "]";
 		}
 	}
+	
+	public static record OrHolderSet<T>(List<HolderSet<T>> values) implements CompositeHolderSet<T>
+	{
+	    public static <T> OrHolderSet<T> create(List<HolderSet<T>> values)
+	    {
+	        return new OrHolderSet<>(values);
+	    }
+
+        @Override
+        public Stream<Holder<T>> stream()
+        {
+            return this.values.stream().flatMap(HolderSet::stream).distinct();
+        }
+
+        @Override
+        public boolean contains(Holder<T> holder)
+        {
+            for (HolderSet<T> subset : this.values)
+            {
+                if (subset.contains(holder))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public boolean isValidInRegistry(Registry<T> registry)
+        {
+            for (HolderSet<T> subset : this.values)
+            {
+                if (!subset.isValidInRegistry(registry))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "OrHolderSet[" + this.values + "]";
+        }
+	}
+	
+	public static record ExclusionHolderSet<T>(HolderSet<T> include, HolderSet<T> exclude) implements CompositeHolderSet<T>
+	{
+	    public static <T> ExclusionHolderSet<T> create(HolderSet<T> include, HolderSet<T> exclude)
+	    {
+	        return new ExclusionHolderSet<>(include, exclude);
+	    }
+
+        @Override
+        public Stream<Holder<T>> stream()
+        {
+            return this.include.stream().filter(holder -> !exclude.contains(holder));
+        }
+
+        @Override
+        public boolean contains(Holder<T> holder)
+        {
+            return this.include.contains(holder) && !this.exclude.contains(holder);
+        }
+
+        @Override
+        public boolean isValidInRegistry(Registry<T> registry)
+        {
+            return this.include.isValidInRegistry(registry) && this.exclude.isValidInRegistry(registry);
+        }
+
+        @Override
+        public String toString()
+        {
+            return "ExclusionHolderSet{include=" + this.include + ", exclude=" + this.exclude + "}";
+        }
+	}
+    
+	/**
+	 * Represents a HolderSet composed of other HolderSets and defines some default common method implementations.
+	 * This has some tricky requirements to fulfill as the sub-holdersets may
+	 * be tag-based holdersets, which are mutable and can change-in-place during runtime.
+	 * We cannot determine whether tags have changed, and so cannot cache our list of holders.
+	 */
+    public static interface CompositeHolderSet<T> extends HolderSet<T>
+    {
+        @Override
+        default Iterator<Holder<T>> iterator()
+        {
+            return this.stream().iterator();
+        }
+
+        @Override
+        default int size()
+        {
+            return (int)this.stream().count();
+        }
+
+        @Override
+        default Either<TagKey<T>, List<Holder<T>>> unwrap()
+        {
+            return Either.right(this.stream().toList());
+        }
+
+        @Override
+        default Optional<Holder<T>> getRandomElement(RandomSource random)
+        {
+            final List<Holder<T>> list = this.stream().toList();
+            return list.isEmpty() ? Optional.empty() : Optional.of(list.get(random.nextInt(list.size())));
+        }
+
+        @Override
+        default Holder<T> get(int i)
+        {
+            return this.stream().toList().get(i);
+        }
+    }
 }
