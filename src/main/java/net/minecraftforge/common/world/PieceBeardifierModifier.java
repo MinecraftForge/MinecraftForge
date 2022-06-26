43fb61a72032292c9a3d65dc2899c728ed4bb1a6
Drullkus <failbookftw@gmail.com>
2022-06-25 22:23:20 -0700

    Create PieceBeardifierModifier to re-enable piecewise beardifier definitions

---
+++ src/main/java/net/minecraftforge/common/world/PieceBeardifierModifier.java
@@ -0,0 +1,13 @@
+package net.minecraftforge.common.world;
+
+import net.minecraft.world.level.levelgen.structure.BoundingBox;
+import net.minecraft.world.level.levelgen.structure.TerrainAdjustment;
+
+/** Implement this interface in a {@link net.minecraft.world.level.levelgen.structure.StructurePiece} class extension to modify its {@link net.minecraft.world.level.levelgen.Beardifier} behavior. */
+public interface PieceBeardifierModifier {
+    BoundingBox getBeardifierBox();
+
+    TerrainAdjustment getTerrainAdjustment();
+
+    int getGroundLevelDelta();
+}
