bdadaee04e0e784b3e0829db8cc19a77405ba680
Marc Hermans <marc.hermans@ldtteam.com>
2022-07-07 21:13:45 +0200

    Use boolean supplier and fix wildcard import.

--- src/main/java/net/minecraftforge/resource/ResourceCacheManager.java
+++ src/main/java/net/minecraftforge/resource/ResourceCacheManager.java
@@ -13,14 +13,18 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.BiFunction;
+import java.util.function.BooleanSupplier;
 import java.util.function.Predicate;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -35,11 +39,11 @@ public class ResourceCacheManager {
      * Indicates if the underlying namespaced managers need to support reloading.
      * Disabled for the (in-jar / downloaded) default vanilla pack.
      */
-    private final Supplier<Boolean> supportsReloading;
+    private final BooleanSupplier supportsReloading;
     /**
      * Indicates if the indexing of the file-tree should happen off-thread or on-thread and block the process accordingly.
      */
-    private final Supplier<Boolean> indexOffThread;
+    private final BooleanSupplier indexOffThread;
     /**
      * The path builder (different users have different requirements for how they want to handle this)
      */
@@ -56,7 +60,7 @@ public class ResourceCacheManager {
      * @param indexOffThread    True to index the file-tree off-thread.
      * @param pathBuilder       The path builder to use.
      */
-    public ResourceCacheManager(final Supplier<Boolean> supportsReloading, final Supplier<Boolean> indexOffThread, final BiFunction<PackType, String, Path> pathBuilder) {
+    public ResourceCacheManager(final BooleanSupplier supportsReloading, final BooleanSupplier indexOffThread, final BiFunction<PackType, String, Path> pathBuilder) {
         this.supportsReloading = supportsReloading;
         this.indexOffThread = indexOffThread;
         this.pathBuilder = pathBuilder;
@@ -70,12 +74,12 @@ public class ResourceCacheManager {
     public void index(final String namespace) {
         for (final PackType packType : PackType.values()) {
             final PackTypeAndNamespace key = new PackTypeAndNamespace(packType, namespace); //Make a key.
-            if (managersByNamespace.containsKey(key) && !supportsReloading.get()) { //If we do not support reloading we just skip this.
+            if (managersByNamespace.containsKey(key) && !supportsReloading.getAsBoolean()) { //If we do not support reloading we just skip this.
                 return;
             }
 
             //Create a new manager, overriding the previous one if it exists.
-            this.managersByNamespace.put(key, new NamespacedResourceCacheManager(packType, namespace, indexOffThread.get(), pathBuilder, this::createWalkingStream));
+            this.managersByNamespace.put(key, new NamespacedResourceCacheManager(packType, namespace, indexOffThread.getAsBoolean(), pathBuilder, this::createWalkingStream));
 
             //Index the inner manager (which will for sure exist, and will know the pack type and namespace already)
             this.managersByNamespace.get(key).index();
