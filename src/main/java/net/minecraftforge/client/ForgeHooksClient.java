746eb46b2da4162068fb9b19638d6d33d3d34b64
amadornes <amadornes@gmail.com>
2022-07-19 17:57:11 +0200

    Re-add helper in ForgeHooksClient and forward call

--- src/main/java/net/minecraftforge/client/ForgeHooksClient.java
+++ src/main/java/net/minecraftforge/client/ForgeHooksClient.java
@@ -953,6 +953,16 @@ public class ForgeHooksClient
         return MinecraftForge.EVENT_BUS.post(event) ? "" : event.getMessage();
     }
 
+    /**
+     * Mimics the behavior of {@link net.minecraft.client.renderer.ItemBlockRenderTypes#getRenderType(BlockState, boolean)}
+     * for the input {@link RenderType}.
+     */
+    @NotNull
+    public static RenderType getEntityRenderType(RenderType chunkRenderType, boolean cull)
+    {
+        return RenderTypeHelper.getEntityRenderType(chunkRenderType, cull);
+    }
+
     @Mod.EventBusSubscriber(value = Dist.CLIENT, modid="forge", bus= Mod.EventBusSubscriber.Bus.MOD)
     public static class ClientEvents
     {
