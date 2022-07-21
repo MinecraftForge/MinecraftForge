f41d493552133bfde457e4a345fa512d7ebeef68
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2022-07-21 16:06:02 -0400

    Allow modded blocks overriding canStickTo prevent sticking to vanilla blocks/other modded blocks (#8837)

--- src/test/java/net/minecraftforge/debug/block/StickyBlockTest.java
+++ src/test/java/net/minecraftforge/debug/block/StickyBlockTest.java
@@ -6,16 +6,23 @@
 package net.minecraftforge.debug.block;
 
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.ObjectHolder;
 import net.minecraftforge.registries.RegisterEvent;
+import net.minecraftforge.registries.RegistryObject;
+import org.jetbrains.annotations.Nullable;
 
 @Mod(StickyBlockTest.MODID)
 @Mod.EventBusSubscriber(bus = Bus.MOD)
@@ -23,26 +30,49 @@ public class StickyBlockTest
 {
     static final String MODID = "custom_slime_block_test";
     static final String BLOCK_ID = "test_block";
+    static final String BLOCK_ID_2 = "test_block_2";
 
-    @ObjectHolder(registryName = "block", value = BLOCK_ID)
-    public static Block BLUE_BLOCK;
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS.getRegistryName(), MODID);
 
-    @SubscribeEvent
-    public static void registerBlocks(RegisterEvent event)
+    public static final RegistryObject<Block> BLUE_BLOCK = BLOCKS.register(BLOCK_ID, () -> new Block(Block.Properties.of(Material.STONE))
     {
-        event.register(ForgeRegistries.Keys.BLOCKS, helper -> helper.register(BLOCK_ID, new Block(Block.Properties.of(Material.STONE))
+        @Override
+        public boolean isStickyBlock(BlockState state)
         {
-            @Override
-            public boolean isStickyBlock(BlockState state)
-            {
-                return true;
-            }
-        }));
-    }
+            return true;
+        }
+    });
 
-    @SubscribeEvent
-    public static void registerItems(RegisterEvent event)
+    public static final RegistryObject<Block> RED_BLOCK = BLOCKS.register(BLOCK_ID_2, () -> new Block(Block.Properties.of(Material.STONE))
     {
-        event.register(ForgeRegistries.Keys.ITEMS, helper -> helper.register(BLOCK_ID, new BlockItem(BLUE_BLOCK, new Item.Properties())));
+        @Override
+        public boolean isStickyBlock(BlockState state)
+        {
+            return true;
+        }
+
+        @Override
+        public boolean isSlimeBlock(BlockState state)
+        {
+            return true;
+        }
+
+        @Override
+        public boolean canStickTo(BlockState state, BlockState other)
+        {
+            if (state.getBlock() == RED_BLOCK.get() && other.getBlock() == Blocks.SLIME_BLOCK) return false;
+            return state.isStickyBlock() || other.isStickyBlock();
+        }
+    });
+
+    public static final RegistryObject<Item> BLUE_BLOCK_ITEM = ITEMS.register(BLOCK_ID, () -> new BlockItem(BLUE_BLOCK.get(), new Item.Properties()));
+    public static final RegistryObject<Item> RED_BLOCK_ITEM = ITEMS.register(BLOCK_ID_2, () -> new BlockItem(RED_BLOCK.get(), new Item.Properties()));
+
+    public StickyBlockTest()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        BLOCKS.register(modEventBus);
+        ITEMS.register(modEventBus);
     }
 }
