9b1e6f72195e88ff40d8ae634e0d5602d282943a
Paint_Ninja <PaintNinja@users.noreply.github.com>
2022-08-04 22:45:52 +0100

    Update javadoc with intended usage examples

--- src/main/java/net/minecraftforge/event/server/ServerOverloadedEvent.java
+++ src/main/java/net/minecraftforge/event/server/ServerOverloadedEvent.java
@@ -14,6 +14,15 @@ import net.minecraftforge.server.ServerLifecycleHooks;
  * ServerOverloadedEvent is fired when the server is running behind ticking schedule and can't keep up.
  * You should avoid doing any heavy work in this event as doing so may cause the server watchdog to timeout and crash the server.<br>
  * <br>
+ * Please do NOT use this event as a trigger for disabling Vanilla features - this may cause unexpected behaviour for players and
+ * hard to reproduce compatibility issues with other mods that expect consistent Vanilla behaviour.<br>
+ * <br>
+ * Here's a couple of examples of intended usage of this event:
+ * <ol>
+ *     <li> to log data that could be useful for diagnosing why the server is overloaded
+ *     <li> for sending automated alerts to server staff outside of the game
+ * </ol>
+ * <br>
  * This event is fired via {@link ServerLifecycleHooks#onServerOverloaded(MinecraftServer, long, long, long)},
  * which is executed from {@link MinecraftServer#runServer()} <br>
  * <br>
