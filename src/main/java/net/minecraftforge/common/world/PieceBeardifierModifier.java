a2fc2f3f6800366b111939f83e5d92b4c5ad8c2a
Sara Freimer <sara@freimer.com>
2022-08-05 19:44:04 -0500

    Add values to VersionSupportMatrix to support loading mods that restrict versions to 1.19.1 on 1.19.2

--- src/main/java/net/minecraftforge/common/world/PieceBeardifierModifier.java
+++ src/main/java/net/minecraftforge/common/world/PieceBeardifierModifier.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) Forge Development LLC and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 package net.minecraftforge.common.world;
 
 import net.minecraft.world.level.levelgen.structure.BoundingBox;
