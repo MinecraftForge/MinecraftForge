7f752830651884c40349df4a8c1a73601ee8dac8
Budschie <jul.kp06@gmail.com>
2022-08-11 14:09:32 +0200

    Removed LivingSetAttackTargetEvent#setTarget/getCurrentTarget.

--- src/main/java/net/minecraftforge/event/entity/living/LivingSetAttackTargetEvent.java
+++ src/main/java/net/minecraftforge/event/entity/living/LivingSetAttackTargetEvent.java
@@ -5,16 +5,8 @@
 
 package net.minecraftforge.event.entity.living;
 
-import java.util.Optional;
-import java.util.function.BiConsumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-
-import org.jetbrains.annotations.Nullable;
-
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.common.MinecraftForge;
 
@@ -43,8 +35,6 @@ public class LivingSetAttackTargetEvent extends LivingEvent
         super(entity);
         this.targetType = LivingTargetType.GOAL_TARGET;        
         this.originalTarget = target;
-        
-        assertLivingIsValid();
     }
     
     public LivingSetAttackTargetEvent(LivingEntity entity, LivingEntity target, ILivingTargetType targetType)
@@ -52,42 +42,15 @@ public class LivingSetAttackTargetEvent extends LivingEvent
         super(entity);
         this.targetType = targetType;
         this.originalTarget = target;
-        
-        assertLivingIsValid();
     }
 
     /**
      * {@return the target this living entity had after changing its target to a new
-     * target, but before posting this event. The return value of this method cannot be
-     * affected by {@link LivingSetAttackTargetEvent#setTarget}}
+     * target, but before posting this event.}
      */
     public LivingEntity getTarget()
     {
         return originalTarget;
-    }
-    
-    /**
-     * {@return the current target this living entity has. The return value of this
-     * method may be affected by {@link LivingSetAttackTargetEvent#setTarget}}
-     */
-    public Optional<LivingEntity> getCurrentTarget()
-    {
-        return targetType.getTarget(getEntity());
-    }
-
-    /**
-     * @param target The living entity that shall be set as a target.
-     * {@return {@code true} when the target is valid, otherwise {@code false}.}
-     */
-    public boolean setTarget(@Nullable LivingEntity target)
-    {
-        if(targetType.isValidTarget(target))
-        {
-            targetType.setTarget(getEntity(), target);
-            return true;
-        }
-        
-        return false;
     }
     
     /**
@@ -98,105 +61,14 @@ public class LivingSetAttackTargetEvent extends LivingEvent
         return targetType;
     }
     
-    /**
-     * Throws an exception when the living entity is not valid for this target type.
-     */
-    private void assertLivingIsValid()
-    {
-        if(!this.targetType.isValidLiving(getEntity()))
-        {
-            throw new IllegalArgumentException("The living entity provided to this event is not compatible with the chosen target type.");
-        }
-    }
-    
-    // Short note on why this is needed: If we were to just let the event "return" data like in the ComputeFovModifierEvent,
-    // this would introduce lots of bugs and breaking changes, so we need to do this mess.
-    // 
-    // Note for any future developer that might stumble across this: Please introduce a breaking change
-    // later down the line (like, when porting forge to 1.20 or 1.21; this is the reaons why all the members of this class are marked for removal) 
-    // and do all of this like it is done in the ComputeFovModifierEvent. By that time, most mod developers will
-    // (hopefully) use LivingSetAttackTargetEvent#setTarget instead of setting the target on their own, which will
-    // minimize the impact of such a breaking change.
     public static interface ILivingTargetType
     {
-        /**
-         * @param living The living entity whose target shall be returned.
-         * {@return the current target of this living entity.}
-         */
-        @Deprecated(forRemoval = true)
-        Optional<LivingEntity> getTarget(LivingEntity living);
         
-       
-        /**
-         * Sets the target of the living entity.
-         * @param living The entity whose target shall be set.
-         * @param target The new target.
-         */
-        @Deprecated(forRemoval = true)
-        void setTarget(LivingEntity living, LivingEntity target);
-        
-        /**
-         * Tests whether the given living entity whose target may be set later is valid for this living target type.
-         * 
-         * @param living The living entity which shall be tested.
-         * {@return {@code true} when the test was successful, {@code false} otherwise}
-         */
-        @Deprecated(forRemoval = true)
-        boolean isValidLiving(LivingEntity living);
-        
-        /**
-         * Tests whether the given target entity is valid or not.
-         * 
-         * @param The target which shall be tested.
-         * {@return {@code true} when the target is valid for this target type, {@code false} otherwise}
-         */
-        @Deprecated(forRemoval = true)
-        boolean isValidTarget(LivingEntity target);
     }
     
     public static enum LivingTargetType implements ILivingTargetType
     {
-        GOAL_TARGET(living -> Optional.ofNullable(((Mob) living).getTarget()), (living, target) -> ((Mob) living).setTarget(target), livingValid -> livingValid == null || livingValid instanceof Mob,
-                targetValid -> true),
-        BEHAVIOR_TARGET(living -> living.getBrain().getMemory(MemoryModuleType.ATTACK_TARGET),
-                (living, target) -> living.getBrain().setMemory(MemoryModuleType.ATTACK_TARGET, target), livingValid -> true, targetValid -> true);
-        
-        private Function<LivingEntity, Optional<LivingEntity>> targetGetter;
-        private BiConsumer<LivingEntity, LivingEntity> targetSetter;
-        private Predicate<LivingEntity> livingValid;
-        private Predicate<LivingEntity> targetValid;
-        
-        private LivingTargetType(Function<LivingEntity, Optional<LivingEntity>> targetGetter, BiConsumer<LivingEntity, LivingEntity> targetSetter,
-                Predicate<LivingEntity> livingValid, Predicate<LivingEntity> targetValid)
-        {
-            this.targetGetter = targetGetter;
-            this.targetSetter = targetSetter;
-            this.livingValid = livingValid;
-            this.targetValid = targetValid;
-        }
-        
-        @Override
-        public Optional<LivingEntity> getTarget(LivingEntity living)
-        {
-            return targetGetter.apply(living);
-        }
-        
-        @Override
-        public void setTarget(LivingEntity living, LivingEntity target)
-        {
-            targetSetter.accept(living, target);
-        }
-        
-        @Override
-        public boolean isValidLiving(LivingEntity living)
-        {
-            return livingValid.test(living);
-        }
-        
-        @Override
-        public boolean isValidTarget(LivingEntity target)
-        {
-            return targetValid.test(target);
-        }
+        GOAL_TARGET,
+        BEHAVIOR_TARGET;
     }
 }
--- src/test/java/net/minecraftforge/debug/entity/living/LivingSetAttackTargetEventTest.java
+++ src/test/java/net/minecraftforge/debug/entity/living/LivingSetAttackTargetEventTest.java
@@ -1,8 +1,5 @@
 package net.minecraftforge.debug.entity.living;
 
-import java.util.Optional;
-
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.monster.piglin.AbstractPiglin;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Items;
@@ -27,23 +24,10 @@ public class LivingSetAttackTargetEventTest
     @SubscribeEvent
     public void onLivingSetAttackTarget(LivingSetAttackTargetEvent event)
     {
-        LivingEntity originalTarget = event.getTarget();
-        
-        // Make piglins peaceful when the player holds a stick in their hands.
-        // Exception: The player hit the piglin
-        if (event.getTarget() instanceof Player player && event.getEntity() instanceof AbstractPiglin piglin
-                && player.getMainHandItem().getItem() == Items.STICK && event.getTarget() != event.getEntity().getLastHurtByMob())
+        // Make piglins die when they start attacking a player who is holding a stick in their hands.
+        if (event.getTarget() instanceof Player player && event.getEntity() instanceof AbstractPiglin piglin && player.getMainHandItem().getItem() == Items.STICK)
         {
-            event.setTarget(null);
-            
-            Optional<LivingEntity> newTarget = event.getCurrentTarget();
-            
-            // Check if LivingSetAttackTargetEvent#getTarget does not return the same result as before and throw an exception if that is the case.
-            // If LivingSetAttackTargetEvent#getCurrentTarget does not return an empty optional, we shall throw an exception as well.
-            if(originalTarget != event.getTarget() || newTarget.isPresent())
-            {
-                throw new RuntimeException("Sanity checks for LivingSetAttackTargetEvent did NOT succeed. This indicates that a bug is present.");
-            }
+            event.getEntity().kill();
         }
     }
 }
