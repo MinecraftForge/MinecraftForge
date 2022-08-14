bf615f9a569544c28cd17db60780184d4422e194
LexManos <LexManos@gmail.com>
2022-08-14 11:41:24 -0700

    Remove reflective implementation of ICustomPacket. Make vanilla custom packets able to be sent multiple times.

--- src/main/java/net/minecraftforge/network/ICustomPacket.java
+++ src/main/java/net/minecraftforge/network/ICustomPacket.java
@@ -5,83 +5,24 @@
 
 package net.minecraftforge.network;
 
-import it.unimi.dsi.fastutil.objects.Reference2ReferenceArrayMap;
 import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.network.protocol.login.ServerboundCustomQueryPacket;
-import net.minecraft.network.protocol.login.ClientboundCustomQueryPacket;
-import net.minecraft.network.protocol.game.ServerboundCustomPayloadPacket;
-import net.minecraft.network.protocol.game.ClientboundCustomPayloadPacket;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.fml.unsafe.UnsafeHacks;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
-import java.util.Optional;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import org.jetbrains.annotations.Nullable;
+
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
 
 public interface ICustomPacket<T extends Packet<?>> {
-    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
-    enum Fields {
-        CPACKETCUSTOMPAYLOAD(ServerboundCustomPayloadPacket.class),
-        SPACKETCUSTOMPAYLOAD(ClientboundCustomPayloadPacket.class),
-        CPACKETCUSTOMLOGIN(ServerboundCustomQueryPacket.class),
-        SPACKETCUSTOMLOGIN(ClientboundCustomQueryPacket.class),
-        ;
+    /**
+     * Returns a unsafe reference to this packet's internal data.
+     * Any modifications to this buffer will be reflected in the main buffer.
+     */
+    @Nullable
+    FriendlyByteBuf getInternalData();
 
-        static final Reference2ReferenceArrayMap<Class<?>, Fields> lookup;
-        static {
-            lookup = Stream.of(values()).
-                    collect(Collectors.toMap(Fields::getClazz, Function.identity(), (m1, m2)->m1, Reference2ReferenceArrayMap::new));
-        }
+    ResourceLocation getName();
 
-        private final Class<?> clazz;
-
-        final Optional<Field> data;
-        final Optional<Field> channel;
-        final Optional<Field> index;
-
-        Fields(Class<?> customPacketClass)
-        {
-            this.clazz = customPacketClass;
-            Field[] fields = customPacketClass.getDeclaredFields();
-            data = Arrays.stream(fields).filter(f-> !Modifier.isStatic(f.getModifiers()) && f.getType() == FriendlyByteBuf.class).findFirst();
-            channel = Arrays.stream(fields).filter(f->!Modifier.isStatic(f.getModifiers()) && f.getType() == ResourceLocation.class).findFirst();
-            index = Arrays.stream(fields).filter(f->!Modifier.isStatic(f.getModifiers()) && f.getType() == int.class).findFirst();
-        }
-
-        private Class<?> getClazz()
-        {
-            return clazz;
-        }
-    }
-
-    default FriendlyByteBuf getInternalData() {
-        return Fields.lookup.get(this.getClass()).data.map(f->UnsafeHacks.<FriendlyByteBuf>getField(f, this)).orElse(null);
-    }
-
-    default ResourceLocation getName() {
-        return Fields.lookup.get(this.getClass()).channel.map(f->UnsafeHacks.<ResourceLocation>getField(f, this)).orElse(LoginWrapper.WRAPPER);
-    }
-
-    default int getIndex() {
-        return Fields.lookup.get(this.getClass()).index.map(f->UnsafeHacks.getIntField(f, this)).orElse(Integer.MIN_VALUE);
-    }
-
-    default void setData(FriendlyByteBuf buffer) {
-        Fields.lookup.get(this.getClass()).data.ifPresent(f->UnsafeHacks.setField(f, this, buffer));
-    }
-
-    default void setName(ResourceLocation channelName) {
-        Fields.lookup.get(this.getClass()).channel.ifPresent(f->UnsafeHacks.setField(f, this, channelName));
-    }
-
-    default void setIndex(int index) {
-        Fields.lookup.get(this.getClass()).index.ifPresent(f->UnsafeHacks.setIntField(f, this, index));
-    }
+    int getIndex();
 
     default NetworkDirection getDirection() {
         return NetworkDirection.directionFor(this.getClass());
