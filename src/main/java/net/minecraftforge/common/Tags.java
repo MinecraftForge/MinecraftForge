8ba6e9bfc371af571ce691b1cf339469eecb37a5
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-08-23 12:21:10 -0400

    Adjustments

--- src/main/java/net/minecraftforge/common/Tags.java
+++ src/main/java/net/minecraftforge/common/Tags.java
@@ -7,7 +7,6 @@ package net.minecraftforge.common;
 
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.BiomeTags;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.tags.ItemTags;
@@ -17,7 +16,6 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.material.Fluid;
-import net.minecraftforge.registries.ForgeRegistries;
 
 public class Tags
 {
@@ -553,6 +551,8 @@ public class Tags
         public static final TagKey<Biome> IS_COLD_NETHER = tag("is_cold/nether");
         public static final TagKey<Biome> IS_COLD_END = tag("is_cold/end");
 
+
+        public static final TagKey<Biome> IS_TEMPERATE_OVERWORLD = tag("is_temperate/overworld");
 
         public static final TagKey<Biome> IS_TEMPERATE = tag("is_temperate");
 
