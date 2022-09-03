d5a056cb24ec2eb335b11ccd46ab2b50c4efe483
Technici4n <13494793+Technici4n@users.noreply.github.com>
2022-09-03 14:20:21 +0200

    Expand testmod to also test simulate and tryEmptyContainer

--- src/test/java/net/minecraftforge/debug/FluidUtilTest.java
+++ src/test/java/net/minecraftforge/debug/FluidUtilTest.java
@@ -1,14 +1,23 @@
+/*
+ * Copyright (c) Forge Development LLC and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 package net.minecraftforge.debug;
 
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.Fluids;
+import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidUtil;
 import net.minecraftforge.fluids.capability.templates.FluidTank;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.items.ItemStackHandler;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 
 @Mod(FluidUtilTest.MODID)
@@ -23,27 +32,81 @@ public class FluidUtilTest
 
 	private static void runTests(FMLCommonSetupEvent commonSetupEvent)
 	{
+		test_tryEmptyContainer();
 		test_tryEmptyContainerAndStow_stackable();
 
 		LogManager.getLogger().info("FluidUtilTest ok!");
 	}
 
+	/**
+	 * Ensures that tryEmptyContainer doesn't change the target fluid handler when simulating.
+	 * Regression test for the root cause of https://github.com/MinecraftForge/MinecraftForge/issues/6796.
+	 */
+	private static void test_tryEmptyContainer()
+	{
+		var sourceStack = new ItemStack(Items.WATER_BUCKET, 2);
+		var targetTank = new FluidTank(10000);
+
+		// Simulate is not supposed to modify anything
+		var simulateResult = FluidUtil.tryEmptyContainer(sourceStack, targetTank, 1000, null, false);
+		if (!simulateResult.isSuccess())
+			throw new AssertionError("Failed to transfer.");
+		checkItemStack(simulateResult.getResult(), Items.BUCKET, 1);
+		// Tank and stack shouldn't be modified
+		checkItemStack(sourceStack, Items.WATER_BUCKET, 2);
+		checkFluidStack(targetTank.getFluid(), Fluids.EMPTY, 0);
+
+		// Execute should modify
+		var executeResult = FluidUtil.tryEmptyContainer(sourceStack, targetTank, 1000, null, true);
+		if (!executeResult.isSuccess())
+			throw new AssertionError("Failed to transfer.");
+		checkItemStack(executeResult.getResult(), Items.BUCKET, 1);
+		checkFluidStack(targetTank.getFluid(), Fluids.WATER, 1000);
+		checkItemStack(sourceStack, Items.WATER_BUCKET, 2); // Apparently the stack is not supposed to be modified
+	}
+
+	/**
+	 * Ensures that tryEmptyContainerAndStow doesn't duplicate fluids in the target when the container is stackable.
+	 * Regression test for https://github.com/MinecraftForge/MinecraftForge/issues/6796.
+	 */
 	private static void test_tryEmptyContainerAndStow_stackable()
 	{
 		var sourceStack = new ItemStack(Items.WATER_BUCKET, 2);
 		var targetTank = new FluidTank(10000);
 		var overflowInventory = new ItemStackHandler(1);
 
-		var result = FluidUtil.tryEmptyContainerAndStow(sourceStack, targetTank, overflowInventory, 1000, null, true);
-		var resultStack = result.getResult();
-
-		if (!result.isSuccess())
+		// Simulate first: it's not supposed to modify anything!
+		var simulateResult = FluidUtil.tryEmptyContainerAndStow(sourceStack, targetTank, overflowInventory, 1000, null, false);
+		if (!simulateResult.isSuccess())
 			throw new AssertionError("Failed to transfer.");
-		if (resultStack.getItem() != Items.WATER_BUCKET || resultStack.getCount() != 1)
-			throw new AssertionError("Expected 1 water bucket, got: " + resultStack);
-		if (targetTank.getFluid().getFluid() != Fluids.WATER || targetTank.getFluidAmount() != 1000)
-			throw new AssertionError("Expected 1000 water in target, got: " + targetTank.getFluidAmount() + " of " + targetTank.getFluid().getFluid());
-		if (overflowInventory.getStackInSlot(0).getItem() != Items.BUCKET || overflowInventory.getStackInSlot(0).getCount() != 1)
-			throw new AssertionError("Expected 1 empty bucket, got: " + overflowInventory.getStackInSlot(0));
+		checkItemStack(simulateResult.getResult(), Items.WATER_BUCKET, 1);
+		// Tank and inv shouldn't be modified for simulate
+		checkItemStack(sourceStack, Items.WATER_BUCKET, 2);
+		checkFluidStack(targetTank.getFluid(), Fluids.EMPTY, 0);
+		checkItemStack(overflowInventory.getStackInSlot(0), Items.AIR, 0);
+
+		// Now test with execute
+		var executeResult = FluidUtil.tryEmptyContainerAndStow(sourceStack, targetTank, overflowInventory, 1000, null, true);
+		if (!executeResult.isSuccess())
+			throw new AssertionError("Failed to transfer.");
+		checkItemStack(executeResult.getResult(), Items.WATER_BUCKET, 1);
+		checkFluidStack(targetTank.getFluid(), Fluids.WATER, 1000);
+		checkItemStack(overflowInventory.getStackInSlot(0), Items.BUCKET, 1);
+	}
+
+	private static void checkItemStack(ItemStack stack, Item item, int count)
+	{
+		if (stack.getItem() != item)
+			throw new AssertionError("Expected item " + ForgeRegistries.ITEMS.getKey(item) + ", got: " + ForgeRegistries.ITEMS.getKey(stack.getItem()));
+		if (stack.getCount() != count)
+			throw new AssertionError("Expected count " + count + ", got: " + stack.getCount());
+	}
+
+	private static void checkFluidStack(FluidStack stack, Fluid fluid, int amount)
+	{
+		if (stack.getFluid() != fluid)
+			throw new AssertionError("Expected fluid " + ForgeRegistries.FLUIDS.getKey(fluid) + ", got: " + ForgeRegistries.FLUIDS.getKey(stack.getFluid()));
+		if (stack.getAmount() != amount)
+			throw new AssertionError("Expected amount " + amount + ", got: " + stack.getAmount());
 	}
 }
