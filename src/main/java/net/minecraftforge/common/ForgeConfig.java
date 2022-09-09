4d5a32ebfccb058daa0f676e1e7cfe95913f839a
Brennan Ward <Bward7864@gmail.com>
2022-09-08 22:32:30 -0400

    Revert "[1.18.x] Backport: Cache resource listing calls in resource packs (#8868)"
    This reverts commit 4df024ef43524d8ec48928c70f561903f7a8e184.

--- src/main/java/net/minecraftforge/common/ForgeConfig.java
+++ src/main/java/net/minecraftforge/common/ForgeConfig.java
@@ -98,9 +98,6 @@ public class ForgeConfig {
      */
     public static class Common {
         public final ForgeConfigSpec.ConfigValue<? extends String> defaultWorldType;
-        public final BooleanValue cachePackAccess;
-        public final BooleanValue indexVanillaPackCachesOnThread;
-        public final BooleanValue indexModPackCachesOnThread;
 
         Common(ForgeConfigSpec.Builder builder) {
             builder.comment("General configuration settings")
@@ -111,24 +108,6 @@ public class ForgeConfig {
                              "The modded world types are registry names which should include the registry namespace, such as 'examplemod:example_world_type'.")
                     .translation("forge.configgui.defaultWorldType")
                     .define("defaultWorldType", "default");
-
-            cachePackAccess = builder
-                    .comment("Set this to true to cache resource listings in resource and data packs")
-                    .translation("forge.configgui.cachePackAccess")
-                    .worldRestart()
-                    .define("cachePackAccess", true);
-
-            indexVanillaPackCachesOnThread = builder
-                    .comment("Set this to true to index vanilla resource and data packs on thread")
-                    .translation("forge.configgui.indexVanillaPackCachesOnThread")
-                    .worldRestart()
-                    .define("indexVanillaPackCachesOnThread", false);
-
-            indexModPackCachesOnThread = builder
-                    .comment("Set this to true to index mod resource and data packs on thread")
-                    .translation("forge.configgui.indexModPackCachesOnThread")
-                    .worldRestart()
-                    .define("indexModPackCachesOnThread", false);
 
             builder.pop();
         }
--- src/main/java/net/minecraftforge/common/extensions/IForgePackResources.java
+++ src/main/java/net/minecraftforge/common/extensions/IForgePackResources.java
@@ -5,16 +5,10 @@
 
 package net.minecraftforge.common.extensions;
 
-import net.minecraft.server.packs.PackType;
-
 public interface IForgePackResources
 {
     default boolean isHidden()
     {
         return false;
     }
-
-    default void initForNamespace(final String nameSpace) {}
-
-    default void init(final PackType packType) {}
 }
