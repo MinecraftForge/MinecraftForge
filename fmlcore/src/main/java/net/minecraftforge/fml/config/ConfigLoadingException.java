455a4169db20db935cd3d7651abd64c3dcc321f8
Marc Hermans <marc.hermans@ldtteam.com>
2022-09-18 11:05:10 +0200

    Cleanup the PR

--- fmlcore/src/main/java/net/minecraftforge/fml/config/ConfigLoadingException.java
+++
@@ -1,7 +0,0 @@
-package net.minecraftforge.fml.config;
-
-public class ConfigLoadingException extends RuntimeException {
-    public ConfigLoadingException(final String configFileName, final String typeName, final String modId, Exception cause) {
-        super("Failed loading config file " + configFileName + " of type " + typeName + " for modid " + modId, cause);
-    }
-}
