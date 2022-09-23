26bf0c9f0303c8c888f9880b23b2ee500c79ddea
bconlon <aidan90803@icloud.com>
2022-09-22 18:00:31 -0700

    rework of manual category sorting for recipes to be based on the recipe itself.

---
+++ src/main/java/net/minecraftforge/common/crafting/IRecipeWithCategory.java
@@ -0,0 +1,7 @@
+package net.minecraftforge.common.crafting;
+
+import net.minecraft.client.RecipeBookCategories;
+
+public interface IRecipeWithCategory {
+    RecipeBookCategories getCategory();
+}
