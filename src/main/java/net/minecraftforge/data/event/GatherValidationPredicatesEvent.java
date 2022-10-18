7552b42345594416da92f642c7276ab76b4c8ba3
Matyrobbrt <matyrobbrt@gmail.com>
2022-10-18 17:00:53 +0300

    Licenses

--- src/main/java/net/minecraftforge/data/event/GatherValidationPredicatesEvent.java
+++ src/main/java/net/minecraftforge/data/event/GatherValidationPredicatesEvent.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) Forge Development LLC and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 package net.minecraftforge.data.event;
 
 import com.mojang.logging.LogUtils;
