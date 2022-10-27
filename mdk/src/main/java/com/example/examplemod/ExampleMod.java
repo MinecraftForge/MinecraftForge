1319e6bb4a6f0e289a2540a5befa8905dc130880
Nexus-Dino <86781290+Nexus-Dino@users.noreply.github.com>
2022-10-27 16:17:54 +0100

    Update ExampleMod.java
    Add missing semi-colon near the Dist import statement.

--- mdk/src/main/java/com/example/examplemod/ExampleMod.java
+++ mdk/src/main/java/com/example/examplemod/ExampleMod.java
@@ -9,7 +9,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
-import net.minecraftforge.api.distmarker.Dist
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
