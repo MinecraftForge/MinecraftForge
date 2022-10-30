3003e1458d8419c151f2e6672c0344a0a8a607d3
Su5eD <su5ed@outlook.com>
2022-10-30 10:19:24 +0100

    Allow discovering modular services

--- fmlloader/src/main/java/net/minecraftforge/fml/loading/ModDirTransformerDiscoverer.java
+++ fmlloader/src/main/java/net/minecraftforge/fml/loading/ModDirTransformerDiscoverer.java
@@ -6,21 +6,27 @@
 package net.minecraftforge.fml.loading;
 
 import com.mojang.logging.LogUtils;
+import cpw.mods.jarhandling.SecureJar;
 import cpw.mods.modlauncher.api.LamdbaExceptionUtils;
 import cpw.mods.modlauncher.api.NamedPath;
 import cpw.mods.modlauncher.serviceapi.ITransformerDiscoveryService;
 import org.slf4j.Logger;
 
-import java.io.File;
 import java.io.IOException;
+import java.lang.module.ModuleDescriptor;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.zip.ZipFile;
+import java.util.Set;
 
 public class ModDirTransformerDiscoverer implements ITransformerDiscoveryService {
     private static final Logger LOGGER = LogUtils.getLogger();
+    private static final Set<String> SERVICES = Set.of(
+        "cpw.mods.modlauncher.api.ITransformationService",
+        "net.minecraftforge.forgespi.locating.IModLocator",
+        "net.minecraftforge.forgespi.locating.IDependencyLocator"
+    );
 
     @Override
     public List<NamedPath> candidates(final Path gameDirectory) {
@@ -51,14 +57,11 @@ public class ModDirTransformerDiscoverer implements ITransformerDiscoveryService
         if (!Files.isRegularFile(path)) return;
         if (!path.toString().endsWith(".jar")) return;
         if (LamdbaExceptionUtils.uncheck(() -> Files.size(path)) == 0) return;
-        try (ZipFile zf = new ZipFile(new File(path.toUri()))) {
-            if (zf.getEntry("META-INF/services/cpw.mods.modlauncher.api.ITransformationService") != null) {
-                found.add(new NamedPath(zf.getName(), path));
-            } else if (zf.getEntry("META-INF/services/net.minecraftforge.forgespi.locating.IModLocator") != null) {
-                found.add(new NamedPath(zf.getName(), path));
-            }
-        } catch (IOException ioe) {
-            LOGGER.error("Zip Error when loading jar file {}", path, ioe);
-        }
+
+        SecureJar jar = SecureJar.from(path);
+        jar.moduleDataProvider().descriptor().provides().stream()
+            .map(ModuleDescriptor.Provides::service)
+            .filter(SERVICES::contains)
+            .forEach(s -> found.add(new NamedPath(s, path)));
     }
 }
