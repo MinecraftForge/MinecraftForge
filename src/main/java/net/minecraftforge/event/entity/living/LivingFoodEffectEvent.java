f80ec0d1a6c1ac7c15fd7c990fec7710227416c4
Adneths <bill.lui0921@gmail.com>
2022-11-01 20:50:01 -0700

    Implemented LivingEatEvent & LivingFoodEffectEvent

---
+++ src/main/java/net/minecraftforge/event/entity/living/LivingFoodEffectEvent.java
@@ -0,0 +1,51 @@
+package net.minecraftforge.event.entity.living;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mojang.datafixers.util.Pair;
+
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.food.FoodData;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.ForgeHooks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.Cancelable;
+
+/**
+ * LivingFoodEffectEvent is fire when an LivingEntity eats food item.<br>
+ * Specifically it is fired for the method call {@link LivingEntity#addEatEffect()}.<br>
+ * <br>
+ * This event is fired via the {@link ForgeHooks#onLivingFoodEffect()} .<br>
+ * <br>
+ * {@link #foodItem} contains the {@link ItemStack} that the entity ate (if available) for reference<br>
+ * {@link effectPairs} contains the effect-probability pair of {@link #foodItem} which can be modified 
+ * to change the applied effects and probability<br>
+ * <br>
+ * This event is {@link Cancelable}.<br>
+ * If this event is canceled, no effect will be applied.<br>
+ * <br>
+ * This event is fired on the {@link MinecraftForge#EVENT_BUS}.
+ */
+@Cancelable
+public class LivingFoodEffectEvent extends LivingEvent {
+
+	private final ItemStack foodItem;
+	private List<Pair<MobEffectInstance, Float>> effectPairs;
+	
+	public LivingFoodEffectEvent(LivingEntity entity, ItemStack foodItem) {
+		super(entity);
+		this.foodItem = foodItem;
+		effectPairs = foodItem.getFoodProperties(entity).getEffects();
+	}
+
+	public ItemStack getFoodItem() {
+		return foodItem;
+	}
+
+	public List<Pair<MobEffectInstance, Float>> getEffects() {
+		return effectPairs;
+	}
+
+}
---
+++ src/test/java/net/minecraftforge/debug/entity/living/LivingFoodEventTest.java
@@ -0,0 +1,58 @@
+package net.minecraftforge.debug.entity.living;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.mojang.datafixers.util.Pair;
+
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingEatEvent;
+import net.minecraftforge.event.entity.living.LivingFoodEffectEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("living_food_event_test")
+public class LivingFoodEventTest
+{
+    public static final Logger LOGGER = LogManager.getLogger();
+    public LivingFoodEventTest()
+    {
+        MinecraftForge.EVENT_BUS.addListener(this::onLivingEat);
+        MinecraftForge.EVENT_BUS.addListener(this::onLivingFoodEffect);
+    }
+
+    public void onLivingEat(LivingEatEvent event)
+    {
+    	LOGGER.info("{} is about to eat {} with {} nutrition and {} saturation", 
+    			event.getEntityLiving().getType(), event.getFoodItem().getItem(),
+    			event.getFoodAmount(), event.getSaturaionAmount());
+    	if(event.getFoodItem().getItem() == Items.COOKED_BEEF)
+    	{
+    		event.setCanceled(true);
+	    	LOGGER.info("Canceled eating Steak");
+    	}
+    	else
+    	{
+	    	event.setFoodAmount(1);
+	    	event.setSaturaionAmount(0);
+	    	LOGGER.info("Changed to 1 nutrition and 0 saturation");
+    	}
+    }
+    public void onLivingFoodEffect(LivingFoodEffectEvent event)
+    {
+    	Item item = event.getFoodItem().getItem();
+    	if(item == Items.GOLDEN_APPLE)
+    	{
+    		event.getEffects().clear();
+    		LOGGER.info("Removed effects from Golden Apple");
+    	}
+    	else if(item == Items.PUFFERFISH)
+    	{
+    		event.getEffects().add(new Pair<MobEffectInstance,Float>(new MobEffectInstance(MobEffects.REGENERATION,100,3),1f));
+    		LOGGER.info("Added Regeneration to PufferFish");
+    	}
+    }
+}
