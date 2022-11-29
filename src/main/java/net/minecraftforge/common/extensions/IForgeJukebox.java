ff8fce969513eb7d3f13c38246ced481d01a2353
Waterpicker <waterpickerenternity@gmail.com>
2022-11-29 11:52:13 -0600

    insertRecord instead of setRecord in IForgeJukebox

--- src/main/java/net/minecraftforge/common/extensions/IForgeJukebox.java
+++ src/main/java/net/minecraftforge/common/extensions/IForgeJukebox.java
@@ -4,19 +4,27 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.JukeboxBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
 import javax.annotation.Nullable;
 
 public interface IForgeJukebox
 {
+    private JukeboxBlock self()
+    {
+        return (JukeboxBlock) this;
+    }
+
     /**
-     * Fires when an entity inserting an {@link net.minecraft.world.item.ItemStack} into a jukebox.
      * @param entity The entity inserting into jukebox. This can be null.
      * @param level The level of the jukebox.
      * @param pos The position of the jukebox.
      * @param state The blockstate of the jukebox
      * @param stack The itemstack being inserted.
      */
-    void setRecord(@Nullable Entity entity, LevelAccessor level, BlockPos pos, BlockState state, ItemStack stack);
+    default void insertRecord(@Nullable Entity entity, LevelAccessor level, BlockPos pos, BlockState state, ItemStack stack)
+    {
+        self().setRecord(entity, level, pos, state, stack);
+    }
 }
