8820a0189301abf30d7035ca12fc7a1da183ffbd
Waterpicker <waterpickerenternity@gmail.com>
2022-12-01 16:47:54 -0600

    Fixed various issues with IForgeJukeboxBlock

--- src/main/java/net/minecraftforge/common/extensions/IForgeJukeboxBlock.java
+++ src/main/java/net/minecraftforge/common/extensions/IForgeJukeboxBlock.java
@@ -22,7 +22,7 @@ import org.jetbrains.annotations.Nullable;
 
 /**
  * Extension-Interface providing methods for various jukebox activiites that are currently hardcoded to only the vanilla jukebox.
- * Any @{@link Block} implementing will be treated as if it was a jukebox.
+ * Any {@link Block} implementing will be treated as if it was a jukebox.
  *
  * @see JukeboxBlock
  */
@@ -30,15 +30,16 @@ public interface IForgeJukeboxBlock
 {
 
     /**
-     * Called by @{@link RecordItem} to insert a record into the jukebox. The actual action taken by the jukebox is up to it and not the record.
+     * Called by {@link RecordItem} to insert a record into the jukebox.
+     * The actual action taken by the jukebox is up to it and not the record.
      *
-     * @param entity The entity inserting into jukebox. This can be null.
+     * @param state The blockstate of the jukebox.
      * @param level The level of the jukebox.
      * @param pos The position of the jukebox.
-     * @param state The blockstate of the jukebox.
+     * @param entity The entity inserting into jukebox. This can be null.
      * @param stack The itemstack being inserted.
      */
-    default void insertRecord(@Nullable Entity entity, LevelAccessor level, BlockPos pos, BlockState state, ItemStack stack)
+    default void insertRecord(BlockState state, Level level, BlockPos pos, @Nullable Entity entity, ItemStack stack)
     {
         if (this instanceof JukeboxBlock jukebox)
         {
@@ -47,39 +48,56 @@ public interface IForgeJukeboxBlock
     }
 
     /**
-     * Gets the @{@link ItemStack} of the current record in the jukebox. The current record can be empty or any item. In vanilla,
+     * Gets the {@link ItemStack} of the current record in the jukebox.
+     * The current record can be empty or any item. In vanilla, this can only be records.
      *
+     * @param state The blockstate of the jukebox.
      * @param level The level of the jukebox.
      * @param pos The position of the jukebox.
      * @return the current record or empty.
      */
-    default ItemStack getRecord(LevelAccessor level, BlockPos pos) {
-        if (level.getBlockEntity(pos) instanceof JukeboxBlockEntity jukebox) return jukebox.getRecord();
-        return ItemStack.EMPTY;
+    default ItemStack getRecord(BlockState state, Level level, BlockPos pos) {
+        if (level.getBlockEntity(pos) instanceof JukeboxBlockEntity jukebox) {
+            return jukebox.getRecord();
+        } else {
+            return ItemStack.EMPTY;
+        }
     }
 
     /**
-     * Checks if an entity is allow to dance close to the jukebox. The actual behavior is controlled by the entity. Right now only the vanilla parrot/allay currently use this.
+     * Checks if an entity is allow to dance close to the jukebox.
+     * The actual behavior is controlled by the entity.
+     * Right now only the vanilla parrot/allay currently use this.
      *
+     * @param state The blockstate of the jukebox
      * @param entity The entity being checked.
      * @return true if the entity can dance.
      */
-    default boolean canDance(Entity entity)
+    default boolean canDance(BlockState state, Entity entity)
     {
-        return entity instanceof Allay || entity instanceof Parrot;
+        if (entity instanceof Allay) {
+            return true;
+        } else if (entity instanceof Parrot) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
      * Gets if the jukebox currently has a record inside it.
      *
+     * @param state The blockstate of the jukebox.
      * @param level The level of the jukebox.
      * @param pos The position of the jukebox.
-     * @param state The blockstate of the jukebox.
      * @return true if jukebox has a record.
      */
-    default boolean hasRecord(Level level, BlockPos pos, BlockState state)
+    default boolean hasRecord(BlockState state, Level level, BlockPos pos)
     {
-        if (this instanceof JukeboxBlock) return !state.getValue(JukeboxBlock.HAS_RECORD);
-        return false;
+        if (this instanceof JukeboxBlock) {
+            return !state.getValue(JukeboxBlock.HAS_RECORD);
+        } else {
+            return false;
+        }
     }
 }
