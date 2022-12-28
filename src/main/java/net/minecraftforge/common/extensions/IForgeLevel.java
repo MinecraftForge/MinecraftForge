78e6dd778834f706fe9ae8a7d0af0db89cb27203
tmvkrpxl0 <ironman466@naver.com>
2022-12-28 20:10:08 +0900

    Reworking on based on Orion's comments

--- src/main/java/net/minecraftforge/common/extensions/IForgeLevel.java
+++ src/main/java/net/minecraftforge/common/extensions/IForgeLevel.java
@@ -7,17 +7,9 @@ package net.minecraftforge.common.extensions;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
-import java.util.function.Consumer;
 
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.Level;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
-import net.minecraftforge.common.util.TaskScheduler;
 import net.minecraftforge.entity.PartEntity;
-import net.minecraftforge.event.TickEvent;
-import org.jetbrains.annotations.Nullable;
 
 public interface IForgeLevel extends ICapabilityProvider
 {
@@ -41,20 +33,5 @@ public interface IForgeLevel extends ICapabilityProvider
     public default Collection<PartEntity<?>> getPartEntities()
     {
         return Collections.emptyList();
-    }
-
-    public default void requestTask(TaskScheduler.ForgeTask<?> task)
-    {
-        TaskScheduler.requestTask(task);
-    }
-
-    public default void requestTask(@Nullable ResourceLocation name, TickEvent.Phase phase, int initialTickDelay, Consumer<TaskScheduler.ForgeTask<?>> toRun)
-    {
-        TaskScheduler.requestTask(name, (Level) this, phase, initialTickDelay, toRun);
-    }
-
-    public default <T> void requestRepeatingTask(@Nullable ResourceLocation name, TickEvent.Phase phase, int initialTickDelay, int repeatingTickDelay, BiFunction<TaskScheduler.ForgeTask<T>, T, T> toRepeat, BiConsumer<TaskScheduler.ForgeTask<T>, T> whenDone, T initialSharedInfo)
-    {
-        TaskScheduler.requestRepeatingTask(name, (Level) this, phase, initialTickDelay, repeatingTickDelay, toRepeat, whenDone, initialSharedInfo);
     }
 }
--- src/test/java/net/minecraftforge/debug/TaskTest.java
+++ src/test/java/net/minecraftforge/debug/TaskTest.java
@@ -6,6 +6,7 @@
 package net.minecraftforge.debug;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.TickTask;
 import net.minecraft.world.entity.animal.AbstractFish;
 import net.minecraft.world.entity.animal.Salmon;
 import net.minecraft.world.item.Items;
@@ -16,6 +17,7 @@ import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.server.ServerLifecycleHooks;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -30,34 +32,18 @@ public class TaskTest {
 
     private static void yeetFish(PlayerInteractEvent.EntityInteract event) {
         if (event.getSide() != LogicalSide.SERVER) return;
-        if (!(event.getTarget() instanceof AbstractFish)) return;
+        if (!(event.getTarget() instanceof AbstractFish fish)) return;
         if (event.getItemStack().getItem() != Items.GOLD_INGOT) return;
 
-        ResourceLocation name = new ResourceLocation(MODID, "fish_yeeter");
-        event.getLevel().requestRepeatingTask(name, TickEvent.Phase.END, 20, 10, (task, shared) -> {
-            LOGGER.info(shared.repeatCount + " repeat");
-            if (shared.repeatCount == 5) task.done();
+        int gameTime = ServerLifecycleHooks.getCurrentServer().getTickCount();
+        TickTask task = new TickTask(gameTime, () -> fish.addDeltaMovement(new Vec3(0.0, 1.0, 0.0))) {
+            int counter = 5;
 
-            shared.clicked.addDeltaMovement(new Vec3(0.0, 0.7, 0.0));
-            shared.clicked.hurtMarked = true;
-            shared.repeatCount++;
-
-            return shared;
-        }, (task, shared) -> {
-            LOGGER.info("done");
-            if (shared.clicked instanceof Salmon) {
-                Vec3 position = shared.clicked.position();
-                event.getLevel().explode(event.getTarget(), position.x, position.y, position.z, 4F, Level.ExplosionInteraction.TNT);
+            @Override
+            public boolean shouldRun() {
+                counter--;
+                return counter != 0;
             }
-        }, new SharedInfo((AbstractFish) event.getTarget()));
-    }
-
-    private static final class SharedInfo {
-        private int repeatCount = 0;
-        private AbstractFish clicked;
-
-        private SharedInfo(AbstractFish clicked) {
-            this.clicked = clicked;
-        }
+        };
     }
 }
