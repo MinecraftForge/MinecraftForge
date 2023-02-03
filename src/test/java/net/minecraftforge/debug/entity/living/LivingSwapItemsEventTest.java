4fbb39106c018f20a9f00dc04ae690ae2ae5edcc
JustRed23 <manuel.bormans@gmail.com>
2023-02-03 01:43:55 +0100

    Update test

--- src/test/java/net/minecraftforge/debug/entity/living/LivingSwapItemsEventTest.java
+++ src/test/java/net/minecraftforge/debug/entity/living/LivingSwapItemsEventTest.java
@@ -7,28 +7,28 @@ package net.minecraftforge.debug.entity.living;
 
 import net.minecraft.world.item.Items;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.living.LivingSwapHandItemsEvent;
+import net.minecraftforge.event.entity.living.LivingSwapItemsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod("living_swap_hand_items_event_test")
-public class LivingSwapHandItemsEventTest
+@Mod("living_swap_items_event_test")
+public class LivingSwapItemsEventTest
 {
     public static final boolean ENABLE = true;
     public static final Logger LOGGER = LogManager.getLogger();
 
-    public LivingSwapHandItemsEventTest()
+    public LivingSwapItemsEventTest()
     {
         if (ENABLE)
             MinecraftForge.EVENT_BUS.register(this);
     }
 
     @SubscribeEvent
-    public void livingSwapHandItems(LivingSwapHandItemsEvent event)
+    public void livingSwapItems(LivingSwapItemsEvent.Hands event)
     {
-        LOGGER.info("{} swapping hands from {} to {}", event.getEntity(), event.getOffHandItem(), event.getMainHandItem());
+        LOGGER.info("{} swapping hands. To offhand: {}, to main hand: {}", event.getEntity(), event.getItemSwappedToOffHand(), event.getItemSwappedToMainHand());
         if (event.getEntity().getMainHandItem().is(Items.DIAMOND))
             event.setCanceled(true);
     }
