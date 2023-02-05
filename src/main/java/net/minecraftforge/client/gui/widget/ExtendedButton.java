94af5cc43aa87fb2cb2f5f0f535b2c5fcbdaecf5
matyrobbrt <65940752+Matyrobbrt@users.noreply.github.com>
2023-02-05 19:58:08 +0200

    [1.19.x] Fix `ForgeSlider` not respecting custom height (#9237)

--- src/main/java/net/minecraftforge/client/gui/widget/ExtendedButton.java
+++ src/main/java/net/minecraftforge/client/gui/widget/ExtendedButton.java
@@ -8,7 +8,9 @@ package net.minecraftforge.client.gui.widget;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.locale.Language;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.FormattedText;
 import net.minecraftforge.client.gui.ScreenUtils;
 
 /**
@@ -49,14 +51,7 @@ public class ExtendedButton extends Button
         ScreenUtils.blitWithBorder(poseStack, WIDGETS_LOCATION, this.getX(), this.getY(), 0, 46 + k * 20, this.width, this.height, 200, 20, 2, 3, 2, 2, this.getBlitOffset());
         this.renderBg(poseStack, mc, mouseX, mouseY);
 
-        Component buttonText = this.getMessage();
-        int strWidth = mc.font.width(buttonText);
-        int ellipsisWidth = mc.font.width("...");
-
-        if (strWidth > width - 6 && strWidth > ellipsisWidth)
-            //TODO, srg names make it hard to figure out how to append to an ITextProperties from this trim operation, wraping this in StringTextComponent is kinda dirty.
-            buttonText = Component.literal(mc.font.substrByWidth(buttonText, width - 6 - ellipsisWidth).getString() + "...");
-
-        drawCenteredString(poseStack, mc.font, buttonText, this.getX() + this.width / 2, this.getY() + (this.height - 8) / 2, getFGColor());
+        final FormattedText buttonText = mc.font.ellipsize(this.getMessage(), this.width - 6); // Remove 6 pixels so that the text is always contained within the button's borders
+        drawCenteredString(poseStack, mc.font, Language.getInstance().getVisualOrder(buttonText), this.getX() + this.width / 2, this.getY() + (this.height - 8) / 2, getFGColor());
     }
 }
