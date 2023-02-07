00d4801414fff31e7df2c0eb0439bfa6334f1d35
Apex <robsonlawson3@gmail.com>
2023-02-07 04:39:25 +0000

    Stop implementing StringRepresentable interface on ItemPickupReason

--- src/main/java/net/minecraftforge/items/pickup/ItemPickupReason.java
+++ src/main/java/net/minecraftforge/items/pickup/ItemPickupReason.java
@@ -7,9 +7,6 @@ package net.minecraftforge.items.pickup;
 
 import com.google.common.collect.Maps;
 import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-
-import net.minecraft.util.StringRepresentable;
 
 import java.util.Map;
 import java.util.Objects;
@@ -18,7 +15,7 @@ import java.util.Objects;
  * Class representing a reason in which an ItemEntity is being picked up.
  * @see ItemPickupReasons
  */
-public final class ItemPickupReason implements StringRepresentable
+public final class ItemPickupReason
 {
     /**
      * package level of visibility to be accessible in {@link ItemPickupReasons}.
@@ -44,13 +41,6 @@ public final class ItemPickupReason implements StringRepresentable
      * @return The name of this pickup reason.
      */
     public String name()
-    {
-        return name;
-    }
-
-    @NotNull
-    @Override
-    public String getSerializedName()
     {
         return name;
     }
