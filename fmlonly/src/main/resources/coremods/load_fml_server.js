86694b9d41c822b27ae74a250880d0b82cc8dba2
Matyrobbrt <matyrobbrt@gmail.com>
2023-03-17 23:55:23 +0200

    Fix FMLOnly

---
+++ fmlonly/src/main/resources/coremods/load_fml_server.js
@@ -0,0 +1,27 @@
+var ASMAPI = Java.type('net.minecraftforge.coremod.api.ASMAPI')
+var Opcodes = Java.type('org.objectweb.asm.Opcodes')
+
+function initializeCoreMod() {
+    return {
+        'minecraft': {
+            'target': {
+                'type': 'METHOD',
+                'class': 'net.minecraft.server.Main',
+                'methodName': 'main',
+                'methodDesc': '([Ljava/lang/String;)V'
+            },
+            'transformer': function(methodNode) {
+                var meth = ASMAPI.getMethodNode();
+                meth.visitMethodInsn(Opcodes.INVOKESTATIC, 'net/minecraftforge/fmlonlyclient/ServerModLoader', 'load', '()V', false)
+                for (var i=0; i<methodNode.instructions.size(); i++) {
+                    var ain = methodNode.instructions.get(i);
+                    if (ain.getOpcode() == Opcodes.NEW && ain.desc == 'net/minecraft/server/dedicated/DedicatedServerSettings') {
+                        methodNode.instructions.insert(ain, meth.instructions);
+                        break;
+                    }
+                }
+                return methodNode;
+            }
+        },
+    }
+}
