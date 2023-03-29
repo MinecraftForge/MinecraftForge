0d42f63e7a7d7e1015bf36e7ff6831afab083c9c
Matyrobbrt <matyrobbrt@gmail.com>
2023-03-29 22:09:49 +0300

    Update TeamcityRequests.groovy

--- buildSrc/src/main/groovy/net/minecraftforge/forge/tasks/TeamcityRequests.groovy
+++ buildSrc/src/main/groovy/net/minecraftforge/forge/tasks/TeamcityRequests.groovy
@@ -6,6 +6,7 @@ import groovy.transform.CompileStatic
 import org.eclipse.jgit.api.Git
 
 import javax.annotation.Nullable
+import java.util.stream.Collectors
 
 @CompileStatic
 class TeamcityRequests {
@@ -29,16 +30,22 @@ class TeamcityRequests {
     }
 
     @Nullable
-    static Build findMatching(String commitId) throws IOException {
-        jsonRequest(new TypeToken<Builds>() {}, "https://teamcity.minecraftforge.net/guestAuth/app/rest/builds?locator=revision:($commitId),buildType:(id:MinecraftForge_MinecraftForge_MinecraftForge_MinecraftForge__Build),status:SUCCESS")?.build?.find()
+    static Map<String, Build> buildsByCommit() throws IOException {
+        final Map<String, Build> builds = [:]
+        jsonRequest(new TypeToken<Builds>() {}, "https://teamcity.minecraftforge.net/guestAuth/app/rest/builds?fields=build:(revisions,number)&locator=buildType:(id:MinecraftForge_MinecraftForge_MinecraftForge_MinecraftForge__Build),count:300,status:SUCCESS")
+                ?.build?.forEach {
+            it.revisions.revision.each { rev -> builds[rev.version] = it }
+        }
+        return builds
     }
 
     @Nullable
     static String attemptFindBase(File gitPath) {
         try (final git = Git.open(gitPath)) {
+            final Map<String, Build> buildByCommit = buildsByCommit()
             for (final commit in git.log().setMaxCount(100).call()) {
                 // Find the first commit which was built on CI
-                final build = findMatching(commit.id.name)
+                final build = buildByCommit[commit.id.name]
                 if (build) {
                     return build.number
                 }
@@ -48,14 +55,20 @@ class TeamcityRequests {
     }
 
     static final class Builds {
-        public int count
-        public String href
         public List<Build> build
     }
 
     static final class Build {
-        public int id
         public String number
-        public String buildTypeId
+        public Revisions revisions
+    }
+
+    static final class Revisions {
+        public int count
+        public List<Revision> revision
+    }
+
+    static final class Revision {
+        public String version
     }
 }
