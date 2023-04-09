92eb3204477d71a1bee6af25cabffc1a3d26147e
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2023-04-08 23:38:43 -0700

    Fixed Bracing for TestScreen.java

--- src/test/java/net/minecraftforge/debug/client/TestScreen.java
+++ src/test/java/net/minecraftforge/debug/client/TestScreen.java
@@ -6,22 +6,27 @@
 package net.minecraftforge.debug.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.brigadier.Command;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+
 public class TestScreen extends Screen
 {
-    public static int open() {
-        Minecraft.getInstance().setScreen(new TestScreen(Component.translatable("screen.patch")));
-        return 1;
+    public static int open()
+    {
+        Minecraft.getInstance().setScreen(new TestScreen());
+        return Command.SINGLE_SUCCESS;
     }
 
-    public TestScreen(Component pComponent) {
-        super(pComponent);
+    public TestScreen()
+    {
+        super(Component.literal("testscreen"));
     }
 
     @Override
-    public void render(PoseStack pPoseStack, int pMouseX, int pMouseY, float pPartialTick) {
-        renderBackground(pPoseStack);
+    public void render(PoseStack PoseStack, int MouseX, int MouseY, float PartialTick)
+    {
+        renderBackground(PoseStack);
     }
 }
