d4f211b81586d24ab6b734c7815fc067579b09b9
matyrobbrt <65940752+Matyrobbrt@users.noreply.github.com>
2023-04-24 20:59:56 +0300

    Determine the Forge version the PR was built against when running PR compat checks (#9374)

--- buildSrc/src/main/groovy/net/minecraftforge/forge/tasks/Util.groovy
+++ buildSrc/src/main/groovy/net/minecraftforge/forge/tasks/Util.groovy
@@ -150,9 +150,10 @@ public class Util {
 		}
 	}
 
-    public static String getLatestForgeVersion(mcVersion) {
-        def json = new JsonSlurper().parseText(new URL("https://files.minecraftforge.net/net/minecraftforge/forge/promotions_slim.json").getText("UTF-8"))
-        return json.promos["$mcVersion-latest"]
+    static String getLatestForgeVersion(mcVersion) {
+        final json = new JsonSlurper().parseText(new URL('https://files.minecraftforge.net/net/minecraftforge/forge/promotions_slim.json').getText('UTF-8'))
+        final ver = json.promos["$mcVersion-latest"]
+        ver === null ? null : (mcVersion + '-' + ver)
     }
 
     static void processClassNodes(File file, Closure process) {
