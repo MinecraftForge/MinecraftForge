6805db0df50021790a188e12d9de61898afb9f6c
DBotThePony <dbotthepony@yandex.ru>
2023-04-29 16:12:54 +0700

    Make spawnCause be stored as UUID Add missing license header

--- src/main/java/net/minecraftforge/event/entity/player/PlayerSpawnPhantomsEvent.java
+++ src/main/java/net/minecraftforge/event/entity/player/PlayerSpawnPhantomsEvent.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) Forge Development LLC and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 package net.minecraftforge.event.entity.player;
 
 import net.minecraft.world.entity.player.Player;
