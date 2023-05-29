92cc8fc679199024b1675e77447737caa85da008
Dennis C <xfacthd@gmx.de>
2023-05-30 00:35:15 +0200

    [1.18.x] Add Forge "bosses" tag (#9513)

---
+++ src/main/java/net/minecraftforge/common/data/ForgeEntityTypeTagsProvider.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) Forge Development LLC and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+package net.minecraftforge.common.data;
+
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.tags.EntityTypeTagsProvider;
+import net.minecraft.world.entity.EntityType;
+import net.minecraftforge.common.Tags;
+
+public final class ForgeEntityTypeTagsProvider extends EntityTypeTagsProvider
+{
+    public ForgeEntityTypeTagsProvider(DataGenerator generator, ExistingFileHelper existingFileHelper)
+    {
+        super(generator, "forge", existingFileHelper);
+    }
+
+    @Override
+    protected void addTags()
+    {
+        tag(Tags.EntityTypes.BOSSES).add(EntityType.ENDER_DRAGON, EntityType.WITHER);
+    }
+
+    @Override
+    public String getName()
+    {
+        return "Forge EntityType Tags";
+    }
+}
