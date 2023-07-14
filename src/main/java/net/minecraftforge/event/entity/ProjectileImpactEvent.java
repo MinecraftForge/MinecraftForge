2f229c5bb1dd6c8db70e3daac2aad05948ad12a8
JustRed23 <manuel.bormans@gmail.com>
2023-07-13 17:00:12 -0700

    Change ProjectileHitEvent to return a result instead of being cancelable. Closes #9642

--- src/main/java/net/minecraftforge/event/entity/ProjectileImpactEvent.java
+++ src/main/java/net/minecraftforge/event/entity/ProjectileImpactEvent.java
@@ -10,6 +10,9 @@ import net.minecraft.world.phys.HitResult;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.ForgeEventFactory;
 import net.minecraftforge.eventbus.api.Cancelable;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 /**
  * This event is fired on the {@link MinecraftForge#EVENT_BUS}.<br>
@@ -19,6 +22,8 @@ import net.minecraftforge.eventbus.api.Cancelable;
  * custom projectiles should fire this event and check the result in a similar fashion.
  * This event is cancelable. When canceled, the impact will not be processed and the projectile will continue flying.
  * Killing or other handling of the entity after event cancellation is up to the modder.
+ * You can also directly set the {@link ImpactResult} to change the impact behaviour.
+ * @see #setImpactResult(ImpactResult)
  */
 @Cancelable
 public class ProjectileImpactEvent extends EntityEvent
@@ -26,11 +31,23 @@ public class ProjectileImpactEvent extends EntityEvent
     private final HitResult ray;
     private final Projectile projectile;
 
+    private ImpactResult result = ImpactResult.DEFAULT;
+
     public ProjectileImpactEvent(Projectile projectile, HitResult ray)
     {
         super(projectile);
         this.ray = ray;
         this.projectile = projectile;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @deprecated Use {@link #setImpactResult(ImpactResult)} instead.
+     */
+    @Deprecated(forRemoval = true, since = "1.20.1")
+    public void setCanceled(boolean cancel)
+    {
+        super.setCanceled(cancel);
     }
 
     public HitResult getRayTraceResult()
@@ -41,5 +58,35 @@ public class ProjectileImpactEvent extends EntityEvent
     public Projectile getProjectile()
     {
         return projectile;
+    }
+
+    public void setImpactResult(@NotNull ImpactResult newResult)
+    {
+        this.result = Objects.requireNonNull(newResult, "ImpactResult cannot be null");
+    }
+
+    public ImpactResult getImpactResult()
+    {
+        return result;
+    }
+
+    public enum ImpactResult
+    {
+        /**
+         * The default behaviour, the projectile will be destroyed and the hit will be processed.
+         */
+        DEFAULT,
+        /**
+         * The projectile will pass through the current entity as if it wasn't there. This will return default behaviour if there is no entity.
+         */
+        SKIP_ENTITY,
+        /**
+         * Damage the entity and stop the projectile here, the projectile will not pierce.
+         */
+        STOP_AT_CURRENT,
+        /**
+         * Cancel the piercing aspect of the projectile, and do not damage the entity.
+         */
+        STOP_AT_CURRENT_NO_DAMAGE
     }
 }
