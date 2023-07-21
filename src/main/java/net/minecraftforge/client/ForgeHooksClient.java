2a3653278ba24bdf85d8a355e251e173e46fb27e
Kovak <kovac1066@gmail.com>
2023-07-20 17:26:10 -0700

    errant return

--- src/main/java/net/minecraftforge/client/ForgeHooksClient.java
+++ src/main/java/net/minecraftforge/client/ForgeHooksClient.java
@@ -1247,7 +1247,6 @@ public class ForgeHooksClient
        }
     }
 
-
     // Make sure the below method is only ever called once (by forge).
     private static boolean initializedClientHooks = false;
     // Runs during Minecraft construction, before initial resource loading.
