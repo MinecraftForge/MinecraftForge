dd139115ed9a4efdff9ab1cbaef41948a3916fd4
LexManos <LexManos@gmail.com>
2023-09-20 15:24:15 -0700

    Move old tests.

---
+++ src/test/java/net/minecraftforge/test/Empty.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) Forge Development LLC and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+package net.minecraftforge.test;
+
+import net.minecraftforge.fml.common.Mod;
+
+/* Empty test mod to make FML happy if there are no others */
+@Mod("empty")
+public class Empty {
+    
+}
