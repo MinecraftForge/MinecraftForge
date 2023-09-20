cbb1837efcdb96094dd2b8a41ad13fdc2740343d
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2023-09-20 00:17:08 -0700

    Inital Commit for Rework of AttachCapabilitiesEvent

---
+++ src/test/java/net/minecraftforge/debug/caps/MyItem.java
@@ -0,0 +1,9 @@
+package net.minecraftforge.debug.caps;
+
+import net.minecraft.world.item.Item;
+
+public class MyItem extends Item {
+    public MyItem(Properties p_41383_) {
+        super(p_41383_);
+    }
+}
