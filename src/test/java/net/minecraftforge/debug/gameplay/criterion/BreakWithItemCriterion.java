ae775749d06d131d63fb949ae43d18068a972d1b
DBotThePony <dbotthepony@yandex.ru>
2023-10-11 08:02:29 +0700

    Indent with spaces to match forge codebase

--- src/test/java/net/minecraftforge/debug/gameplay/criterion/BreakWithItemCriterion.java
+++ src/test/java/net/minecraftforge/debug/gameplay/criterion/BreakWithItemCriterion.java
@@ -35,90 +35,90 @@ import java.util.stream.Stream;
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public final class BreakWithItemCriterion extends SimpleCriterionTrigger<BreakWithItemCriterion.Instance> {
-	// this will be removed once Mojang figure out how to decode context aware predicates with codecs
-	private static final ThreadLocal<ArrayDeque<Optional<ContextAwarePredicate>>> stack = ThreadLocal.withInitial(ArrayDeque::new);
+    // this will be removed once Mojang figure out how to decode context aware predicates with codecs
+    private static final ThreadLocal<ArrayDeque<Optional<ContextAwarePredicate>>> stack = ThreadLocal.withInitial(ArrayDeque::new);
 
-	private static final class ContextPredicate extends MapCodec<Optional<ContextAwarePredicate>> {
-		public static final ContextPredicate INSTANCE = new ContextPredicate();
+    private static final class ContextPredicate extends MapCodec<Optional<ContextAwarePredicate>> {
+        public static final ContextPredicate INSTANCE = new ContextPredicate();
 
-		@Override
-		public <T> Stream<T> keys(DynamicOps<T> ops) {
-			return Stream.of(ops.createString("player"));
-		}
+        @Override
+        public <T> Stream<T> keys(DynamicOps<T> ops) {
+            return Stream.of(ops.createString("player"));
+        }
 
-		@Override
-		public <T> DataResult<Optional<ContextAwarePredicate>> decode(DynamicOps<T> ops, MapLike<T> input) {
-			if (stack.get().isEmpty())
-				return DataResult.error(() -> "Not deserializing criterion");
-			else
-				return DataResult.success(stack.get().getLast());
-		}
+        @Override
+        public <T> DataResult<Optional<ContextAwarePredicate>> decode(DynamicOps<T> ops, MapLike<T> input) {
+            if (stack.get().isEmpty())
+                return DataResult.error(() -> "Not deserializing criterion");
+            else
+                return DataResult.success(stack.get().getLast());
+        }
 
-		@Override
-		public <T> RecordBuilder<T> encode(Optional<ContextAwarePredicate> input, DynamicOps<T> ops, RecordBuilder<T> prefix) {
-			if (input.isPresent())
-				return prefix.add("player", JsonOps.INSTANCE.convertTo(ops, input.get().toJson()));
+        @Override
+        public <T> RecordBuilder<T> encode(Optional<ContextAwarePredicate> input, DynamicOps<T> ops, RecordBuilder<T> prefix) {
+            if (input.isPresent())
+                return prefix.add("player", JsonOps.INSTANCE.convertTo(ops, input.get().toJson()));
 
-			return prefix;
-		}
-	}
+            return prefix;
+        }
+    }
 
-	private static final Codec<Instance> CODEC = RecordCodecBuilder.create(it -> {
-		return it.group(
-			ContextPredicate.INSTANCE.forGetter(AbstractCriterionTriggerInstance::playerPredicate),
-			BlockPredicate.CODEC.fieldOf("breakingBlock").forGetter(i -> i.breakingBlock),
-			ItemPredicate.CODEC.fieldOf("holdingItem").forGetter(i -> i.holdingItem),
-			Codec.BOOL.optionalFieldOf("allowOffhand", false).forGetter(i -> i.allowOffhand)
-		).apply(it, Instance::new);
-	});
+    private static final Codec<Instance> CODEC = RecordCodecBuilder.create(it -> {
+        return it.group(
+            ContextPredicate.INSTANCE.forGetter(AbstractCriterionTriggerInstance::playerPredicate),
+            BlockPredicate.CODEC.fieldOf("breakingBlock").forGetter(i -> i.breakingBlock),
+            ItemPredicate.CODEC.fieldOf("holdingItem").forGetter(i -> i.holdingItem),
+            Codec.BOOL.optionalFieldOf("allowOffhand", false).forGetter(i -> i.allowOffhand)
+        ).apply(it, Instance::new);
+    });
 
-	@Override
-	protected Instance createInstance(JsonObject data, Optional<ContextAwarePredicate> playerPredicate, DeserializationContext context) {
-		try {
-			stack.get().addLast(playerPredicate);
+    @Override
+    protected Instance createInstance(JsonObject data, Optional<ContextAwarePredicate> playerPredicate, DeserializationContext context) {
+        try {
+            stack.get().addLast(playerPredicate);
 
-			return CODEC.decode(JsonOps.INSTANCE, data)
-				.getOrThrow(false, e -> {
-					throw new JsonSyntaxException("Unable to deserialize criterion instance: " + e);
-				})
-				.getFirst();
-		} finally {
-			stack.get().removeLast();
-		}
-	}
+            return CODEC.decode(JsonOps.INSTANCE, data)
+                .getOrThrow(false, e -> {
+                    throw new JsonSyntaxException("Unable to deserialize criterion instance: " + e);
+                })
+                .getFirst();
+        } finally {
+            stack.get().removeLast();
+        }
+    }
 
-	public void trigger(ServerPlayer player, BlockPos block) {
-		this.trigger(player, it ->
-			it.breakingBlock.matches((ServerLevel) player.level(), block) && (
-				it.holdingItem.matches(player.getItemInHand(InteractionHand.MAIN_HAND)) ||
-					it.allowOffhand && it.holdingItem.matches(player.getItemInHand(InteractionHand.OFF_HAND))
-			));
-	}
+    public void trigger(ServerPlayer player, BlockPos block) {
+        this.trigger(player, it ->
+            it.breakingBlock.matches((ServerLevel) player.level(), block) && (
+                it.holdingItem.matches(player.getItemInHand(InteractionHand.MAIN_HAND)) ||
+                    it.allowOffhand && it.holdingItem.matches(player.getItemInHand(InteractionHand.OFF_HAND))
+            ));
+    }
 
-	@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
-	public static final class Instance extends AbstractCriterionTriggerInstance {
-		public final BlockPredicate breakingBlock;
-		public final ItemPredicate holdingItem;
-		public final boolean allowOffhand;
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    public static final class Instance extends AbstractCriterionTriggerInstance {
+        public final BlockPredicate breakingBlock;
+        public final ItemPredicate holdingItem;
+        public final boolean allowOffhand;
 
-		public Instance(Optional<ContextAwarePredicate> playerPredicate, BlockPredicate breakingBlock, ItemPredicate holdingItem, boolean allowOffhand) {
-			super(playerPredicate);
-			this.breakingBlock = breakingBlock;
-			this.holdingItem = holdingItem;
-			this.allowOffhand = allowOffhand;
-		}
+        public Instance(Optional<ContextAwarePredicate> playerPredicate, BlockPredicate breakingBlock, ItemPredicate holdingItem, boolean allowOffhand) {
+            super(playerPredicate);
+            this.breakingBlock = breakingBlock;
+            this.holdingItem = holdingItem;
+            this.allowOffhand = allowOffhand;
+        }
 
-		public Instance(BlockPredicate breakingBlock, ItemPredicate holdingItem) {
-			this(Optional.empty(), breakingBlock, holdingItem, false);
-		}
+        public Instance(BlockPredicate breakingBlock, ItemPredicate holdingItem) {
+            this(Optional.empty(), breakingBlock, holdingItem, false);
+        }
 
-		@Override
-		public JsonObject serializeToJson() {
-			return (JsonObject) CODEC
-				.encodeStart(JsonOps.INSTANCE, this)
-				.getOrThrow(false, e -> {
-					throw new JsonSyntaxException("Unable to serialize criterion instance: " + e);
-				});
-		}
-	}
+        @Override
+        public JsonObject serializeToJson() {
+            return (JsonObject) CODEC
+                .encodeStart(JsonOps.INSTANCE, this)
+                .getOrThrow(false, e -> {
+                    throw new JsonSyntaxException("Unable to serialize criterion instance: " + e);
+                });
+        }
+    }
 }
--- src/test/java/net/minecraftforge/debug/gameplay/criterion/CriterionTest.java
+++ src/test/java/net/minecraftforge/debug/gameplay/criterion/CriterionTest.java
@@ -51,120 +51,120 @@ import java.util.Objects;
 @MethodsReturnNonnullByDefault
 @GameTestHolder("forge.custom_criterions")
 public final class CriterionTest extends BaseTestMod {
-	public static final String MOD_ID = "criterion_test";
-	public static final BreakWithItemCriterion CRITERION = new BreakWithItemCriterion();
-	public static final ResourceLocation CRITERION_ID = new ResourceLocation(MOD_ID, "criterion");
-	public static final String TEST_CRITERION_ID = "break_glass_with_fish";
-	public static final ResourceLocation TEST_ADVANCEMENT_ID = new ResourceLocation(MOD_ID, TEST_CRITERION_ID);
+    public static final String MOD_ID = "criterion_test";
+    public static final BreakWithItemCriterion CRITERION = new BreakWithItemCriterion();
+    public static final ResourceLocation CRITERION_ID = new ResourceLocation(MOD_ID, "criterion");
+    public static final String TEST_CRITERION_ID = "break_glass_with_fish";
+    public static final ResourceLocation TEST_ADVANCEMENT_ID = new ResourceLocation(MOD_ID, TEST_CRITERION_ID);
 
-	static {
-		// unsafe!
-		CriteriaTriggers.register(CRITERION_ID.toString(), CRITERION);
-	}
+    static {
+        // unsafe!
+        CriteriaTriggers.register(CRITERION_ID.toString(), CRITERION);
+    }
 
-	// doesn't work in datagen
-	/*
-	@SubscribeEvent
-	public void commonSetup(final FMLCommonSetupEvent event) {
-		event.enqueueWork(() -> {
-			CriteriaTriggers.register(CRITERION_ID.toString(), CRITERION);
-		});
-	}
-	*/
+    // doesn't work in datagen
+    /*
+    @SubscribeEvent
+    public void commonSetup(final FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+            CriteriaTriggers.register(CRITERION_ID.toString(), CRITERION);
+        });
+    }
+    */
 
-	public CriterionTest() {
-		super();
-		MinecraftForge.EVENT_BUS.addListener(this::onBlockBreak);
-	}
+    public CriterionTest() {
+        super();
+        MinecraftForge.EVENT_BUS.addListener(this::onBlockBreak);
+    }
 
-	@GameTest(template = "forge:empty3x3x3")
-	public static void test_custom_criterion(GameTestHelper helper) {
-		var advancement = helper.getLevel().getServer().getAdvancements().get(TEST_ADVANCEMENT_ID);
+    @GameTest(template = "forge:empty3x3x3")
+    public static void test_custom_criterion(GameTestHelper helper) {
+        var advancement = helper.getLevel().getServer().getAdvancements().get(TEST_ADVANCEMENT_ID);
 
-		if (advancement == null)
-			throw new GameTestAssertException("No advancement is present");
+        if (advancement == null)
+            throw new GameTestAssertException("No advancement is present");
 
-		var center = new BlockPos(1, 1, 1);
-		helper.setBlock(center, Blocks.GLASS);
-		helper.assertBlock(center, block -> block == Blocks.GLASS, "Failed to set glass block");
+        var center = new BlockPos(1, 1, 1);
+        helper.setBlock(center, Blocks.GLASS);
+        helper.assertBlock(center, block -> block == Blocks.GLASS, "Failed to set glass block");
 
-		var player = helper.makeMockServerPlayer();
-		player.setItemSlot(EquipmentSlot.MAINHAND, new ItemStack(Items.DIAMOND_SWORD));
-		player.gameMode.destroyBlock(helper.absolutePos(center));
+        var player = helper.makeMockServerPlayer();
+        player.setItemSlot(EquipmentSlot.MAINHAND, new ItemStack(Items.DIAMOND_SWORD));
+        player.gameMode.destroyBlock(helper.absolutePos(center));
 
-		helper.assertFalse(player.getAdvancements().revoke(advancement, TEST_CRITERION_ID), "Advancement was granted to player in invalid conditions (breaking glass with diamond sword)");
+        helper.assertFalse(player.getAdvancements().revoke(advancement, TEST_CRITERION_ID), "Advancement was granted to player in invalid conditions (breaking glass with diamond sword)");
 
-		helper.setBlock(center, Blocks.DIRT);
-		helper.assertBlock(center, block -> block == Blocks.DIRT, "Failed to set dirt block");
-		player.gameMode.destroyBlock(helper.absolutePos(center));
+        helper.setBlock(center, Blocks.DIRT);
+        helper.assertBlock(center, block -> block == Blocks.DIRT, "Failed to set dirt block");
+        player.gameMode.destroyBlock(helper.absolutePos(center));
 
-		helper.assertFalse(player.getAdvancements().revoke(advancement, TEST_CRITERION_ID), "Advancement was granted to player in invalid conditions (breaking dirt with diamond sword)");
+        helper.assertFalse(player.getAdvancements().revoke(advancement, TEST_CRITERION_ID), "Advancement was granted to player in invalid conditions (breaking dirt with diamond sword)");
 
-		player.setItemSlot(EquipmentSlot.MAINHAND, new ItemStack(Items.COD));
+        player.setItemSlot(EquipmentSlot.MAINHAND, new ItemStack(Items.COD));
 
-		helper.setBlock(center, Blocks.DIRT);
-		helper.assertBlock(center, block -> block == Blocks.DIRT, "Failed to set dirt block");
-		player.gameMode.destroyBlock(helper.absolutePos(center));
+        helper.setBlock(center, Blocks.DIRT);
+        helper.assertBlock(center, block -> block == Blocks.DIRT, "Failed to set dirt block");
+        player.gameMode.destroyBlock(helper.absolutePos(center));
 
-		helper.assertFalse(player.getAdvancements().revoke(advancement, TEST_CRITERION_ID), "Advancement was granted to player in invalid conditions (breaking dirt with cod)");
+        helper.assertFalse(player.getAdvancements().revoke(advancement, TEST_CRITERION_ID), "Advancement was granted to player in invalid conditions (breaking dirt with cod)");
 
-		helper.setBlock(center, Blocks.GLASS);
-		helper.assertBlock(center, block -> block == Blocks.GLASS, "Failed to set glass block");
-		player.gameMode.destroyBlock(helper.absolutePos(center));
+        helper.setBlock(center, Blocks.GLASS);
+        helper.assertBlock(center, block -> block == Blocks.GLASS, "Failed to set glass block");
+        player.gameMode.destroyBlock(helper.absolutePos(center));
 
-		helper.assertTrue(player.getAdvancements().revoke(advancement, TEST_CRITERION_ID), "Advancement was not granted to player in correct conditions (breaking glass with cod)");
+        helper.assertTrue(player.getAdvancements().revoke(advancement, TEST_CRITERION_ID), "Advancement was not granted to player in correct conditions (breaking glass with cod)");
 
-		helper.succeed();
-	}
+        helper.succeed();
+    }
 
-	public void onBlockBreak(BlockEvent.BreakEvent event) {
-		if (event.getPlayer() instanceof ServerPlayer player) {
-			CRITERION.trigger(player, event.getPos());
-		}
-	}
+    public void onBlockBreak(BlockEvent.BreakEvent event) {
+        if (event.getPlayer() instanceof ServerPlayer player) {
+            CRITERION.trigger(player, event.getPos());
+        }
+    }
 
-	@SubscribeEvent
-	public void onGatherData(GatherDataEvent event) {
-		var tag = TagKey.create(Registries.ITEM, new ResourceLocation(MOD_ID, "fish"));
+    @SubscribeEvent
+    public void onGatherData(GatherDataEvent event) {
+        var tag = TagKey.create(Registries.ITEM, new ResourceLocation(MOD_ID, "fish"));
 
-		event.getGenerator().addProvider(true, new TagsProvider<Item>(event.getGenerator().getPackOutput(), Registries.ITEM, event.getLookupProvider(), MOD_ID, event.getExistingFileHelper()) {
-			@Override
-			protected void addTags(HolderLookup.Provider lookup) {
-				this.tag(tag)
-					.add(TagEntry.element(Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(Items.COD))))
-					.add(TagEntry.element(Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(Items.SALMON))))
-					.add(TagEntry.element(Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(Items.TROPICAL_FISH))))
-					.add(TagEntry.element(Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(Items.PUFFERFISH))))
-					.add(TagEntry.element(Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(Items.COOKED_COD))))
-					.add(TagEntry.element(Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(Items.COOKED_SALMON))))
-				;
-			}
-		});
+        event.getGenerator().addProvider(true, new TagsProvider<Item>(event.getGenerator().getPackOutput(), Registries.ITEM, event.getLookupProvider(), MOD_ID, event.getExistingFileHelper()) {
+            @Override
+            protected void addTags(HolderLookup.Provider lookup) {
+                this.tag(tag)
+                    .add(TagEntry.element(Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(Items.COD))))
+                    .add(TagEntry.element(Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(Items.SALMON))))
+                    .add(TagEntry.element(Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(Items.TROPICAL_FISH))))
+                    .add(TagEntry.element(Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(Items.PUFFERFISH))))
+                    .add(TagEntry.element(Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(Items.COOKED_COD))))
+                    .add(TagEntry.element(Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(Items.COOKED_SALMON))))
+                ;
+            }
+        });
 
-		event.getGenerator().addProvider(true,
-			new ForgeAdvancementProvider(
-				event.getGenerator().getPackOutput(),
-				event.getLookupProvider(),
-				event.getExistingFileHelper(),
-				List.of(((registries, saver, existingFileHelper) -> {
-					//noinspection removal
-					saver.accept(new Advancement.Builder()
-							.display(new DisplayInfo(
-								new ItemStack(Items.COD),
-								Component.literal("Fish vs Glass"),
-								Component.literal("Fish wins!"),
-								null,
-								FrameType.TASK,
-								true, true, false
-							))
-							.parent(new ResourceLocation("minecraft", "story/root"))
-							.requirements(AdvancementRequirements.Strategy.AND)
-							.addCriterion(TEST_CRITERION_ID, new Criterion<>(CRITERION,
-								new BreakWithItemCriterion.Instance(
-									BlockPredicate.Builder.block().of(Tags.Blocks.GLASS).build(),
-									ItemPredicate.Builder.item().of(tag).build()
-								)))
-							.build(TEST_ADVANCEMENT_ID));
-				}))));
-	}
+        event.getGenerator().addProvider(true,
+            new ForgeAdvancementProvider(
+                event.getGenerator().getPackOutput(),
+                event.getLookupProvider(),
+                event.getExistingFileHelper(),
+                List.of(((registries, saver, existingFileHelper) -> {
+                    //noinspection removal
+                    saver.accept(new Advancement.Builder()
+                            .display(new DisplayInfo(
+                                new ItemStack(Items.COD),
+                                Component.literal("Fish vs Glass"),
+                                Component.literal("Fish wins!"),
+                                null,
+                                FrameType.TASK,
+                                true, true, false
+                            ))
+                            .parent(new ResourceLocation("minecraft", "story/root"))
+                            .requirements(AdvancementRequirements.Strategy.AND)
+                            .addCriterion(TEST_CRITERION_ID, new Criterion<>(CRITERION,
+                                new BreakWithItemCriterion.Instance(
+                                    BlockPredicate.Builder.block().of(Tags.Blocks.GLASS).build(),
+                                    ItemPredicate.Builder.item().of(tag).build()
+                                )))
+                            .build(TEST_ADVANCEMENT_ID));
+                }))));
+    }
 }
