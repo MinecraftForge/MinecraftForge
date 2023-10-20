18317141487624c47a391151fcf53f2414dc5bc1
SrRapero720 <SrRapero720@hotmail.com>
2023-10-20 12:59:48 -0600

    correcting code formatting and added docs

--- src/main/java/net/minecraftforge/client/event/PauseGameEvent.java
+++ src/main/java/net/minecraftforge/client/event/PauseGameEvent.java
@@ -1,14 +1,34 @@
 package net.minecraftforge.client.event;
 
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.Cancelable;
 import net.minecraftforge.eventbus.api.Event;
 
-public class PauseGameEvent extends Event {
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.fml.LogicalSide;
+
+/**
+ * Fired when {@linkplain Minecraft#pause Minecraft.pause} value got updated by the {@linkplain Minecraft#runTick(boolean) Minecraft.runTick(...)}
+ *
+ * <p>This event is not {@linkplain Cancelable cancellable}, and does not {@linkplain HasResult have a result}.</p>
+ *
+ * <p>This event is fired on the {@linkplain MinecraftForge#EVENT_BUS main Forge event bus},
+ * only on the {@linkplain LogicalSide#CLIENT logical client}.</p>
+ */
+public class PauseGameEvent extends Event
+{
     private final boolean paused;
-    public PauseGameEvent(boolean isPaused) {
+
+    public PauseGameEvent(boolean isPaused)
+    {
         this.paused = isPaused;
     }
 
-    public boolean isPaused() {
+    /**
+     * {@return game is paused}
+     */
+    public boolean isPaused()
+    {
         return paused;
     }
 }
