94e180d9e7c4e497fdc03450f7a9eac4c7e5c19e
Paint_Ninja <PaintNinja@users.noreply.github.com>
2023-11-02 18:07:25 +0000

    Improve mod description formatting in mods screen
    Backport of #9768
    
    Co-Authored-By: Su5eD <su5ed@outlook.com>

--- fmlloader/src/main/java/net/minecraftforge/fml/loading/moddiscovery/ModInfo.java
+++ fmlloader/src/main/java/net/minecraftforge/fml/loading/moddiscovery/ModInfo.java
@@ -69,7 +69,9 @@ public class ModInfo implements IModInfo, IConfigurable
                 .map(s -> StringSubstitutor.replace(s, ownFile.map(ModFileInfo::getFile).orElse(null)))
                 .map(DefaultArtifactVersion::new).orElse(DEFAULT_VERSION);
         this.displayName = config.<String>getConfigElement("displayName").orElse(this.modId);
-        this.description = config.<String>getConfigElement("description").orElse("MISSING DESCRIPTION");
+        this.description = config.<String>getConfigElement("description")
+                .orElse("MISSING DESCRIPTION")
+                .replace("\r\n", "\n").stripIndent();
 
         this.logoFile = Optional.ofNullable(config.<String>getConfigElement("logoFile")
                 .orElseGet(() -> ownFile.flatMap(mf -> mf.<String>getConfigElement("logoFile")).orElse(null)));
@@ -196,7 +198,7 @@ public class ModInfo implements IModInfo, IConfigurable
         private final boolean mandatory;
         private final Ordering ordering;
         private final DependencySide side;
-        private Optional<URL> referralUrl;
+        private final Optional<URL> referralUrl;
 
         public ModVersion(final IModInfo owner, final IConfigurable config) {
             this.owner = owner;
