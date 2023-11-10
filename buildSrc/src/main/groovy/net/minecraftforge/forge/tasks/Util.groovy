87ff99d9219bd65a456b7a75523e44a8f3d0dbb7
LexManos <LexManos@gmail.com>
2023-11-09 20:26:11 -0800

    Tabs

--- buildSrc/src/main/groovy/net/minecraftforge/forge/tasks/Util.groovy
+++ buildSrc/src/main/groovy/net/minecraftforge/forge/tasks/Util.groovy
@@ -16,33 +16,33 @@ import java.util.zip.ZipInputStream
 public class Util {
     static final ASM_LEVEL = Opcodes.ASM9
 
-	static void init() {
-		File.metaClass.sha1 = { ->
-			MessageDigest md = MessageDigest.getInstance('SHA-1')
-			delegate.eachByte 4096, {bytes, size ->
-				md.update(bytes, 0, size)
-			}
-			return md.digest().collect {String.format "%02x", it}.join()
-		}
+    static void init() {
+        File.metaClass.sha1 = { ->
+            MessageDigest md = MessageDigest.getInstance('SHA-1')
+            delegate.eachByte 4096, {bytes, size ->
+                md.update(bytes, 0, size)
+            }
+            return md.digest().collect {String.format "%02x", it}.join()
+        }
         File.metaClass.getSha1 = { !delegate.exists() ? null : delegate.sha1() }
-		File.metaClass.sha256 = { ->
-			MessageDigest md = MessageDigest.getInstance('SHA-256')
-			delegate.eachByte 4096, {bytes, size ->
-				md.update(bytes, 0, size)
-			}
-			return md.digest().collect {String.format "%02x", it}.join()
-		}
+        File.metaClass.sha256 = { ->
+            MessageDigest md = MessageDigest.getInstance('SHA-256')
+            delegate.eachByte 4096, {bytes, size ->
+                md.update(bytes, 0, size)
+            }
+            return md.digest().collect {String.format "%02x", it}.join()
+        }
         File.metaClass.getSha256 = { !delegate.exists() ? null : delegate.sha256() }
 
-		File.metaClass.json = { -> new JsonSlurper().parseText(delegate.text) }
+        File.metaClass.json = { -> new JsonSlurper().parseText(delegate.text) }
         File.metaClass.getJson = { return delegate.exists() ? new JsonSlurper().parse(delegate) : [:] }
         File.metaClass.setJson = { json -> delegate.text = new JsonBuilder(json).toPrettyString() }
 
-		Date.metaClass.iso8601 = { ->
-			def format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
-			def result = format.format(delegate)
-			return result[0..21] + ':' + result[22..-1]
-		}
+        Date.metaClass.iso8601 = { ->
+            def format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+            def result = format.format(delegate)
+            return result[0..21] + ':' + result[22..-1]
+        }
 
         String.metaClass.rsplit = { String del, int limit = -1 ->
             def lst = new ArrayList()
@@ -55,40 +55,40 @@ public class Util {
             lst.add(0, tmp)
             return lst
         }
-	}
+    }
 
-	public static String[] getClasspath(project, libs, artifact) {
-		def ret = []
-		artifactTree(project, artifact).each { key, lib ->
-			libs[lib.name] = lib
-			if (lib.name != artifact)
-				ret.add(lib.name)
-		}
-		return ret
-	}
+    public static String[] getClasspath(project, libs, artifact) {
+        def ret = []
+        artifactTree(project, artifact).each { key, lib ->
+            libs[lib.name] = lib
+            if (lib.name != artifact)
+                ret.add(lib.name)
+        }
+        return ret
+    }
 
-	public static def getArtifacts(project, config) {
-		def ret = [:]
-		config.resolvedConfiguration.resolvedArtifacts.each { dep ->
+    public static def getArtifacts(project, config) {
+        def ret = [:]
+        config.resolvedConfiguration.resolvedArtifacts.each { dep ->
             def info = getMavenInfoFromDep(dep)
-			def url = "https://libraries.minecraft.net/$info.path"
-			if (!checkExists(url))
-				url = "https://maven.minecraftforge.net/$info.path"
+            def url = "https://libraries.minecraft.net/$info.path"
+            if (!checkExists(url))
+                url = "https://maven.minecraftforge.net/$info.path"
                 
-			ret[info.key] = [
-				name: info.name,
-				downloads: [
-					artifact: [
-						path: info.path,
-						url: url,
-						sha1: dep.file.sha1(),
-						size: dep.file.length()
-					]
-				]
-			]
-		}
-		return ret
-	}
+            ret[info.key] = [
+                name: info.name,
+                downloads: [
+                    artifact: [
+                        path: info.path,
+                        url: url,
+                        sha1: dep.file.sha1(),
+                        size: dep.file.length()
+                    ]
+                ]
+            ]
+        }
+        return ret
+    }
     
     public static def getMavenInfoFromDep(dep) {
         return getMavenInfoFromMap([
@@ -131,41 +131,41 @@ public class Util {
         ]
     }
 
-	public static def iso8601Now() { new Date().iso8601() }
+    public static def iso8601Now() { new Date().iso8601() }
 
-	public static def sha1(file) {
-		MessageDigest md = MessageDigest.getInstance('SHA-1')
-		file.eachByte 4096, {bytes, size ->
-			md.update(bytes, 0, size)
-		}
-		return md.digest().collect {String.format "%02x", it}.join()
-	}
+    public static def sha1(file) {
+        MessageDigest md = MessageDigest.getInstance('SHA-1')
+        file.eachByte 4096, {bytes, size ->
+            md.update(bytes, 0, size)
+        }
+        return md.digest().collect {String.format "%02x", it}.join()
+    }
 
-	private static def artifactTree(project, artifact, transitive = true) {
-		if (!project.ext.has('tree_resolver'))
-			project.ext.tree_resolver = 1
-		def cfg = project.configurations.create('tree_resolver_' + project.ext.tree_resolver++)
+    private static def artifactTree(project, artifact, transitive = true) {
+        if (!project.ext.has('tree_resolver'))
+            project.ext.tree_resolver = 1
+        def cfg = project.configurations.create('tree_resolver_' + project.ext.tree_resolver++)
         cfg.transitive = transitive
-		def dep = project.dependencies.create(artifact)
-		cfg.dependencies.add(dep)
-		def files = cfg.resolve()
-		return getArtifacts(project, cfg)
-	}
+        def dep = project.dependencies.create(artifact)
+        cfg.dependencies.add(dep)
+        def files = cfg.resolve()
+        return getArtifacts(project, cfg)
+    }
 
-	static boolean checkExists(url) {
-		try {
-			def code = new URL(url).openConnection().with {
-				requestMethod = 'HEAD'
-				connect()
-				responseCode
-			}
-			return code == 200
-		} catch (Exception e) {
-			if (e.toString().contains('unable to find valid certification path to requested target'))
-				throw new RuntimeException('Failed to connect to ' + url + ': Missing certificate root authority, try updating java')
-			throw e
-		}
-	}
+    static boolean checkExists(url) {
+        try {
+            def code = new URL(url).openConnection().with {
+                requestMethod = 'HEAD'
+                connect()
+                responseCode
+            }
+            return code == 200
+        } catch (Exception e) {
+            if (e.toString().contains('unable to find valid certification path to requested target'))
+                throw new RuntimeException('Failed to connect to ' + url + ': Missing certificate root authority, try updating java')
+            throw e
+        }
+    }
 
     static String getLatestForgeVersion(mcVersion) {
         final json = new JsonSlurper().parseText(new URL('https://files.minecraftforge.net/net/minecraftforge/forge/promotions_slim.json').getText('UTF-8'))
