b33e168969be6360bc6b962e31bcc62c5e54fe4c
LexManos <LexManos@gmail.com>
2023-11-28 16:16:00 -0800

    Fix KeyModifiers not being properly taken into account. Closes #9806

--- src/main/java/net/minecraftforge/client/settings/KeyModifier.java
+++ src/main/java/net/minecraftforge/client/settings/KeyModifier.java
@@ -18,134 +18,102 @@ import org.lwjgl.glfw.GLFW;
 public enum KeyModifier {
     CONTROL {
         @Override
-        public boolean matches(InputConstants.Key key)
-        {
+        public boolean matches(InputConstants.Key key) {
             int keyCode = key.getValue();
             if (Minecraft.ON_OSX)
-            {
                 return keyCode == GLFW.GLFW_KEY_LEFT_SUPER || keyCode == GLFW.GLFW_KEY_RIGHT_SUPER;
-            }
             else
-            {
                 return keyCode == GLFW.GLFW_KEY_LEFT_CONTROL || keyCode == GLFW.GLFW_KEY_RIGHT_CONTROL;
-            }
         }
 
         @Override
-        public boolean isActive(@Nullable IKeyConflictContext conflictContext)
-        {
+        public boolean isActive(@Nullable IKeyConflictContext conflictContext) {
             return Screen.hasControlDown();
         }
 
         @Override
-        public Component getCombinedName(InputConstants.Key key, Supplier<Component> defaultLogic)
-        {
+        public Component getCombinedName(InputConstants.Key key, Supplier<Component> defaultLogic) {
             String localizationFormatKey = Minecraft.ON_OSX ? "forge.controlsgui.control.mac" : "forge.controlsgui.control";
             return Component.translatable(localizationFormatKey, defaultLogic.get());
         }
     },
     SHIFT {
         @Override
-        public boolean matches(InputConstants.Key key)
-        {
+        public boolean matches(InputConstants.Key key) {
             return key.getValue() == GLFW.GLFW_KEY_LEFT_SHIFT || key.getValue() == GLFW.GLFW_KEY_RIGHT_SHIFT;
         }
 
         @Override
-        public boolean isActive(@Nullable IKeyConflictContext conflictContext)
-        {
+        public boolean isActive(@Nullable IKeyConflictContext conflictContext) {
             return Screen.hasShiftDown();
         }
 
         @Override
-        public Component getCombinedName(InputConstants.Key key, Supplier<Component> defaultLogic)
-        {
+        public Component getCombinedName(InputConstants.Key key, Supplier<Component> defaultLogic) {
             return Component.translatable("forge.controlsgui.shift", defaultLogic.get());
         }
     },
     ALT {
         @Override
-        public boolean matches(InputConstants.Key key)
-        {
+        public boolean matches(InputConstants.Key key) {
             return key.getValue() == GLFW.GLFW_KEY_LEFT_ALT || key.getValue() == GLFW.GLFW_KEY_RIGHT_ALT;
         }
 
         @Override
-        public boolean isActive(@Nullable IKeyConflictContext conflictContext)
-        {
+        public boolean isActive(@Nullable IKeyConflictContext conflictContext) {
             return Screen.hasAltDown();
         }
 
         @Override
-        public Component getCombinedName(InputConstants.Key keyCode, Supplier<Component> defaultLogic)
-        {
+        public Component getCombinedName(InputConstants.Key keyCode, Supplier<Component> defaultLogic) {
             return Component.translatable("forge.controlsgui.alt", defaultLogic.get());
         }
     },
     NONE {
         @Override
-        public boolean matches(InputConstants.Key key)
-        {
+        public boolean matches(InputConstants.Key key) {
             return false;
         }
 
         @Override
-        public boolean isActive(@Nullable IKeyConflictContext conflictContext)
-        {
-            if (conflictContext != null && !conflictContext.conflicts(KeyConflictContext.IN_GAME))
-            {
-                for (KeyModifier keyModifier : MODIFIER_VALUES)
-                {
+        public boolean isActive(@Nullable IKeyConflictContext conflictContext) {
+            if (conflictContext != null && !conflictContext.conflicts(KeyConflictContext.IN_GAME)) {
+                for (KeyModifier keyModifier : MODIFIER_VALUES) {
                     if (keyModifier.isActive(conflictContext))
-                    {
                         return false;
-                    }
                 }
             }
             return true;
         }
 
         @Override
-        public Component getCombinedName(InputConstants.Key key, Supplier<Component> defaultLogic)
-        {
+        public Component getCombinedName(InputConstants.Key key, Supplier<Component> defaultLogic) {
             return defaultLogic.get();
         }
     };
 
     public static final KeyModifier[] MODIFIER_VALUES = {SHIFT, CONTROL, ALT};
 
-    public static KeyModifier getActiveModifier()
-    {
-        for (KeyModifier keyModifier : MODIFIER_VALUES)
-        {
+    public static KeyModifier getActiveModifier() {
+        for (KeyModifier keyModifier : MODIFIER_VALUES) {
             if (keyModifier.isActive(null))
-            {
                 return keyModifier;
-            }
         }
         return NONE;
     }
 
-    public static boolean isKeyCodeModifier(InputConstants.Key key)
-    {
-        for (KeyModifier keyModifier : MODIFIER_VALUES)
-        {
+    public static boolean isKeyCodeModifier(InputConstants.Key key) {
+        for (KeyModifier keyModifier : MODIFIER_VALUES) {
             if (keyModifier.matches(key))
-            {
                 return true;
-            }
         }
         return false;
     }
 
-    public static KeyModifier valueFromString(String stringValue)
-    {
-        try
-        {
+    public static KeyModifier valueFromString(String stringValue) {
+        try {
             return valueOf(stringValue);
-        }
-        catch (NullPointerException | IllegalArgumentException ignored)
-        {
+        } catch (NullPointerException | IllegalArgumentException ignored) {
             return NONE;
         }
     }
