f63e39fe5278ea5415642ec760c2e0f3ae571b9b
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-12-17 20:41:27 -0500

    Fix license check errors

--- src/test/java/net/minecraftforge/debug/client/AudioStreamTest.java
+++ src/test/java/net/minecraftforge/debug/client/AudioStreamTest.java
@@ -1,7 +1,7 @@
- /*
-  * Copyright (c) Forge Development LLC and contributors
-  * SPDX-License-Identifier: LGPL-2.1-only
-  */
+/*
+ * Copyright (c) Forge Development LLC and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
 
 package net.minecraftforge.debug.client;
 
