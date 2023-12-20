93df495864e15a3c2494eb118a1692725b231a80
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-12-20 10:20:06 -0500

    Rename TransformationMatrix methods to prevent reobf, add back vanilla name via coremod

--- src/main/resources/coremods/add_bouncer_method.js
+++ src/main/resources/coremods/add_bouncer_method.js
@@ -8,6 +8,8 @@ var InsnNode = Java.type('org.objectweb.asm.tree.InsnNode')
 function initializeCoreMod() {
     return {
         "getLanguage_bouncer": addBouncer("net.minecraft.network.play.client.CClientSettingsPacket", "func_149524_c", "getLanguage", "()Ljava/lang/String;"),
+        "compose_bouncer": addRedirect("net.minecraft.util.math.vector.TransformationMatrix", "func_227985_a_", "composeVanilla", "(Lnet/minecraft/util/math/vector/TransformationMatrix;)Lnet/minecraft/util/math/vector/TransformationMatrix;", 2),
+        "inverse_bouncer": addRedirect("net.minecraft.util.math.vector.TransformationMatrix", "func_227987_b_", "inverseVanilla", "()Lnet/minecraft/util/math/vector/TransformationMatrix;", 1),
     }
 }
 
@@ -34,6 +36,37 @@ function addBouncer(className, conflictedName, expectedName, descriptor, signatu
 
             method.instructions.add(new VarInsnNode(Opcodes.ALOAD, 0)); /*this*/
             method.instructions.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL, className.replaceAll("\\.","/"), mappedName, descriptor));
+            method.instructions.add(new InsnNode(Opcodes.ARETURN));
+
+            node.methods.add(method);
+
+            return node;
+       }
+   }
+}
+
+// Generate a method with name oldName that redirects to expectedName. oldName is remapped, expectedName isn't.
+function addRedirect(className, oldName, expectedName, descriptor, numArgs) {
+    return {
+       'target': {
+           'type': 'CLASS',
+           'name': className
+       },
+       'transformer': function(node) {
+            var mappedName = ASMAPI.mapMethod(oldName);
+
+            var method = new MethodNode(
+                /* access = */ Opcodes.ACC_PUBLIC,
+                /* name = */ mappedName,
+                /* descriptor = */ descriptor,
+                /* signature = */ descriptor,
+                /* exceptions = */ null
+            );
+
+            for(var i = 0; i < numArgs; i++) {
+                 method.instructions.add(new VarInsnNode(Opcodes.ALOAD, i));
+            }
+            method.instructions.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL, className.replaceAll("\\.","/"), expectedName, descriptor));
             method.instructions.add(new InsnNode(Opcodes.ARETURN));
 
             node.methods.add(method);
