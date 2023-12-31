1515adf0262a67e176a1f0100d7c066558e5d03e
Paint_Ninja <PaintNinja@users.noreply.github.com>
2023-12-31 16:03:59 +0000

    Make common DisplayTest registration tasks easier (#9839)
    Backport of #9822 to 1.18.2

--- fmlcore/src/main/java/net/minecraftforge/fml/ModContainer.java
+++ fmlcore/src/main/java/net/minecraftforge/fml/ModContainer.java
@@ -20,6 +20,7 @@ import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -63,9 +64,9 @@ public abstract class ModContainer
                     () -> new IExtensionPoint.DisplayTest(() -> this.modInfo.getVersion().toString(),
                             (incoming, isNetwork) -> Objects.equals(incoming, this.modInfo.getVersion().toString()));
             case "IGNORE_SERVER_VERSION" -> // Ignores any version information coming from the server - use for server only mods
-                    () -> new IExtensionPoint.DisplayTest(() -> IExtensionPoint.DisplayTest.IGNORESERVERONLY, (incoming, isNetwork) -> true);
+                    IExtensionPoint.DisplayTest.IGNORE_SERVER_VERSION;
             case "IGNORE_ALL_VERSION" -> // Ignores all information and provides no information
-                    () -> new IExtensionPoint.DisplayTest(() -> "", (incoming, isNetwork) -> true);
+                    IExtensionPoint.DisplayTest.IGNORE_ALL_VERSION;
             case "NONE" -> null; // NO display test at all - use this if you're going to do your own display test
             default -> // any other value throws an exception
                     throw new IllegalArgumentException("Invalid displayTest value supplied in mods.toml");
@@ -140,6 +141,22 @@ public abstract class ModContainer
     public <T extends Record & IExtensionPoint<T>> void registerExtensionPoint(Class<? extends IExtensionPoint<T>> point, Supplier<T> extension)
     {
         extensionPoints.put(point, extension);
+    }
+
+    public void registerDisplayTest(IExtensionPoint.DisplayTest displayTest) {
+        registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> displayTest);
+    }
+
+    public void registerDisplayTest(Supplier<IExtensionPoint.DisplayTest> displayTest) {
+        registerExtensionPoint(IExtensionPoint.DisplayTest.class, displayTest);
+    }
+
+    public void registerDisplayTest(String version, BiPredicate<String, Boolean> remoteVersionTest) {
+        registerDisplayTest(new IExtensionPoint.DisplayTest(version, remoteVersionTest));
+    }
+
+    public void registerDisplayTest(Supplier<String> suppliedVersion, BiPredicate<String, Boolean> remoteVersionTest) {
+        registerDisplayTest(new IExtensionPoint.DisplayTest(suppliedVersion, remoteVersionTest));
     }
 
     public void addConfig(final ModConfig modConfig) {
