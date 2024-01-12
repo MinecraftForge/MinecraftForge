875c312362b39e6f578bbcc602668348595126b8
Paint_Ninja <PaintNinja@users.noreply.github.com>
2024-01-12 16:10:06 +0000

    Fix accidental hardcoding in run.sh

--- server_files/run.sh
+++ server_files/run.sh
@@ -4,4 +4,4 @@
 java -jar @SHIM_JAR_FILE@ --onlyCheckJava || exit 1
 
 # Add custom program arguments (such as nogui) to the next line before the "$@" or pass them to this script directly
-java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.2-pre3-43.1.329-1.20.2-dev/unix_args.txt "$@"
+java @user_jvm_args.txt @libraries/@MAVEN_PATH@/unix_args.txt "$@"
