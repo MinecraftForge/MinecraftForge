25727a0e9341734b6ba589c0060987bdd1b71a25
LexManos <LexManos@gmail.com>
2024-01-29 12:16:21 -0800

    Fix Criterion Test Mod

--- src/test/java/net/minecraftforge/debug/gameplay/criterion/CriterionTest.java
+++ src/test/java/net/minecraftforge/debug/gameplay/criterion/CriterionTest.java
@@ -5,17 +5,16 @@
 
 package net.minecraftforge.debug.gameplay.criterion;
 
-import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementHolder;
 import net.minecraft.advancements.AdvancementRequirements;
-import net.minecraft.advancements.CriteriaTriggers;
-import net.minecraft.advancements.Criterion;
-import net.minecraft.advancements.DisplayInfo;
-import net.minecraft.advancements.FrameType;
+import net.minecraft.advancements.AdvancementType;
+import net.minecraft.advancements.CriterionTrigger;
 import net.minecraft.advancements.critereon.BlockPredicate;
 import net.minecraft.advancements.critereon.ItemPredicate;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.tags.TagsProvider;
 import net.minecraft.gametest.framework.GameTest;
@@ -39,41 +38,24 @@ import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.gametest.GameTestHolder;
+import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.test.BaseTestMod;
 
-import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
 import java.util.Objects;
 
 @Mod(CriterionTest.MOD_ID)
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
 @GameTestHolder("forge.custom_criterions")
 public final class CriterionTest extends BaseTestMod {
     public static final String MOD_ID = "criterion_test";
-    public static final BreakWithItemCriterion CRITERION = new BreakWithItemCriterion();
-    public static final ResourceLocation CRITERION_ID = new ResourceLocation(MOD_ID, "criterion");
+    private static final DeferredRegister<CriterionTrigger<?>> TRIGGERS = DeferredRegister.create(BuiltInRegistries.TRIGGER_TYPES.key(), MOD_ID);
+    public static final RegistryObject<BreakWithItemCriterion> CRITERION = TRIGGERS.register("criterion", BreakWithItemCriterion::new);
     public static final String TEST_CRITERION_ID = "break_glass_with_fish";
     public static final ResourceLocation TEST_ADVANCEMENT_ID = new ResourceLocation(MOD_ID, TEST_CRITERION_ID);
 
-    static {
-        // unsafe!
-        CriteriaTriggers.register(CRITERION_ID.toString(), CRITERION);
-    }
-
-    // doesn't work in datagen
-    /*
-    @SubscribeEvent
-    public void commonSetup(final FMLCommonSetupEvent event) {
-        event.enqueueWork(() -> {
-            CriteriaTriggers.register(CRITERION_ID.toString(), CRITERION);
-        });
-    }
-    */
-
     public CriterionTest() {
-        super();
         MinecraftForge.EVENT_BUS.addListener(this::onBlockBreak);
     }
 
@@ -119,7 +101,7 @@ public final class CriterionTest extends BaseTestMod {
 
     public void onBlockBreak(BlockEvent.BreakEvent event) {
         if (event.getPlayer() instanceof ServerPlayer player) {
-            CRITERION.trigger(player, event.getPos());
+            CRITERION.get().trigger(player, event.getPos());
         }
     }
 
@@ -141,30 +123,32 @@ public final class CriterionTest extends BaseTestMod {
             }
         });
 
+        var STORY_ROOT = new AdvancementHolder(new ResourceLocation("minecraft", "story/root"), null);
+
         event.getGenerator().addProvider(true,
             new ForgeAdvancementProvider(
                 event.getGenerator().getPackOutput(),
                 event.getLookupProvider(),
                 event.getExistingFileHelper(),
                 List.of(((registries, saver, existingFileHelper) -> {
-                    //noinspection removal
                     saver.accept(new Advancement.Builder()
-                            .display(new DisplayInfo(
-                                new ItemStack(Items.COD),
-                                Component.literal("Fish vs Glass"),
-                                Component.literal("Fish wins!"),
-                                null,
-                                FrameType.TASK,
-                                true, true, false
-                            ))
-                            .parent(new ResourceLocation("minecraft", "story/root"))
-                            .requirements(AdvancementRequirements.Strategy.AND)
-                            .addCriterion(TEST_CRITERION_ID, new Criterion<>(CRITERION,
-                                new BreakWithItemCriterion.Instance(
-                                    BlockPredicate.Builder.block().of(Tags.Blocks.GLASS).build(),
-                                    ItemPredicate.Builder.item().of(tag).build()
-                                )))
-                            .build(TEST_ADVANCEMENT_ID));
-                }))));
+                        .display(Items.COD,
+                            Component.literal("Fish vs Glass"),
+                            Component.literal("Fish wins!"),
+                            null,
+                            AdvancementType.TASK,
+                            true, true, false
+                        )
+                        .parent(STORY_ROOT)
+                        .requirements(AdvancementRequirements.Strategy.AND)
+                        .addCriterion(TEST_CRITERION_ID, CRITERION.get().instance(
+                            BlockPredicate.Builder.block().of(Tags.Blocks.GLASS).build(),
+                            ItemPredicate.Builder.item().of(tag).build(),
+                            true
+                        ))
+                        .build(TEST_ADVANCEMENT_ID));
+                }))
+            )
+        );
     }
 }
