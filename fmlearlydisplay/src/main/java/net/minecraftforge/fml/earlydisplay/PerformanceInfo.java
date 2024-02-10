eef9ccd47be715572bc35d2c0742a6e45437127c
Paint_Ninja <PaintNinja@users.noreply.github.com>
2024-02-10 16:19:00 +0000

    Add CPU usage config option to early window

--- fmlearlydisplay/src/main/java/net/minecraftforge/fml/earlydisplay/PerformanceInfo.java
+++ fmlearlydisplay/src/main/java/net/minecraftforge/fml/earlydisplay/PerformanceInfo.java
@@ -6,6 +6,7 @@
 package net.minecraftforge.fml.earlydisplay;
 
 import com.sun.management.OperatingSystemMXBean;
+import net.minecraftforge.fml.loading.FMLConfig;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
@@ -13,19 +14,28 @@ import java.lang.management.MemoryUsage;
 
 public class PerformanceInfo {
 
+    private final boolean showCPUUsage;
     private final OperatingSystemMXBean osBean;
     private final MemoryMXBean memoryBean;
-    float memory;
+
+    private float memory;
     private String text;
 
     PerformanceInfo() {
-        osBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
+        showCPUUsage = FMLConfig.getBoolConfigValue(FMLConfig.ConfigValue.EARLY_WINDOW_SHOW_CPU);
+        osBean = showCPUUsage ? ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class) : null;
         memoryBean = ManagementFactory.getMemoryMXBean();
     }
 
     void update() {
         final MemoryUsage heapusage = memoryBean.getHeapMemoryUsage();
         memory = (float) heapusage.getUsed() / heapusage.getMax();
+
+        if (!showCPUUsage) {
+            text = String.format("Heap: %d/%d MB (%.1f%%) OffHeap: %d MB", heapusage.getUsed() >> 20, heapusage.getMax() >> 20, memory * 100.0, memoryBean.getNonHeapMemoryUsage().getUsed() >> 20);
+            return;
+        }
+
         var cpuLoad = osBean.getProcessCpuLoad();
         String cpuText;
         if (cpuLoad == -1) {
