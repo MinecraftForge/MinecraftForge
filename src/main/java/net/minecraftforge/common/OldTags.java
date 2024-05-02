a0a7796eee8eaf363bb22646ab2c0d532248e567
Paint_Ninja <PaintNinja@users.noreply.github.com>
2024-05-02 12:32:47 +0100

    Prepare for common tags work
    Format the Tags class nicely and make a copy of it for comparison, to help avoid accidentally removing Forge-specific tags and make it easier to keep track of what needs doing.

--- src/main/java/net/minecraftforge/common/OldTags.java
+++ src/main/java/net/minecraftforge/common/OldTags.java
@@ -18,7 +18,7 @@ import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.material.Fluid;
 
-public class Tags {
+public class OldTags {
     public static void init() {
         Blocks.init();
         EntityTypes.init();
@@ -404,55 +404,55 @@ public class Tags {
         /**
          * A tag containing all existing tools.
          *
-         * Note: This tag is not an alternative or a substitute to {@link net.minecraftforge.common.ToolActions}.
+         * Note: This tag is not an alternative or a substitute to {@link ToolActions}.
          *
-         * @see net.minecraftforge.common.ToolAction
-         * @see net.minecraftforge.common.ToolActions
+         * @see ToolAction
+         * @see ToolActions
          */
         public static final TagKey<Item> TOOLS = tag("tools");
         /**
          * A tag containing all existing shields.
          *
-         * Note: This tag is not an alternative or a substitute to {@link net.minecraftforge.common.ToolActions}.
+         * Note: This tag is not an alternative or a substitute to {@link ToolActions}.
          *
-         * @see net.minecraftforge.common.ToolAction
-         * @see net.minecraftforge.common.ToolActions
+         * @see ToolAction
+         * @see ToolActions
          */
         public static final TagKey<Item> TOOLS_SHIELDS = tag("tools/shields");
         /**
          * A tag containing all existing bows.
          *
-         * Note: This tag is not an alternative or a substitute to {@link net.minecraftforge.common.ToolActions}.
+         * Note: This tag is not an alternative or a substitute to {@link ToolActions}.
          *
-         * @see net.minecraftforge.common.ToolAction
-         * @see net.minecraftforge.common.ToolActions
+         * @see ToolAction
+         * @see ToolActions
          */
         public static final TagKey<Item> TOOLS_BOWS = tag("tools/bows");
         /**
          * A tag containing all existing crossbows.
          *
-         * Note: This tag is not an alternative or a substitute to {@link net.minecraftforge.common.ToolActions}.
+         * Note: This tag is not an alternative or a substitute to {@link ToolActions}.
          *
-         * @see net.minecraftforge.common.ToolAction
-         * @see net.minecraftforge.common.ToolActions
+         * @see ToolAction
+         * @see ToolActions
          */
         public static final TagKey<Item> TOOLS_CROSSBOWS = tag("tools/crossbows");
         /**
          * A tag containing all existing fishing rods.
          *
-         * Note: This tag is not an alternative or a substitute to {@link net.minecraftforge.common.ToolActions}.
+         * Note: This tag is not an alternative or a substitute to {@link ToolActions}.
          *
-         * @see net.minecraftforge.common.ToolAction
-         * @see net.minecraftforge.common.ToolActions
+         * @see ToolAction
+         * @see ToolActions
          */
         public static final TagKey<Item> TOOLS_FISHING_RODS = tag("tools/fishing_rods");
         /**
          * A tag containing all existing tridents.
          *
-         * Note: This tag is not an alternative or a substitute to {@link net.minecraftforge.common.ToolActions}.
+         * Note: This tag is not an alternative or a substitute to {@link ToolActions}.
          *
-         * @see net.minecraftforge.common.ToolAction
-         * @see net.minecraftforge.common.ToolActions
+         * @see ToolAction
+         * @see ToolActions
          */
         public static final TagKey<Item> TOOLS_TRIDENTS = tag("tools/tridents");
         /**
