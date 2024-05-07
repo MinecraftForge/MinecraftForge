8dbbd7f775b80cc7911a84f9b2cc6ffef37a8dca
LexManos <LexManos@gmail.com>
2024-05-07 11:51:28 -0700

    Add 'addMain' to SimpleFlow

--- src/main/java/net/minecraftforge/network/simple/SimpleFlow.java
+++ src/main/java/net/minecraftforge/network/simple/SimpleFlow.java
@@ -25,4 +25,17 @@ public interface SimpleFlow<BUF extends FriendlyByteBuf, BASE> extends SimplePro
      * client thread.
      */
     <MSG extends BASE> SimpleFlow<BUF, BASE> add(Class<MSG> type, StreamCodec<BUF, MSG> codec, BiConsumer<MSG, CustomPayloadEvent.Context> handler);
+
+    /**
+     * Adds a packet to this channel that has it's protocol validated whenever sent or received.
+     * <p>
+     * Unlike {@link #add(Class,StreamCodec,BiConsumer)}, the consumer is called on the main thread, and so can
+     * interact with most game state by default.
+     */
+    default <MSG extends BASE> SimpleFlow<BUF, BASE> addMain(Class<MSG> type, StreamCodec<BUF, MSG> codec, BiConsumer<MSG, CustomPayloadEvent.Context> handler) {
+        return add(type, codec, (msg, ctx) -> {
+            ctx.enqueueWork(() -> handler.accept(msg, ctx));
+            ctx.setPacketHandled(true);
+        });
+    }
 }
