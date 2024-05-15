b232bd1490afb32f43851b155688e87dbe0a3529
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2024-05-14 19:03:25 -0700

    Finished GameTests, Fixed build_forge.gradle, now test runs work.

--- src/test/java/net/minecraftforge/debug/gameplay/item/GatherComponentsEventTest.java
+++ src/test/java/net/minecraftforge/debug/gameplay/item/GatherComponentsEventTest.java
@@ -1,16 +1,20 @@
 package net.minecraftforge.debug.gameplay.item;
 
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.gametest.framework.GameTest;
+import net.minecraft.gametest.framework.GameTestHelper;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Items;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.GatherComponentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.gametest.GameTestHolder;
 import net.minecraftforge.test.BaseTestMod;
 
 import java.util.List;
 
+@GameTestHolder("forge." + GatherComponentsEventTest.MOD_ID)
 @Mod(GatherComponentsEventTest.MOD_ID)
 public class GatherComponentsEventTest extends BaseTestMod {
     public static final String MOD_ID = "gather_components_test_event";
@@ -30,5 +34,21 @@ public class GatherComponentsEventTest extends BaseTestMod {
                     List.of()
             ));
         }
+    }
+
+    @GameTest(template = "forge:empty3x3x3")
+    public static void onTestForFood1(GameTestHelper helper) {
+        if (Items.IRON_NUGGET.components().has(DataComponents.FOOD))
+            helper.succeed();
+        else
+            helper.fail("Iron Nugget is not edible, failed to apply DataComponents.FOOD to it");
+    }
+
+    @GameTest(template = "forge:empty3x3x3")
+    public static void onTestForFood2(GameTestHelper helper) {
+        if (Items.IRON_INGOT.components().has(DataComponents.FOOD))
+            helper.fail("Iron Ingot is edible, should not have DataComponents.FOOD");
+        else
+            helper.succeed();
     }
 }
