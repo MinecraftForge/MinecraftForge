8461af2e894cb7735429d3b4e5215a62b7d14197
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2024-05-20 15:11:54 -0700

    Cleaned up the test mod

--- src/test/java/net/minecraftforge/debug/gameplay/biome/BiomeTestMod.java
+++ src/test/java/net/minecraftforge/debug/gameplay/biome/BiomeTestMod.java
@@ -30,129 +30,64 @@ import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
 import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
 import net.minecraftforge.common.world.BiomeModifier;
 import net.minecraftforge.common.world.ForgeBiomeModifiers;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.test.BaseTestMod;
-
 import java.util.List;
-import java.util.function.Supplier;
 
 
 @Mod(BiomeTestMod.MOD_ID)
 public class BiomeTestMod extends BaseTestMod {
     public static final String MOD_ID = "biome_test_mod";
 
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, BiomeTestMod.MOD_ID);
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, BiomeTestMod.MOD_ID);
 
-    public BiomeTestMod() {
-        var modBus = FMLJavaModLoadingContext.get().getModEventBus();
+    public static final RegistryObject<Block> SILVER_ORE = BLOCKS.register("silver_ore",
+            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.GOLD_ORE)));
+    public static final RegistryObject<Item> SILVER_ORE_ITEM = ITEMS.register("silver_ore",
+            () -> new BlockItem(SILVER_ORE.get(), new Item.Properties()));
 
-        ModBlocks.register(modBus);
-        ModItems.register(modBus);
+    public static final ResourceKey<PlacedFeature> OVERWORLD_SILVER_ORE_PLACED_KEY = ResourceKey.create(Registries.PLACED_FEATURE, new ResourceLocation(BiomeTestMod.MOD_ID, "overworld_silver_ore_placed"));
+    public static final ResourceKey<BiomeModifier> ADD_OVERWORLD_SILVER_ORE = ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, new ResourceLocation(BiomeTestMod.MOD_ID, "add_overworld_silver_ore"));
+    public static final ResourceKey<ConfiguredFeature<?, ?>> OVERWORLD_SILVER_ORE_KEY = ResourceKey.create(Registries.CONFIGURED_FEATURE, new ResourceLocation(BiomeTestMod.MOD_ID, "overworld_silver_ore"));
+
+    public BiomeTestMod() {}
+
+    public static List<PlacementModifier> orePlacement(PlacementModifier m1, PlacementModifier m2) {
+        return List.of(m1, InSquarePlacement.spread(), m2, BiomeFilter.biome());
     }
 
-    public class ModPlacedFeatures {
-
-        public static final ResourceKey<PlacedFeature> OVERWORLD_SILVER_ORE_PLACED_KEY = registerKey("overworld_silver_ore_placed");
-
-        public static void bootstrap(BootstrapContext<PlacedFeature> context) {
-            HolderGetter<ConfiguredFeature<?, ?>> configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
-
-            register(context, OVERWORLD_SILVER_ORE_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.OVERWORLD_SILVER_ORE_KEY),
-                    ModOrePlacement.commonOrePlacement(4, HeightRangePlacement.triangle(VerticalAnchor.absolute(-64), VerticalAnchor.absolute(32))));
-        }
-
-        private static ResourceKey<PlacedFeature> registerKey(String name) {
-            return ResourceKey.create(Registries.PLACED_FEATURE, new ResourceLocation(BiomeTestMod.MOD_ID, name));
-        }
-
-        private static void register(BootstrapContext<PlacedFeature> context, ResourceKey<PlacedFeature> key, Holder<ConfiguredFeature<?, ?>> configuration, List<PlacementModifier> modifiers) {
-            context.register(key, new PlacedFeature(configuration, List.copyOf(modifiers)));
-        }
+    public static List<PlacementModifier> commonOrePlacement(int count, PlacementModifier heightRange) {
+        return orePlacement(CountPlacement.of(count), heightRange);
     }
 
-    public static class ModOrePlacement {
-        public static List<PlacementModifier> orePlacement(PlacementModifier m1, PlacementModifier m2) {
-            return List.of(m1, InSquarePlacement.spread(), m2, BiomeFilter.biome());
-        }
-
-        public static List<PlacementModifier> commonOrePlacement(int count, PlacementModifier heightRange) {
-            return orePlacement(CountPlacement.of(count), heightRange);
-        }
+    public static void bootstrapConfiguredFeature(BootstrapContext<ConfiguredFeature<?, ?>> context) {
+        RuleTest stoneReplaceables = new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES);
+        List<OreConfiguration.TargetBlockState> overworldSilverOres = List.of(OreConfiguration.target(stoneReplaceables, SILVER_ORE.get().defaultBlockState()));
+        context.register(OVERWORLD_SILVER_ORE_KEY, new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(overworldSilverOres, 9)));
     }
 
-    public static class ModConfiguredFeatures {
+    public static void bootstrapBiomeModifier(BootstrapContext<BiomeModifier> context) {
+        var placedFeatures = context.lookup(Registries.PLACED_FEATURE);
+        var biomes = context.lookup(Registries.BIOME);
 
-        public static final ResourceKey<ConfiguredFeature<?, ?>> OVERWORLD_SILVER_ORE_KEY = registerKey("overworld_silver_ore");
-
-        public static void bootstrap(BootstrapContext<ConfiguredFeature<?, ?>> context) {
-            RuleTest stoneReplaceables = new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES);
-
-            List<OreConfiguration.TargetBlockState> overworldSilverOres = List.of(OreConfiguration.target(stoneReplaceables,
-                    ModBlocks.SILVER_ORE.get().defaultBlockState()));
-
-            register(context, OVERWORLD_SILVER_ORE_KEY, Feature.ORE, new OreConfiguration(overworldSilverOres, 9));
-
-        }
-
-        public static ResourceKey<ConfiguredFeature<?, ?>> registerKey(String name) {
-            return ResourceKey.create(Registries.CONFIGURED_FEATURE, new ResourceLocation(BiomeTestMod.MOD_ID, name));
-        }
-
-        private static <FC extends FeatureConfiguration, F extends Feature<FC>> void register(BootstrapContext<ConfiguredFeature<?, ?>> context, ResourceKey<ConfiguredFeature<?, ?>> key, F feature, FC configuration) {
-            context.register(key, new ConfiguredFeature<>(feature, configuration));
-        }
+        context.register(ADD_OVERWORLD_SILVER_ORE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+                biomes.getOrThrow(BiomeTags.IS_OVERWORLD),
+                HolderSet.direct(placedFeatures.getOrThrow(OVERWORLD_SILVER_ORE_PLACED_KEY)),
+                GenerationStep.Decoration.UNDERGROUND_ORES)
+        );
     }
 
-    public static class ModBiomeModifiers {
-
-        public static final ResourceKey<BiomeModifier> ADD_OVERWORLD_SILVER_ORE = registerKey("add_overworld_silver_ore");
-
-        public static void bootstrap(BootstrapContext<BiomeModifier> context) {
-            var placedFeatures = context.lookup(Registries.PLACED_FEATURE);
-            var biomes = context.lookup(Registries.BIOME);
-
-            context.register(ADD_OVERWORLD_SILVER_ORE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
-                    biomes.getOrThrow(BiomeTags.IS_OVERWORLD),
-                    HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.OVERWORLD_SILVER_ORE_PLACED_KEY)),
-                    GenerationStep.Decoration.UNDERGROUND_ORES));
-        }
-
-        private static ResourceKey<BiomeModifier> registerKey(String name) {
-            return ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, new ResourceLocation(BiomeTestMod.MOD_ID, name));
-        }
-    }
-
-    public static class ModBlocks {
-        public static final DeferredRegister<Block> BLOCKS =
-                DeferredRegister.create(ForgeRegistries.BLOCKS, BiomeTestMod.MOD_ID);
-        public static final RegistryObject<Block> SILVER_ORE = registerBlock("silver_ore",
-                () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.GOLD_ORE)));
-
-        private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
-            RegistryObject<T> toReturn = BLOCKS.register(name, block);
-            registerBlockItem(name, toReturn);
-            return toReturn;
-        }
-
-        private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
-            return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
-        }
-
-        public static void register(IEventBus eventBus) {
-            BLOCKS.register(eventBus);
-        }
-    }
-
-    public static class ModItems {
-        public static final DeferredRegister<Item> ITEMS =
-                DeferredRegister.create(ForgeRegistries.ITEMS, BiomeTestMod.MOD_ID);
-
-        public static void register(IEventBus eventBus) {
-            ITEMS.register(eventBus);
-        }
+    public static void bootstrapPlacedFeature(BootstrapContext<PlacedFeature> context) {
+        HolderGetter<ConfiguredFeature<?, ?>> configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
+        context.register(OVERWORLD_SILVER_ORE_PLACED_KEY, new PlacedFeature(
+                configuredFeatures.getOrThrow(OVERWORLD_SILVER_ORE_KEY),
+                List.copyOf(commonOrePlacement(4, HeightRangePlacement.triangle(VerticalAnchor.absolute(-64), VerticalAnchor.absolute(32))))
+        ));
     }
 }
--- src/test/java/net/minecraftforge/debug/gameplay/biome/DataGen.java
+++ src/test/java/net/minecraftforge/debug/gameplay/biome/DataGen.java
@@ -7,7 +7,6 @@ import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
-import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -15,7 +14,6 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
-import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -26,48 +24,26 @@ import java.util.concurrent.CompletableFuture;
 
 @Mod.EventBusSubscriber(modid = BiomeTestMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DataGen {
+
     @SubscribeEvent
     public static void gatherdata(GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
         PackOutput packOutput = generator.getPackOutput();
-        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
 
-        generator.addProvider(event.includeServer(), ModLootTableProvider.create(packOutput, lookupProvider));
-
-        //generator.addProvider(event.includeClient(), new ModBlockStateProvider(packOutput, existingFileHelper));
-
+        generator.addProvider(event.includeServer(), createLootTable(packOutput, lookupProvider));
         generator.addProvider(event.includeServer(), new ModWorldGenProvider(packOutput, lookupProvider));
-
     }
 
-    public static class ModBlockStateProvider extends BlockStateProvider {
-        public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
-            super(output, BiomeTestMod.MOD_ID, exFileHelper);
-        }
-
-        @Override
-        protected void registerStatesAndModels() {
-            blockWithItem(BiomeTestMod.ModBlocks.SILVER_ORE);
-        }
-
-        private void blockWithItem(RegistryObject<Block> blockRegistryObject) {
-            simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
-        }
-    }
-
-    public static class ModLootTableProvider {
-        public static LootTableProvider create(PackOutput output, CompletableFuture<HolderLookup.Provider> pRegistries) {
-            return new LootTableProvider(output, Set.of(), List.of(
-                    new LootTableProvider.SubProviderEntry(ModBlockLootTables::new, LootContextParamSets.BLOCK)), pRegistries);
-        }
+    public static LootTableProvider createLootTable(PackOutput output, CompletableFuture<HolderLookup.Provider> pRegistries) {
+        return new LootTableProvider(output, Set.of(), List.of(new LootTableProvider.SubProviderEntry(ModBlockLootTables::new, LootContextParamSets.BLOCK)), pRegistries);
     }
 
     public static class ModWorldGenProvider extends DatapackBuiltinEntriesProvider {
         public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
-                .add(Registries.CONFIGURED_FEATURE, BiomeTestMod.ModConfiguredFeatures::bootstrap)
-                .add(Registries.PLACED_FEATURE, BiomeTestMod.ModPlacedFeatures::bootstrap)
-                .add(ForgeRegistries.Keys.BIOME_MODIFIERS, BiomeTestMod.ModBiomeModifiers::bootstrap);
+                .add(Registries.CONFIGURED_FEATURE, BiomeTestMod::bootstrapConfiguredFeature)
+                .add(Registries.PLACED_FEATURE, BiomeTestMod::bootstrapPlacedFeature)
+                .add(ForgeRegistries.Keys.BIOME_MODIFIERS, BiomeTestMod::bootstrapBiomeModifier);
 
         public ModWorldGenProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
             super(output, registries, BUILDER, Set.of(BiomeTestMod.MOD_ID));
@@ -81,12 +57,12 @@ public class DataGen {
 
         @Override
         protected void generate() {
-            this.dropSelf(BiomeTestMod.ModBlocks.SILVER_ORE.get());
+            this.dropSelf(BiomeTestMod.SILVER_ORE.get());
         }
 
         @Override
         protected Iterable<Block> getKnownBlocks() {
-            return BiomeTestMod.ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get)::iterator;
+            return BiomeTestMod.BLOCKS.getEntries().stream().map(RegistryObject::get)::iterator;
         }
     }
 }
