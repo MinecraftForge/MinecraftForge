ea7317e28ab0ff19079abfc9b54a93187cc0851c
Mysticdrew <drewhaas@gmail.com>
2024-05-21 13:56:50 -0500

    fix tests

--- src/test/java/net/minecraftforge/debug/client/CustomizeGuiOverlayEventTest.java
+++ src/test/java/net/minecraftforge/debug/client/CustomizeGuiOverlayEventTest.java
@@ -1,4 +1,4 @@
-package net.minecraftforge.debug.client.event;
+package net.minecraftforge.debug.client;
 
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.CustomizeGuiOverlayEvent;
@@ -11,11 +11,11 @@ public class CustomizeGuiOverlayEventTest
 {
     private static final boolean ENABLED = true;
 
-    public static final String MODID = "cust_gui_event_test";
+    public static final String MODID = "custom_gui_event_test";
     @Mod.EventBusSubscriber(value = Dist.CLIENT, modid = MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
     public static class ClientEvents {
         @SubscribeEvent(priority = EventPriority.NORMAL)
-        public void onRenderOverlayDebug(CustomizeGuiOverlayEvent.DebugText event) {
+        public static void onRenderOverlayDebug(CustomizeGuiOverlayEvent.DebugText event) {
             if(ENABLED) {
                 if(CustomizeGuiOverlayEvent.DebugText.Side.Left.equals(event.getSide())) {
                     event.getText().add("Left Side");
@@ -26,9 +26,9 @@ public class CustomizeGuiOverlayEventTest
         }
 
         @SubscribeEvent(priority = EventPriority.NORMAL)
-        public void onRenderOverlaychat(CustomizeGuiOverlayEvent.Chat event) {
+        public static void onRenderOverlaychat(CustomizeGuiOverlayEvent.Chat event) {
             if(ENABLED) {
-                event.setPosY(event.getPosY() - 100);
+                event.setPosY(-200);
             }
         }
     }
