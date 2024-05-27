be93d2ccda7b5f9971e0c1f57ac04652fcc88813
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2024-05-26 23:27:15 -0700

    Changes were made here....

--- javafmllanguage/src/main/java/net/minecraftforge/fml/javafmlmod/FMLModContainer.java
+++ javafmllanguage/src/main/java/net/minecraftforge/fml/javafmlmod/FMLModContainer.java
@@ -73,7 +73,7 @@ public class FMLModContainer extends ModContainer {
                 this.modInstance = constructors[0].newInstance();
             } else {
                 if (parameters[0].getType() == FMLModContainer.class) {
-                    this.modInstance = constructors[0].newInstance(eventBus);
+                    this.modInstance = constructors[0].newInstance(this);
                 } else {
                     throw new IllegalStateException("Could not find a constructor with allowed parameter of net.minecraftforge.fml.javafmlmod.FMLModContainer");
                 }
