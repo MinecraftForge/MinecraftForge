2c3b59c0aa7995e1ee32583eaa17ed3576a7af65
Paint_Ninja <PaintNinja@users.noreply.github.com>
2024-08-15 20:36:22 +0100

    Make VersionSupportMatrix immutable

--- fmlloader/src/main/java/net/minecraftforge/fml/loading/VersionSupportMatrix.java
+++ fmlloader/src/main/java/net/minecraftforge/fml/loading/VersionSupportMatrix.java
@@ -5,48 +5,55 @@
 
 package net.minecraftforge.fml.loading;
 
-import net.minecraftforge.forgespi.language.MavenVersionAdapter;
 import org.apache.maven.artifact.versioning.ArtifactVersion;
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 import org.apache.maven.artifact.versioning.VersionRange;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiPredicate;
 
 public class VersionSupportMatrix {
-    private static final boolean ENABLED = false;
+    private static final boolean ENABLED = FMLLoader.versionInfo().mcVersion().equals("1.21.1");
     private static final Map<String, List<ArtifactVersion>> overrideVersions;
 
     static {
         if (ENABLED) {
-            overrideVersions = new HashMap<>();
-            final ArtifactVersion version = new DefaultArtifactVersion(FMLLoader.versionInfo().mcVersion());
-            final ArtifactVersion version = new DefaultArtifactVersion(FMLLoader.versionInfo().mcVersion());
-            if (MavenVersionAdapter.createFromVersionSpec("[1.21.1]").containsVersion(version)) {
-                // 1.21.1 is Compatible with 1.21
-                add("languageloader.javafml", "51");
-                add("mod.minecraft",          "1.21");
-                add("mod.forge",              "51.0.33");
-            }
+            overrideVersions = Map.ofEntries(
+                    // 1.21.1 is Compatible with 1.21
+                    entry("languageloader.javafml", "51"),
+                    entry("mod.minecraft",          "1.21"),
+                    entry("mod.forge",              "51.0.33")
+            );
         } else {
-            overrideVersions = null;
+            overrideVersions = Collections.emptyMap();
         }
     }
 
-    private static void add(String key, String value) {
-        overrideVersions.computeIfAbsent(key, k -> new ArrayList<>()).add(new DefaultArtifactVersion(value));
-    }
-
+    /**
+     * @deprecated Use {@link #testVersionSupportMatrix(VersionRange, String, String)} instead, unwrapping your BiPredicate.
+     */
+    @Deprecated(forRemoval = true, since = "1.21.1")
     public static boolean testVersionSupportMatrix(VersionRange declaredRange, String lookupId, String type, BiPredicate<String, VersionRange> standardLookup) {
         if (standardLookup.test(lookupId, declaredRange)) return true;
+        return testVersionSupportMatrix(declaredRange, lookupId, type);
+    }
+
+    public static boolean testVersionSupportMatrix(VersionRange declaredRange, String lookupId, String type) {
         List<ArtifactVersion> custom = overrideVersions.get(type + "." + lookupId);
-        return custom == null ? false  : custom.stream().anyMatch(declaredRange::containsVersion);
+        return custom != null && custom.stream().anyMatch(declaredRange::containsVersion);
     }
 
     static boolean isEnabled() {
         return ENABLED;
+    }
+
+    private static Map.Entry<String, List<ArtifactVersion>> entry(String typeAndLookupId, String declaredRange) {
+        return Map.entry(typeAndLookupId, List.of(new DefaultArtifactVersion(declaredRange)));
+    }
+
+    private static Map.Entry<String, List<ArtifactVersion>> entry(String typeAndLookupId, List<String> declaredRanges) {
+        return Map.entry(typeAndLookupId, declaredRanges.stream().map(DefaultArtifactVersion::new).map(it -> (ArtifactVersion) it).toList());
     }
 }
