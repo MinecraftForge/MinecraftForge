4ef93772a383abf12b09a194e3f99818d1172b96
daedalus4096 <michael.a.bunting@gmail.com>
2024-09-05 20:42:14 -0700

    Unregister event listener after test completion

--- src/test/java/net/minecraftforge/debug/event/VanillaGameEventTest.java
+++ src/test/java/net/minecraftforge/debug/event/VanillaGameEventTest.java
@@ -1,12 +1,14 @@
 package net.minecraftforge.debug.event;
 
+import java.util.function.Consumer;
+
 import net.minecraft.core.BlockPos;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.VanillaGameEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.gametest.GameTestHolder;
@@ -23,26 +25,30 @@ public class VanillaGameEventTest extends BaseTestMod {
 
     @GameTest(template = "forge:empty3x3x3")
     public static void game_event_matches(GameTestHelper helper) {
-        // Place a button in the test area
-        var pos = new BlockPos(1, 1, 1);
-        helper.setBlock(pos, Blocks.STONE_BUTTON);
-        
-        // Press the button, which is supposed to trigger the BLOCK_ACTIVATE game event
-        helper.pressButton(pos);
-        
-        // Confirm that the button has been replaced by a diamond block
-        helper.assertBlockPresent(Blocks.DIAMOND_BLOCK, pos);
-        helper.succeed();
-    }
-
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
-    public static class Listener {
-        @SubscribeEvent
-        public static void vanillaEvent(VanillaGameEvent event) {
+        // Create and register the event listener
+        Consumer<VanillaGameEvent> listener = event -> {
             // Replace any activated block with a diamond block
             if (GameEvent.BLOCK_ACTIVATE.is(event.getVanillaEventHolder())) {
                 event.getLevel().setBlock(BlockPos.containing(event.getEventPosition()), Blocks.DIAMOND_BLOCK.defaultBlockState(), 3);
             }
+        };
+        MinecraftForge.EVENT_BUS.addListener(listener);
+        
+        try {
+            // Place a button in the test area
+            var pos = new BlockPos(1, 1, 1);
+            helper.setBlock(pos, Blocks.STONE_BUTTON);
+            
+            // Press the button, which is supposed to trigger the BLOCK_ACTIVATE game event
+            helper.pressButton(pos);
+            
+            // Confirm that the button has been replaced by a diamond block
+            helper.assertBlockPresent(Blocks.DIAMOND_BLOCK, pos);
+            helper.succeed();
+        }
+        finally {
+            // Unregister the listener when no longer needed
+            MinecraftForge.EVENT_BUS.unregister(listener);
         }
     }
 }
