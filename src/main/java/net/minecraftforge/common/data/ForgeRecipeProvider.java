068c3043881bd90380d58246f273c07be3235208
Paint_Ninja <PaintNinja@users.noreply.github.com>
2024-09-30 14:08:04 +0100

    Fix oversight in ForgeRecipeProvider

--- src/main/java/net/minecraftforge/common/data/ForgeRecipeProvider.java
+++ src/main/java/net/minecraftforge/common/data/ForgeRecipeProvider.java
@@ -73,8 +73,8 @@ public final class ForgeRecipeProvider extends VanillaRecipeProvider {
         replace(Items.DIAMOND, Tags.Items.GEMS_DIAMOND);
         replace(Items.EMERALD, Tags.Items.GEMS_EMERALD);
         replace(Items.CHEST, Tags.Items.CHESTS_WOODEN);
-        replace(Blocks.COBBLESTONE, Tags.Items.COBBLESTONES_NORMAL);
-        replace(Blocks.COBBLED_DEEPSLATE, Tags.Items.COBBLESTONES_DEEPSLATE);
+        replace(Blocks.COBBLESTONE, Tags.Items.COBBLESTONE_NORMAL);
+        replace(Blocks.COBBLED_DEEPSLATE, Tags.Items.COBBLESTONE_DEEPSLATE);
 
         replace(Items.STRING, Tags.Items.STRINGS);
         exclude(getConversionRecipeName(Blocks.WHITE_WOOL, Items.STRING));
