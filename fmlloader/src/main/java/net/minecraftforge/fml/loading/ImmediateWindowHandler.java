c4b1e79863f4ee4dce66e5089d8e9b57f9846a20
LexManos <LexManos@gmail.com>
2024-10-09 17:02:13 -0700

    Properly allow normal mods.toml mods to specify the module names and use a full module-info. Forge is now the `net.minecraftforge.forge` module instead of `forge`

--- fmlloader/src/main/java/net/minecraftforge/fml/loading/ImmediateWindowHandler.java
+++ fmlloader/src/main/java/net/minecraftforge/fml/loading/ImmediateWindowHandler.java
@@ -140,7 +140,7 @@ public class ImmediateWindowHandler {
 
         @Override
         public void updateModuleReads(final ModuleLayer layer) {
-            var fm = layer.findModule("forge");
+            var fm = layer.findModule("net.minecraftforge.forge");
             if (fm.isPresent()) {
                 getClass().getModule().addReads(fm.get());
                 var clz = fm.map(l -> Class.forName(l, "net.minecraftforge.client.loading.NoVizFallback")).orElseThrow();
