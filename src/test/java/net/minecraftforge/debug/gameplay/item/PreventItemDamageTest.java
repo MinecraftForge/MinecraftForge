b7b3bc0cede04afe170a00cb456c9813732e4755
Jonathing <me@jonathing.me>
2025-01-28 18:37:31 -0500

    Fix issues due to backporting

--- src/test/java/net/minecraftforge/debug/gameplay/item/PreventItemDamageTest.java
+++ src/test/java/net/minecraftforge/debug/gameplay/item/PreventItemDamageTest.java
@@ -32,17 +32,16 @@ public class PreventItemDamageTest extends BaseTestMod {
     static final String MOD_ID = "prevent_item_damage";
 
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
-    private static final RegistryObject<Item> FAKE_SHIELD = ITEMS.register("fake_shield", () -> new ShieldItem(new Item.Properties().setId(ITEMS.key("fake_shield")).durability(10)) {
+    private static final RegistryObject<Item> FAKE_SHIELD = ITEMS.register("fake_shield", () -> new ShieldItem(new Item.Properties().durability(10)) {
         @Override
-        public int damageItem(ItemStack stack, int damage, ServerLevel level, @Nullable ServerPlayer player, Consumer<Item> onBroken) {
-            onBroken.accept(this);
+        public <T extends LivingEntity> int damageItem(ItemStack stack, int amount, T entity, Consumer<T> onBroken) {
+            onBroken.accept(entity);
             return 0;
         }
     });
 
-    public PreventItemDamageTest(FMLJavaModLoadingContext context) {
-        super(context);
-        this.testItem(lookup -> new ItemStack(FAKE_SHIELD.get()));
+    public PreventItemDamageTest() {
+        this.testItem(() -> new ItemStack(FAKE_SHIELD.get()));
     }
 
     @GameTest(template = "forge:empty3x3x3")
@@ -70,7 +69,7 @@ public class PreventItemDamageTest extends BaseTestMod {
         player.lookAt(EntityAnchorArgument.Anchor.EYES, enemy.position());
 
         // hit the player
-        player.hurtServer(helper.getLevel(), enemy.damageSources().mobAttack(enemy), 5.0F);
+        player.hurt(enemy.damageSources().mobAttack(enemy), 5.0F);
 
         // shield on cooldown?
         helper.assertTrue(initialDamage == shield.getDamageValue(), "Fake shield took damage! Expected: " + initialDamage + ", Actual: " + shield.getDamageValue());
