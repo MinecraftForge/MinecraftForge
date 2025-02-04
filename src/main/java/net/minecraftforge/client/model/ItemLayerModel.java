6e89c16bd4b9a38bf07fdfaa4766ab3b4dcab28f
Jonathing <me@jonathing.me>
2025-02-03 20:59:14 -0500

    Account for "render_type_fast" in item layers

--- src/main/java/net/minecraftforge/client/model/ItemLayerModel.java
+++ src/main/java/net/minecraftforge/client/model/ItemLayerModel.java
@@ -45,12 +45,22 @@ public class ItemLayerModel implements IUnbakedGeometry<ItemLayerModel>
     private ImmutableList<Material> textures;
     private final Int2ObjectMap<ForgeFaceData> layerData;
     private final Int2ObjectMap<ResourceLocation> renderTypeNames;
+    private final Int2ObjectMap<ResourceLocation> renderTypeNamesFast;
 
     private ItemLayerModel(@Nullable ImmutableList<Material> textures, Int2ObjectMap<ForgeFaceData> layerData, Int2ObjectMap<ResourceLocation> renderTypeNames)
     {
         this.textures = textures;
         this.layerData = layerData;
         this.renderTypeNames = renderTypeNames;
+        this.renderTypeNamesFast = new Int2ObjectOpenHashMap<>();
+    }
+
+    private ItemLayerModel(@Nullable ImmutableList<Material> textures, Int2ObjectMap<ForgeFaceData> layerData, Int2ObjectMap<ResourceLocation> renderTypeNames, Int2ObjectMap<ResourceLocation> renderTypeNamesFast)
+    {
+        this.textures = textures;
+        this.layerData = layerData;
+        this.renderTypeNames = renderTypeNames;
+        this.renderTypeNamesFast = renderTypeNamesFast;
     }
 
     @Override
@@ -82,7 +92,9 @@ public class ItemLayerModel implements IUnbakedGeometry<ItemLayerModel>
             var quads = UnbakedGeometryHelper.bakeElements(unbaked, $ -> sprite, modelState, modelLocation);
             var renderTypeName = renderTypeNames.get(i);
             var renderTypes = renderTypeName != null ? context.getRenderType(renderTypeName) : null;
-            builder.addQuads(renderTypes != null ? renderTypes : normalRenderTypes, quads);
+            var renderTypeNameFast = renderTypeNamesFast.get(i);
+            var renderTypesFast = renderTypeNameFast != null ? context.getRenderType(renderTypeNameFast) : null;
+            builder.addQuads(renderTypes != null ? renderTypes : normalRenderTypes, renderTypesFast != null ? renderTypesFast : RenderTypeGroup.EMPTY, quads);
         }
 
         return builder.build();
@@ -108,16 +120,34 @@ public class ItemLayerModel implements IUnbakedGeometry<ItemLayerModel>
                 }
             }
 
+            var renderTypeNamesFast = new Int2ObjectOpenHashMap<ResourceLocation>();
+            if (jsonObject.has("render_types_fast"))
+            {
+                var renderTypes = jsonObject.getAsJsonObject("render_types_fast");
+                for (Map.Entry<String, JsonElement> entry : renderTypes.entrySet())
+                {
+                    var renderType = new ResourceLocation(entry.getKey());
+                    for (var layer : entry.getValue().getAsJsonArray())
+                        if (renderTypeNamesFast.put(layer.getAsInt(), renderType) != null)
+                            throw new JsonParseException("Registered duplicate fast graphics render type for layer " + layer);
+                }
+            }
+
             var emissiveLayers = new Int2ObjectArrayMap<ForgeFaceData>();
             if(jsonObject.has("forge_data"))
             {
                 JsonObject forgeData = jsonObject.get("forge_data").getAsJsonObject();
-                readLayerData(forgeData, "layers", renderTypeNames, emissiveLayers, false);    
+                readLayerData(forgeData, "layers", renderTypeNames, renderTypeNamesFast, emissiveLayers, false);
             }
-            return new ItemLayerModel(null, emissiveLayers, renderTypeNames);
+            return new ItemLayerModel(null, emissiveLayers, renderTypeNames, renderTypeNamesFast);
         }
 
         protected void readLayerData(JsonObject jsonObject, String name, Int2ObjectOpenHashMap<ResourceLocation> renderTypeNames, Int2ObjectMap<ForgeFaceData> layerData, boolean logWarning)
+        {
+            this.readLayerData(jsonObject, name, renderTypeNames, new Int2ObjectOpenHashMap<>(), layerData, logWarning);
+        }
+
+        protected void readLayerData(JsonObject jsonObject, String name, Int2ObjectOpenHashMap<ResourceLocation> renderTypeNames, Int2ObjectOpenHashMap<ResourceLocation> renderTypeNamesFast, Int2ObjectMap<ForgeFaceData> layerData, boolean logWarning)
         {
             if (!jsonObject.has(name))
             {
--- src/main/java/net/minecraftforge/client/model/generators/loaders/ItemLayerModelBuilder.java
+++ src/main/java/net/minecraftforge/client/model/generators/loaders/ItemLayerModelBuilder.java
@@ -33,6 +33,7 @@ public class ItemLayerModelBuilder<T extends ModelBuilder<T>> extends CustomLoad
 
     private final Int2ObjectMap<ForgeFaceData> faceData = new Int2ObjectOpenHashMap<>();
     private final Map<ResourceLocation, IntSet> renderTypes = new LinkedHashMap<>();
+    private final Map<ResourceLocation, IntSet> renderTypesFast = new LinkedHashMap<>();
     private final IntSet layersWithRenderTypes = new IntOpenHashSet();
 
     protected ItemLayerModelBuilder(T parent, ExistingFileHelper existingFileHelper)
@@ -115,6 +116,23 @@ public class ItemLayerModelBuilder<T extends ModelBuilder<T>> extends CustomLoad
         return renderType(asLoc, layers);
     }
 
+    public ItemLayerModelBuilder<T> renderType(String renderType, String renderTypeFast, int... layers)
+    {
+        Preconditions.checkNotNull(renderType, "Render type must not be null");
+        Preconditions.checkNotNull(renderTypeFast, "Fast graphics render type must not be null");
+        ResourceLocation asLoc;
+        if (renderType.contains(":"))
+            asLoc = new ResourceLocation(renderType);
+        else
+            asLoc = new ResourceLocation(parent.getLocation().getNamespace(), renderType);
+        ResourceLocation asLocFast;
+        if (renderTypeFast.contains(":"))
+            asLocFast = new ResourceLocation(renderTypeFast);
+        else
+            asLocFast = new ResourceLocation(parent.getLocation().getNamespace(), renderTypeFast);
+        return renderType(asLoc, asLocFast, layers);
+    }
+
     /**
      * Set the render type for a set of layers.
      *
@@ -144,6 +162,25 @@ public class ItemLayerModelBuilder<T extends ModelBuilder<T>> extends CustomLoad
         return this;
     }
 
+    public ItemLayerModelBuilder<T> renderType(ResourceLocation renderType, ResourceLocation renderTypeFast, int... layers)
+    {
+        Preconditions.checkNotNull(renderType, "Render type must not be null");
+        Preconditions.checkNotNull(renderTypeFast, "Fast graphics render type must not be null");
+        Preconditions.checkNotNull(layers, "Layers must not be null");
+        Preconditions.checkArgument(layers.length > 0, "At least one layer must be specified");
+        Preconditions.checkArgument(Arrays.stream(layers).allMatch(i -> i >= 0), "All layers must be >= 0");
+        var alreadyAssigned = Arrays.stream(layers).filter(layersWithRenderTypes::contains).toArray();
+        Preconditions.checkArgument(alreadyAssigned.length == 0, "Attempted to re-assign layer render types: " + Arrays.toString(alreadyAssigned));
+        var renderTypeLayers = renderTypes.computeIfAbsent(renderType, $ -> new IntOpenHashSet());
+        var renderTypeFastLayers = renderTypesFast.computeIfAbsent(renderType, $ -> new IntOpenHashSet());
+        Arrays.stream(layers).forEach(layer -> {
+            renderTypeLayers.add(layer);
+            renderTypeFastLayers.add(layer);
+            layersWithRenderTypes.add(layer);
+        });
+        return this;
+    }
+
     @Override
     public JsonObject toJson(JsonObject json)
     {
@@ -167,6 +204,14 @@ public class ItemLayerModelBuilder<T extends ModelBuilder<T>> extends CustomLoad
             renderTypes.add(renderType.toString(), array);
         });
         json.add("render_types", renderTypes);
+
+        JsonObject renderTypesFast = new JsonObject();
+        this.renderTypesFast.forEach((renderType, layers) -> {
+            JsonArray array = new JsonArray();
+            layers.intStream().sorted().forEach(array::add);
+            renderTypesFast.add(renderType.toString(), array);
+        });
+        json.add("render_types_fast", renderTypesFast);
 
         return json;
     }
