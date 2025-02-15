26a588d7edfbbf26b2b628a9bc236025dc401909
Jonathing <me@jonathing.me>
2025-02-15 17:59:44 -0500

    Convert WrappedStateAwareListener into a record

--- src/main/java/net/minecraftforge/event/AddReloadListenerEvent.java
+++ src/main/java/net/minecraftforge/event/AddReloadListenerEvent.java
@@ -72,13 +72,7 @@ public class AddReloadListenerEvent extends Event {
         return registryAccess;
     }
 
-    private static class WrappedStateAwareListener implements PreparableReloadListener {
-        private final PreparableReloadListener wrapped;
-
-        private WrappedStateAwareListener(final PreparableReloadListener wrapped) {
-            this.wrapped = wrapped;
-        }
-
+    private record WrappedStateAwareListener(PreparableReloadListener wrapped) implements PreparableReloadListener {
         @Override
         public CompletableFuture<Void> reload(final PreparationBarrier stage, final ResourceManager resourceManager, final ProfilerFiller preparationsProfiler, final ProfilerFiller reloadProfiler, final Executor backgroundExecutor, final Executor gameExecutor) {
             if (ModLoader.isLoadingStateValid())
