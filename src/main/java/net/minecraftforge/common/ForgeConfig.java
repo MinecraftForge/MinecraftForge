d6a4edd646c951c806793aa35a9781535836c355
Jonathing <me@jonathing.me>
2025-02-15 20:15:50 -0500

    Make block pos assertions behave like in 1.21.3
    !!! THIS CAN BE DISABLED IN FORGE'S COMMON CONFIG !!!

--- src/main/java/net/minecraftforge/common/ForgeConfig.java
+++ src/main/java/net/minecraftforge/common/ForgeConfig.java
@@ -78,9 +78,23 @@ public class ForgeConfig {
      */
     public static class Common {
 
+        public final ForgeConfigSpec.BooleanValue fixGameTestBlockPosAsserts;
+
         Common(ForgeConfigSpec.Builder builder) {
             builder.comment("[DEPRECATED / NO EFFECT]: General configuration settings")
                     .push("general");
+
+            migrationHelperMode = builder
+                    .comment("A config option to help developers find known legacy modded tags that have common convention equivalents when running on integrated server. Defaults to OFF.")
+                    .translation("forge.configgui.migrationHelperMode")
+                    .defineEnum("logLegacyTagWarnings", MigrationHelperMode.OFF);
+
+            fixGameTestBlockPosAsserts = builder
+                    .comment("""
+                    Set this to true to fix BlockPos assertions in game tests to account for rotations and generally behave as they would in 1.21.3.
+                    This is kept in the common config so that development environments that rely on old behavior do not need to continuously set this in a server config.""")
+                    .translation("forge.configgui.fixGameTestBlockPosAsserts")
+                    .define("fixGameTestBlockPosAsserts", true);
 
             builder.pop();
         }
