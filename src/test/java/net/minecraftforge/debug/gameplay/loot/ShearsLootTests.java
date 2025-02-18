c0db990d862f78383f27065f17b799e05e853590
LexManos <LexManos@gmail.com>
2025-02-17 18:01:24 -0800

    Update ShearsLootTests.java
    Add missing blocks to tests

--- src/test/java/net/minecraftforge/debug/gameplay/loot/ShearsLootTests.java
+++ src/test/java/net/minecraftforge/debug/gameplay/loot/ShearsLootTests.java
@@ -5,6 +5,8 @@
 
 package net.minecraftforge.debug.gameplay.loot;
 
+import org.jetbrains.annotations.Nullable;
+
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.gametest.framework.GameTest;
@@ -12,9 +14,14 @@ import net.minecraft.gametest.framework.GameTestHelper;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ShearsItem;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DoublePlantBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.gametest.GameTestHolder;
@@ -41,48 +48,77 @@ public class ShearsLootTests extends BaseTestMod {
 
     @GameTest(template = "forge:empty3x3x3")
     public static void test_shears_drop_blocks(GameTestHelper helper) {
-        Block[] shearsDropBlocks = {
-                //leaves
-                Blocks.ACACIA_LEAVES,
-                Blocks.AZALEA_LEAVES,
-                Blocks.BIRCH_LEAVES,
-                Blocks.CHERRY_LEAVES,
-                Blocks.DARK_OAK_LEAVES,
-                Blocks.FLOWERING_AZALEA_LEAVES,
-                Blocks.JUNGLE_LEAVES,
-                Blocks.MANGROVE_LEAVES,
-                Blocks.OAK_LEAVES,
-                Blocks.PALE_OAK_LEAVES,
-                Blocks.SPRUCE_LEAVES,
-                //misc
-                Blocks.COBWEB,
-                Blocks.DEAD_BUSH,
-                Blocks.FERN,
-                Blocks.HANGING_ROOTS,
-                Blocks.NETHER_SPROUTS,
-                Blocks.PALE_HANGING_MOSS,
-                Blocks.SEAGRASS,
-                Blocks.SHORT_GRASS,
-                Blocks.TWISTING_VINES,
-                Blocks.VINE,
-                Blocks.WEEPING_VINES
+        Test[] tests = {
+            //leaves
+            Test.of(Blocks.ACACIA_LEAVES),
+            Test.of(Blocks.AZALEA_LEAVES),
+            Test.of(Blocks.BIRCH_LEAVES),
+            Test.of(Blocks.CHERRY_LEAVES),
+            Test.of(Blocks.DARK_OAK_LEAVES),
+            Test.of(Blocks.FLOWERING_AZALEA_LEAVES),
+            Test.of(Blocks.JUNGLE_LEAVES),
+            Test.of(Blocks.MANGROVE_LEAVES),
+            Test.of(Blocks.OAK_LEAVES),
+            Test.of(Blocks.PALE_OAK_LEAVES),
+            Test.of(Blocks.SPRUCE_LEAVES),
+
+            //misc
+            Test.of(Blocks.COBWEB),
+            Test.of(Blocks.DEAD_BUSH),
+            Test.of(Blocks.FERN),
+            Test.of(Blocks.HANGING_ROOTS),
+            Test.of(Blocks.NETHER_SPROUTS),
+            Test.of(Blocks.PALE_HANGING_MOSS),
+            Test.of(Blocks.SEAGRASS),
+            Test.of(Blocks.SHORT_GRASS),
+            Test.of(Blocks.TWISTING_VINES),
+            Test.of(Blocks.VINE),
+            Test.of(Blocks.WEEPING_VINES),
+
+            // Tall things that need surrounding blocks
+            Test.tall(Blocks.LARGE_FERN, Items.FERN),
+            Test.tall(Blocks.TALL_GRASS, Items.SHORT_GRASS),
+            Test.tall(Blocks.TALL_SEAGRASS, Items.SEAGRASS),
+
+            // Things that need a state different then default
+            Test.of(Blocks.GLOW_LICHEN.defaultBlockState().setValue(BlockStateProperties.NORTH, true), Items.GLOW_LICHEN),
         };
 
-        var player = helper.makeMockServerPlayer();
+        helper.makeFloor(); // Seagrass makes water
+        var player = helper.makeMockServerPlayer(false); // Plants prevent loot for creative players
         var center = new BlockPos(1, 1, 1);
 
         player.setItemSlot(EquipmentSlot.MAINHAND, new ItemStack(MODDED_SHEARS.get()));
 
-        for (Block block : shearsDropBlocks) {
-            helper.setAndAssertBlock(center, block);
+        for (Test test : tests) {
+            helper.setAndAssertBlock(center, test.state());
+            if (test.below() != null)
+                helper.setAndAssertBlock(center.below(), test.below());
 
             player.gameMode.destroyBlock(helper.absolutePos(center));
 
-            helper.assertItemEntityPresent(block.asItem(), center, 1.0);
+            helper.assertItemEntityPresent(test.item(), center, 1.0);
+
+            // Cleanup after ourselves
+            helper.setBlock(center, Blocks.AIR);
+            helper.setBlock(center.below(), Blocks.AIR);
+
             helper.removeAllItemEntitiesInRange(center, 1.0);
         }
 
         helper.succeed();
     }
 
+
+    private record Test(BlockState state, @Nullable BlockState below, Item item) {
+        private static Test of(Block block) {
+            return new Test(block.defaultBlockState(), null, block.asItem());
+        }
+        private static Test of(BlockState state, Item item) {
+            return new Test(state, null, item);
+        }
+        private static Test tall(Block block, Item item) {
+            return new Test(block.defaultBlockState().setValue(DoublePlantBlock.HALF, DoubleBlockHalf.UPPER), block.defaultBlockState().setValue(DoublePlantBlock.HALF, DoubleBlockHalf.LOWER), item);
+        }
+    }
 }
