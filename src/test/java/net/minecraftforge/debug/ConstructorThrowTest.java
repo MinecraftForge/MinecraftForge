58eb965945608c1dd0ab35f0aa57065627550adc
Jonathing <me@jonathing.me>
2025-03-04 15:16:44 -0500

    Comment out ConstructorThrowTest
    Even though it's commented out in mods.toml, the annotation scanner
    still finds it and causes the game to kill itself

--- src/test/java/net/minecraftforge/debug/ConstructorThrowTest.java
+++ src/test/java/net/minecraftforge/debug/ConstructorThrowTest.java
@@ -3,17 +3,17 @@
  * SPDX-License-Identifier: LGPL-2.1-only
  */
 
-package net.minecraftforge.debug;
-
-import net.minecraftforge.fml.common.Mod;
-
-/**
- * Test that the mod loading error screen shows the cause of the crash when a mod constructor throws an exception.
- * <p>Enable or disable this test in the mods.toml</p>
- */
-@Mod("constructor_throw_test")
-public class ConstructorThrowTest {
-    public ConstructorThrowTest() {
-        throw new RuntimeException("Test");
-    }
-}
+//package net.minecraftforge.debug;
+//
+//import net.minecraftforge.fml.common.Mod;
+//
+///**
+// * Test that the mod loading error screen shows the cause of the crash when a mod constructor throws an exception.
+// * <p>Enable or disable this test in the mods.toml</p>
+// */
+//@Mod("constructor_throw_test")
+//public class ConstructorThrowTest {
+//    public ConstructorThrowTest() {
+//        throw new RuntimeException("Test");
+//    }
+//}
