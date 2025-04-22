df3e8f1dc5510d3faeb6f2429e79946866083b77
Paint_Ninja <PaintNinja@users.noreply.github.com>
2025-04-22 16:50:06 -0400

    Game test
    Co-authored-by: Jonathing <me@jonathing.me>

---
+++ src/test/java/net/minecraftforge/debug/dist/DistExecutorTest.java
@@ -0,0 +1,34 @@
+package net.minecraftforge.debug.dist;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.gametest.framework.GameTestHelper;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.gametest.GameTest;
+import net.minecraftforge.gametest.GameTestNamespace;
+import net.minecraftforge.test.BaseTestMod;
+
+@Mod(DistExecutorTest.MOD_ID)
+@GameTestNamespace("forge")
+public class DistExecutorTest extends BaseTestMod {
+    static final String MOD_ID = "dist_executor";
+
+    public DistExecutorTest(FMLJavaModLoadingContext context) {
+        super(context);
+    }
+
+    @GameTest
+    public static void preventClientOnServer(GameTestHelper helper) {
+        try {
+            if (FMLEnvironment.dist == Dist.CLIENT) {
+                Minecraft.getInstance().getWindow();
+            }
+        } catch (Throwable throwable) {
+            helper.fail("Client class was loaded on server! " + throwable);
+        }
+
+        helper.succeed();
+    }
+}
