05e5f466994faf0adf94b2282739f78e020d30ca
Paint_Ninja <PaintNinja@users.noreply.github.com>
2025-08-02 12:40:29 +0100

    Revert logger change in LowCodeModContainer

--- lowcodelanguage/src/main/java/net/minecraftforge/fml/lowcodemod/LowCodeModContainer.java
+++ lowcodelanguage/src/main/java/net/minecraftforge/fml/lowcodemod/LowCodeModContainer.java
@@ -5,20 +5,21 @@
 
 package net.minecraftforge.fml.lowcodemod;
 
+import com.mojang.logging.LogUtils;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModContainer;
 import net.minecraftforge.fml.loading.LogMarkers;
 import net.minecraftforge.forgespi.language.IModInfo;
 import net.minecraftforge.forgespi.language.ModFileScanData;
-import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 public class LowCodeModContainer extends ModContainer {
+    private static final Logger LOGGER = LogUtils.getLogger();
     private final Object modInstance;
 
     public LowCodeModContainer(IModInfo info, ModFileScanData modFileScanResults, ModuleLayer gameLayer) {
         super(info);
-        LoggerFactory.getLogger(LowCodeModContainer.class)
-                .debug(LogMarkers.LOADING, "Creating LowCodeModContainer for {}", info.getModId());
+        LOGGER.debug(LogMarkers.LOADING, "Creating LowCodeModContainer for {}", info.getModId());
         this.modInstance = new Object();
         this.contextExtension = () -> null;
         this.extensionPoints.remove(IExtensionPoint.DisplayTest.class);
