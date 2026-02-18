c6932c91909331367c70beff369220d7107f673a
Paint_Ninja <PaintNinja@users.noreply.github.com>
2026-02-18 22:40:02 +0000

    Make loaderVersion optional in mods.toml

--- fmlloader/src/main/java/net/minecraftforge/fml/loading/moddiscovery/ModFileInfo.java
+++ fmlloader/src/main/java/net/minecraftforge/fml/loading/moddiscovery/ModFileInfo.java
@@ -64,7 +64,7 @@ public class ModFileInfo implements IModFileInfo, IConfigurable {
                 .orElseThrow(()->new InvalidModFileException("Missing ModLoader in file", this));
         // as is modloader version
         var modLoaderVerStr = config.<String>getConfigElement("loaderVersion")
-                .orElseThrow(()->new InvalidModFileException("Missing ModLoader version in file", this));
+                .orElse("*");
         var modLoaderVersion = MavenVersionAdapter.createFromVersionSpec(modLoaderVerStr);
         this.languageSpecs = new ArrayList<>(List.of(new LanguageSpec(modLoader, modLoaderVersion)));
 
