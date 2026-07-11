5e3a3137c69d5093c8fc0ba87e0132efaa0436f9
Paint_Ninja <PaintNinja@users.noreply.github.com>
2026-07-11 10:55:28 +0100

    Add support for multiple access transformer configs (#10785)
    Backport of #10760 to 1.21.10
    Co-authored-by: LexManos <LexManos@gmail.com>
    Co-authored-by: Fealtous <myfavgamer@gmail.com>

--- fmlloader/src/main/java/net/minecraftforge/fml/loading/LoadingModList.java
+++ fmlloader/src/main/java/net/minecraftforge/fml/loading/LoadingModList.java
@@ -5,11 +5,14 @@
 
 package net.minecraftforge.fml.loading;
 
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.loading.EarlyLoadingException.ExceptionData;
 import net.minecraftforge.fml.loading.moddiscovery.BackgroundScanHandler;
 import net.minecraftforge.fml.loading.moddiscovery.ModFile;
 import net.minecraftforge.fml.loading.moddiscovery.ModFileInfo;
 import net.minecraftforge.fml.loading.moddiscovery.ModInfo;
 import net.minecraftforge.forgespi.locating.IModFile;
+import org.slf4j.Logger;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -24,6 +27,7 @@ import java.util.stream.Collectors;
  * loading package</em>
  */
 public class LoadingModList {
+    private static final Logger LOGGER = LogUtils.getLogger();
     private static LoadingModList INSTANCE;
     private final List<ModFileInfo> modFiles;
     private final List<ModInfo> sortedList;
@@ -65,13 +69,22 @@ public class LoadingModList {
     }
 
     public void addAccessTransformers() {
+        var errors = new ArrayList<ExceptionData>();
+
         for (ModFileInfo modFile : modFiles) {
             ModFile mod = modFile.getFile();
-            var at = mod.getAccessTransformer().orElse(null);
-            if (at != null) {
-                FMLLoader.addAccessTransformer(at, mod);
+            for (var at : mod.getAccessTransformers()) {
+                if (!Files.exists(at)) {
+                    var message = "Invalid mod file: " + modFile.getFile().getFileName() + ". Missing Access Transformer: " + at;
+                    errors.add(new ExceptionData(message));
+                    LOGGER.error(message);
+                } else
+                    FMLLoader.addAccessTransformer(at, mod);
             }
         }
+
+        if (!errors.isEmpty())
+            preLoadErrors.add(new EarlyLoadingException("Invalid Access Transformers", null, errors));
     }
 
     public void addForScanning(BackgroundScanHandler backgroundScanHandler) {
--- fmlloader/src/main/java/net/minecraftforge/fml/loading/moddiscovery/ModFile.java
+++ fmlloader/src/main/java/net/minecraftforge/fml/loading/moddiscovery/ModFile.java
@@ -50,6 +50,7 @@ public class ModFile implements IModFile {
     private CompletableFuture<ModFileScanData> futureScanResult;
     private List<CoreModFile> coreMods;
     private Path accessTransformer;
+    private List<Path> accessTransformers = Collections.emptyList();
 
     static final Attributes.Name TYPE = new Attributes.Name("FMLModType");
     private SecureJar.Status securityStatus;
@@ -92,8 +93,14 @@ public class ModFile implements IModFile {
         return modFileInfo.getMods();
     }
 
+    /** @deprecated Use {@link #getAccessTransformers()} instead */
+    @Deprecated(forRemoval = true, since = "1.21.11")
     public Optional<Path> getAccessTransformer() {
-        return Optional.ofNullable(Files.exists(accessTransformer) ? accessTransformer : null);
+        return Optional.ofNullable(accessTransformer);
+    }
+
+    public List<Path> getAccessTransformers() {
+        return this.accessTransformers;
     }
 
     public boolean identifyMods() {
@@ -104,7 +111,29 @@ public class ModFile implements IModFile {
             LOGGER.warn("Mod file {} contains javascript coremods! JS CoreMods are deprecated and will be removed in a future release. Consider using Mixin or ModLauncher transformers.", this.getFileName());
         for (var mi : this.coreMods)
             LOGGER.debug(LogMarkers.LOADING, "Found coremod {}", mi.getPath());
-        this.accessTransformer = findResource("META-INF", "accesstransformer.cfg");
+
+        List<String> cfg;
+        // Note: Some mods may have invalid tomls copied from other projects.
+        // Unfortunately we must protect against those landmines.
+        try {
+            cfg = this.modFileInfo.getConfig().<List<String>>getConfigElement("accessTransformers").orElse(null);
+        } catch (Exception e) {
+            cfg = null;
+            LOGGER.warn("{} contains an invalid 'accessTransformers' TOML entry. Should be e.g. accessTransformers = [\"META-INF/accesstransformer.cfg\", \"META-INF/extra_at.cfg\"] or accessTransformers = [] for no ATs. Falling back to default.", this.getFileName());
+        }
+        if (cfg == null) {
+            var path = findResource("META-INF", "accesstransformer.cfg");
+            if (Files.exists(path)) {
+                this.accessTransformer = path;
+                this.accessTransformers = List.of(path);
+            }
+        } else if (!cfg.isEmpty()) {
+            var paths = new ArrayList<Path>(cfg.size());
+            for (var path : cfg)
+                paths.add(getSecureJar().getPath(path.replace('\\', '/')));
+            this.accessTransformers = List.copyOf(paths);
+        }
+
         return true;
     }
 
