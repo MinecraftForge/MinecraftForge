a171012411680f9bb06f467e503c49496dda8703
LexManos <LexManos@gmail.com>
2026-07-27 14:46:02 -0700

    Bump Dependencies, Notably ASM for Java 27 support

--- src/main/java/net/minecraftforge/gametest/GameTest.java
+++ src/main/java/net/minecraftforge/gametest/GameTest.java
@@ -5,17 +5,16 @@
 
 package net.minecraftforge.gametest;
 
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import net.minecraft.gametest.framework.GameTestEnvironments;
 import net.minecraft.world.level.block.Rotation;
 
-@Retention(RUNTIME)
-@Target(METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface GameTest {
     String name() default "";
 
